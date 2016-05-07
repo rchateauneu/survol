b7aacdd9621cb1a8633c037d8f621fcb8f1a9217
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-05-07 23:11:23 +0100

    Now nm works with 64 bits platforms.

---
+++ htbin/revlib/lib_nm.py
@@ -0,0 +1,83 @@
+import os
+import os.path
+import sys
+import psutil
+import socket
+import urllib
+import platform
+import rdflib
+import lib_common
+
+def GetSymbols(fileSharedLib):
+
+	if os.path.isfile(fileSharedLib):
+		sys.stderr.write("File %s exists\n" % fileSharedLib)
+	else:
+		lib_common.ErrorMessageHtml("File %s does not exist" % fileSharedLib)
+
+	if not 'linux' in sys.platform:
+		lib_common.ErrorMessageHtml("NM on Linux platform only")
+	nmCmd = "nm -DC " + fileSharedLib
+	sys.stderr.write("Running %s\n" % nmCmd)
+	stream = os.popen(nmCmd)
+
+	# Just to have a sort of clean switch.
+
+	# 0001d75c A __bss_start
+	#         U __cxa_allocate_exception
+
+	# Fedora 64 bits:
+	#                 w _ITM_registerTMCloneTable
+	#                 w _Jv_RegisterClasses
+	#0000000000000000 A LIBPAM_1.0
+	#0000000000000000 A LIBPAM_EXTENSION_1.0
+	if platform.architecture()[0] == "64bit":
+		addrWidth = 16
+	else:
+		addrWidth = 8
+
+	#"A" The symbol's value is absolute, and will not be changed by further linking.
+	#"B"
+	#"b" The symbol is in the uninitialized data section (known as BSS).
+	#"C" The symbol is common.  Common symbols are uninitialized data.  When linking, multiple common
+	#    symbols may appear with the same name.  If the symbol is defined anywhere, the common symbols
+	#    are treated as undefined references.
+	#"D"
+	#"d" The symbol is in the initialized data section.
+	#"G"
+	#"g" The symbol is in an initialized data section for small objects.  Some object file formats
+	#    permit more efficient access to small data objects, such as a global int variable as opposed to
+	#    a large global array.
+	#"I" The symbol is an indirect reference to another symbol.  This is a GNU extension to the a.out
+	#    object file format which is rarely used.
+	#"i" The symbol is in a section specific to the implementation of DLLs.
+	#"N" The symbol is a debugging symbol.
+	#"p" The symbols is in a stack unwind section.
+	#"R"
+	#"r" The symbol is in a read only data section.
+	#"S"
+	#"s" The symbol is in an uninitialized data section for small objects.
+	#"T"
+	#"t" The symbol is in the text (code) section.
+	#"U" The symbol is undefined.
+	#"V"
+	#"v" The symbol is a weak object.  When a weak defined symbol is linked with a normal defined
+	#    symbol, the normal defined symbol is used with no error.  When a weak undefined symbol is
+	#    linked and the symbol is not defined, the value of the weak symbol becomes zero with no erro
+	#    On some systems, uppercase indicates that a default value has been specified.
+	#"W"
+	#"w" The symbol is a weak symbol that has not been specifically tagged as a weak object symbol.
+	#    When a weak defined symbol is linked with a normal defined symbol, the normal defined symbol is
+	#    used with no error.  When a weak undefined symbol is linked and the symbol is not defined, the
+	#    value of the symbol is determined in a system-specific manner without error.  On some systems,
+	#    uppercase indicates that a default value has been specified.
+	#"-" The symbol is a stabs symbol in an a.out object file.  In this case, the next values printed
+	#    are the stabs other field, the stabs desc field, and the stab type.  Stabs symbols are used to
+	#    hold debugging information.
+	#"?" The symbol type is unknown, or object file format specific.
+	for line in stream:
+		type = line[addrWidth + 1].upper()
+		tail = line[addrWidth + 3:-1]
+
+		yield ( type,tail)
+	return
--- htbin/sources_types/file/linux_nm_classes.py
+++ htbin/sources_types/file/linux_nm_classes.py
@@ -7,7 +7,7 @@ import socket
 import urllib
 
 import rdflib
-
+import lib_nm
 import lib_util
 import lib_common
 from lib_properties import pc
@@ -95,68 +95,15 @@ def Main():
 
 	grph = rdflib.Graph()
 
-	if not 'linux' in sys.platform:
-		lib_common.ErrorMessageHtml("NM on Linux platform only")
-
 	nodeSharedLib = lib_common.gUriGen.FileUri( fileSharedLib )
 
-	nmCmd = "nm -DC " + fileSharedLib
-	sys.stderr.write("Running %s\n" % nmCmd)
-	stream = os.popen(nmCmd)
-
-	# Just to have a sort of clean switch.
-
-	# 0001d75c A __bss_start
-	#         U __cxa_allocate_exception
-
 	cnt = 0
-	for line in stream:
-		type = line[9].upper()
-		tail = line[11:-1]
-
-		#"A" The symbol's value is absolute, and will not be changed by further linking.
-		#"B"
-		#"b" The symbol is in the uninitialized data section (known as BSS).
-		#"C" The symbol is common.  Common symbols are uninitialized data.  When linking, multiple common
-		#    symbols may appear with the same name.  If the symbol is defined anywhere, the common symbols
-		#    are treated as undefined references.
-		#"D"
-		#"d" The symbol is in the initialized data section.
-		#"G"
-		#"g" The symbol is in an initialized data section for small objects.  Some object file formats
-		#    permit more efficient access to small data objects, such as a global int variable as opposed to
-		#    a large global array.
-		#"I" The symbol is an indirect reference to another symbol.  This is a GNU extension to the a.out
-		#    object file format which is rarely used.
-		#"i" The symbol is in a section specific to the implementation of DLLs.
-		#"N" The symbol is a debugging symbol.
-		#"p" The symbols is in a stack unwind section.
-		#"R"
-		#"r" The symbol is in a read only data section.
-		#"S"
-		#"s" The symbol is in an uninitialized data section for small objects.
-		#"T"
-		#"t" The symbol is in the text (code) section.
+	for type,tail in lib_nm.GetSymbols(fileSharedLib):
 		if type == 'T' or type == 't':
-			AddKnown( tail, fileSharedLib, type )
+			AddKnown( grph, nodeSharedLib, tail, fileSharedLib, type )
 			#"U" The symbol is undefined.
 		elif type == 'U' :
-			AddUnknown( tail )
-			#"V"
-			#"v" The symbol is a weak object.  When a weak defined symbol is linked with a normal defined
-			#    symbol, the normal defined symbol is used with no error.  When a weak undefined symbol is
-			#    linked and the symbol is not defined, the value of the weak symbol becomes zero with no erro
-			#    On some systems, uppercase indicates that a default value has been specified.
-			#"W"
-			#"w" The symbol is a weak symbol that has not been specifically tagged as a weak object symbol.
-			#    When a weak defined symbol is linked with a normal defined symbol, the normal defined symbol is
-			#    used with no error.  When a weak undefined symbol is linked and the symbol is not defined, the
-			#    value of the symbol is determined in a system-specific manner without error.  On some systems,
-			#    uppercase indicates that a default value has been specified.
-			#"-" The symbol is a stabs symbol in an a.out object file.  In this case, the next values printed
-			#    are the stabs other field, the stabs desc field, and the stab type.  Stabs symbols are used to
-			#    hold debugging information.
-			#"?" The symbol type is unknown, or object file format specific.
+			AddUnknown( grph, nodeSharedLib, tail )
 		else:
 			# Does not display all symbols because it is too much information.
 			# AddKnown( tail, fileSharedLib, type )
