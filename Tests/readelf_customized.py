fe566672f8105ba4e1beb3192774a0f837eab01c
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-05-15 22:46:51 +0100

    Updated test script.

--- Tests/readelf_customized.py
+++ Tests/readelf_customized.py
@@ -9,36 +9,34 @@ def main():
         fil = "/usr/lib64/libxerces-c-3.1.so"
     else:
         fil = sys.argv[1]
-    with open(fil, 'rb') as file:
-        readelf = lib_elf.ReadElf(file)
-        listNotes = readelf.display_notes()
-        for pr in listNotes:
-            sys.stdout.write("%s %s\n" % pr )
+    readelf = lib_elf.ReadElf(fil)
+    listNotes = readelf.display_notes()
+    for pr in listNotes:
+        sys.stdout.write("%s %s\n" % pr )
+    sys.stdout.write("\n")
+
+    listSyms, setClasses = readelf.display_symbol_tables()
+
+    setMoreClasses = lib_elf.GetClassesFromCTorDTor(listSyms)
+
+    unionClasses = setClasses.union( setMoreClasses )
+
+    # setNamespaces = GetNamespacesHeuristics( unionClasses )
+    # PROBLEM: What is the difference between a namespace
+    # and a non-virtual class with static methods and members only ???
+    # ALSO: The symbol name gives no information about the return type
+    # and the object parameter whose presence would indicate with certainty
+    # a class as opposed to a namespace.
+    for sm in listSyms:
+        sys.stdout.write("%s\n" % sm.m_name )
+        sys.stdout.write("%s\n" % str(sm) )
         sys.stdout.write("\n")
 
-        listSyms, setClasses = readelf.display_symbol_tables()
-
-        setMoreClasses = lib_elf.GetClassesFromCTorDTor(listSyms)
-
-        unionClasses = setClasses.union( setMoreClasses )
-
-        # setNamespaces = GetNamespacesHeuristics( unionClasses )
-        # PROBLEM: What is the difference between a namespace
-        # and a non-virtual class with static methods and members only ???
-        # ALSO: The symbol name gives no information about the return type
-        # and the object parameter whose presence would indicate with certainty
-        # a class as opposed to a namespace.
-        lib_elf.CalcNamspaces( unionClasses, listSyms )
-        for sm in listSyms:
-            sys.stdout.write("%s\n" % sm.m_name )
-            sys.stdout.write("%s\n" % str(sm) )
-            sys.stdout.write("\n")
-
-            #print(str(sorted(setClasses)))
-            #print("")
-            #print(str(sorted(setMoreClasses)))
-            #print("")
-            #print(str(sorted(unionClasses)))
+        #print(str(sorted(setClasses)))
+        #print("")
+        #print(str(sorted(setMoreClasses)))
+        #print("")
+        #print(str(sorted(unionClasses)))
 
 
 #-------------------------------------------------------------------------------
