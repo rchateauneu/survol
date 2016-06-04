c781150c2468dcc2354984fa6b71a3a028c127fc
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-06-04 09:39:48 +0100

    Cleanup.

--- htbin/file_to_mime.py
+++ htbin/file_to_mime.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+"""Display MIME content"""
+
 # It receives as CGI arguments, the entity type which must be "file", and the filename.
 # It must then return the content of the file, with the right MIME type,
 
@@ -16,11 +18,8 @@ import lib_util
 
 
 cgiEnv = lib_common.CgiEnv()
-should_be_file = cgiEnv.m_entity_type
-# fileName = cgiEnv.m_entity_id
-fileName = cgiEnv.GetId()
 
-# fileName = "C://Users/rchateau/AlsaSeveralSoundCards.txt"
+fileName = cgiEnv.GetId()
 
 mime_stuff = mimetypes.guess_type( fileName )
 
@@ -30,34 +29,14 @@ mime_type = mime_stuff[0]
 
 # It could also be a binary stream.
 if mime_type == None:
-	mime_type = "text/html"
+	lib_common.ErrorMessageHtml("No mime type for %s"%fileName)
 
+try:
+	# Read and write by chunks, so that it does not use all memory.
+	lib_util.CopyFile( mime_type, fileName, sys.stdout )
+except Exception:
+	exc = sys.exc_info()[1]
+	lib_common.ErrorMessageHtml("Reading %s, caught:%s" % ( fileName, str(exc) ) )
 
-if True: # False:
-	# This works for text files but not the rest, when this is not a text file.
-	# Maybe encoding problems.
-	try:
-		# Read and write by chunks, so that it does not use all memory.
-		lib_util.CopyFile( mime_type, fileName, sys.stdout )
-	except Exception:
-		exc = sys.exc_info()[1]
-		lib_common.ErrorMessageHtml("Reading %s, caught:%s" % ( fileName, str(exc) ) )
-
-	sys.stdout.flush()
-else:
-	pass
-	#Servir le fichier avec SimpleHTTPServer
-	#On va d abord tester avec du texte.
-	# TODO: ON FAIT DIFFEREMENT:
-	# http://www.2ality.com/2014/06/simple-http-server.html
-	# SimpleHTTPServer et ca va marcher aussi pour les directory.
-	# Eventuellement on va le customiser en fonction du type.
-
-	# TODO: DEPENDING ON THE TYPE OF THE FILE, WE COULD EXTRACT SOME ENTITIES ?
-	# - /proc file system.
-	# - Symbolic links.
-
-	# On le fera aussi pour les directory, en ajoutant des icones MIME et des liens
-	# vers nos entites.
-	# TODO: Ca va aussi permettre d'explorer les pseudos-fichiers comme "/proc" !!!!!!!
+sys.stdout.flush()
 
