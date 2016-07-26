0664686712753e0cd3b2eaaa46fb4da734cb2fdc
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-07-27 00:18:12 +0100

    Faster and mode nodes displayed. Still there is a limit...

--- htbin/sources_top/Linux/modules_dependencies.py
+++ htbin/sources_top/Linux/modules_dependencies.py
@@ -28,6 +28,9 @@ Usable = lib_util.UsableLinux
 #
 #
 
+
+
+
 def Main():
 	cgiEnv = lib_common.CgiEnv()
 
@@ -40,6 +43,17 @@ def Main():
 	version_file = open("/proc/version","r")
 	version_line = version_file.read()
 	version = version_line.split(' ')[2]
+
+	Main.DictModules = dict()
+
+	def ModuleToNode(modnam):
+		try:
+			return Main.DictModules[modnam]
+		except KeyError:
+			nod = lib_common.gUriGen.FileUri(modnam)
+			Main.DictModules[modnam] = nod
+			return nod
+
 
 	# The dependency network is very messy, so we put a limit,
 	# for the moment.
@@ -55,10 +69,13 @@ def Main():
 		# NOT TOO MUCH NODES: BEYOND THIS, IT IS FAR TOO SLOW, UNUSABLE.
 		# HARDCODE_LIMIT
 		maxCnt += 1
-		if maxCnt > 500:
+		if maxCnt > 2000:
 			break
 
+		file_parent = ModuleToNode(module_name)
+		file_child = None
 		for module_dep in module_deps_list:
+			module_dep = module_dep.strip()
 			if module_dep == "":
 				continue
 
@@ -66,10 +83,13 @@ def Main():
 
 			# This generates a directed acyclic graph,
 			# but not a tree in the general case.
-			file_parent = lib_common.gUriGen.FileUri(module_name)
-			file_child = lib_common.gUriGen.FileUri(module_dep)
+			file_child = ModuleToNode(module_dep)
 
 			grph.add( ( file_parent, pc.property_module_dep, file_child ) )
+		# TODO: Ugly trick, otherwise nodes without connections are not displayed.
+		# TODO: I think this is a BUG in the dot file generation. Or in RDF ?...
+		if file_child is None:
+			grph.add( ( file_parent, pc.property_information, rdflib.Literal("") ) )
 
 	# Splines are rather slow.
 	if maxCnt > 100:
