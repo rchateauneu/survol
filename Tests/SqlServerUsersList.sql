a20aae9ad2c72e7567c7f9af2d4873dfebb8f57c
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-10-07 08:28:27 +0100

    More doc added.

--- Tests/SqlServerUsersList.sql
+++ Tests/SqlServerUsersList.sql
@@ -20,3 +20,6 @@ FROM sys.dm_exec_requests req
 CROSS APPLY sys.dm_exec_sql_text(sql_handle) AS sqltext
 , sys.dm_exec_sessions sess
 where sess.session_id = req.session_id
+
+SELECT host_name,host_process_id,session_id,program_name,client_interface_name,original_login_name,nt_domain,nt_user_name
+FROM sys.dm_exec_sessions where host_name is not null
