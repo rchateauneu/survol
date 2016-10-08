7c4b5f32c13e934dee4e08215330ae5e13850c5f
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-10-08 17:24:04 +0100

    Displays more information.

--- htbin/sources_top/win32/win32_sqldatasources.py
+++ htbin/sources_top/win32/win32_sqldatasources.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-ODBC Data sources
+ODBC Data sources (ODBC module)
 """
 
 import sys
@@ -10,26 +10,44 @@ import lib_util
 import lib_common
 from lib_properties import pc
 
-import win32com.client
-import win32net
-
 import odbc
 
-def show_odbc_sources(grph):
-	odbc_iter_code = odbc.SQL_FETCH_FIRST
+# https://github.com/mkleehammer/pyodbc/wiki/Connecting-to-SQL-Server-from-Windows
+#    {SQL Server} - released with SQL Server 2000
+#    {SQL Native Client} - released with SQL Server 2005 (also known as version 9.0)
+#    {SQL Server Native Client 10.0} - released with SQL Server 2008
+#    {SQL Server Native Client 11.0} - released with SQL Server 2012
+# The connection strings for all these drivers are essentially the same, for example:
+# DRIVER={SQL Server Native Client 11.0};SERVER=test;DATABASE=test;UID=user;PWD=password
+# conn = pyodbc.connect(r'DSN=mynewdsn;UID=user;PWD=password')
+
+# The code would be odbc.SQL_FETCH_FIRST to get all DSNs.
+def DisplayDsns(grph,fetch_code,dsn_type):
+	odbc_iter_code = fetch_code
+
+	propDsnType = lib_common.MakeProp("Source type")
+	littDsnType = rdflib.Literal(dsn_type)
+
 	while True:
-		# System DSN only. For users DSN, I do not know.
 		source = odbc.SQLDataSources(odbc_iter_code)
 		if source == None:
 			break
 		# TODO: Prints the description and other data.
 		dsn, driver = source
-		sys.stderr.write("dsn=%s driver=%s\n" % ( dsn, driver) )
+		sys.stderr.write("dsn=%s driver=%s type=%s\n" % ( dsn, driver, dsn_type) )
 		odbc_iter_code = odbc.SQL_FETCH_NEXT
 
 		nodeDsn = lib_common.gUriGen.OdbcDsnUri( dsn )
 		grph.add( (lib_common.nodeMachine, pc.property_odbc_dsn, nodeDsn ) )
 		grph.add( (nodeDsn, pc.property_odbc_driver, rdflib.Literal(driver) ) )
+		grph.add( (nodeDsn, propDsnType, littDsnType ) )
+
+
+def show_odbc_sources(grph):
+	sys.stderr.write("odbc=%s\n" % str(dir(odbc)) )
+
+	DisplayDsns(grph,odbc.SQL_FETCH_FIRST_USER,"User")
+	DisplayDsns(grph,odbc.SQL_FETCH_FIRST_SYSTEM,"System")
 
 def Main():
 	if not lib_util.isPlatformWindows:
