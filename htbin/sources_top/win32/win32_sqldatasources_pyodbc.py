23f3f4b84bea0a8da607ebadcca75c2427d6a7da
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-10-08 17:19:41 +0100

    Same as existing script but based on pyodbc instead of odbc Python module.

---
+++ htbin/sources_top/win32/win32_sqldatasources_pyodbc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+"""
+ODBC Data sources (pyODBC module)
+"""
+
+import sys
+import rdflib
+import lib_util
+import lib_common
+from lib_properties import pc
+
+import pyodbc
+
+# http://code.activestate.com/recipes/578815-printing-list-of-odbc-data-sources-with-pyodbc-mod/
+
+def display_data_sources(grph):
+	sources = pyodbc.dataSources()
+
+	for dsn in sources:
+		driver = sources[dsn]
+
+		sys.stderr.write("dsn=%s driver=%s\n" % ( dsn, driver) )
+
+		nodeDsn = lib_common.gUriGen.OdbcDsnUri( dsn )
+		grph.add( (lib_common.nodeMachine, pc.property_odbc_dsn, nodeDsn ) )
+		grph.add( (nodeDsn, pc.property_odbc_driver, rdflib.Literal(driver) ) )
+
+def Main():
+	if not lib_util.isPlatformWindows:
+		lib_common.ErrorMessageHtml("ODBC Data sources only on Windows platforms")
+
+	cgiEnv = lib_common.CgiEnv()
+
+	grph = rdflib.Graph()
+	display_data_sources(grph)
+
+	cgiEnv.OutCgiRdf(grph)
+
+if __name__ == '__main__':
+	Main()
