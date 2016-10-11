7d838b8ebc6fb0e213d2c8956de1d255ebed6b81
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-10-11 23:58:42 +0100

    Added test with CROSS APPLY statement.

--- Tests/SqlServerUsersList.sql
+++ Tests/SqlServerUsersList.sql
@@ -1,5 +1,7 @@
 SELECT * FROM sys.dm_exec_sessions where host_name is not null
 
+select * from sys.dm_exec_requests
+
 /*
 SELECT sqltext.TEXT,
 req.session_id,
@@ -11,6 +13,7 @@ FROM sys.dm_exec_requests req
 CROSS APPLY sys.dm_exec_sql_text(sql_handle) AS sqltext
 */
 
+/*
 SELECT sqltext.TEXT,
 req.session_id,
 req.status,
@@ -21,5 +24,21 @@ CROSS APPLY sys.dm_exec_sql_text(sql_handle) AS sqltext
 , sys.dm_exec_sessions sess
 where sess.session_id = req.session_id
 
-SELECT host_name,host_process_id,session_id,program_name,client_interface_name,original_login_name,nt_domain,nt_user_name
-FROM sys.dm_exec_sessions where host_name is not null
+	SELECT host_name,host_process_id,session_id,program_name,client_interface_name,original_login_name,nt_domain,nt_user_name
+	FROM sys.dm_exec_sessions where host_name is not null
+
+
+SELECT sqltext.TEXT,
+req.session_id,
+req.status,
+req.command,
+sess.host_process_id
+FROM sys.dm_exec_requests req
+CROSS APPLY sys.dm_exec_sql_text(sql_handle) AS sqltext
+, sys.dm_exec_sessions sess
+where sess.session_id = req.session_id
+*/
+
+select * from sys.dm_exec_connections
+
+/* exec sp_columns sys.dm_exec_connections */
