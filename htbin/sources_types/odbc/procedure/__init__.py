33317ca603184ab0b059f8bc1af6b610656b5f7a
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-10-15 15:31:52 +0100

    Fix ODBC procedures.

--- htbin/sources_types/odbc/procedure/__init__.py
+++ htbin/sources_types/odbc/procedure/__init__.py
@@ -1,6 +1,9 @@
 import lib_util
 import lib_common
 
+def EntityOntology():
+	return ( ["Dsn", "Procedure"], )
+
 def MakeUri(dsnName,procNam):
 	return lib_common.gUriGen.UriMakeFromDict("odbc/procedure", { "Dsn" : lib_util.EncodeUri(dsnName), "Procedure" : procNam })
 
