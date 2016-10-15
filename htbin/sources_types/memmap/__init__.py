5727d2388c710316bb0efd65dd3b5a5598b82d45
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-10-15 16:55:25 +0100

    Now display the file node along with the memory mapped file.

--- htbin/sources_types/memmap/__init__.py
+++ htbin/sources_types/memmap/__init__.py
@@ -1,1 +1,9 @@
-__author__ = 'rchateau'
+import lib_common
+
+# A map file is associated to a file.
+def AddInfo(grph,node,entity_ids_arr):
+	nameMappedFile = entity_ids_arr[0]
+	# sys.stderr.write("AddInfo entity_id=%s\n" % pidProc )
+	exec_node = lib_common.gUriGen.FileUri( nameMappedFile )
+	grph.add( ( node, lib_common.MakeProp("Mapped file"), exec_node ) )
+
