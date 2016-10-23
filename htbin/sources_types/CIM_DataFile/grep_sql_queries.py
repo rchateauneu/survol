6b34631bf1d0ad43ab3a61b9695f08b39a95b796
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-10-23 01:17:14 +0100

    New scripts.

---
+++ htbin/sources_types/CIM_DataFile/grep_sql_queries.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+"""
+Search SQL queries from source files.
+"""
+
+import os
+import os.path
+import re
+import sys
+import rdflib
+import lib_sql
+import lib_util
+import lib_common
+import lib_modules
+from lib_properties import pc
+
+# Any type of source file can contain SQL queries.
+extensionsSQLSourcesFiles = [
+	".c",".cc",".cxx",".cpp",".c++",".java",".ii",".ixx",".ipp",".i++",
+	".pc",".pcc"
+	".inl",".idl",".ddl",".odl",
+	".h",".hh",".hxx",".hpp",".h++",
+	".cs",".d",".php",".php4",".php5",".phtml",".inc",
+	".py",".pyw",
+	".f90",".f",".for",
+	".tcl",".as",".js",
+	".sh",".csh",".bash"
+]
+
+def Usable(entity_type,entity_ids_arr):
+	filNam = entity_ids_arr[0]
+	filExt = os.path.splitext(filNam)[1]
+	if filExt.lower() in extensionsSQLSourcesFiles:
+		return True
+
+	# On Unix, we could also check if the file is a Shell script, whatever the extension is.
+	return os.path.isdir(filNam)
+
+# There must be another script for object files and libraries,
+# because the search should not be done in the entire file,
+# but only in the DATA segment.
+
+
+
+def Main():
+	cgiEnv = lib_common.CgiEnv()
+	filNam = int( cgiEnv.GetId() )
+
+	grph = rdflib.Graph()
+
+	nodeFile = lib_common.gUriGen.FileUri(filNam)
+
+	try:
+		# The regular expressions are indexed with a key such as "INSERT", "SELECT" etc...
+		# which gives a hint about what the query does, and is transformed into a RDF property.
+		# Also, the regular expressions are compiled for better performence.
+		# This creates an dictionary mapping the RDF property to the compiled regular expression.
+		dictRegexSQL = lib_sql.SqlRegularExpressions()
+
+		arrProps = []
+		for rgxKey in dictRegexSQL:
+			rgxSQL = dictRegexSQL[rgxKey]
+			rgxProp = lib_common.MakeProp(rgxKey)
+			arrProps.append(rgxProp)
+
+			compiledRgx = re.compile(rgxSQL)
+
+			opFil = open(filNam, 'r')
+			for linFil in opFil:
+				matchedSqls = compiledRgx.findall(linFil)
+
+				# For the moment, we just print the query.
+				for sqlQry in matchedSqls:
+					# grph.add( ( node_process, pc.property_rdf_data_nolist1, nodePortalWbem ) )
+					grph.add( ( nodeFile, rgxProp, rdflib.Literal(sqlQry) ) )
+
+	except Exception:
+		exc = sys.exc_info()[1]
+		lib_common.ErrorMessageHtml("Error:%s. Protection ?"%str(exc))
+
+	cgiEnv.OutCgiRdf(grph,"LAYOUT_RECT",arrProps)
+
+if __name__ == '__main__':
+	Main()
+
---
+++ htbin/sources_types/sql/query/query_vs_databases.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+"""
+SQL query checked against available databases
+"""
+
+
+# This SQL query is analysed with the available databases.
+# It might check the exact SQL syntax.
+# It would be nice to detect the database library which would give a hint.
