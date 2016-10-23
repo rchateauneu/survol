be1303f6861486f8c53a30b62898b332aacefc6e
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-10-23 22:29:16 +0100

    Future use.

---
+++ htbin/direct_access.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# TODO: There should be a form do go directly to an object such as a file:
+# TODO: Select the entity type from a list, then enter its abbreviated URL,
+# TODO: or have a a text cell for each parameter, with the parameter name.
+
