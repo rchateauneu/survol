f68abc038c7442bcb168c1e02428d3b71161d89d
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-11-28 23:13:48 +0000

    Code cleanup.

--- htbin/sources_types/python/__init__.py
+++ htbin/sources_types/python/__init__.py
@@ -2,10 +2,6 @@ import os
 import sys
 import lib_uris
 import lib_common
-#from sources_types import python
-#from sources_types.python import package
-#import package
-
 
 pyExtensions = {
 	".py" : "Python source",
