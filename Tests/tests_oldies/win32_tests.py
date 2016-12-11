79c022d6a7f09d9196c7b80a9bc5e035e7afca6b
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-12-11 12:10:42 +0000

    More experimentations.

--- Tests/tests_oldies/win32_tests.py
+++ Tests/tests_oldies/win32_tests.py
@@ -8,7 +8,7 @@ import traceback
   
 verbose_level = 1
   
-server = None # Run on local machine.
+GlobalServer = None # Run on local machine.
   
 def verbose(msg):
     if verbose_level:
@@ -35,7 +35,7 @@ def UserEnum():
     resume = 0
     nuser = 0
     while 1:
-        data, total, resume = win32net.NetUserEnum(server, 3, win32netcon.FILTER_NORMAL_ACCOUNT, resume)
+        data, total, resume = win32net.NetUserEnum(GlobalServer, 3, win32netcon.FILTER_NORMAL_ACCOUNT, resume)
         verbose("Call to NetUserEnum obtained %d entries of %d total" % (len(data), total))
         for user in data:
             verbose("Found user %s" % user['name'])
@@ -49,13 +49,13 @@ def GroupEnum():
     nmembers = 0
     resume = 0
     while 1:
-        data, total, resume = win32net.NetGroupEnum(server, 1, resume)
+        data, total, resume = win32net.NetGroupEnum(GlobalServer, 1, resume)
 #               print "Call to NetGroupEnum obtained %d entries of %d total" % (len(data), total)
         for group in data:
             verbose("Found group %(name)s:%(comment)s " % group)
             memberresume = 0
             while 1:
-                memberdata, total, memberresume = win32net.NetGroupGetUsers(server, group['name'], 0, resume)
+                memberdata, total, memberresume = win32net.NetGroupGetUsers(GlobalServer, group['name'], 0, resume)
                 for member in memberdata:
                     verbose(" Member %(name)s" % member)
                     nmembers = nmembers + 1
@@ -68,13 +68,16 @@ def ServerEnum():
     "Enumerates all servers on the network"
     resume = 0
     while 1:
-        data, total, resume = win32net.NetServerEnum(server, 100, win32netcon.SV_TYPE_ALL, None, resume)
+        data, total, resume = win32net.NetServerEnum("", 100, win32netcon.SV_TYPE_ALL, None, resume)
+        # data, total, resume = win32net.NetServerEnum("\\\\RCHATEAU-HP", 100, win32netcon.SV_TYPE_ALL, None, resume)
+        print(str(total))
+        print(str(resume))
         for s in data:
             verbose("Server %s" % s['name'])
             # Now loop over the shares.
             shareresume=0
             while 1:
-                sharedata, total, shareresume = win32net.NetShareEnum(server, 2, shareresume)
+                sharedata, total, shareresume = win32net.NetShareEnum(GlobalServer, 2, shareresume)
                 for share in sharedata:
                     verbose("    %(netname)s (%(path)s):%(remark)s - in use by %(current_uses)d users" % share)
                 if not shareresume:
@@ -86,21 +89,41 @@ def GetInfo(userName=None):
     "Dumps level 3 information about the current user"
     if userName is None: userName=win32api.GetUserName()
     print( "Dumping level 3 information about user")
-    info = win32net.NetUserGetInfo(server, userName, 3)
+    info = win32net.NetUserGetInfo(GlobalServer, userName, 3)
     for key, val in info.items():
         verbose("%s=%s" % (key,val))
   
 
+def getall_boxes(domain='',server=''):
+    res=1
+    wrk_lst=[]
+    try:
+        while res: #loop until res2
+            # (wrk_list2,total,res2)=win32net.NetServerEnum('',100,win32netcon.SV_TYPE_ALL,server,res,win32netcon.MAX_PREFERRED_LENGTH)
+            (wrk_list2,total,res2)=win32net.NetServerEnum(None,100,win32netcon.SV_TYPE_ALL,server,res,1000)
+            wrk_lst.extend(wrk_list2)
+            res=res2
+    except win32net.error:
+        print traceback.format_tb(sys.exc_info()[2]),'\n',sys.exc_type,'\n',sys.exc_value
 
+    final_lst=[]
+    for i in wrk_lst:
+        final_lst.append(str(i['name']))
+    return final_lst
+
+# http://timgolden.me.uk/pywin32-docs/html/win32/help/win32net.html#machines
+# print getall_boxes('bedrock',r'\\rubble')
+getall_boxes()
+print("")
 
 show_odbc_sources()
-print("")
+print("\nUserEnum")
 UserEnum()
-print("")
+print("\nGroupEnum")
 GroupEnum()
-print("")
+print("\nServerEnum")
 ServerEnum()
-print("")
+print("\nGetInfo")
 GetInfo()
 
 print("Fini")
