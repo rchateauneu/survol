e0f0afcea9991ec7e6f3bd3750fdf43f5b511219
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-01-22 21:46:59 +0000

    Fix script structure.

--- htbin/sources_types/user/user_processes.py
+++ htbin/sources_types/user/user_processes.py
@@ -14,73 +14,79 @@ from lib_properties import pc
 
 from sources_types import CIM_Process
 
-cgiEnv = lib_common.CgiEnv("User processes")
-userNameWithHost = cgiEnv.GetId()
+Usable = lib_util.UsableLinux
 
-if not lib_util.isPlatformLinux:
-	lib_common.ErrorMessageHtml("Linux only")
 
-# Usernames have the syntax user@host
-# Example: UK936025@LONW00052257.euro.net.intra
-userSplit = userNameWithHost.split('@')
-userName = userSplit[0]
+def Main():
+	cgiEnv = lib_common.CgiEnv("User processes")
+	userNameWithHost = cgiEnv.GetId()
 
-# TODO: Should factorize this code.
-if len( userSplit ) > 1:
-	userHost = userSplit[1]
-	if userHost != lib_util.currentHostname:
-		# TODO: Should interrogate other host with "finger" protocol.
-		lib_common.ErrorMessageHtml("Cannot get user properties on different host:" + userHost)
+	if not lib_util.isPlatformLinux:
+		lib_common.ErrorMessageHtml("Linux only")
 
-grph = rdflib.Graph()
+	# Usernames have the syntax user@host
+	# Example: UK936025@LONW00052257.euro.net.intra
+	userSplit = userNameWithHost.split('@')
+	userName = userSplit[0]
 
-# It will be possible to transform this into a Json tree by
-# selecting only the RDF predicate property_ppid.
-# This will be done in a gui cgi script which takes as input
-# parameter a CGI script, visible by SLP, stored in a bookmark page 
-# or anything else.
+	# TODO: Should factorize this code.
+	if len( userSplit ) > 1:
+		userHost = userSplit[1]
+		if userHost != lib_util.currentHostname:
+			# TODO: Should interrogate other host with "finger" protocol.
+			lib_common.ErrorMessageHtml("Cannot get user properties on different host:" + userHost)
 
-# See http://stackoverflow.com/questions/17967686/retrieving-specific-rdf-graph-triples-based-on-predicate-nodes
-# on how to select triples on a given predicate only.
+	grph = rdflib.Graph()
 
-# But in the general case, we cannot know if the RDF graph will be a tree,
-# something similar to a CSV file (That is, flat) or a general graph.
+	# It will be possible to transform this into a Json tree by
+	# selecting only the RDF predicate property_ppid.
+	# This will be done in a gui cgi script which takes as input
+	# parameter a CGI script, visible by SLP, stored in a bookmark page
+	# or anything else.
 
-# So we might have to process the resulting graph on the fly, to see
-# which visualising methods are applicable.
+	# See http://stackoverflow.com/questions/17967686/retrieving-specific-rdf-graph-triples-based-on-predicate-nodes
+	# on how to select triples on a given predicate only.
 
-# Also, in the case of a tree, we must find ourselves what is its root.
+	# But in the general case, we cannot know if the RDF graph will be a tree,
+	# something similar to a CSV file (That is, flat) or a general graph.
 
-for proc in psutil.process_iter():
+	# So we might have to process the resulting graph on the fly, to see
+	# which visualising methods are applicable.
 
-	procUsername = CIM_Process.PsutilProcToUser(proc)
+	# Also, in the case of a tree, we must find ourselves what is its root.
 
-	sys.stderr.write("procUsername=%s userName=%s\n" % ( procUsername, userName ) )
-	# procUsername=EURO\\UK936025 userName=UK936025
-	# procUsername=NT AUTHORITY\\NETWORK SERVICE
-	# procUsername=NT AUTHORITY\\SYSTEM
-	# procUsername=EURO\\UK936025
-	# procUsername=NT AUTHORITY\\SYSTEM
-	# if procUsername + "@" + lib_util.currentHostname != userName:
-	if procUsername != userName:
-		continue
+	for proc in psutil.process_iter():
 
-	if lib_common.UselessProc(proc):
-		continue
+		procUsername = CIM_Process.PsutilProcToUser(proc)
 
-	procName = proc.name
+		sys.stderr.write("procUsername=%s userName=%s\n" % ( procUsername, userName ) )
+		# procUsername=EURO\\UK936025 userName=UK936025
+		# procUsername=NT AUTHORITY\\NETWORK SERVICE
+		# procUsername=NT AUTHORITY\\SYSTEM
+		# procUsername=EURO\\UK936025
+		# procUsername=NT AUTHORITY\\SYSTEM
+		# if procUsername + "@" + lib_util.currentHostname != userName:
+		if procUsername != userName:
+			continue
 
-	pid = proc.pid
-	parent_pid = CIM_Process.PsutilProcToPPid(proc)
+		if lib_common.UselessProc(proc):
+			continue
 
-	# Built the same way in other RDF documents.
-	node_process = lib_common.gUriGen.PidUri(pid)
-	parent_node_process = lib_common.gUriGen.PidUri(parent_pid)
+		procName = proc.name
 
-	# We avoid duplicating the edges. Why would the RFD merge do?
-	grph.add( ( node_process, pc.property_ppid, parent_node_process ) )
-	grph.add( ( node_process, pc.property_pid, rdflib.Literal(pid) ) )
-	# grph.add( ( node_process, pc.property_information, rdflib.Literal(procUsername) ) )
+		pid = proc.pid
+		parent_pid = CIM_Process.PsutilProcToPPid(proc)
 
-cgiEnv.OutCgiRdf(grph)
+		# Built the same way in other RDF documents.
+		node_process = lib_common.gUriGen.PidUri(pid)
+		parent_node_process = lib_common.gUriGen.PidUri(parent_pid)
 
+		# We avoid duplicating the edges. Why would the RFD merge do?
+		grph.add( ( node_process, pc.property_ppid, parent_node_process ) )
+		grph.add( ( node_process, pc.property_pid, rdflib.Literal(pid) ) )
+		# grph.add( ( node_process, pc.property_information, rdflib.Literal(procUsername) ) )
+
+	cgiEnv.OutCgiRdf(grph)
+
+if __name__ == '__main__':
+	Main()
