8ae9688baead4afeb84e3c641ba0b8ec8b80459b
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-02-03 22:29:20 +0000

    Fixed import.

--- htbin/sources_types/CIM_Process/process_gdbstack.py
+++ htbin/sources_types/CIM_Process/process_gdbstack.py
@@ -11,7 +11,7 @@ import rdflib
 import lib_util
 import lib_common
 from sources_types import CIM_Process
-from sources_types import survol as survol_symbol
+from sources_types import symbol as survol_symbol
 from lib_properties import pc
 
 Usable = lib_util.UsableLinux
