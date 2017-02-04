e2fdd6752edd08d1f0702d4dd6f2e3889c65c0ff
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-02-04 01:19:57 +0000

    Linux only.

---
+++ htbin/sources_types/CIM_DiskDrive/linux_disk_partitions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+"""
+Linux disk partitions
+"""
+
+import sys
+import socket
+import rdflib
+import psutil
+import lib_util
+import lib_common
+from lib_properties import pc
+
+Usable = lib_util.UsableLinux
+
+def Main():
+	cgiEnv = lib_common.CgiEnv()
+
+	grph = rdflib.Graph()
+
+	cgiEnv.OutCgiRdf(grph)
+
+if __name__ == '__main__':
+	Main()
