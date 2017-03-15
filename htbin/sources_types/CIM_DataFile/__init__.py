186ea43e0f84e5690bb43fddccc1c03e684b3ab6
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-03-15 07:59:09 +0000

    Factorise file_stat.py and dir_stat.py. Display file owner.

--- htbin/sources_types/CIM_DataFile/__init__.py
+++ htbin/sources_types/CIM_DataFile/__init__.py
@@ -6,6 +6,8 @@ import lib_common
 import lib_util
 import lib_uris
 from lib_properties import pc
+import mimetypes
+import json
 
 def EntityOntology():
 	return ( ["Name"], )
@@ -82,6 +84,173 @@ def AddParentDir( grph, filNode, filNam ):
 		# We do not use the property pc.property_directory because it breaks the display.
 		# Also, the direction is inverted so the current file is displayed on the left.
 		grph.add( ( filNode, lib_common.MakeProp("Top directory"), dirNode ) )
+
+# Plain call to stat with some filtering if the file does not exists.
+def GetInfoStat(filNam):
+	try:
+		info = os.stat(filNam)
+	except Exception:
+		# On recent Python versions, we would catch IOError or FileNotFoundError.
+		exc = sys.exc_info()[1]
+		lib_common.ErrorMessageHtml("Caught:"+str(exc))
+	except IOError:
+		lib_common.ErrorMessageHtml("IOError:"+filNam)
+	except FileNotFoundError:
+		lib_common.ErrorMessageHtml("File not found:"+filNam)
+	except PermissionError:
+		lib_common.ErrorMessageHtml("Permission error:"+filNam)
+	except OSError:
+		lib_common.ErrorMessageHtml("Incorrect syntax:"+filNam)
+
+	return info
+
+def AddDevice(grph,filNode,info):
+	deviceName = "Device:"+str(info.st_dev)
+	if lib_util.isPlatformLinux:
+		# TODO: How to get the device name on Windows ???
+		for line in file('/proc/mounts'):
+			# lines are device, mountpoint, filesystem, <rest>
+			# later entries override earlier ones
+			line = [s.decode('string_escape') for s in line.split()[:3]]
+			try:
+				if os.lstat(line[1]).st_dev == info.st_dev:
+					deviceName = line[1]
+					break
+			except OSError:
+				# Beware, index 1, not 0:
+				# "[Errno 13] Permission denied: '/run/user/42/gvfs'"
+				# Better display the error message.
+				exc = sys.exc_info()[1]
+				deviceName=str(exc)
+				break
+
+		deviceNode = lib_common.gUriGen.DiskPartitionUri(deviceName)
+		grph.add( ( filNode, pc.property_file_device, deviceNode ) )
+
+def AddFileProperties(grph,currNode,currFilNam):
+	try:
+		import win32api
+		import lib_win32
+
+		propDict = lib_win32.getFileProperties(currFilNam)
+		for prp, val in propDict.items():
+			val = propDict[prp]
+			if val is None:
+				continue
+
+			if isinstance( val, dict ):
+				# val = ", ".join( "%s=%s" % (k,val[k]) for k in val )
+				val = json.dumps(val)
+				# TODO: Unicode error encoding=ascii
+				# 169	251	A9	10101001	"Copyright"	&#169;	&copy;	Copyright sign
+				# Might contain this: "LegalCopyright Copyright \u00a9 2010"
+				val = val.replace("\\","\\\\")
+			grph.add( ( currNode, lib_common.MakeProp(prp), rdflib.Literal(val) ) )
+	except ImportError:
+		pass
+
+	mimTy = mimetypes.guess_type(currFilNam)
+	if mimTy:
+		if mimTy[0]:
+			grph.add( ( currNode, lib_common.MakeProp("Mime type"), rdflib.Literal(str(mimTy)) ) )
+
+
+
+def AffFileOwner(grph, filNode, filNam):
+
+	def AddFileOwnerWindows(grph, filNode, filNam):
+		import win32api
+		import win32con
+		import win32security
+
+		from sources_types import Win32_UserAccount
+		from sources_types import Win32_Group
+
+		def SID_CodeToName(typeSID):
+			mapSIDList = {
+				win32security.SidTypeUser: "User SID",
+				win32security.SidTypeGroup: "Group SID",
+				win32security.SidTypeDomain: "Domain SID",
+				win32security.SidTypeAlias: "Alias SID",
+				win32security.SidTypeWellKnownGroup: "Well-known group",
+				win32security.SidTypeDeletedAccount: "Deleted account",
+				win32security.SidTypeInvalid: "Invalid SID",
+				win32security.SidTypeUnknown: "Unknown type SID",
+				win32security.SidTypeComputer: "Computer SID",
+				# win32security.SidTypeLabel: "Mandatory integrity label SID" # NOT DEFINED
+			}
+
+			try:
+				return mapSIDList[typeSID]
+			except:
+				return "Unknown SID"
+
+		#print "I am", win32api.GetUserNameEx (win32con.NameSamCompatible)
+
+		sd = win32security.GetFileSecurity (filNam, win32security.OWNER_SECURITY_INFORMATION)
+		owner_sid = sd.GetSecurityDescriptorOwner ()
+		accountName, domainName, typeCode = win32security.LookupAccountSid (None, owner_sid)
+		typNam = SID_CodeToName(typeCode)
+		sys.stderr.write("Domain=%s Name=%s Type=%s\n" % (domainName, accountName,typNam) )
+
+		if typeCode == win32security.SidTypeUser:
+			accountNode = Win32_UserAccount.MakeUri(accountName,domainName)
+		elif typeCode == win32security.SidTypeGroup:
+			accountNode = Win32_Group.MakeUri(accountName,domainName)
+		elif typeCode == win32security.SidTypeWellKnownGroup:
+			accountNode = Win32_Group.MakeUri(accountName,domainName)
+		else:
+			# What else can we do ?
+			accountNode = Win32_UserAccount.MakeUri(accountName,domainName)
+
+		# TODO: What can we do with the domain ?
+		grph.add( ( accountNode, lib_common.MakeProp("Domain"), rdflib.Literal(domainName) ) )
+		grph.add( ( accountNode, lib_common.MakeProp("SID"), rdflib.Literal(typNam) ) )
+		grph.add( ( accountNode, pc.property_owner, filNode ) )
+
+
+	def AddFileOwnerLinux(grph, filNode, filNam):
+		# Do it a second time, but this is very fast.
+		info = GetInfoStat(filNam)
+
+		# st_uid: user id of owner.
+		try:
+			# Can work on Unix only.
+			import pwd
+			user = pwd.getpwuid( info.st_uid )
+			userName = user[0]
+			userNode = lib_common.gUriGen.UserUri(userName)
+			grph.add( ( filNode, pc.property_owner, userNode ) )
+		except ImportError:
+			pass
+
+		# st_gid: group id of owner.
+		try:
+			# Can work on Unix only.
+			import grp
+			group = grp.getgrgid( info.st_gid )
+			groupName = group[0]
+			groupNode = lib_common.gUriGen.GroupUri(groupName)
+			grph.add( ( filNode, pc.property_group, groupNode ) )
+		except ImportError:
+			pass
+
+		return
+
+	sys.stderr.write("AddFileOwner %s\n"%filNam)
+
+	try:
+		if lib_util.isPlatformWindows:
+			AddFileOwnerWindows(grph, filNode, filNam)
+		elif lib_util.isPlatformLinux:
+			AddFileOwnerLinux(grph, filNode, filNam)
+		else:
+			sys.stderr.write("unknown OS")
+			pass
+	except:
+		raise
+		pass
+
 
 
 # Each entity can have such a file with its name as file name.
--- htbin/sources_types/CIM_Directory/dir_stat.py
+++ htbin/sources_types/CIM_Directory/dir_stat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-File stat information
+Directory stat information
 """
 
 # BEWARE: Do NOT rename it as stat.py otherwise strange errors happen,
@@ -21,90 +21,31 @@ import lib_properties
 from lib_properties import pc
 import mimetypes # In Python standard library.
 
-try:
-	import win32api
-	import lib_win32
-	FilNamToProperties = lib_win32.getFileProperties
-except ImportError:
-	def FilNamToProperties(filNam):
-		return {}
-
 def Main():
 	cgiEnv = lib_common.CgiEnv()
 	filNam = cgiEnv.GetId()
+	filNam = filNam.replace("\\","/")
+
 	sys.stderr.write("filNam=%s\n" % filNam )
 
-	filNode = lib_common.gUriGen.FileUri(filNam )
+	filNode = lib_common.gUriGen.DirectoryUri(filNam )
 
 	grph = rdflib.Graph()
 
-	try:
-		info = os.stat(filNam)
-	except Exception:
-		# On recent Python versions, we would catch IOError or FileNotFoundError.
-		exc = sys.exc_info()[1]
-		lib_common.ErrorMessageHtml("Caught:"+str(exc))
-	except IOError:
-		lib_common.ErrorMessageHtml("IOError:"+filNam)
-	except FileNotFoundError:
-		lib_common.ErrorMessageHtml("File not found:"+filNam)
-	except PermissionError:
-		lib_common.ErrorMessageHtml("Permission error:"+filNam)
-	except OSError:
-		lib_common.ErrorMessageHtml("Incorrect syntax:"+filNam)
+	info = CIM_DataFile.GetInfoStat(filNam)
 
 	# st_mode: protection bits.
 	# st_ino: inode number.
 
 	# st_dev: device.
-	deviceName = "Device:"+str(info.st_dev)
-	if lib_util.isPlatformLinux:
-		# TODO: How to get the device name on Windows ???
-		for line in file('/proc/mounts'):
-			# lines are device, mountpoint, filesystem, <rest>
-			# later entries override earlier ones
-			line = [s.decode('string_escape') for s in line.split()[:3]]
-			try:
-				if os.lstat(line[1]).st_dev == info.st_dev:
-					deviceName = line[1]
-					break
-			except OSError:
-				# Beware, index 1, not 0:
-				# "[Errno 13] Permission denied: '/run/user/42/gvfs'"
-				# Better display the error message.
-				exc = sys.exc_info()[1]
-				deviceName=str(exc)
-				break
-
-		deviceNode = lib_common.gUriGen.DiskPartitionUri(deviceName)
-		grph.add( ( filNode, pc.property_file_device, deviceNode ) )
+	CIM_DataFile.AddDevice(grph,filNode,info)
 
 	CIM_DataFile.AddStatNode( grph, filNode, info )
 	CIM_DataFile.AddMagic( grph, filNode, filNam )
 
 	# st_nlink: number of hard links.
 
-	# st_uid: user id of owner.
-	try:
-		# Can work on Unix only.
-		import pwd
-		user = pwd.getpwuid( info.st_uid )
-		userName = user[0]
-		userNode = lib_common.gUriGen.UserUri(userName)
-		grph.add( ( filNode, pc.property_owner, userNode ) )
-	except ImportError:
-		pass
-
-	# st_gid: group id of owner.
-	try:
-		# Can work on Unix only.
-		import grp
-		group = grp.getgrgid( info.st_gid )
-		groupName = group[0]
-		groupNode = lib_common.gUriGen.GroupUri(groupName)
-		grph.add( ( filNode, pc.property_group, groupNode ) )
-	except ImportError:
-		pass
+	CIM_DataFile.AffFileOwner(grph, filNode, filNam)
 
 	# Displays the file and the parent directories/
 	currFilNam = filNam
@@ -115,32 +56,13 @@ def Main():
 			break
 		if dirPath == "":
 			break
-		dirNode = lib_common.gUriGen.FileUri( dirPath )
+		dirNode = lib_common.gUriGen.DirectoryUri( dirPath )
 		grph.add( ( dirNode, pc.property_directory, currNode ) )
 		sys.stderr.write("dirPath=%s\n" % dirPath)
 		statPath = os.stat(dirPath)
 		CIM_DataFile.AddStatNode( grph, dirNode, statPath )
 
-
-		propDict = FilNamToProperties(currFilNam)
-		for prp, val in propDict.items():
-			val = propDict[prp]
-			if val is None:
-				continue
-
-			if isinstance( val, dict ):
-				# val = ", ".join( "%s=%s" % (k,val[k]) for k in val )
-				val = json.dumps(val)
-				# TODO: Unicode error encoding=ascii
-				# 169	251	A9	10101001	"Copyright"	&#169;	&copy;	Copyright sign
-				# Might contain this: "LegalCopyright Copyright \u00a9 2010"
-				val = val.replace("\\","\\\\")
-			grph.add( ( currNode, lib_properties.MakeProp(prp), rdflib.Literal(val) ) )
-
-		mimTy = mimetypes.guess_type(currFilNam)
-		if mimTy:
-			if mimTy[0]:
-				grph.add( ( currNode, lib_properties.MakeProp("Mime type"), rdflib.Literal(str(mimTy)) ) )
+		CIM_DataFile.AddFileProperties(grph,currNode,currFilNam)
 
 		currFilNam = dirPath
 		currNode = dirNode
