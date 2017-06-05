1d4bdb965a6d3248909dcf5ad3cc32360f7d1115
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-06-05 22:24:13 +0100

    Now use entity_dirmenu_only.py.

--- htbin/entity.py
+++ htbin/entity.py
@@ -9,12 +9,12 @@ import re
 import sys
 import psutil
 import rdflib
-
-# With Linux, Python2 and cgiserver, it can import it,
-# but after that it cannot "import lib_common"
 import lib_util
 import lib_common
 from lib_properties import pc
+
+# This script is also used as a module.
+import entity_dirmenu_only # Also used with the CGI parameter mode=menu
 
 from sources_types import CIM_Process
 from sources_types import CIM_ComputerSystem
@@ -31,229 +31,8 @@ from sources_types import CIM_ComputerSystem
 # And we must display the node of the host as seen from the local machine.
 
 
-# TODO: CharTypesComposer
-# Actuellement on parcourt toute l'arborescence et on affiche tous les scripts
-# a partir d un directory. Ne pas descendre dans les sous-directories s'il y a un sous-type.
-# Mais le faire quand meme si le decoupage est plutot un namespace.
-# Comment a la fois utiliser des sous-directories comme types derives
-# et comme namespaces ?
-# Dans le directory principal, on peut n avoir que des names-spaces
-# car un sous-type n a pas de sens. Et on affichera recursivement.
-# Toutefois pour le directory des types,
-# il faut descendre dans les namespaces mais pas dans les sous-types.
-# En fait, pour travailleur naturellement, il faudrait inverser la hierarchie:
-# Que les sous-types pointent vers les types.
-# Peut-etre mettre dans le __init__.py du directory d'un sous-type,
-# une reference vers le type de base. Ou bien se servir du nom ?
-# Avec le separateur un sous-type contiendrait la liste de ses types de base ?
-
-# On peut aussi donner une syntaxe specifique aux sous-directory namespaces,
-# et DirToMenu ne descendra dans les sous-dir que si namespaces.
-# Ou bien: Chaque directory contient dans le __init__.py
-# une fonction qui dit si on peut afficher ou non:
-# Cette fonction prend en parametre le entity_type, os.platform.
-# Seul inconvenient:
-#  - Il faut assigner un role a la classe de base, qui sert de directory de depart.
-#  - La classe de base sert aussi pour la liste des parametres et les couleurs.
-#  - Confusions classe de base et namespace: "oracle,table" et "mysql,table"
-#    Valable pour les couleurs (Ca sert d avoir une couleur commune a tous
-#    les objets d un meme namespace) mais pas pour les parametres evidemment.
-#    Autre confusion si namespaces et classes de base ont la meme structure:
-#    - Impossible d'apparier les hierarchies avec WBEM et WMI. Par exemple on pourrait
-#    deriver localement de CIM_Process.
-#    - Pourrait-on representer la hierarchie user/CIM_Account et user/LMI_Account ?
-# Si on melange sous-types et namespaces, on descend toujours dans les dir des namespaces
-# si la fonction de __init__.py le permet ? Probleme: le nom du type pourrait etre:
-# "linux,file,dir" ou "windows,file,dir" ? ou "file,linux,symlink" ?
-# Ou bien que "symlink" et on parcourerait toujours l arborescence ?
-# Non: Le nom de la sous-classe doit toujours comporter le namespace.
-# OU ALORS: Si namespace, c'est une hierachie a part:
-# portable/sources_types/file/dir
-# portable/sources_top/file/dir
-# oracle/sources_types/table
-# linux/sources_types/user
-# windows_com/enumerate.Win32_Process
-# Avantage: On deplace un namespace en copiant uniquement un directory.
-# Et meme pourquoi ne pas reutiliser la syntaxe des sous-classes de WMI et WBEM ?
-
-def TestUsability(importedMod,entity_type,entity_ids_arr):
-	try:
-		isUsable = importedMod.Usable(entity_type,entity_ids_arr)
-	except AttributeError:
-		return None
-
-	# sys.stderr.write("Module %s : %d\t" %(argFil,isUsable	))
-	if isUsable:
-		return None
-
-	errorMsg = importedMod.Usable.__doc__
-	if not errorMsg:
-		errorMsg = importedMod.__name__ + " not usable"
-		if not errorMsg:
-			errorMsg = "No message"
-	return errorMsg
-
-def DirMenuReport(depthCall,strMsg):
-	txtMargin = ( "    " * depthCall )
-	sys.stderr.write(txtMargin + strMsg)
-
-# This lists the scripts and generate RDF nodes.
-# Returns True if something was added.
-def DirToMenu(grph,parentNode,curr_dir,relative_dir,depthCall = 1):
-	# DirMenuReport( depthCall, "curr_dir=%s relative_dir=%s\n"%(curr_dir,relative_dir))
-	# In case there is nothing.
-	dirs = None
-	for path, dirs, files in os.walk(curr_dir):
-		break
-
-	# Maybe this class is not defined in our ontology.
-	if dirs == None:
-		# sys.stderr.write("No content in "+curr_dir)
-		return False
-
-	# Will still be None if nothing is added.
-	rdfNode = None
-	sub_path = path[ len(curr_dir) : ]
-
-	relative_dir_sub_path = relative_dir + sub_path
-
-	argDir = relative_dir_sub_path.replace("/",".")[1:]
-
-	# Maybe there is a usability test in the current module.
-	# The goal is to control all scripts in the subdirectories, from here.
-	try:
-		entity_class = ".".join( relative_dir.split("/")[2:] )
-		# DirMenuReport( depthCall, "entity_class=%s\n"%(entity_class))
-
-		importedMod = lib_util.GetEntityModule(entity_class)
-		if importedMod:
-			errorMsg = TestUsability(importedMod,entity_type,entity_ids_arr)
-			# if flagShowAll and errorMsg ???
-			if errorMsg:
-				# Surprisingly, the message is not displayed as a subdirectory, but in a separate square.
-				grph.add( ( parentNode, lib_common.MakeProp("Usability"), rdflib.Literal(errorMsg) ) )
-				return False
-	except IndexError:
-		# If we are at the top-level, no interest for the module.
-		pass
-
-
-	containsSomething = False
-	for dir in dirs:
-		# DirMenuReport( depthCall, "dir=%s\n"%(dir))
-		# Might be generated by our Python interpreter.
-		if dir == "__pycache__":
-			continue
-
-		full_sub_dir = curr_dir + "/" + dir
-		full_sub_dir = full_sub_dir.replace("\\","/")
-
-		currDirNode = lib_util.DirDocNode(argDir,dir)
-
-		if not currDirNode:
-			DirMenuReport( depthCall, "currDirNode NONE: argDir=%s dir=%s\n"%(argDir,dir))
-			continue
-
-		sub_relative_dir = relative_dir + "/" + dir
-
-		sub_entity_class = ".".join( sub_relative_dir.split("/")[2:] )
-		ontoKeys = lib_util.OntologyClassKeys(sub_entity_class)
-		# DirMenuReport( depthCall, "Checked ontology of %s: ontoKeys=%s\n"%(sub_entity_class,str(ontoKeys)))
-
-		# TODO: Beware, if not ontology, returns empty array. Why not returning None ?
-		if ontoKeys != []:
-			# DirMenuReport( depthCall, "Module %s has an ontology so it is a class. Skipping\n"%(sub_relative_dir))
-			# BEWARE: NO MORE DEFAULT ONTOLOGY ["Id"]
-			continue
-
-		somethingAdded = DirToMenu(grph,currDirNode, full_sub_dir,sub_relative_dir, depthCall + 1)
-		# This adds the directory name only if it contains a script.
-		if somethingAdded:
-			# CA MARCHE DANS LES DEUX CAS. SI PROPRIETE DIFFERENTE, ON AURA SIMPLEMENT DEUX PAVES, UN POUR LES DIR, L AUTRE POUR LES FICHIERS.
-			# grph.add( ( parentNode, pc.property_directory, currDirNode ) )
-			grph.add( ( parentNode, pc.property_rdf_data1, currDirNode ) )
-		containsSomething = containsSomething | somethingAdded
-
-	for fil in files:
-		# We want to list only the usable Python scripts.
-		if not fil.endswith(".py") or fil == "__init__.py":
-			continue
-
-		script_path = relative_dir_sub_path + "/" + fil
-
-		# DirMenuReport( depthCall, "DirToMenu encodedEntityId=%s\n" % encodedEntityId)
-		if is_host_remote:
-			genObj = lib_common.RemoteBox(entity_host)
-		else:
-			genObj = lib_common.gUriGen
-
-		url_rdf = genObj.MakeTheNodeFromScript( script_path, entity_type, encodedEntityId )
-
-		errorMsg = None
-
-		try:
-			importedMod = lib_util.GetScriptModule(argDir, fil)
-		except Exception:
-			errorMsg = sys.exc_info()[1]
-			DirMenuReport( depthCall, "Cannot import=%s. Caught: %s\n" % (script_path, errorMsg ) )
-			importedMod = None
-			if not flagShowAll:
-				continue
-
-		if not errorMsg:
-			# Show only scripts which want to be shown. Each script can have an optional function
-			# called Usable(): If it is there and returns False, the script is not displayed.
-			errorMsg = TestUsability(importedMod,entity_type,entity_ids_arr)
-
-		if not flagShowAll and errorMsg:
-			continue
-
-		# If the entity is on another host, does this work on remote entities ?
-		if is_host_remote:
-			try:
-				# Script can be used on a remote entity.
-				can_process_remote = importedMod.CanProcessRemote
-			except AttributeError:
-				can_process_remote = False
-
-			if not can_process_remote:
-				if not errorMsg:
-					errorMsg = "%s is local" % ( argFil[1:] )
-				DirMenuReport( depthCall, "Script %s %s cannot work on remote entities: %s at %s\n" % ( currentModule, argFil, entity_id , entity_host ) )
-
-				if not flagShowAll:
-					continue
-
-		# Here, we are sure that the script is added.
-		# TODO: If no script is added, should not add the directory?
-		rdfNode = rdflib.term.URIRef(url_rdf)
-		grph.add( ( parentNode, pc.property_rdf_data1, rdfNode ) )
-
-		# Default doc text is file name minus the ".py" extension.
-		nodModu = lib_util.FromModuleToDoc(importedMod,fil[:-3])
-
-		grph.add( ( rdfNode, pc.property_information, nodModu ) )
-
-		if errorMsg:
-			grph.add( ( rdfNode, lib_common.MakeProp("Error"), rdflib.Literal(errorMsg) ) )
-
-	# This tells if a script was added in this directory or one of the subdirs.
-	return ( rdfNode is not None ) | containsSomething
 
 ################################################################################
-
-
-
-# Si entity_type != "" mais entity_id == "", ca n'a pas de sens
-# d'afficher les scripts du directory htbin/sources/<type>
-# car on n'a pas d'id. En revanche, on pourrait afficher selectivement
-# des scripts dans "top" qui affichent toutes les entites de ce type.
-# Ca revient a selectionner certains scripts.
-# On peut faire ca grossierement en filtrant sur le nom.
-# Mais on voudrait en fait les afficher directement.
-# On peut donc avoir des scripts appeles top/<type>.index.xyzw.py .
-# Mais on voudrait en avoir plusieurs, eventuellement.
-
 
 def CurrentUser():
 	currProc = psutil.Process(os.getpid())
@@ -306,14 +85,6 @@ def AddDefaultScripts(grph,rootNode,entity_host):
 ################################################################################
 
 def Main():
-	# They are defined global so that DirToMenu can access them without code change.
-	global is_host_remote
-	global entity_type
-	global entity_id
-	global encodedEntityId
-	global entity_host
-	global entity_ids_arr
-	global flagShowAll
 
 	paramkeyShowAll = "Show all scripts"
 
@@ -336,23 +107,14 @@ def Main():
 	if not is_host_remote:
 		entity_host = ""
 
-
-	if entity_type:
-		# entity_type might contain a slash, for example: "sqlite/table"
-		relative_dir = "/sources_types/" + entity_type
-	else:
-		relative_dir = "/sources_types"
-
-	# sys.stderr.write("entity: lib_util.gblTopScripts=%s relative_dir=%s\n" % ( lib_util.gblTopScripts, relative_dir ) )
-
-	directory = lib_util.gblTopScripts + relative_dir
-
 	grph = rdflib.Graph()
 
 	rootNode = lib_util.RootUri()
 
+
 	if entity_id != "" or entity_type == "":
 		entity_ids_arr = lib_util.EntityIdToArray( entity_type, entity_id )
+		# entity_info_only.AddInformation(grph,rootNode,entity_id, entity_type)
 
 		# Each entity type ("process","file" etc... ) can have a small library
 		# of its own, for displaying a rdf node of this type.
@@ -366,17 +128,7 @@ def Main():
 					sys.stderr.write("No AddInfo for %s %s: %s\n"%( entity_type, entity_id, str(exc) ))
 		else:
 			sys.stderr.write("No lib_entities for %s %s\n"%( entity_type, entity_id ))
-
-		encodedEntityId=lib_util.EncodeUri(entity_id)
-
-		# TODO: Plutot qu'attacher tous les sous-directory a node parent,
-		# ce serait peut-etre mieux d'avoir un seul lien, et d'afficher
-		# les enfants dans une table, un record etc...
-		# OU: Certaines proprietes arborescentes seraient representees en mettant
-		# les objets dans des boites imbriquees: Tables ou records.
-		# Ca peut marcher quand la propriete forme PAR CONSTRUCTION
-		# un DAG (Direct Acyclic Graph) qui serait alors traite de facon specifique.
-		DirToMenu(grph,rootNode,directory,relative_dir)
+		entity_dirmenu_only.DirToMenu(grph,rootNode,entity_type,entity_id,is_host_remote,flagShowAll)
 
 	if entity_type != "":
 		CIM_ComputerSystem.AddWbemWmiServers(grph,rootNode, entity_host, nameSpace, entity_type, entity_id)
