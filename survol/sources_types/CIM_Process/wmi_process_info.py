c1f089bc80cec860111aa4990d56f7bc53fc261a
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-08-14 23:16:11 +0100

    Display the unit with the numbers.

--- survol/sources_types/CIM_Process/wmi_process_info.py
+++ survol/sources_types/CIM_Process/wmi_process_info.py
@@ -84,47 +84,87 @@ def Main():
 	#        CommandLine = "\"C:\\Windows\\system32\\SearchFilterHost
 	#        ExecutablePath = "C:\\Windows\\system32\\SearchFilterHos
 
+	lstPropNames = [
+		"CreationDate",
+		"CSName",
+		"HandleCount",
+		"KernelModeTime",
+		"Name",
+		"OSName",
+		"OtherOperationCount",
+		"OtherTransferCount",
+		"PageFaults",
+		"PageFileUsage",
+		"PeakPageFileUsage",
+		"PeakVirtualSize",
+		"PeakWorkingSetSize",
+		"Priority",
+		"PrivatePageCount",
+		"QuotaNonPagedPoolUsage",
+		"QuotaPagedPoolUsage",
+		"QuotaPeakNonPagedPoolUsage",
+		"QuotaPeakPagedPoolUsage",
+		"ReadOperationCount",
+		"ReadTransferCount",
+		"SessionId",
+		"ThreadCount",
+		"UserModeTime",
+		"VirtualSize",
+		"WorkingSetSize",
+		"WriteOperationCount",
+		"WriteTransferCount"]
+
+
+
+	# WMI amene les informations d'unites !!!!!
+	# https://msdn.microsoft.com/en-us/library/aa393650%28v=vs.85%29.aspx
+	#
+	# "All CIM-compliant implementations must handle a standard set of qualifiers.
+	# Units
+	# Data type: string
+	# Applies to: properties, methods, parameters
+	# Type of unit in which the associated data item is expressed. The default is NULL.
+	# For example, a size data item might have a value of "bytes" for Units."
+
+	className = "CIM_Process"
+	theCls = lib_wmi.GetWmiClassFlagUseAmendedQualifiersn(cnnct, className)
+
+	mapPropUnits = {}
+
+	if theCls:
+		for propObj in theCls.Properties_:
+			# sys.stderr.write("    propObj.Qualifiers_('Description')=%s\n"%str(propObj.Qualifiers_("Description")))
+			# sys.stderr.write("    propObj.Qualifiers_('Units')=%s\n"%str(propObj.Qualifiers_("Units")))
+			propNam = propObj.Name # 'str(propObj.Qualifiers_("DisplayName"))'
+			try:
+				unitNam = str(propObj.Qualifiers_("Units"))
+			# except pywintypes.com_error:
+			except :
+				unitNam = ""
+
+			sys.stderr.write("Prop=%s unit=%s\n"%(propNam,unitNam))
+
+			mapPropUnits[propNam] = unitNam
+
+			#for qual in propObj.Qualifiers_:
+			#	sys.stderr.write("        qual=%s => %s \n"%(qual.Name,qual.Value))
 
 	# There should be one process only.
 	for wmiProc in lstProcs:
 		sys.stderr.write("wmiProc=%s\n" % str(wmiProc))
 		grph.add( ( node_process, pc.property_information, lib_common.NodeLiteral( wmiProc.Description ) ) )
 
-		for prpProc in [
-			"CreationDate",
-			"CSName",
-			"HandleCount",
-			"KernelModeTime",
-			"Name",
-			"OSName",
-			"OtherOperationCount",
-			"OtherTransferCount",
-			"PageFaults",
-			"PageFileUsage",
-			"PeakPageFileUsage",
-			"PeakVirtualSize",
-			"PeakWorkingSetSize",
-			"Priority",
-			"PrivatePageCount",
-			"QuotaNonPagedPoolUsage",
-			"QuotaPagedPoolUsage",
-			"QuotaPeakNonPagedPoolUsage",
-			"QuotaPeakPagedPoolUsage",
-			"ReadOperationCount",
-			"ReadTransferCount",
-			"SessionId",
-			"ThreadCount",
-			"UserModeTime",
-			"VirtualSize",
-			"WorkingSetSize",
-			"WriteOperationCount",
-			"WriteTransferCount"]:
+		for prpProc in lstPropNames:
 			valProc = getattr(wmiProc, prpProc)
-			grph.add( ( node_process, lib_common.MakeProp(prpProc), lib_common.NodeLiteral( valProc ) ) )
+			try:
+				valUnit = mapPropUnits[prpProc]
+			except KeyError:
+				valUnit = ""
+			valProcUnit = str(valProc) + " " + valUnit
+			grph.add( ( node_process, lib_common.MakeProp(prpProc), lib_common.NodeLiteral( valProcUnit ) ) )
 
 		parent_node_process = serverBox.PidUri(wmiProc.ParentProcessId)
 		grph.add( ( node_process, pc.property_ppid, parent_node_process ) )
-
 
 	cgiEnv.OutCgiRdf()
 
