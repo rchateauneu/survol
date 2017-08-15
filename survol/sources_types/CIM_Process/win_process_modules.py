1e5f21291034139c48bc03d6415b14591c01ebda
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-08-15 23:17:50 +0100

    Cleanup.

--- survol/sources_types/CIM_Process/win_process_modules.py
+++ survol/sources_types/CIM_Process/win_process_modules.py
@@ -4,12 +4,10 @@
 Windows process modules
 """
 
-#import os
 import sys
 import lib_util
 import lib_common
 from sources_types import CIM_Process
-#import lib_entities.CIM_Process
 from lib_properties import pc
 
 Usable = lib_util.UsableWindows
