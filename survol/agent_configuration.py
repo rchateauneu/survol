59e0f274518aed4721210298217a51ea6113db4b
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-09-04 08:04:03 +0100

    New scripts which tells index.htm about the Agent configuration.

---
+++ survol/agent_configuration.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+"""
+Agent configuration
+"""
+
+# This tells what are the capabilities of the agent running on the server:
+# If DOT is there.
+# Machine, server type.
+#
+# It is interrogated by index.htm and returns information in JSON format.
+#
+
