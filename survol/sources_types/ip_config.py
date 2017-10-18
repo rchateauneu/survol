e8e6bf68e8c2069ef2d4d992910a4fb3129f8b19
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-10-18 09:08:48 +0100

    New script displaying IP config. Initial revision.

---
+++ survol/sources_types/ip_config.py
@@ -0,0 +1,127 @@
+# IP configuraton
+
+# C:\Users\rchateau>ipconfig /all
+#
+# Windows IP Configuration
+#
+#    Host Name . . . . . . . . . . . . : rchateau-HP
+#    Primary Dns Suffix  . . . . . . . :
+#    Node Type . . . . . . . . . . . . : Hybrid
+#    IP Routing Enabled. . . . . . . . : No
+#    WINS Proxy Enabled. . . . . . . . : No
+#
+# Wireless LAN adapter Wireless Network Connection:
+#
+#    Connection-specific DNS Suffix  . :
+#    Description . . . . . . . . . . . : NETGEAR WNDA3100v3 N600 Wireless Dual Band USB Adapter
+#    Physical Address. . . . . . . . . : B0-7F-B9-FF-3A-70
+#    DHCP Enabled. . . . . . . . . . . : Yes
+#    Autoconfiguration Enabled . . . . : Yes
+#    Link-local IPv6 Address . . . . . : fe80::e884:fb9a:c1b3:5fbc%20(Preferred)
+#    IPv4 Address. . . . . . . . . . . : 192.168.0.20(Preferred)
+#    Subnet Mask . . . . . . . . . . . : 255.255.255.0
+#    Lease Obtained. . . . . . . . . . : 14 October 2017 21:41:07
+#    Lease Expires . . . . . . . . . . : 24 October 2017 21:41:09
+#    Default Gateway . . . . . . . . . : 192.168.0.1
+#    DHCP Server . . . . . . . . . . . : 192.168.0.1
+#    DHCPv6 IAID . . . . . . . . . . . : 464551865
+#    DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-1C-9B-61-ED-8C-DC-D4-34-D4-38
+#    DNS Servers . . . . . . . . . . . : 194.168.4.100
+#                                        194.168.8.100
+#    NetBIOS over Tcpip. . . . . . . . : Enabled
+#
+# Ethernet adapter Local Area Connection:
+#
+#    Connection-specific DNS Suffix  . :
+#    Description . . . . . . . . . . . : Realtek PCIe GBE Family Controller
+#    Physical Address. . . . . . . . . : 8C-DC-D4-34-D4-38
+#    DHCP Enabled. . . . . . . . . . . : Yes
+#    Autoconfiguration Enabled . . . . : Yes
+#    Link-local IPv6 Address . . . . . : fe80::3c7a:339:64f0:2161%11(Preferred)
+#    IPv4 Address. . . . . . . . . . . : 192.168.0.14(Preferred)
+#    Subnet Mask . . . . . . . . . . . : 255.255.255.0
+#    Lease Obtained. . . . . . . . . . : 14 October 2017 21:40:58
+#    Lease Expires . . . . . . . . . . : 24 October 2017 21:40:58
+#    Default Gateway . . . . . . . . . : 192.168.0.1
+#    DHCP Server . . . . . . . . . . . : 192.168.0.1
+#    DHCPv6 IAID . . . . . . . . . . . : 244112596
+#    DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-1C-9B-61-ED-8C-DC-D4-34-D4-38
+#    DNS Servers . . . . . . . . . . . : 194.168.4.100
+#                                        194.168.8.100
+#    NetBIOS over Tcpip. . . . . . . . : Enabled
+#
+# Ethernet adapter VirtualBox Host-Only Network:
+#
+#    Connection-specific DNS Suffix  . :
+#    Description . . . . . . . . . . . : VirtualBox Host-Only Ethernet Adapter
+#    Physical Address. . . . . . . . . : 08-00-27-00-98-C2
+#    DHCP Enabled. . . . . . . . . . . : No
+#    Autoconfiguration Enabled . . . . : Yes
+#    Link-local IPv6 Address . . . . . : fe80::c9af:fa02:c433:5ebf%15(Preferred)
+#    IPv4 Address. . . . . . . . . . . : 192.168.56.1(Preferred)
+#    Subnet Mask . . . . . . . . . . . : 255.255.255.0
+#    Default Gateway . . . . . . . . . :
+#    DHCPv6 IAID . . . . . . . . . . . : 336068647
+#    DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-1C-9B-61-ED-8C-DC-D4-34-D4-38
+#    DNS Servers . . . . . . . . . . . : fec0:0:0:ffff::1%1
+#                                        fec0:0:0:ffff::2%1
+#                                        fec0:0:0:ffff::3%1
+#    NetBIOS over Tcpip. . . . . . . . : Enabled
+#
+# Tunnel adapter isatap.{372DB82B-FE28-489B-B744-FC1C0F726791}:
+#
+#    Media State . . . . . . . . . . . : Media disconnected
+#    Connection-specific DNS Suffix  . :
+#    Description . . . . . . . . . . . : Microsoft ISATAP Adapter
+#    Physical Address. . . . . . . . . : 00-00-00-00-00-00-00-E0
+#    DHCP Enabled. . . . . . . . . . . : No
+#    Autoconfiguration Enabled . . . . : Yes
+#
+# Tunnel adapter isatap.{BCAE0703-FC45-4644-92D4-031350F78C6E}:
+#
+#    Media State . . . . . . . . . . . : Media disconnected
+#    Connection-specific DNS Suffix  . :
+#    Description . . . . . . . . . . . : Microsoft ISATAP Adapter #2
+#    Physical Address. . . . . . . . . : 00-00-00-00-00-00-00-E0
+#    DHCP Enabled. . . . . . . . . . . : No
+#    Autoconfiguration Enabled . . . . : Yes
+#
+# Tunnel adapter Teredo Tunneling Pseudo-Interface:
+#
+#    Media State . . . . . . . . . . . : Media disconnected
+#    Connection-specific DNS Suffix  . :
+#    Description . . . . . . . . . . . : Teredo Tunneling Pseudo-Interface
+#    Physical Address. . . . . . . . . : 00-00-00-00-00-00-00-E0
+#    DHCP Enabled. . . . . . . . . . . : No
+#    Autoconfiguration Enabled . . . . : Yes
+#
+# Tunnel adapter isatap.{CF185B35-1F88-46CF-A6CE-BDECFBB59B4F}:
+#
+#    Media State . . . . . . . . . . . : Media disconnected
+#    Connection-specific DNS Suffix  . :
+#    Description . . . . . . . . . . . : Microsoft ISATAP Adapter #3
+#    Physical Address. . . . . . . . . : 00-00-00-00-00-00-00-E0
+#    DHCP Enabled. . . . . . . . . . . : No
+#    Autoconfiguration Enabled . . . . : Yes
+
+
+import subprocess
+
+mapIpconfigs = dict()
+currItf = ""
+proc = subprocess.Popen(['ipconfig'],['/all'],stdout=subprocess.PIPE)
+for currLine in proc.stdout.readlines():
+	currLine = currLine.decode("utf-8").rstrip()
+	if currLine:
+		if currLine[0] != " ":
+			currItf = currLine.strip()[:-1]
+			mapIpconfigs[currItf] = []
+		else:
+			idxColon = currLine.find(":")
+			if idxColon >= 0:
+				currKey = currLine[:idxColon].replace(". ","").strip()
+				currVal = currLine[idxColon+1:].strip()
+			else:
+				currVal = currLine.strip()
+			mapIpconfigs[currItf].append( (currKey, currVal))
+
