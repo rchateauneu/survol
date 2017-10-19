704da67c569b696d69ff8552321ad3ae63295a61
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-10-19 04:41:54 +0100

    CIM_NetworkAdapter and ip config progress.

--- survol/sources_types/CIM_NetworkAdapter/__init__.py
+++ survol/sources_types/CIM_NetworkAdapter/__init__.py
@@ -2,7 +2,10 @@
 Network adapter
 """
 
+import lib_common
+
 def EntityOntology():
     return ( ["Name"], )
 
-
+def MakeUri(naNam):
+	return lib_common.gUriGen.UriMakeFromDict("CIM_NetworkAdapter",{ "Name":naNam })
--- survol/sources_types/win32/ip_config.py
+++ survol/sources_types/win32/ip_config.py
@@ -1,126 +1,158 @@
+#!/usr/bin/python
+
+"""
+IP configuration
+"""
+
+import os
+import sys
+import subprocess
+import lib_common
+import lib_util
+from lib_properties import pc
+
+from sources_types import CIM_NetworkAdapter
+
 # IP configuraton
 
-# C:\Users\rchateau>ipconfig /all
+# >>> for n in w.Win32_NetworkAdapter():
+# ...     print(n)
 #
-# Windows IP Configuration
+# instance of Win32_NetworkAdapter
+# {
+#         AdapterType = "Ethernet 802.3";
+#         AdapterTypeId = 0;
+#         Availability = 3;
+#         Caption = "[00000007] Realtek PCIe GBE Family Controller";
+#         ConfigManagerErrorCode = 0;
+#         ConfigManagerUserConfig = FALSE;
+#         CreationClassName = "Win32_NetworkAdapter";
+#         Description = "Realtek PCIe GBE Family Controller";
+#         DeviceID = "7";
+#         GUID = "{372DB82B-FE28-489B-B744-FC1C0F726791}";
+#         Index = 7;
+#         Installed = TRUE;
+#         InterfaceIndex = 11;
+#         MACAddress = "8C:DC:D4:34:D4:38";
+#         Manufacturer = "Realtek";
+#         MaxNumberControlled = 0;
+#         Name = "Realtek PCIe GBE Family Controller";
+#         NetConnectionID = "Local Area Connection";
+#         NetConnectionStatus = 2;
+#         NetEnabled = TRUE;
+#         PhysicalAdapter = TRUE;
+#         PNPDeviceID = "PCI\\VEN_10EC&DEV_8168&SUBSYS_18E9103C&REV_0C\\4&11DD9C9B&0&00E2";
+#         PowerManagementSupported = FALSE;
+#         ProductName = "Realtek PCIe GBE Family Controller";
+#         ServiceName = "RTL8167";
+#         Speed = "100000000";
+#         SystemCreationClassName = "Win32_ComputerSystem";
+#         SystemName = "RCHATEAU-HP";
+#         TimeOfLastReset = "20171014214032.375199+060";
+# };
 #
-#    Host Name . . . . . . . . . . . . : rchateau-HP
-#    Primary Dns Suffix  . . . . . . . :
-#    Node Type . . . . . . . . . . . . : Hybrid
-#    IP Routing Enabled. . . . . . . . : No
-#    WINS Proxy Enabled. . . . . . . . : No
-#
-# Wireless LAN adapter Wireless Network Connection:
-#
-#    Connection-specific DNS Suffix  . :
-#    Description . . . . . . . . . . . : NETGEAR WNDA3100v3 N600 Wireless Dual Band USB Adapter
-#    Physical Address. . . . . . . . . : B0-7F-B9-FF-3A-70
-#    DHCP Enabled. . . . . . . . . . . : Yes
-#    Autoconfiguration Enabled . . . . : Yes
-#    Link-local IPv6 Address . . . . . : fe80::e884:fb9a:c1b3:5fbc%20(Preferred)
-#    IPv4 Address. . . . . . . . . . . : 192.168.0.20(Preferred)
-#    Subnet Mask . . . . . . . . . . . : 255.255.255.0
-#    Lease Obtained. . . . . . . . . . : 14 October 2017 21:41:07
-#    Lease Expires . . . . . . . . . . : 24 October 2017 21:41:09
-#    Default Gateway . . . . . . . . . : 192.168.0.1
-#    DHCP Server . . . . . . . . . . . : 192.168.0.1
-#    DHCPv6 IAID . . . . . . . . . . . : 464551865
-#    DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-1C-9B-61-ED-8C-DC-D4-34-D4-38
-#    DNS Servers . . . . . . . . . . . : 194.168.4.100
-#                                        194.168.8.100
-#    NetBIOS over Tcpip. . . . . . . . : Enabled
-#
-# Ethernet adapter Local Area Connection:
-#
-#    Connection-specific DNS Suffix  . :
-#    Description . . . . . . . . . . . : Realtek PCIe GBE Family Controller
-#    Physical Address. . . . . . . . . : 8C-DC-D4-34-D4-38
-#    DHCP Enabled. . . . . . . . . . . : Yes
-#    Autoconfiguration Enabled . . . . : Yes
-#    Link-local IPv6 Address . . . . . : fe80::3c7a:339:64f0:2161%11(Preferred)
-#    IPv4 Address. . . . . . . . . . . : 192.168.0.14(Preferred)
-#    Subnet Mask . . . . . . . . . . . : 255.255.255.0
-#    Lease Obtained. . . . . . . . . . : 14 October 2017 21:40:58
-#    Lease Expires . . . . . . . . . . : 24 October 2017 21:40:58
-#    Default Gateway . . . . . . . . . : 192.168.0.1
-#    DHCP Server . . . . . . . . . . . : 192.168.0.1
-#    DHCPv6 IAID . . . . . . . . . . . : 244112596
-#    DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-1C-9B-61-ED-8C-DC-D4-34-D4-38
-#    DNS Servers . . . . . . . . . . . : 194.168.4.100
-#                                        194.168.8.100
-#    NetBIOS over Tcpip. . . . . . . . : Enabled
-#
-# Ethernet adapter VirtualBox Host-Only Network:
-#
-#    Connection-specific DNS Suffix  . :
-#    Description . . . . . . . . . . . : VirtualBox Host-Only Ethernet Adapter
-#    Physical Address. . . . . . . . . : 08-00-27-00-98-C2
-#    DHCP Enabled. . . . . . . . . . . : No
-#    Autoconfiguration Enabled . . . . : Yes
-#    Link-local IPv6 Address . . . . . : fe80::c9af:fa02:c433:5ebf%15(Preferred)
-#    IPv4 Address. . . . . . . . . . . : 192.168.56.1(Preferred)
-#    Subnet Mask . . . . . . . . . . . : 255.255.255.0
-#    Default Gateway . . . . . . . . . :
-#    DHCPv6 IAID . . . . . . . . . . . : 336068647
-#    DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-1C-9B-61-ED-8C-DC-D4-34-D4-38
-#    DNS Servers . . . . . . . . . . . : fec0:0:0:ffff::1%1
-#                                        fec0:0:0:ffff::2%1
-#                                        fec0:0:0:ffff::3%1
-#    NetBIOS over Tcpip. . . . . . . . : Enabled
-#
-# Tunnel adapter isatap.{372DB82B-FE28-489B-B744-FC1C0F726791}:
-#
-#    Media State . . . . . . . . . . . : Media disconnected
-#    Connection-specific DNS Suffix  . :
-#    Description . . . . . . . . . . . : Microsoft ISATAP Adapter
-#    Physical Address. . . . . . . . . : 00-00-00-00-00-00-00-E0
-#    DHCP Enabled. . . . . . . . . . . : No
-#    Autoconfiguration Enabled . . . . : Yes
-#
-# Tunnel adapter isatap.{BCAE0703-FC45-4644-92D4-031350F78C6E}:
-#
-#    Media State . . . . . . . . . . . : Media disconnected
-#    Connection-specific DNS Suffix  . :
-#    Description . . . . . . . . . . . : Microsoft ISATAP Adapter #2
-#    Physical Address. . . . . . . . . : 00-00-00-00-00-00-00-E0
-#    DHCP Enabled. . . . . . . . . . . : No
-#    Autoconfiguration Enabled . . . . : Yes
-#
-# Tunnel adapter Teredo Tunneling Pseudo-Interface:
-#
-#    Media State . . . . . . . . . . . : Media disconnected
-#    Connection-specific DNS Suffix  . :
-#    Description . . . . . . . . . . . : Teredo Tunneling Pseudo-Interface
-#    Physical Address. . . . . . . . . : 00-00-00-00-00-00-00-E0
-#    DHCP Enabled. . . . . . . . . . . : No
-#    Autoconfiguration Enabled . . . . : Yes
-#
-# Tunnel adapter isatap.{CF185B35-1F88-46CF-A6CE-BDECFBB59B4F}:
-#
-#    Media State . . . . . . . . . . . : Media disconnected
-#    Connection-specific DNS Suffix  . :
-#    Description . . . . . . . . . . . : Microsoft ISATAP Adapter #3
-#    Physical Address. . . . . . . . . : 00-00-00-00-00-00-00-E0
-#    DHCP Enabled. . . . . . . . . . . : No
-#    Autoconfiguration Enabled . . . . : Yes
 
-import subprocess
+# C:\Python27\python.exe C:/Users/rchateau/Developpement/ReverseEngineeringApps/PythonStyle/Experimental/Test_ip_config.py
+# Ethernet adapter Local Area Connection
+#     Connection-specific DNS Suffix :
+#   = DESCRIPTION:Realtek PCIe GBE Family Controller
+#     Physical Address : 8C-DC-D4-34-D4-38
+#     DHCP Enabled : Yes
+#     Autoconfiguration Enabled : Yes
+#     Link-local IPv6 Address : fe80::3c7a:339:64f0:2161%11(Preferred)
+#   = ADDRESS:192.168.0.14()
+#     Subnet Mask : 255.255.255.0
+#     Lease Obtained : 14 October 2017 21:40:58
+#     Lease Expires : 24 October 2017 21:40:58
+#   = GATEWAY:192.168.0.1
+#   = DHCP:192.168.0.1
+#     DHCPv6 IAID : 244112596
+#     DHCPv6 Client DUID : 00-01-00-01-1C-9B-61-ED-8C-DC-D4-34-D4-38
+#   = DNS:194.168.4.100
+#   = DNS:194.168.8.100
+#     NetBIOS over Tcpip : Enabled
 
-mapIpconfigs = dict()
-currItf = ""
-proc = subprocess.Popen(['ipconfig'],['/all'],stdout=subprocess.PIPE)
-for currLine in proc.stdout.readlines():
-	currLine = currLine.decode("utf-8").rstrip()
-	if currLine:
-		if currLine[0] != " ":
-			currItf = currLine.strip()[:-1]
-			mapIpconfigs[currItf] = []
-		else:
-			idxColon = currLine.find(":")
-			if idxColon >= 0:
-				currKey = currLine[:idxColon].replace(". ","").strip()
-				currVal = currLine[idxColon+1:].strip()
+
+def CreateIpConfigData():
+	"""
+		This loads into a map the result of IPCONFIG command.
+	"""
+	mapIpconfigs = dict()
+	currItf = ""
+	proc = subprocess.Popen(['ipconfig','/all'],stdout=subprocess.PIPE)
+	for currLine in proc.stdout.readlines():
+		currLine = currLine.decode("utf-8").rstrip()
+		if currLine:
+			if currLine[0] != " ":
+				currItf = currLine.strip()
+				if currItf[-1] == ":":
+					currItf = currItf[:-1]
+				mapIpconfigs[currItf] = []
 			else:
-				currVal = currLine.strip()
-			mapIpconfigs[currItf].append( (currKey, currVal))
+				idxColon = currLine.find(":")
+				if idxColon >= 0:
+					currKey = currLine[:idxColon].replace(". ","").strip()
+					currVal = currLine[idxColon+1:].strip()
+				else:
+					currVal = currLine.strip()
+				mapIpconfigs[currItf].append( (currKey, currVal))
+	return mapIpconfigs
+
+def AddOneNodeIpConfig(grph,rootNode,keyMap,subMapIpconfigs):
+
+	txtDescription = None
+
+	# if key.startswith("Ethernet adapter") or key.startswith("Wireless LAN adapter"):
+	for kvPair in subMapIpconfigs:
+		if kvPair[0] == "Description":
+			txtDescription = kvPair[1]
+			break
+
+	if not txtDescription:
+		return None
+
+	naNode = CIM_NetworkAdapter.MakeUri(txtDescription)
+
+	prpDHCP_Server = lib_common.MakeProp("DHCP Server")
+	prpDHCP_Server = lib_common.MakeProp("DHCP Server")
+
+	# if key.startswith("Ethernet adapter") or key.startswith("Wireless LAN adapter"):
+	for kvPair in subMapIpconfigs:
+		propName = kvPair[0]
+		paramVal = kvPair[1]
+		prp = lib_common.MakeProp(propName)
+
+		if propName in ["IPv4 Address","DHCP Server","DNS Servers","Default Gateway"]:
+			ipAddr = paramVal.replace("(Preferred)","")
+			if ipAddr:
+				hostNode = lib_common.gUriGen.HostnameUri( ipAddr )
+			grph.add( (naNode, prp, hostNode ) )
+		else:
+			grph.add( (naNode, prp, lib_common.NodeLiteral(paramVal) ) )
+
+	return naNode
+
+def AddNodesIpConfig(grph,rootNode,mapIpconfigs):
+
+	prpNetAdapt = lib_common.MakeProp("Network adapter")
+	for keyMap in mapIpconfigs:
+		subMapIpconfigs = mapIpconfigs[keyMap]
+		naNode = AddOneNodeIpConfig(grph,rootNode,keyMap,subMapIpconfigs)
+		if naNode:
+			grph.add( (rootNode, prpNetAdapt, naNode ) )
+
+def Main():
+	cgiEnv = lib_common.CgiEnv()
+
+	grph = cgiEnv.GetGraph()
+
+	rootNode = lib_common.nodeMachine
+	mapIpconfigs = CreateIpConfigData()
+
+	AddNodesIpConfig(grph,rootNode,mapIpconfigs)
+
+	cgiEnv.OutCgiRdf()
+
+if __name__ == '__main__':
+	Main()
 
