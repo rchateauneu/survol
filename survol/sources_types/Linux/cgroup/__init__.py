d1e436500de1037ef67c7fff16c52f36af86f694
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-10-19 04:39:47 +0100

    cgroup progress.

--- survol/sources_types/Linux/cgroup/__init__.py
+++ survol/sources_types/Linux/cgroup/__init__.py
@@ -2,3 +2,16 @@
 """
 Linux cgroup
 """
+
+# No need to define Usable as we are in the Linux subdirectory.
+
+import sys
+import lib_common
+from lib_properties import pc
+
+def EntityOntology():
+    return ( ["Name"], )
+
+def EntityName(entity_ids_arr,entity_host):
+	entity_id = entity_ids_arr[0]
+	return entity_id
