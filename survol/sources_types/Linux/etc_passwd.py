9f01af6768b1a2b4923b59e42bc3c22215dc8709
primhilltc <primhilltc@ssh02.cluster023.gra.hosting.ovh.net>
2017-10-20 06:38:12 +0000

    Executable and home path are now links.

--- survol/sources_types/Linux/etc_passwd.py
+++ survol/sources_types/Linux/etc_passwd.py
@@ -19,16 +19,37 @@ def Main():
 
 	grph = cgiEnv.GetGraph()
 
+
+
 	usersList = survol_user.LoadEtcPasswd()
 
+	# User name
+	# Information used to validate a user's password; in most modern uses.
+	# user identifier number.
+	# group identifier number.
+	# Gecos field, commentary that describes the person or account.
+	# Path to the user's home directory.
+	# Program that is started every time the user logs into the system.
+	#
 	# polkituser:x:17:17:system user for policykit:/:/sbin/nologin
+	# puppet:x:103:106:Puppet configuration management daemon,,,:/var/lib/puppet:/bin/false
 	for userNam, splitLin in list( usersList.items() ):
 		userNode = lib_common.gUriGen.UserUri( userNam )
 		comment = splitLin[4]
 		# Sometimes the comment equals the user, so nothing to mention.
 		if comment != "" and comment != userNam:
 			grph.add( ( userNode, pc.property_information, lib_common.NodeLiteral( comment ) ) )
-		grph.add( ( userNode, pc.property_information, lib_common.NodeLiteral( splitLin[6] ) ) )
+		if splitLin[5]:
+			homeNode = lib_common.gUriGen.DirectoryUri( splitLin[5] )
+			grph.add( ( userNode, pc.property_information, homeNode ) )
+		execName = splitLin[6].strip()
+		if execName:
+			pass
+			if execName == "/bin/false":
+				grph.add( ( userNode, pc.property_information, lib_common.NodeLiteral(execName) ) )
+			else:
+				execNode = lib_common.gUriGen.FileUri( execName )
+				grph.add( ( userNode, pc.property_information, execNode ) )
 
 	cgiEnv.OutCgiRdf()
 
