8b448557f48ee477cf8cdc65fa9506fd75758caa
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-11-01 22:34:59 +0000

    Comments.

--- survol/direct_access.py
+++ survol/direct_access.py
@@ -3,4 +3,11 @@
 # TODO: There should be a form do go directly to an object such as a file:
 # TODO: Select the entity type from a list, then enter its abbreviated URL,
 # TODO: or have a a text cell for each parameter, with the parameter name.
+# Get the parameters from the ontology
+# Maybe go from here from objtypes.py
+# BTW: What is the difference between class_wbem.py and objtypes_wbem.py ?
 
+# Same ideai: Point to here from a WMI class.
+# Select the properties and allow to enter them.
+# Create a WMI query.
+# Same idea in WBEM.
