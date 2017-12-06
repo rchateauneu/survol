3413911c43c742d836d3b5b75c566203626e6df4
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-12-06 07:04:44 +0000

    Added implementation.

--- survol/sources_types/nmap/nmap_broadcast_netbios_master_browser.py
+++ survol/sources_types/nmap/nmap_broadcast_netbios_master_browser.py
@@ -24,3 +24,42 @@ from lib_properties import pc
 # WARNING: No targets were specified, so 0 hosts scanned.
 # Nmap done: 0 IP addresses (0 hosts up) scanned in 4.32 seconds
 #
+
+
+
+def Main():
+	cgiEnv = lib_common.CgiEnv()
+
+	args = ["nmap", '-oX', '-', '--script', "broadcast-netbios-master-browser", ]
+
+	# The returned IP address is wrong when launched from a Windows machine where the DB is running.
+	p = lib_common.SubProcPOpen(args)
+
+	grph = cgiEnv.GetGraph()
+
+	(nmap_last_output, nmap_err) = p.communicate()
+
+	dom = xml.dom.minidom.parseString(nmap_last_output)
+
+	# <script id="broadcast-netbios-master-browser" output="..."/>
+
+	for aScript in dom.getElementsByTagName('script'):
+		# output="&#xa;ip server domain&#xa;192.168.0.15  WDMYCLOUDMIRROR  WORKGROUP&#xa;"
+		anOutput = aScript.getAttributeNode('output').value.strip()
+		sys.stderr.write("anOutput=%s\n"%str(anOutput))
+		arrSplit = [ aWrd.strip() for aWrd in anOutput.split("\n") ]
+
+		sys.stderr.write("arrSplit=%s\n"%str(arrSplit))
+
+		theMachFull = arrSplit[1].strip()
+		machIp, machNam, nameDomain = [ subWrd.split() for subWrd in theMachFull.split(" +") ]
+
+		nodeHost = lib_common.gUriGen.HostnameUri( machNam )
+		grph.add( ( nodeHost, lib_common.MakeProp("IP address"), lib_common.NodeLiteral( machIp ) ) )
+		grph.add( ( nodeHost, lib_common.MakeProp("Domain"), lib_common.NodeLiteral( nameDomain ) ) )
+		grph.add( ( nodeHost, pc.property_information, lib_common.NodeLiteral( arrSplit[0] ) ) )
+
+	cgiEnv.OutCgiRdf()
+
+if __name__ == '__main__':
+	Main()
