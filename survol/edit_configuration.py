8fcc7b96a670deddb5bc6e6cc576bfe1ac23f0c3
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-12-13 01:34:24 +0000

    Added bookmarks parameter.

--- survol/edit_configuration.py
+++ survol/edit_configuration.py
@@ -15,14 +15,24 @@ def Main():
     lib_export_html.DisplayHtmlTextHeader("Configuration")
 
     WrtAsUtf("""
-    <body><h2>Edit Survol configuration</h2><br>
+    <body><h2>Edit Survol configuration</h2>
     """)
 
     WrtAsUtf("""
     <form method="post" action="edit_configuration.py" name="ServerConfiguration">
-    CGI server port number:
-    <input name="server_port" value="8000"><br><br>
-    <input value="Submit configuration" name="Hello" type="submit"><br>
+    <table border="0">
+    <tr>
+    <td>CGI server port number:</td>
+    <td><input name="server_port" value="8000"></td>
+    </tr>
+    <tr>
+    <td>Bookmarks file or URL:</td>
+    <td><input name="bookmark_url" value="bookmarks.htm"></td>
+    </tr>
+    <tr>
+    <td colspan="2"><input value="Submit configuration" name="Hello" type="submit"></td>
+    </tr>
+    </table>
     </form>
     """)
 
