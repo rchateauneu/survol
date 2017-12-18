d79c82a94617460cc6fb1005c6a3a309fde754cd
primhilltc <primhilltc@ssh02.cluster023.gra.hosting.ovh.net>
2017-12-18 16:59:07 +0000

    Now displays a node for the mysqlserver, along with the database.

--- survol/sources_types/mysql/database/__init__.py
+++ survol/sources_types/mysql/database/__init__.py
@@ -14,3 +14,9 @@ def MakeUri(hostName,dbName):
 
 def EntityName(entity_ids_arr,entity_host):
 	return entity_ids_arr[1]+ "@" + entity_ids_arr[0]
+
+def AddInfo(grph,node,entity_ids_arr):
+	hostMySql = entity_ids_arr[0]
+	nodeHostMySql = lib_common.gUriGen.HostnameUri( hostMySql )
+	grph.add( ( node, lib_common.MakeProp("Mysql server"), nodeHostMySql ) )
+
