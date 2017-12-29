be0b44b98f24b882f511f35f2111ba259ae9ae49
Remi Chateauneu <remi.chateauneu@gmail.com>
2017-12-29 15:32:34 +0000

    Main page documentation for Python remove useless code.

--- survol/sources_types/__init__.py
+++ survol/sources_types/__init__.py
@@ -1,15 +1,32 @@
 """
-Windows user account
+Top-level scripts directory
 """
 
-#import sys
-#import lib_common
-#from lib_properties import pc
+# Special syntax for Doxygen
+# https://stackoverflow.com/questions/4302870/doxygen-syntax-in-python
 
-def EntityOntology():
-	return ( ["Name"],)
+## \mainpage Scripts documentation
+#
+# Survol library is made of packages and subpackages which represents its classes model.
+#
+#  \subsection cls_vs_ns Classes vs namespaces
+#
+# A class is a module where the function Ontology() is present,
+# whether it is defined by the module or one of its parents modules.
+# The return value of EntityOntology() is a list of string, each representing an attribute of the class.
+# If a module nor none of its parent modules define the function EntityOntology(), it is a namespace.
+#
+#  \subsection step1 Adding user code
+#
+# To add a new class, or new scripts, to Survol,
+# one just need to copy a file tree at the right place in this source tree.
+#
+#
 
-# This must add information about the user.
-def AddInfo(grph,node,entity_ids_arr):
-	# Nothing to do.
-	return
+# def EntityOntology():
+# 	return ( ["Name"],)
+#
+# # This must add information about the user.
+# def AddInfo(grph,node,entity_ids_arr):
+# 	# Nothing to do.
+# 	return
