e1d28967f10221ca4e08435e4a690b12dd962025
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-01-03 22:01:06 +0000

    More progress.

--- Experimental/TestCapstone.py
+++ Experimental/TestCapstone.py
@@ -1,5 +1,6 @@
 # import capstone
 
+import pefile
 from capstone import *
 from capstone.arm import *
 
@@ -37,9 +38,92 @@ def TestThree():
 				print("%u" %g),
 			print
 
-TestHeavy()
-TestLight()
-TestThree()
+
+# https://stackoverflow.com/questions/37439627/which-bytes-should-i-pass-to-capstone-to-disassemble-the-executable-code-of-a-pe
+def TestDisassembleFile(file_path):
+    print("File=%s"%file_path)
+    pe = pefile.PE(file_path)
+
+    eop = pe.OPTIONAL_HEADER.AddressOfEntryPoint
+    code_section = pe.get_section_by_rva(eop)
+
+    code_dump = code_section.get_data()
+
+    code_addr = pe.OPTIONAL_HEADER.ImageBase + code_section.VirtualAddress
+
+    md = Cs(CS_ARCH_X86, CS_MODE_64)
+
+    for i in md.disasm(code_dump, code_addr):
+        print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+
+# https://stackoverflow.com/questions/37439627/which-bytes-should-i-pass-to-capstone-to-disassemble-the-executable-code-of-a-pe
+def TestDisassembleFileGroups(file_path):
+    print("File=%s"%file_path)
+    pe = pefile.PE(file_path)
+
+    eop = pe.OPTIONAL_HEADER.AddressOfEntryPoint
+    code_section = pe.get_section_by_rva(eop)
+
+    code_dump = code_section.get_data()
+
+    code_addr = pe.OPTIONAL_HEADER.ImageBase + code_section.VirtualAddress
+
+    md = Cs(CS_ARCH_X86, CS_MODE_64)
+    md.detail = True
+
+    for i in md.disasm(code_dump, code_addr):
+        print("0x%x:\t%-20s\t%-40s\t%10s" %(i.address, i.mnemonic, i.op_str, i.id))
+
+
+def SplitPE(file_path):
+	pe = pefile.PE(file_path)
+
+	for section in pe.sections:
+		# ('.bss\x00\x00\x00\x00', '0xf000', '0x14c', 0)
+		# ('.data\x00\x00\x00', '0x5e000', '0x3a24', 11264)
+		# ('.edata\x00\x00', '0x10000', '0x2067', 8704)
+		# ('.idata\x00\x00', '0x18000', '0x76c', 2048)
+		# ('.rdata\x00\x00', '0x15000', '0x1972', 6656)
+		# ('.reloc\x00\x00', '0x63000', '0x53c0', 21504)
+		# ('.rsrc\x00\x00\x00', '0x62000', '0x3d8', 1024)
+		# ('.stab\x00\x00\x00', '0x15000', '0x33660', 210944)
+		# ('.stabstr', '0x49000', '0xa2d5f', 667136)
+		# ('.text\x00\x00\x00', '0x1000', '0x5cd75', 380416)
+		# ('.textbss', '0x1000', '0x10000', 0)
+
+		# typedef struct _IMAGE_SECTION_HEADER {
+		#   BYTE  Name[IMAGE_SIZEOF_SHORT_NAME];
+		#   union {
+		# 	DWORD PhysicalAddress;
+		# 	DWORD VirtualSize;
+		#   } Misc;
+		#   DWORD VirtualAddress;
+		#   DWORD SizeOfRawData;
+		#   DWORD PointerToRawData;
+		#   DWORD PointerToRelocations;
+		#   DWORD PointerToLinenumbers;
+		#   WORD  NumberOfRelocations;
+		#   WORD  NumberOfLinenumbers;
+		#   DWORD Characteristics;
+		# } IMAGE_SECTION_HEADER, *PIMAGE_SECTION_HEADER;
+		print (section.Name, hex(section.VirtualAddress), hex(section.Misc_VirtualSize), section.SizeOfRawData )
+		print ( str(section.Name), section.VirtualAddress, section.Misc_VirtualSize, section.SizeOfRawData )
+
+
+if False:
+	TestHeavy()
+	TestLight()
+	TestThree()
+
+for file_path in [
+	#r"C:\Program Files (x86)\Git\bin\libaprutil-0-0.dll",
+	r"C:\Program Files (x86)\Hewlett-Packard\Energy Star\ClearSysReg.exe",
+	#r"C:\Program Files (x86)\Hewlett-Packard\Energy Star\Estar.dll",
+	#r"C:\Program Files (x86)\Hewlett-Packard\Energy Star\msvcp100.dll",
+]:
+	#SplitPE(file_path)
+	#TestDisassembleFile(file_path)
+	TestDisassembleFileGroups(file_path)
 
 rr = raw_input("Press return:")
 
