8c4a99ce02b966eeee4a24e4ac68c3e5b9397568
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-01-05 18:33:38 +0000

    Display Survol agents registered with SLP.

--- survol/sources_types/neighborhood/survol_neighborhood.py
+++ survol/sources_types/neighborhood/survol_neighborhood.py
@@ -2,12 +2,17 @@
 
 """
 Neighboring Survol agents.
+
+Distant Survol agents can broadcast their presence with SLP.
+Set the Service Location Protocol flag to enable this detection.
 """
 
 import sys
 import lib_util
 import lib_common
 import lib_credentials
+from lib_properties import pc
+from sources_types import neighborhood as survol_neighborhood
 
 def AddSurvolNode(grph,hostSurvol,urlSurvolClean):
 	sys.stderr.write("AddSurvolNode hostSurvol=%s\n"%(hostSurvol))
@@ -31,15 +36,31 @@ def AddSurvolNode(grph,hostSurvol,urlSurvolClean):
 		nodeRemoteHost = serverBox.HostnameUri(hostSurvol)
 		grph.add( ( survolHostNode, lib_common.MakeProp("Survol host"), nodeRemoteHost ) )
 
-		grph.add( ( survolHostNode, lib_common.MakeProp("Survol agent"), lib_common.NodeUrl(urlSurvolClean) ) )
+		nodeSurvolUrl = lib_common.NodeUrl(urlSurvolClean)
+		grph.add( ( survolHostNode, lib_common.MakeProp("Survol agent"), nodeSurvolUrl ) )
 
 	else:
 		urlSurvolModed = lib_util.AnyUriModed(urlSurvolClean, currDispMode)
 
+		nodeSurvolUrl = lib_common.NodeUrl(urlSurvolModed)
+
 		# Should check the URL to be sure it is valid.
 
 		# sys.stderr.write("AddSurvolNode urlSurvolModed=%s\n"%(urlSurvolModed))
-		grph.add( ( survolHostNode, lib_common.MakeProp("Survol agent"), lib_common.NodeUrl(urlSurvolModed) ) )
+		grph.add( ( survolHostNode, lib_common.MakeProp("Survol agent"), nodeSurvolUrl ) )
+
+	return nodeSurvolUrl
+
+
+def CallbackNodeAdder(grph,urlSurvol):
+	parsed_url = lib_util.survol_urlparse( urlSurvol )
+	hostSurvol = parsed_url.hostname
+	# sys.stderr.write("SurvolServersDisplay hostSurvol=%s\n"%(hostSurvol))
+	if hostSurvol:
+		nodeSurvolUrl = AddSurvolNode(grph,hostSurvol,urlSurvol)
+		return nodeSurvolUrl
+	else:
+		return None
 
 
 def SurvolServersDisplay(grph):
@@ -50,23 +71,37 @@ def SurvolServersDisplay(grph):
 		# sys.stderr.write("SurvolServersDisplay urlSurvol=%s\n"%(urlSurvol))
 
 		# The credentials are not needed until a Survol agent uses HTTPS.
-		parsed_url = lib_util.survol_urlparse( urlSurvol )
-		hostSurvol = parsed_url.hostname
-		# sys.stderr.write("SurvolServersDisplay hostSurvol=%s\n"%(hostSurvol))
-		if not hostSurvol:
-			continue
-
-		AddSurvolNode(grph,hostSurvol,urlSurvol)
-
+		CallbackNodeAdder(grph,urlSurvol)
 
 
 def Main():
+	# If this flag is set, the script uses SLP (Servicel Location Protocol)
+	# to browse other Survol Agents.
+	paramkeySLP = "Service Location Protocol"
 
-	cgiEnv = lib_common.CgiEnv()
+	cgiEnv = lib_common.CgiEnv(
+		parameters = { paramkeySLP : False }
+	)
+
+	flagSLP = bool(cgiEnv.GetParameters( paramkeySLP ))
 
 	grph = cgiEnv.GetGraph()
 
 	SurvolServersDisplay(grph)
+
+	if flagSLP:
+		dictServices = survol_neighborhood.GetSLPServices("survol")
+		for keyService in dictServices:
+			nodeSurvolUrl = CallbackNodeAdder(grph,keyService)
+			grph.add( ( nodeSurvolUrl,
+						pc.property_information,
+						lib_common.NodeLiteral("Service Location Protocol") ) )
+			attrsService = dictServices[keyService]
+			for keyAttr in attrsService:
+				propAttr = lib_common.MakeProp(keyAttr)
+				valAttr = attrsService[keyAttr]
+				grph.add( ( nodeSurvolUrl, propAttr, lib_common.NodeLiteral(valAttr) ) )
+
 
 	cgiEnv.OutCgiRdf()
 
