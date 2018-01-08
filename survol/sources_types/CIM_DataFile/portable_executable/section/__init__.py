84b1aba4523f303280ff8d884c0e58ee36e1c76c
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-01-08 11:32:09 +0000

    New class.

---
+++ survol/sources_types/CIM_DataFile/portable_executable/section/__init__.py
@@ -0,0 +1,20 @@
+"""
+PE file section
+"""
+
+import os
+import lib_common
+
+def EntityOntology():
+	return ( ["Name","Section"], )
+
+def EntityName(entity_ids_arr,entity_host):
+	fileName = entity_ids_arr[0]
+	sectionName = entity_ids_arr[1]
+
+	# A file name can be very long, so it is truncated.
+	fileNameBase = os.path.basename(fileName)
+	return fileNameBase + ":" + sectionName
+
+def MakeUri(fileName, sectionName):
+	return lib_common.gUriGen.UriMakeFromDict("CIM_Datafile/portable_executable/section", { "Name" : fileName, "Section" : sectionName } )
