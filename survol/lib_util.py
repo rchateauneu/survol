c560f144a1ee140ba805d83a233eaaea1b8b5d1a
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-01-15 16:30:42 +0000

    Created urllib_quote and urllib_unquote

--- survol/lib_util.py
+++ survol/lib_util.py
@@ -20,9 +20,11 @@ import importlib
 # In Python 3, urllib.quote has been moved to urllib.parse.quote and it does handle unicode by default.
 # TODO: Use module six.
 try:
-	from urllib import quote,unquote
+	from urllib import quote as urllib_quote
+	from urllib import unquote as urllib_unquote
 except ImportError:
-	from urllib.parse import quote,unquote
+	from urllib.parse import quote as urllib_quote
+	from urllib.parse import unquote as urllib_unquote
 
 try:
 	from urlparse import urlparse as survol_urlparse
@@ -308,13 +310,13 @@ def EncodeUri(anStr):
 
 	# In Python 3, urllib.quote has been moved to urllib.parse.quote and it does handle unicode by default.
 	if sys.version_info >= (3,):
-		return quote(strTABLE,'')
+		return urllib_quote(strTABLE,'')
 	else:
 
 		# THIS SHOULD NORMALLY BE DONE. BUT WHAT ??
 		###strTABLE = strTABLE.replace("&","%26")
 		# UnicodeDecodeError: 'ascii' codec can't decode byte 0xe9 in position 32
-		return quote(strTABLE,'ascii')
+		return urllib_quote(strTABLE,'ascii')
 
 ################################################################################
 
@@ -451,7 +453,7 @@ def ParseXidLocal(xid ):
 		entity_id_quoted = mtch_entity.group(3)
 
 		# Everything which comes after the dot which follows the class name.
-		entity_id = unquote(entity_id_quoted)
+		entity_id = urllib_unquote(entity_id_quoted)
 
 		return ( entity_type, entity_id, entity_host )
 
@@ -484,7 +486,7 @@ def ParseXidWMI(xid ):
 			# sys.stderr.write("WMI Class Cimom=%s ns_type=%s\n" % ( entity_host, entity_type ))
 		else:
 			# Remove the dot which comes after the class name.
-			entity_id = unquote(entity_id_quoted)[1:]
+			entity_id = urllib_unquote(entity_id_quoted)[1:]
 			# sys.stderr.write("WMI Object Cimom=%s ns_type=%s path=%s\n" % ( entity_host, entity_type, entity_id ))
 
 		return ( entity_type, entity_id, entity_host )
@@ -502,7 +504,7 @@ def ParseXidWMI(xid ):
 			# sys.stderr.write("WMI Class Cimom=%s ns_type=%s\n" % ( entity_host, entity_type ))
 		else:
 			# Remove the dot which comes after the class name.
-			entity_id = unquote(entity_id_quoted)[1:]
+			entity_id = urllib_unquote(entity_id_quoted)[1:]
 			# sys.stderr.write("WMI Object Cimom=%s ns_type=%s path=%s\n" % ( entity_host, entity_type, entity_id ))
 
 		return ( entity_type, entity_id, entity_host )
@@ -527,7 +529,7 @@ def ParseXidWBEM(xid ):
 			# sys.stderr.write("WBEM Class Cimom=%s ns_type=%s\n" % ( entity_host, entity_type ))
 		else:
 			# Remove the dot which comes after the class name.
-			entity_id = unquote(entity_id_quoted)[1:]
+			entity_id = urllib_unquote(entity_id_quoted)[1:]
 			# sys.stderr.write("WBEM Object Cimom=%s ns_type=%s path=%s\n" % ( entity_host, entity_type, entity_id ))
 
 		return ( entity_type, entity_id, entity_host )
@@ -553,7 +555,7 @@ def ParseXid(xid ):
 		return entity_triplet
 
 	# Apparently it is not a problem for the plain old entities.
-	xid = unquote(xid)
+	xid = urllib_unquote(xid)
 
 	entity_triplet = ParseXidWMI(xid )
 	if entity_triplet:
