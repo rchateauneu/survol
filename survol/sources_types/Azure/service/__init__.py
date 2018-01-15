91f78fa041a824b7f57dfbfd3c19946af99cb76c
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-01-15 22:38:42 +0000

    Fix incorrect URIs for Azure.

--- survol/sources_types/Azure/service/__init__.py
+++ survol/sources_types/Azure/service/__init__.py
@@ -2,6 +2,7 @@
 Azure service
 """
 
+import lib_util
 import lib_common
 
 def Graphic_colorbg():
@@ -11,5 +12,8 @@ def EntityOntology():
 	return ( ["Subscription","Service"], )
 
 def MakeUri(serviceName, subscriptionName):
+	# TODO: Because the input parameters could contain a sspace, derive from str() and define ValueDecode()
+	subscriptionName = lib_util.urllib_quote(subscriptionName)
+	serviceName = lib_util.urllib_quote(serviceName)
 	return lib_common.gUriGen.UriMakeFromDict("Azure/service", { "Subscription" : subscriptionName, "Service" : serviceName } )
 
