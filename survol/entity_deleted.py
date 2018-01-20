65eb2e466c58b1732758db71ebfbb32ac05b1fa4
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-01-20 15:59:57 +0000

    Initial draft revision.

---
+++ survol/entity_deleted.py
@@ -0,0 +1,7 @@
+# ASDasd
+# cA INDIQUE QUE LES OBJECTS EN QUESTONS SONT DELETES
+# L ordre des url, dans merge, ne doit pas avoir d importance.
+# Donc on  applique la deletion a la fin de la fusion.
+
+# On prend les parametres comme entity et on selection dans le graphe ce qui matche.
+
