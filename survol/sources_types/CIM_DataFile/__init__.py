85b8e8d80496a46556a5d08895f8996be5c0277c
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-01-27 07:35:17 +0000

    EntityName now takes only one argument.

--- survol/sources_types/CIM_DataFile/__init__.py
+++ survol/sources_types/CIM_DataFile/__init__.py
@@ -16,7 +16,7 @@ import json
 def EntityOntology():
 	return ( ["Name"], )
 
-def EntityName(entity_ids_arr,entity_host):
+def EntityName(entity_ids_arr):
 	entity_id = entity_ids_arr[0]
 	# A file name can be very long, so it is truncated.
 	file_basename = os.path.basename(entity_id)
