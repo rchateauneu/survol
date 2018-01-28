b8185921fe52b6818738878f1212f1a596f9c557
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-01-28 13:11:05 +0000

    Use global function for gethostname.

--- survol/sources_types/addr/__init__.py
+++ survol/sources_types/addr/__init__.py
@@ -7,6 +7,7 @@ import socket
 import threading
 import time
 import socket
+import lib_util
 import lib_common
 from lib_properties import pc
 
@@ -19,23 +20,47 @@ def EntityName(entity_ids_arr):
 	return entity_id
 
 def AddInfo(grph,node,entity_ids_arr):
+	timeStart = time.time()
 	socketNam = entity_ids_arr[0]
 	#sys.stderr.write("socketNam=%s\n"%socketNam)
 	socketSplit = socketNam.split(':')
 	socketAddr = socketSplit[0]
 	#sys.stderr.write("socketAddr=%s\n"%socketAddr)
-	sockIP = socket.gethostbyname(socketAddr)
-	#sys.stderr.write("sockIP=%s\n"%sockIP)
+	sockIP = lib_util.GlobalGetHostByName(socketAddr)
+	timeEnd = time.time()
+	timeDelta = timeEnd - timeStart
+	sys.stderr.write("addr.AddInfo tm=%f sockIP=%s\n"%(timeDelta,sockIP))
 
 	nodeHost = lib_common.gUriGen.HostnameUri( sockIP )
 	# Should be the otherway round, but it makes the graph ugly.
 	grph.add( ( node, pc.property_has_socket, nodeHost ) )
 
 def UniversalAlias(entity_ids_arr,entity_host,entity_class):
+	timeStart = time.time()
 	socketAddr, socketPort = entity_ids_arr[0].split(":")
-	sockIP = socket.gethostbyname(socketAddr)
 
-	return sockIP + ":" + socketPort
+	# Is the host an IP address ?
+	try:
+		socket.inet_aton(socketAddr)
+		sockIP = socketAddr
+	except socket.error:
+		# This is not an IP address, therefore must be converted.
+		sockIP = lib_util.GlobalGetHostByName(socketAddr)
+
+	if sockIP == "127.0.0.1":
+		sockIP = lib_util.GlobalGetHostByName(socket.getfqdn())
+
+	# Just in case this would be a service name, turn into a protocol number.
+	try:
+		socketPortNumber = socket.getservbyname(socketPort)
+	except:
+		socketPortNumber = socketPort
+
+	timeEnd = time.time()
+	timeDelta = timeEnd - timeStart
+	uniAlias = str(sockIP) + ":" + str(socketPortNumber)
+	sys.stderr.write("addr.UniversalAlias tm=%f entity_ids_arr=%s entity_host=%s uniAlias=%s\n"%(timeDelta,str(entity_ids_arr),entity_host,uniAlias))
+	return uniAlias
 
 
 # Add the real url corresponding to this socket so we can nicely click on it.
@@ -121,7 +146,7 @@ class PsutilAddSocketThread(threading.Thread):
 		finally:
 			self.grph_lock.release()
 		# Some throttling, in case there are thousands of nodes.
-		time.sleep(0.001)
+		# time.sleep(0.001)
 
 def PsutilAddSocketToGraphAsync(node_process,connects,grph,flagShowUnconnected):
 	threadsArr = []
