d4bc633d34ba1e33efc3dd5e3b3ad162e60d49d5
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-01-28 09:33:48 +0000

    Fix EntityName with universal alias.

--- survol/sources_types/addr/__init__.py
+++ survol/sources_types/addr/__init__.py
@@ -14,7 +14,7 @@ def EntityOntology():
 	return ( ["Id"],)
 
 # This returns a nice name given the parameter of the object.
-def EntityName(entity_ids_arr,entity_host):
+def EntityName(entity_ids_arr):
 	entity_id = entity_ids_arr[0]
 	return entity_id
 
@@ -30,6 +30,13 @@ def AddInfo(grph,node,entity_ids_arr):
 	nodeHost = lib_common.gUriGen.HostnameUri( sockIP )
 	# Should be the otherway round, but it makes the graph ugly.
 	grph.add( ( node, pc.property_has_socket, nodeHost ) )
+
+def UniversalAlias(entity_ids_arr,entity_host,entity_class):
+	socketAddr, socketPort = entity_ids_arr[0].split(":")
+	sockIP = socket.gethostbyname(socketAddr)
+
+	return sockIP + ":" + socketPort
+
 
 # Add the real url corresponding to this socket so we can nicely click on it.
 # This is a bit expeimental.
