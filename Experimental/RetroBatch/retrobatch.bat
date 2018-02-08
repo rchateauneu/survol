398d72c6972757906bd3963ae64857a47e70d002
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-02-08 22:45:46 +0000

    New project.

---
+++ Experimental/RetroBatch/retrobatch.bat
@@ -0,0 +1,21 @@
+@REM "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\cdb.exe"
+@REM "C:\Program Files (x86)\Windows Kits\10\Debuggers\x86\cdb.exe"
+
+
+cdb.exe -G -logo output.log -cf retrobatch_cdb.ini notepad.exe
+
+
+
+@REM
+@REM
+@REM
+@REM
+@REM
+@REM
+@REM
+@REM
+@REM
+@REM
+@REM
+@REM
+
