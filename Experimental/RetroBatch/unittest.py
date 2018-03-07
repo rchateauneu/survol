82a73fddd70f870565d6650f569d1b7fa0d96e81
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-03-08 00:19:15 +0100

    Added unit test framework.

---
+++ Experimental/RetroBatch/unittest.py
@@ -0,0 +1,16 @@
+import os
+import retrobatch
+
+for subdir, dirs, files in os.walk(rootdir):
+    for inFile in files:
+        #print os.path.join(subdir, file)
+
+        if inFile.endswith("log")i and inFile.startswith("mineit_"):
+            inPath = subdir + os.sep + inFile
+            print (inPath)
+
+            pre, ext = os.path.splitext(inFile)
+            outFile = pre + ".out"
+            outPath = subdir + os.sep + outFile
+
+            retrobatch.UnitTest(inPath,outPath()
