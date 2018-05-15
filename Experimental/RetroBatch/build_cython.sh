21a430d3c3273b4e0f22c397de133ed043e2fb56
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-05-15 21:44:30 +0100

    Initial revision.

---
+++ Experimental/RetroBatch/build_cython.sh
@@ -0,0 +1,8 @@
+cython dockit.py
+# gcc -I /usr/include/python2.7 -l python2.7 dockit.c -o dockit
+
+# gcc -Isrc -fPIC $(pkg-config --cflags --libs python2) -c src/hw.c hw_wrap.c
+# gcc -shared -fPIC -o hw.so hw.o hw_wrap.o
+
+# gcc -I /usr/include/python2.7 -l python2.7 dockit.c -o dockit
+gcc -Isrc -fPIC $(pkg-config --cflags --libs python2) -c dockit.c
