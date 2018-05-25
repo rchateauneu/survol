c6f5c3a7fbea235d5e5c5424fdef28e92349d650
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-05-25 11:45:22 +0100

    Typo.

--- survol/scripts/wget_test.sh
+++ survol/scripts/wget_test.sh
@@ -1,4 +1,4 @@
-# This test the local agent with wget.
+# This tests the local agent with wget.
 # The idea is to load as many URLs as possible,
 # to detect errors, bugs etc..
 wget --timeout=60 --directory-prefix=wget_test_output --recursive --level=2 http://rchateau-hp:8000/survol/entity.py?mode=html
