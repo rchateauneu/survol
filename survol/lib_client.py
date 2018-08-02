fda2b8a2596435df4c913779286c29e666133dba
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-08-02 22:45:27 +0100

    Enhancements.

--- survol/lib_client.py
+++ survol/lib_client.py
@@ -1,36 +1,19 @@
 # This allows to easily handle Survol URLs in Jupyter or any other client.
 
-# Server("127.0.0.1:8000").CIM_Process(Handle=1234) and Server("192.168.0.1:8000").CIM_Datafile(Name='/tmp/toto.txt')
-#
-class Source:
-	# This works with an Url or a local script. Possibly with a static file.
-	def __init__(self,address=None):
-		self.m_address = address
+import json
+import lib_kbase
 
-		self.m_is_file = True
+try:
+	# For Python 3.0 and later
+	from urllib.request import urlopen
+except ImportError:
+	# Fall back to Python 2's urllib2
+	from urllib2 import urlopen
 
-	def __url_with_mode(self,mode):
-		# TODO: "?" or "&"
-		return self.m_address + "?mode=" + mode
 
-	# This executes the script and return the data in the right format.
-	def __execute_script_with_mode(self,mode):
-		# Sets an envirorment variable then imports the script and execute it.
-		# TODO: "?" or "&"
-		return "scripts_prefix/" + self.m_address + "?mode=" + mode
-
-	# Output formats HTML, SVG, JSON, RDF. All are processed differently, so there is no need to unify.
-	def __pair_display(self,mode):
-		if self.IsScript():
-			data_content = self.__execute_script_with_mode("html")
-			return { "data" : data_content }
-
-		if self.isUrl():
-			the_url = self.__url_with_mode(mode)
-			# Add the extension "?mode=html" or "svg" etc...
-			return { "url" : the_url }
-
-		raise Exception("This is unexpected")
+class SourceBase (object):
+	def __init__(self):
+		self.m_current_triplestore = None
 
 	# from IPython.display import HTML
 	# HTML(url="http://vps516494.ovh.net/Survol/survol/sources_types/enumerate_CIM_Process.py?xid=.&mode=html")
@@ -44,31 +27,159 @@ class Source:
 	def pair_svg(self,mode):
 		return self.__pair_display("svg")
 
-	def __content(self,mode):
-		if self.IsScript():
-			data_content = self.__execute_script_with_mode("html")
-			return data_content
-
-		if self.isUrl():
-			the_url = self.__url_with_mode(mode)
-			url_content = load_the_url(the_url)
-			return the_url
-
-		raise Exception("This is unexpected")
-
-	# This return the merge of the two urls.
+	# This returns the merge of the two urls.
 	# Easy of two urls. What of one script and one url ?
-	# TODO: Modify merge_scritps so it can handle urls and scripts.
+	# TODO: Modify merge_scripts.py so it can handle urls and scripts.
 	#
-	def __add__(self, other):
-		return None
+	def __add__(self, otherSource):
+		return SourceMergePlus(self,otherSource)
+
+	def __sub__(self, otherSource):
+		return SourceMergeMinus(self,otherSource)
 
 	# So it can be used with rdflib and its Sparql component.
 	def content_rdf(self):
-		return self.__content("rdf")
+		return self.get_content_moded("rdf")
 
 	def content_json(self):
-		return self.__content("json")
+		return self.get_content_moded("json")
+
+	def get_triplestore(self):
+		docXmlRdf = self.get_content_moded("rdf")
+
+		return lib_kbase.triplestore_from_rdf_xml(docXmlRdf)
+
+	# It has to calculate the content, except if all sub-sources are URLs
+	# on the same Survol server, and in this calse only it can combine them
+	# with the script merge_scripts.py .
+	# In the general case, we are sure to have the content in RDF mode.
+	def __pair_display(self,mode):
+		aTriplestore = self.get_triplestore()
+		# This is done in lib_common when displaying a RDF triplestore.
+		contentModed = lib_common.triplestore_to_mode(aTriplestore,mode)
+
+		return { "data" : contentModed }
+
+
+
+	@staticmethod
+	def Factory(url = None, script = None):
+		if url:
+			return SourceUrl(url)
+		if script:
+			return SourceScript(script)
+		return None
+
+# If it has a class, then it has CGI arguments.
+class SourceCgi (SourceBase):
+	def __init__(self,dictParams):
+		self.m_dictParams = dictParams
+		super(SourceCgi, self).__init__()
+
+	# If it does not have the necessary CGI args,
+	# then loop on the existing objects of this class.
+	def IsCgiComplete(self):
+		return True
+
+
+# Server("127.0.0.1:8000").CIM_Process(Handle=1234) and Server("192.168.0.1:8000").CIM_Datafile(Name='/tmp/toto.txt')
+#
+class SourceUrl (SourceCgi):
+	def __init__(self,anUrl):
+		self.m_url = anUrl
+		dictParams = { "todo": "todo"}
+		super(SourceUrl, self).__init__(dictParams)
+
+	def __url_with_mode(self,mode):
+		# TODO: "?" or "&"
+		return self.m_url + "?mode=" + mode
+
+	# Output formats HTML, SVG, JSON, RDF. All are processed differently, so there is no need to unify.
+	def __pair_display(self,mode):
+		the_url = self.__url_with_mode(mode)
+
+		# Add the extension "?mode=html" or "svg" etc...
+		return { "url" : the_url }
+
+	def get_content_moded(self,mode):
+		the_url = self.__url_with_mode(mode)
+
+		response = urlopen(the_url)
+		data = response.read().decode("utf-8")
+		url_content = json.loads(data)
+		return url_content
+
+class SourceScript (SourceBase):
+	def __init__(self,aScript):
+		self.m_script = aScript
+		dictParams = { "todo": "todo"}
+		super(SourceScript, self).__init__(dictParams)
+
+	# This executes the script and return the data in the right format.
+	def __execute_script_with_mode(self,mode):
+		# Sets an envirorment variable then imports the script and execute it.
+		# TODO: "?" or "&"
+		aScript = "local_scripts_prefix/" + self.m_script + "?mode=" + mode
+
+		return self.execute_script(aScript)
+
+	# Output formats HTML, SVG, JSON, RDF. All are processed differently, so there is no need to unify.
+	def __pair_display(self,mode):
+		data_content = self.__execute_script_with_mode("html")
+		return { "data" : data_content }
+
+	def get_content_moded(self,mode):
+		data_content = self.__execute_script_with_mode(mode)
+		return data_content
+
+	def get_triplestore(self):
+		ExecutethecodelocallyandreturntheRDFobject()
+
+
+class SourceMerge (SourceBase):
+	def __init__(self,srcA,srcB):
+		if not srcA.IsComplete():
+			raise Exception("Left-hand-side URL must be complete")
+		self.m_srcA = srcA
+		self.m_srcB = srcB
+		super(SourceMerge, self).__init__()
+
+	def get_content(self,current_triplestore):
+		triplestoreA = self.m_srcA.get_triplestore()
+		if self.IsComplete():
+			triplestoreB = self.m_src_B.get_triplestore()
+
+			return self.combine_triplestores(triplestoreA,triplestoreB)
+
+		else:
+			# The class cannot be None because the url is not complete
+			objsList = lib_kbase.enumerate_objects_from_class(triplestoreA,self.m_src_B.m_class)
+
+			for anObj in objsList:
+				urlDerived = self.m_src_B.DeriveUrl(anObj)
+				triplestoreB = urlDerived.get_triplestore()
+				triplestoreA = self.combine_triplestores(triplestoreA,triplestoreB)
+			return triplestoreA
+
+
+
+# Function UrlToMergeD3()
+
+class SourceMergePlus (SourceMerge):
+	def __init__(self,srcA,srcB):
+		super(SourceScript, self).__init__(srcA,srcB)
+
+	@staticmethod
+	def combine_triplestores(triplestoreA,triplestoreB):
+		return triplestoreA + triplestoreB
+
+class SourceMergeMinus (SourceMerge):
+	def __init__(self,srcA,srcB):
+		super(SourceScript, self).__init__(srcA,srcB)
+
+	@staticmethod
+	def combine_triplestores(triplestoreA,triplestoreB):
+		return triplestoreA - triplestoreB
 
 
 
