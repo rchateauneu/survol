216aa34069aa844e1eedeab7d1c09b8b83d2cd40
Remi Chateauneu <remi.chateauneu@gmail.com>
2018-11-08 08:04:47 +0000

    ODBC column names fixes, AddInfo.

--- survol/sources_types/odbc/table/__init__.py
+++ survol/sources_types/odbc/table/__init__.py
@@ -5,7 +5,9 @@ Open Database Connectivity table
 import sys
 import lib_util
 import lib_common
+from lib_properties import pc
 from sources_types import odbc as survol_odbc
+from sources_types.odbc import dsn as survol_odbc_dsn
 
 def Graphic_colorbg():
 	return "#66FF33"
@@ -19,3 +21,12 @@ def MakeUri(dsnName,tableNam):
 def EntityName(entity_ids_arr):
 	# sys.stderr.write("EntityName entity_ids_arr=%s\n"%str(entity_ids_arr))
 	return survol_odbc.CgiPropertyDsn().ValueShortDisplay(entity_ids_arr[0]) + "::" + entity_ids_arr[1]
+
+def AddInfo(grph,node,entity_ids_arr):
+	dsnNam = entity_ids_arr[0]
+	# tabNam = entity_ids_arr[1]
+
+	nodeDsn = survol_odbc_dsn.MakeUri( dsnNam )
+
+	grph.add((node,pc.property_odbc_table, nodeDsn))
+
