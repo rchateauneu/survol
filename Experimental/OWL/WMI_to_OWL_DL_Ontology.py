dab0aea843c4e2aedb66aa7df68f83573b707c08
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-02-06 00:16:48 +0000

    New script for converting WMI CIM classes into OWL-DL ontology.

---
+++ Experimental/OWL/WMI_to_OWL_DL_Ontology.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import wmi
+
+cnn = wmi.WMI()
+
+cnt = 0
+
+map_attributes = {}
+
+for class_name in cnn.classes:
+    if cnt > 100: break
+    cnt += 1
+
+    cls_obj = getattr(cnn, class_name)
+    drv_list = cls_obj.derivation()
+    if drv_list:
+        base_class_name = drv_list[0]
+        print("""
+        <OWL:Class rdf:ID="%s">
+        <rdfs:subClassOf rdf:resource="#%s"/>
+        </OWL:Class>""" % (class_name, base_class_name))
+
+    for p in cls_obj.properties:
+        prop_obj = cls_obj.wmi_property(p)
+
+        try:
+            only_read = prop_obj.qualifiers['read']
+        except:
+            only_read = False
+        if not only_read:
+            map_attributes[prop_obj.name] = prop_obj.type
+
+map_types_CIM_to_OWL = {
+    "string":"xsd:string"
+}
+
+for prop_name in map_attributes:
+    prop_type = map_attributes[prop_name]
+    try:
+        owl_type = map_types_CIM_to_OWL[prop_type]
+    except:
+        owl_type = "xsd:" + prop_type
+    print("""
+    <OWL:DataTypeProperty rdf:ID="%s">
+    <rdfs:domain rdf:resources="OWL:Thing"/>
+    <rdfs:range rdf:resource="%s"/>
+    </OWL:DataTypeProperty>""" % (prop_name, owl_type))
+
+print("OK")
