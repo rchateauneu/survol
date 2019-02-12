ed28003c4f60680b5592580e8ef7dc5b2161b9b2
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-02-12 00:19:04 +0000

    Ontology progress

--- Experimental/OWL/WMI_to_OWL_DL_with_rdflib.py
+++ Experimental/OWL/WMI_to_OWL_DL_with_rdflib.py
@@ -85,7 +85,7 @@ def GetWmiClassFlagUseAmendedQualifiersn(connWmi, classNam):
 
 
 # owl_type: "xsd::string" etc... TODO: Transform this into XSD.string etc...
-def AddPropertyToOntology(graph,propertyName,prop_type):
+def AddPropertyToOntology(graph,propertyName,prop_type,prop_desc):
     # <OWL:DataTypeProperty rdf:ID="Name">
     # <rdfs:domain rdf:resource="OWL:Thing"/>
     # <rdfs:range rdf:resource="xsd:string"/>
@@ -93,12 +93,16 @@ def AddPropertyToOntology(graph,propertyName,prop_type):
     MyDatatypePropertyNode = URIRef(LDT[propertyName])
     graph.add((MyDatatypePropertyNode, RDF.type, OWL.DatatypeProperty))
     graph.add((MyDatatypePropertyNode, RDFS.domain, OWL.Thing))
-    owl_type = PropNameToType(prop_type)
-    graph.add((MyDatatypePropertyNode, RDFS.range, owl_type))
+    if prop_desc:
+        graph.add((MyDatatypePropertyNode, RDFS.comment, Literal(prop_desc)))
+    if prop_type:
+        owl_type = PropNameToType(prop_type)
+        graph.add((MyDatatypePropertyNode, RDFS.range, owl_type))
 
     #graph.add((MyClassNode, RDF.type, MyDatatypePropertyNode))
 
 # Construct the linked data tools namespace
+# See lib_properties.py: primns = "http://primhillcomputers.com/survol"
 LDT = Namespace("http://www.primhillcomputers.com/survol#")
 
 # Create the graph
@@ -108,17 +112,6 @@ cnn = wmi.WMI()
 
 cnt = 0
 
-def GetClassDescr(classNam):
-    theCls = GetWmiClassFlagUseAmendedQualifiersn(cnn, classNam)
-    if theCls:
-        try:
-            return str(theCls.Qualifiers_("Description"))
-            # pywintypes.com_error: (-2147352567, 'Exception occurred.', (0, u'SWbemQualifierSet', u'Not found ', None, 0, -2147217406), None)
-        except pywintypes.com_error:
-            return ""
-    return ""
-
-
 map_attributes = {}
 
 for class_name in cnn.classes:
@@ -126,32 +119,69 @@ for class_name in cnn.classes:
     cnt += 1
 
     cls_obj = getattr(cnn, class_name)
+    theCls = None
 
     #klassDescr = cls_obj.Qualifiers_("Description")
     drv_list = cls_obj.derivation()
     if drv_list:
         base_class_name = drv_list[0]
-        #outfil.write("""
-        #<OWL:Class rdf:ID="%s">
-        #<rdfs:subClassOf rdf:resource="#%s"/>
-        #</OWL:Class>\n""" % (class_name, base_class_name))
-        text_descr = GetClassDescr(class_name)
+        theCls = GetWmiClassFlagUseAmendedQualifiersn(cnn, class_name)
+        if theCls:
+            try:
+                text_descr = str(theCls.Qualifiers_("Description"))
+                # pywintypes.com_error: (-2147352567, 'Exception occurred.', (0, u'SWbemQualifierSet', u'Not found ', None, 0, -2147217406), None)
+            except pywintypes.com_error:
+                text_descr = ""
+        text_descr = ""
+
         AddClassToOntology(graph,class_name, base_class_name, text_descr)
 
     for p in cls_obj.properties:
         prop_obj = cls_obj.wmi_property(p)
+        try:
+            map_attributes[prop_obj.name]["type"]
+            continue
+        except KeyError:
+            pass
 
         try:
             only_read = prop_obj.qualifiers['read']
         except:
             only_read = False
         if not only_read:
-            map_attributes[prop_obj.name] = prop_obj.type
+            map_attributes[prop_obj.name] = { "type": prop_obj.type }
+
+    # Second enumeration of properties, different style.
+    if theCls:
+        for propObj in theCls.Properties_:
+            try:
+                map_attributes[propObj.Name]["description"]
+                continue
+            except KeyError:
+                pass
+            try:
+                # UnicodeEncodeError: 'ascii' codec can't encode character u'\xa0' in position 178: ordinal not in range(128)
+                propDsc = propObj.Qualifiers_("Description")
+                try:
+                    propTxt = str(propDsc)
+                except UnicodeEncodeError as exc:
+                    #print(dir(propDsc))
+                    propTxt = unicode(propDsc)
+                    #raise
+                map_attributes.get(propObj.Name,{})["description"] = propTxt
+            except pywintypes.com_error:
+                # pywintypes.com_error: (-2147352567, 'Exception occurred.', (0, u'SWbemQualifierSet', u'Not found ', None, 0, -2147217406), None)
+                pass
+
+
+
 
 for prop_name in map_attributes:
-    prop_type = map_attributes[prop_name]
+    prop_dict = map_attributes[prop_name]
+    prop_type = prop_dict.get("type","")
+    prop_desc = prop_dict.get("description","")
 
-    AddPropertyToOntology(graph,prop_name,prop_type)
+    AddPropertyToOntology(graph,prop_name,prop_type,prop_desc)
     #outfil.write("""
     #<OWL:DataTypeProperty rdf:ID="%s">
     #<rdfs:domain rdf:resources="OWL:Thing"/>
