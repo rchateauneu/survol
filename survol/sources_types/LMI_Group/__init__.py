bd9762b0698b2b90abeac9eac360d41c0b858b39
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-04-16 22:45:14 +0100

    Rename user and class to LMI_Account and LMI_Group.

---
+++ survol/sources_types/LMI_Group/__init__.py
@@ -0,0 +1,11 @@
+"""
+Unix-like users group
+"""
+
+# TODO: There is an OpenLMI provider which defined the class LMI_Group.
+# https://rrakus.fedorapeople.org/openlmi-account/doc/usage.html
+
+def EntityOntology():
+    return ( ["Name"], )
+
+
