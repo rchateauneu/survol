bcdef8c3784f1a79ab696e32fda39cbf882b4453
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-04-28 23:24:54 +0100

    Progress transforming a SPARQL query into WQL/WMI/WBEM.

--- survol/lib_sparql.py
+++ survol/lib_sparql.py
@@ -1,28 +1,10 @@
+# This transforms a SPARQL query into a WMI/WBEM query.
+# This is extremely restricted.
+
 import sys
 import rdflib.plugins.sparql.parser
 
-def print_recursive(data,level = 0):
-    if isinstance(data,(list,tuple)):
-        for elt in data:
-            print_recursive(elt,level+1)
-    elif isinstance(data,dict):
-        for key in data:
-            val = data[key]
-            sys.stdout.write("    "*level)
-            sys.stdout.write(key)
-            print_recursive(val,level+1)
-    else:
-        try:
-            sys.stdout.write("*****\n")
-            for elt in data.items():
-                print_recursive(elt,level+1)
-        except AttributeError:
-            sys.stdout.write("    "*level)
-            sys.stdout.write(str(type(data)))
-            sys.stdout.write(str(dir(data)))
-            sys.stdout.write(data)
-            sys.stdout.write("\n")
-
+import lib_util
 
 # This works, but we are only interested by triples.
 def print_simple(arg_elt,level=1):
@@ -84,16 +66,13 @@ def get_triples(arg_elt):
 # This groups tokens by sequence of three, to create triples.
 # All SPARQL constraints are mixed together
 def aggregate_triples(raw_trpl):
-    print("----------------------------")
     resu = []
     curr_trip = []
     cnt_trip = 0
-    print("Len raw_trpl=",len(raw_trpl))
+    DEBUG("Len raw_trpl=%s",len(raw_trpl))
     for block in raw_trpl:
         for elt in block:
-            #print(elt,type(elt))
             cnt_trip += 1
-            # curr_trip.append(str(elt))
             curr_trip.append(elt)
             if cnt_trip == 3:
                 resu.append(curr_trip)
@@ -102,7 +81,7 @@ def aggregate_triples(raw_trpl):
 
     if cnt_trip:
         resu.append(curr_trip)
-    print("Len resu=",len(resu))
+    DEBUG("Len resu=%s",len(resu))
     return resu
 
 
@@ -116,33 +95,33 @@ def pname_to_string(pname):
     value_name = value_prefix + ":" + value_localname
     return value_name
 
-def process_subject(subj):
+def decode_parsed_subject(subj):
     # PathAlternative_PathAlternative_{'part': [PathSequence_{'part': [PathElt_{'part': pname_{'localname': u'pid', 'prefix': u'survol'}}]}]}
     if isinstance(subj,rdflib.term.Variable):
-        return("VARIABLE=",str(subj))
+        return("TRPL_VARIABLE",str(subj))
         return
     if isinstance(subj,rdflib.term.Literal):
         # rdflib.term.Literal(u'123', datatype=rdflib.term.URIRef(u'http://www.w3.org/2001/XMLSchema#integer')
-        return("Literal=",str(subj))
+        return("TRPL_LITERAL",str(subj))
     if isinstance(subj,rdflib.term.BNode):
         # rdflib.term.BNode('N9b69940d021342f7b9dd341a53ea947b')
-        return("NODE=",str(subj))
+        return("TRPL_NODE",str(subj))
     if isinstance(subj,rdflib.term.URIRef):
         # rdflib.term.URIRef(u'http://nasa.dataincubator.org/spacecraft/1968-089A')
-        return("URIRef=",str(subj))
+        return("TRPL_URIREF",str(subj))
     if isinstance(subj,rdflib.plugins.sparql.parserutils.CompValue):
         if 'string' in subj:
             # literal_literal_{'string': rdflib.term.Literal(u'CIM_Process')}
             litt_string = subj['string']
             # rdflib.term.Literal(u'CIM_Process')
-            return("litt_string",str(litt_string))
+            return("TRPL_LITERAL",str(litt_string))
         if 'localname' in subj:
-            return("value_name",pname_to_string(subj))
-    print("ERRSUBJsubj=",subj)
-    print("ERRSUBJsubj=",type(subj))
+            return("TRPL_VALUE_NAME",pname_to_string(subj))
+    print("ERRSUBJsubj",subj)
+    print("ERRSUBJsubj",type(subj))
     raise Exception("Cannot parse")
 
-def process_predicate(pred):
+def decode_parsed_predicate(pred):
     # PathAlternative_PathAlternative_{'part': [PathSequence_{'part': [PathElt_{'part': pname_{'localname': u'pid', 'prefix': u'survol'}}]}]}
     if isinstance(pred,rdflib.plugins.sparql.parserutils.CompValue):
         try:
@@ -162,13 +141,13 @@ def process_predicate(pred):
                         one_part_part_first_part = one_part_part_first['part']
                         resu = pname_to_string(one_part_part_first_part)
 
-                        return("Predicate",resu)
+                        return("TRPL_PREDICATE",resu)
 
     elif isinstance(pred,rdflib.term.Variable):
         # rdflib.term.Variable(u'p')
-        return("VARIABLE",str(pred))
+        return("TRPL_VARIABLE",str(pred))
     elif isinstance(pred,rdflib.term.BNode):
-        return("NODE",pred)
+        return("TRPL_NODE",pred)
     else:
         print("ERREUR")
 
@@ -190,24 +169,86 @@ def process_predicate(pred):
 #   'item',
 #   "PathAlternative_PathAlternative_{'part': [PathSequence_{'part': [PathElt_{'part': pname_{'localname': u'P2848', 'prefix': u'wdt'}}]}]}",
 #   "pname_pname_{'localname': u'Q1543615', 'prefix': u'wd'}"]
-def prepare_triple(one_trpl):
-    s0 = process_subject(one_trpl[0])
-    s1 = process_predicate(one_trpl[1])
-    s2 = process_subject(one_trpl[2])
+def decode_parsed_triple(one_trpl):
+    s0 = decode_parsed_subject(one_trpl[0])
+    s1 = decode_parsed_predicate(one_trpl[1])
+    s2 = decode_parsed_subject(one_trpl[2])
 
-    #print(one_trpl[1],type(one_trpl[1]))
     clean_trpl = [s0,s1,s2]
     return clean_trpl
 
-def ProcessQuery(qry):
+def GenerateTriplesList(qry):
     parsed = rdflib.plugins.sparql.parser.parseQuery(qry)
 
     raw_trpl = get_triples(parsed)
     trpl_lst = aggregate_triples(raw_trpl)
     for one_trpl in trpl_lst:
-        clean_trpl = prepare_triple(one_trpl)
-        print("--------------------")
-        print("Subj:",clean_trpl[0])
-        print("Pred:",clean_trpl[1])
-        print("Obj:",clean_trpl[2])
+        clean_trpl = decode_parsed_triple(one_trpl)
+        yield clean_trpl
 
+# This receives the list of triples of a SPARQL query.
+# It returns a dictionary of variables with constant attributes.
+# The plan is to implement a minimalistic SPARQL engine,
+# over WMI or WBEM.
+# This is very restricted, because it does not work if there are joins.
+# Also, the CIM class of the object must be known.
+def ExtractEntities(lst_triples):
+    dictVariables = {}
+
+    # Gathers attributes of objects.
+    for one_triple in lst_triples:
+        if one_triple[0][0] != "TRPL_VARIABLE":
+            continue
+        # The predicate type could be "TRPL_VARIABLE"
+        if one_triple[1][0] != "TRPL_PREDICATE":
+            continue
+        # The object type could be "TRPL_VARIABLE"
+        if one_triple[2][0] != "TRPL_LITERAL":
+            continue
+
+        variable_name = one_triple[0][1]
+        try:
+            variable_dict = dictVariables[variable_name]
+        except KeyError:
+            variable_dict = {}
+            dictVariables[variable_name] = variable_dict
+        variable_dict[one_triple[1][1]] = one_triple[2][1]
+
+    return dictVariables
+
+def ClauseToQuery(one_clause):
+    return one_clause[0]
+
+def OneEntityToQuery(variable_name,key_vals):
+    # If the class is not defined, cannot query.
+    # TODO: Consider base classes ??
+    try:
+        class_name = key_vals['rdf:type']
+    except KeyError:
+        return ""
+
+    tests = ""
+    delim = " WHERE "
+    for key,val in key_vals.items():
+        if key == 'rdf:type':
+            continue
+        clause = '%s = "%s"' % ( key, val )
+        tests += delim + clause
+        delim = " AND "
+
+    return "select * from " + class_name + tests
+
+def EntitiesToQuery(lstEntities):
+    return ";".join( OneEntityToQuery(variable_name,key_vals) for variable_name, key_vals in lstEntities.items() )
+
+# http://timgolden.me.uk/python/downloads/wmi-0.6b.py
+
+# Quand on a un triplet de cette forme, trouver toutes les proprietes
+# litterales relatives au sujet.
+# Subj: ('VARIABLE=', 't')
+# Pred: ('Predicate', u'rdf:type')
+# Obj: ('litt_string', 'CIM_Process')# On peut alors en faire des requetes WMI ou WBEM, eventuellement.
+#
+# En theorie, c'est toujours possible mais probablement tres lent.
+#
+# Si on a les bons attributs, on peut executer le script principal dans survol.
