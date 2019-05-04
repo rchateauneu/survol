2956846c3b13b4abd6be603991693a1eac31d296
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-05-04 17:01:08 +0100

    Project.

---
+++ survol/analysis_import_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# TODO:
+
+# Importer un fichier ou un groupe de fichiers dans un zip, et renvoyer un document RDF
+# qui en represente l'analyse.
+
+# Et meme: Creer une session dans laquelle on va stocker les fichiers utilisateurs en isolation,
+# un peu comme un dossier FTP, qui sera un environnement associe a un utilisateur.
+# Ensuite, ouvrir une session de Survol travaillant exclusivement sur ces fichiers.
