e3d5ceafca72251252cf336d9eed3bf7182aacad
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-08-06 01:38:07 +0100

    Dockit unit tests.

---
+++ tests/test_dockit.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+
+import cgitb
+import cgi
+import os
+import sys
+import json
+import socket
+import unittest
+
+# This loads the module from the source, so no need to install it, and no need of virtualenv.
+sys.path.insert(0,"../survol/scripts")
+
+# "rchateau-hp"
+CurrentMachine = socket.gethostname().lower()
+
+# TODO: This should be a parameter.
+# It points to the Survol adhoc CGI server: "http://rchateau-hp:8000"
+RemoteTestAgent = "http://" + CurrentMachine + ":8000"
+
+
+class DockitTest(unittest.TestCase):
+    """
+    Test the execution of the Dockit script.
+    """
+
+    def test_file_strace(self):
+        import dockit
+
+        dockit.UnitTest(
+            inputLogFile = "sample_shell.strace.log",
+            tracer = "strace",
+            topPid = 0,
+            outFile = "result_strace",
+            outputFormat = "JSON",
+            verbose = True,
+            mapParamsSummary = ["CIM_Process","CIM_DataFile.Category=['Others','Shared libraries']"],
+            summaryFormat = "TXT",
+            withWarning = False,
+            withDockerfile = True,
+            updateServer = None)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
