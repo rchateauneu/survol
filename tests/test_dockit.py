2b943c4cfa3155c1da4d61f2324a4d6adda704cf
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-08-09 00:24:12 +0100

    More tests.

--- tests/test_dockit.py
+++ tests/test_dockit.py
@@ -26,22 +26,75 @@ class DockitTest(unittest.TestCase):
     Test the execution of the Dockit script.
     """
 
-    def test_file_strace(self):
+    def test_file_strace_txt(self):
         import dockit
 
-        for out_format in ["JSON", "CSV", "TXT"]:
-            dockit.UnitTest(
-                inputLogFile = "sample_shell.strace.log",
-                tracer = "strace",
-                topPid = 0,
-                baseOutName = "result_strace",
-                outputFormat = out_format,
-                verbose = True,
-                mapParamsSummary = ["CIM_Process","CIM_DataFile.Category=['Others','Shared libraries']"],
-                summaryFormat = "TXT",
-                withWarning = False,
-                withDockerfile = True,
-                updateServer = None)
+        dockit.UnitTest(
+            inputLogFile = "sample_shell.strace.log",
+            tracer = "strace",
+            topPid = 0,
+            baseOutName = "result_strace",
+            outputFormat = "TXT",
+            verbose = True,
+            mapParamsSummary = ["CIM_Process","CIM_DataFile.Category=['Others','Shared libraries']"],
+            summaryFormat = "TXT",
+            withWarning = False,
+            withDockerfile = True,
+            updateServer = None)
+
+        fil_txt = open("result_strace.txt")
+        fil_txt.close()
+
+        fil_summary = open("result_strace.summary.txt")
+        fil_summary.close()
+
+    def test_file_strace_csv(self):
+        import dockit
+
+        dockit.UnitTest(
+            inputLogFile = "sample_shell.strace.log",
+            tracer = "strace",
+            topPid = 0,
+            baseOutName = "result_strace",
+            outputFormat = "CSV",
+            verbose = True,
+            mapParamsSummary = ["CIM_Process","CIM_DataFile.Category=['Others','Shared libraries']"],
+            summaryFormat = "XML",
+            withWarning = False,
+            withDockerfile = False,
+            updateServer = None)
+
+        fil_csv = open("result_strace.csv")
+        fil_csv.close()
+
+        fil_summary = open("result_strace.summary.txt")
+        fil_summary.close()
+
+        fil_docker = open("result_strace.docker/Dockerfile")
+        fil_docker.close()
+
+    def test_file_strace_json(self):
+        import dockit
+
+        dockit.UnitTest(
+            inputLogFile = "sample_shell.strace.log",
+            tracer = "strace",
+            topPid = 0,
+            baseOutName = "result_strace",
+            outputFormat = "JSON",
+            verbose = True,
+            mapParamsSummary = ["CIM_Process","CIM_DataFile.Category=['Others','Shared libraries']"],
+            summaryFormat = "TXT",
+            withWarning = False,
+            withDockerfile = False,
+            updateServer = None)
+
+        fil_json = open("result_strace.json")
+        data = json.load(fil_json)
+        fil_json.close()
+
+        fil_summary = open("result_strace.summary.txt")
+        fil_summary.close()
 
 
     def test_file_ltrace(self):
@@ -59,6 +112,16 @@ class DockitTest(unittest.TestCase):
             withWarning=False,
             withDockerfile=True,
             updateServer=None)
+
+        fil_json = open("result_ltrace.json")
+        data = json.load(fil_json)
+        fil_json.close()
+
+        fil_summary = open("result_ltrace.summary.txt")
+        fil_summary.close()
+
+        fil_docker = open("result_ltrace.docker/Dockerfile")
+        fil_docker.close()
 
 
 if __name__ == '__main__':
