e188a2bb7fca18515cacd1dd991ceb3c559a0c96
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-08-25 19:25:48 +0100

    Comments.

--- tests/init.py
+++ tests/init.py
@@ -1,2 +1,4 @@
 # This is needed to avoid the message:
 # "Hint: make sure your test modules/packages have valid Python names"
+# Confusing and incomplete explanation here:
+# https://stackoverflow.com/questions/41748464/pytest-cannot-import-module-while-python-can
