f6e87bc7e5a62af33c63687c5d0ff5a5a72dd292
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-08-25 13:57:46 +0100

    More and cleaner tests.

--- tests/test_dockit.py
+++ tests/test_dockit.py
@@ -17,6 +17,17 @@ import unittest
 # This is needed when running from PyCharm.
 sys.path.insert(0,"../survol/scripts")
 
+def path_prefix_output_result(*file_path):
+    return os.path.join( os.path.dirname(__file__), "dockit_output_files", *file_path )
+
+def path_prefix_input_file(*file_path):
+    # Travis and PyCharm do not start this unit tests script from the same directory.
+    # The test files are alongside the script.
+    # input_test_files_dir = os.path.dirname(__file__)
+
+    return os.path.join( os.path.dirname(__file__), "dockit_input_test_trace_files", *file_path )
+
+
 # On Travis, getcwd= /home/travis/build/rchateauneu/survol
 # path= ['../survol/scripts', '/home/travis/build/rchateauneu/survol',
 # '../survol', '/home/travis/build/rchateauneu/survol', '/home/travis/virtualenv/python2.7.15/bin',
@@ -41,12 +52,7 @@ CurrentMachine = socket.gethostname().lower()
 # It points to the Survol adhoc CGI server: "http://rchateau-hp:8000"
 RemoteTestAgent = "http://" + CurrentMachine + ":8000"
 
-# Travis and PyCharm do not start this unit tests script from the same directory.
-# The test files are alongside the script.
-input_test_files_dir = os.path.dirname(__file__)
-
-
-class DockitUnitTest(unittest.TestCase):
+class DockitParserTest(unittest.TestCase):
 
     # This is a set of arguments of system function calls as displayed by strace or ltrace.
     # This checks if they are correctly parsed.
@@ -303,7 +309,18 @@ class DockitSummaryXMLTest(unittest.TestCase):
         tmpFilSTrace.write(tstLogFileSTrace)
         tmpFilSTrace.close()
 
-        outputSummaryFile = dockit.UnitTest(tmpFilSTrace.name,"strace",22029,None,None,False,dockit.fullMapParamsSummary,"XML",False,False,None)
+        outputSummaryFile = dockit.UnitTest(
+            tmpFilSTrace.name,
+            "strace",
+            22029,
+            None,
+            None,
+            False,
+            dockit.fullMapParamsSummary,
+            "XML",
+            False,
+            False,
+            None)
 
         sys.stdout.write("\nRebuilding tree\n")
         procTree = DockitSummaryXMLTest.RebuildProcessTree(outputSummaryFile)
@@ -397,17 +414,17 @@ class DockitSummaryXMLTest(unittest.TestCase):
 
 
 
-class DockitFilesTest(unittest.TestCase):
+class DockitTraceFilesTest(unittest.TestCase):
     """
     Test the execution of the Dockit script.
     """
 
     def test_file_strace_txt(self):
         dockit.UnitTest(
-            inputLogFile = os.path.join(input_test_files_dir, "sample_shell.strace.log"),
+            inputLogFile = path_prefix_input_file( "sample_shell.strace.log"),
             tracer = "strace",
             topPid = 0,
-            baseOutName = "result_strace",
+            baseOutName = path_prefix_output_result( "result_strace"),
             outputFormat = "TXT",
             verbose = True,
             mapParamsSummary = ["CIM_Process","CIM_DataFile.Category=['Others','Shared libraries']"],
@@ -416,18 +433,18 @@ class DockitFilesTest(unittest.TestCase):
             withDockerfile = True,
             updateServer = None)
 
-        fil_txt = open("result_strace.txt")
+        fil_txt = open( path_prefix_output_result( "result_strace.txt") )
         fil_txt.close()
 
-        fil_summary = open("result_strace.summary.txt")
+        fil_summary = open( path_prefix_output_result( "result_strace.summary.txt") )
         fil_summary.close()
 
     def test_file_strace_csv(self):
         dockit.UnitTest(
-            inputLogFile = os.path.join(input_test_files_dir, "sample_shell.strace.log"),
+            inputLogFile = path_prefix_input_file( "sample_shell.strace.log"),
             tracer = "strace",
             topPid = 0,
-            baseOutName = "result_strace",
+            baseOutName = path_prefix_output_result( "result_strace" ),
             outputFormat = "CSV",
             verbose = True,
             mapParamsSummary = ["CIM_Process","CIM_DataFile.Category=['Others','Shared libraries']"],
@@ -436,22 +453,22 @@ class DockitFilesTest(unittest.TestCase):
             withDockerfile = False,
             updateServer = None)
 
-        fil_csv = open("result_strace.csv")
+        fil_csv = open( path_prefix_output_result( "result_strace.csv") )
         fil_csv.close()
 
-        fil_summary = open("result_strace.summary.txt")
+        fil_summary = open( path_prefix_output_result( "result_strace.summary.txt") )
         fil_summary.close()
 
-        fil_docker = open("result_strace.docker/Dockerfile")
+        fil_docker = open( path_prefix_output_result( "result_strace.docker", "Dockerfile") )
         fil_docker.close()
 
     def test_file_strace_json(self):
 
         dockit.UnitTest(
-            inputLogFile = os.path.join(input_test_files_dir, "sample_shell.strace.log"),
+            inputLogFile = path_prefix_input_file( "sample_shell.strace.log"),
             tracer = "strace",
             topPid = 0,
-            baseOutName = "result_strace",
+            baseOutName = path_prefix_output_result( "result_strace" ),
             outputFormat = "JSON",
             verbose = True,
             mapParamsSummary = ["CIM_Process","CIM_DataFile.Category=['Others','Shared libraries']"],
@@ -460,20 +477,20 @@ class DockitFilesTest(unittest.TestCase):
             withDockerfile = False,
             updateServer = None)
 
-        fil_json = open("result_strace.json")
+        fil_json = open( path_prefix_output_result( "result_strace.json") )
         data = json.load(fil_json)
         fil_json.close()
 
-        fil_summary = open("result_strace.summary.txt")
+        fil_summary = open( path_prefix_output_result( "result_strace.summary.txt") )
         fil_summary.close()
 
 
     def test_file_ltrace(self):
         dockit.UnitTest(
-            inputLogFile = os.path.join(input_test_files_dir, "sample_shell.ltrace.log"),
+            inputLogFile = path_prefix_input_file( "sample_shell.ltrace.log"),
             tracer="ltrace",
             topPid=0,
-            baseOutName="result_ltrace",
+            baseOutName= path_prefix_output_result( "result_ltrace" ),
             outputFormat="JSON",
             verbose=True,
             mapParamsSummary=["CIM_Process", "CIM_DataFile.Category=['Others','Shared libraries']"],
@@ -482,15 +499,64 @@ class DockitFilesTest(unittest.TestCase):
             withDockerfile=True,
             updateServer=None)
 
-        fil_json = open("result_ltrace.json")
+        fil_json = open( path_prefix_output_result( "result_ltrace.json") )
         data = json.load(fil_json)
         fil_json.close()
 
-        fil_summary = open("result_ltrace.summary.txt")
+        fil_summary = open( path_prefix_output_result( "result_ltrace.summary.txt") )
         fil_summary.close()
 
-        fil_docker = open("result_ltrace.docker/Dockerfile")
+        fil_docker = open( path_prefix_output_result( "result_ltrace.docker", "Dockerfile") )
         fil_docker.close()
+
+
+    def test_all_trace_files(self):
+        mapParamsSummary = dockit.fullMapParamsSummary
+        updateServer = None
+
+        # This iterates on the input test files and generates the "compressed" output.as
+        # After that we can check if the results are as expected.
+
+        # The keys are the prefix of the log files
+        # and the content is an array of actual files
+        # whose output must be reproduced.
+        mapFiles = {}
+
+        # First pass to build a map of files.
+        # This takes only the log files at the top level.
+        for subdir, dirs, files in os.walk( path_prefix_input_file() ):
+            for inFile in files:
+                inputLogFile = subdir + os.sep + inFile
+                baseName, filExt = os.path.splitext(inFile)
+
+                if filExt != ".log":
+                    continue
+
+                # The main process pid might be embedded in the log file name,
+                # just before the extension. If it cannot be found, it is assumed
+                # to be -1..
+                # mtchLog = re.match(".*\.([0-9]*)$", baseName)
+                # if mtchLog:
+                #    aPid = int( mtchLog.group(1) )
+                # else:
+                #    aPid = -1
+
+                tracer = dockit.DefaultTracer(inputLogFile)
+
+                for outputFormat in ["JSON"]:
+                    # In tests, the summary output format is always XML.
+                    dockit.UnitTest(
+                        inputLogFile = inputLogFile,
+                        tracer = tracer,
+                        topPid = -1,
+                        baseOutName = path_prefix_output_result( baseName ),
+                        outputFormat = outputFormat,
+                        verbose = False,
+                        mapParamsSummary = dockit.fullMapParamsSummary,
+                        summaryFormat = "TXT",
+                        withWarning = False,
+                        withDockerfile = True,
+                        updateServer = None)
 
 
 if __name__ == '__main__':
