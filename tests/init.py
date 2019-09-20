e446c55b374508db2a660024d206b47b351564c9
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-09-20 21:36:10 +0100

    Generic code for tests.

--- tests/init.py
+++ tests/init.py
@@ -2,3 +2,21 @@
 # "Hint: make sure your test modules/packages have valid Python names"
 # Confusing and incomplete explanation here:
 # https://stackoverflow.com/questions/41748464/pytest-cannot-import-module-while-python-can
+
+import os
+import socket
+import psutil
+
+CurrentMachine = socket.gethostname().lower()
+try:
+    CurrentUsername = os.environ["USERNAME"]
+    # The class of users is different on Linux and Windows.
+    CurrentUserPath = "Win32_UserAccount.Name=%s,Domain=localhost" % CurrentUsername
+except KeyError:
+    # This is for Linux.
+    CurrentUsername = os.environ["USER"]
+    CurrentUserPath = "LMI_Account.Name=%s,Domain=localhost" % CurrentUsername
+
+CurrentPid = os.getpid()
+CurrentProcessPath = 'CIM_Process.Handle=%d' % CurrentPid
+CurrentParentPid = psutil.Process().ppid()
