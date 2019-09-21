efdd8b6ac9ba1277a17e681e1a0acf508f941d6e
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-09-21 05:23:22 +0100

    Travis CI

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,11 @@
-# This is for tests
+# This is executed by Travis-CI
 
 import sys
+import os
 
 # This is for the test program pytest which is started from the root directory.
 sys.path.append("survol")
+sys.path.append("tests")
 
-print("Tests path=",sys.path)
+print("__init__.py Current dir=",os.getcwd())
+print("__init__.py Tests path=",sys.path)
