93fc1e44451d4073c9f5faadcebdb0d6479b921b
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-11-01 23:46:53 +0000

    Sparql custom evals progress.

--- tests/test_sparql_customevals.py
+++ tests/test_sparql_customevals.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import os
 import sys
 import json
+import collections
 import rdflib
 import unittest
 import psutil
@@ -17,9 +18,12 @@ survol_url = "http://primhillcomputer.com/ontologies/"
 survol_namespace = rdflib.Namespace(survol_url)
 class_CIM_Process = rdflib.term.URIRef(survol_url + "CIM_Process")
 class_CIM_Directory = rdflib.term.URIRef(survol_url + "CIM_Directory")
+class_CIM_DataFile = rdflib.term.URIRef(survol_url + "CIM_DataFile")
+
 predicate_Handle = rdflib.term.URIRef(survol_url + "Handle")
 predicate_Name = rdflib.term.URIRef(survol_url + "Name")
 
+associator_CIM_DirectoryContainsFile = rdflib.term.URIRef(survol_url + "CIM_DirectoryContainsFile")
 
 def add_process_to_graph(graph, pid, process_name = None):
     print("Graph id=", id(graph))
@@ -30,6 +34,7 @@ def add_process_to_graph(graph, pid, process_name = None):
     if not process_name:
         process_name = "Process=%d" % pid
     graph.add((my_process, rdflib.namespace.RDFS.label, rdflib.Literal(process_name)))
+    return my_process
 
 
 def add_directory_to_graph(graph, directory_path):
@@ -38,6 +43,16 @@ def add_directory_to_graph(graph, directory_path):
 
     graph.add((my_dir, rdflib.namespace.RDF.type, class_CIM_Directory))
     graph.add((my_dir, predicate_Name, rdflib.Literal(directory_path)))
+    return my_dir
+
+
+def add_datafile_to_graph(graph, file_path):
+    print("Graph id=", id(graph))
+    my_file = rdflib.term.URIRef(survol_url + "objects/CIM_DataFile?Name=%s" % file_path)
+
+    graph.add((my_file, rdflib.namespace.RDF.type, class_CIM_DataFile))
+    graph.add((my_file, predicate_Name, rdflib.Literal(file_path)))
+    return my_file
 
 
 
@@ -142,60 +157,104 @@ class RdflibCustomEvalsBasicTest(unittest.TestCase):
         self.assertTrue( pathes_only == ['/dev','/opt','/proc','/tmp'])
 
 
+class Instance:
+    def __init__(self, class_name):
+        self.m_class_name = class_name[len(survol_url):]
+        self.m_associators = {}
+        self.m_known_attributes = {}
+        self.m_unknown_attributes = {}
 
-class RdflibCustomEvalsProcessesTest(unittest.TestCase):
+
+def Feeder_CIM_Process(graph, instance_node, instance):
+    # Iterate over all running process
+    for proc in psutil.process_iter():
+        try:
+            add_process_to_graph(graph, proc.pid, proc.name())
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+def Feeder_CIM_Directory(graph, instance_node, instance):
+    try:
+        directory_name = instance.m_known_attributes[predicate_Name]
+        variable_contained = instance.m_associators[associator_CIM_DirectoryContainsFile]
+
+        for subdir, dirs, files in os.walk(directory_name):
+            for file in files:
+                filepath = os.path.join(subdir, file)
+                file_node = add_datafile_to_graph(graph, filepath, instance_node)
+                graph.add((instance_node, associator_CIM_DirectoryContainsFile, file_node))
+
+            for dir in dirs:
+                dirpath = os.path.join(subdir, dir)
+                directory_node = add_directory_to_graph(graph, dirpath, instance_node)
+                graph.add((instance_node, associator_CIM_DirectoryContainsFile, directory_node))
+
+            break
+    except KeyError:
+        pass
+
+feeders_dict = {
+    "CIM_Process":Feeder_CIM_Process,
+    "CIM_Directory":Feeder_CIM_Directory
+}
+
+class RdflibCustomEvalsFeedTest(unittest.TestCase):
 
     # Inspired from https://rdflib.readthedocs.io/en/stable/_modules/examples/custom_eval.html
     @staticmethod
-    def custom_eval_processes(ctx, part):
+    def custom_eval_feed(ctx, part):
         # part.name = "SelectQuery", "Project", "BGP"
         # print("customEval part.name=", part.name)
         if part.name == 'BGP':
+            instances_dict = collections.defaultdict(dict)
+            for part_subject, part_predicate, part_object in part.triples:
+                if part_predicate == rdflib.namespace.RDF.type:
+                    if isinstance(part_subject, rdflib.term.Variable):
+                        class_as_str = part_object.toPython()
+                        if class_as_str.startswith(survol_url):
+                            instances_dict[part_subject] = Instance(class_as_str)
 
-            # Iterate over all running process
-            for proc in psutil.process_iter():
+            for part_subject, part_predicate, part_object in part.triples:
                 try:
-                    add_process_to_graph(ctx.graph, proc.pid, proc.name())
-                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                    current_instance = instances_dict[part_subject]
+                except KeyError:
+                    continue
+
+                if part_predicate == rdflib.namespace.RDF.type:
+                    continue
+
+                if isinstance(part_object, rdflib.term.Variable):
+                    if part_object in instances_dict:
+                        current_instance.m_associators[part_predicate] = part_object
+                    else:
+                        current_instance.m_unknown_attributes[part_predicate] = part_object
+                elif isinstance(part_object, rdflib.term.URIRef):
+                    pass
+                elif isinstance(part_object, rdflib.term.Literal):
+                    current_instance.m_known_attributes[part_predicate] = part_object
+                else:
                     pass
 
+            for part_subject, current_instance in instances_dict.items():
+                subject_class = current_instance.m_class_name
+                feeders_dict[subject_class](ctx.graph, part_subject, current_instance)
 
-            print("Part Triples:")
-            for t in part.triples:
-                print("    ", t)
-
-            add_process_to_graph(ctx.graph, 456)
-
-            # rewrite triples
-            # triples = []
-            # for t in part.triples:
-            #     if t[1] == rdflib.RDF.type:
-            #         bnode = rdflib.BNode()
-            #         triples.append((t[0], t[1], bnode))
-            #     else:
-            #         triples.append(t)
-
-            bnode = rdflib.BNode()
 
             # <type 'generator'>
             ret_BGP = rdflib.plugins.sparql.evaluate.evalBGP(ctx, part.triples)
-            if False:
-                return ret_BGP
-            else:
-                list_BGP = list(ret_BGP)
-                return iter(list_BGP)
+            return ret_BGP
 
         raise NotImplementedError()
 
     def setUp(self):
         # add function directly, normally we would use setuptools and entry_points
-        rdflib.plugins.sparql.CUSTOM_EVALS['custom_eval_processes'] = RdflibCustomEvalsProcessesTest.custom_eval_processes
+        rdflib.plugins.sparql.CUSTOM_EVALS['custom_eval_feed'] = RdflibCustomEvalsFeedTest.custom_eval_feed
 
     def tearDown(self):
-        if 'custom_eval_processes' in rdflib.plugins.sparql.CUSTOM_EVALS:
-            del rdflib.plugins.sparql.CUSTOM_EVALS['custom_eval_processes']
+        if 'custom_eval_feed' in rdflib.plugins.sparql.CUSTOM_EVALS:
+            del rdflib.plugins.sparql.CUSTOM_EVALS['custom_eval_feed']
 
-    def test_query_process(self):
+    def test_query_all_processes(self):
         rdflib_graph = rdflib.Graph()
 
         query_pids = """
@@ -210,6 +269,64 @@ class RdflibCustomEvalsProcessesTest(unittest.TestCase):
         pids_only = sorted([ str(one_result[0]) for one_result in query_result])
         print("Pids only=", pids_only)
         self.assertTrue( str(os.getpid()) in pids_only )
+
+    def test_query_current_process(self):
+        rdflib_graph = rdflib.Graph()
+
+        query_pids = """
+            PREFIX survol: <%s>
+            SELECT ?url_process WHERE {
+                ?url_process a survol:CIM_Process .
+                ?url_process survol:Handle %d .
+            }
+        """ % (survol_namespace, os.getpid())
+
+        query_result = list(rdflib_graph.query(query_pids))
+        self.assertTrue( len(query_result) == 1 )
+
+    def test_query_directory_unknown(self):
+        rdflib_graph = rdflib.Graph()
+
+        query_pids = """
+            PREFIX survol: <%s>
+            SELECT ?directory_name WHERE {
+                ?url_directory a survol:CIM_Directory .
+                ?url_directory survol:Name ?directory_name .
+            }
+        """ % (survol_namespace)
+
+        query_result = list(rdflib_graph.query(query_pids))
+
+    def test_query_directory_known(self):
+        rdflib_graph = rdflib.Graph()
+
+        query_pids = """
+            PREFIX survol: <%s>
+            SELECT ?url_directory WHERE {
+                ?url_directory a survol:CIM_Directory .
+                ?url_directory survol:Name "C:/temp" .
+            }
+        """ % (survol_namespace)
+
+        query_result = list(rdflib_graph.query(query_pids))
+
+    def test_query_sub_directories(self):
+        rdflib_graph = rdflib.Graph()
+
+        query_pids = """
+            PREFIX survol: <%s>
+            SELECT ?directory_name_2 WHERE {
+                ?url_directory_1 a survol:CIM_Directory .
+                ?url_directory_2 a survol:CIM_Directory .
+                ?url_directory_1 survol:CIM_DirectoryContainsFile ?url_directory_2 .
+                ?url_directory_1 survol:Name "C:/temp" .
+                ?url_directory_2 survol:Name ?directory_name_2 .
+            }
+        """ % (survol_namespace)
+
+        query_result = list(rdflib_graph.query(query_pids))
+        names_only = sorted([ str(one_result[0]) for one_result in query_result])
+        print("Names only=", names_only)
 
 
 
