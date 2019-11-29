4e2f726d21cc40c853597bf73a9eacbdeb315575
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-11-29 08:23:43 +0000

    Test pefile.

--- tests/test_lib_pefile.py
+++ tests/test_lib_pefile.py
@@ -41,35 +41,65 @@ update_test_path()
 #     return strRaw
 
 
-tsts = [
-    "??$_Getvals@_W@?$time_get@DV?$istreambuf_iterator@DU?$char_traits@D@std@@@std@@@std@@IAEX_WABV_Locinfo@1@@Z",
-    "??$_Getvals@_W@?$time_get@GV?$istreambuf_iterator@GU?$char_traits@G@std@@@std@@@std@@IAEX_WABV_Locinfo@1@@Z",
-    "??$_Getvals@_W@?$time_get@_WV?$istreambuf_iterator@_WU?$char_traits@_W@std@@@std@@@std@@IAEX_WABV_Locinfo@1@@Z",
-    "??0?$_Yarn@D@std@@QAE@ABV01@@Z",
-    "??0?$_Yarn@D@std@@QAE@PBD@Z",
-    "??0?$_Yarn@D@std@@QAE@XZ",
-    "??0?$_Yarn@_W@std@@QAE@XZ",
-    "??0?$basic_ios@DU?$char_traits@D@std@@@std@@IAE@XZ",
-    "??0?$basic_ios@DU?$char_traits@D@std@@@std@@QAE@PAV?$basic_streambuf@DU?$char_traits@D@std@@@1@@Z",
-    "??0?$basic_ios@GU?$char_traits@G@std@@@std@@IAE@XZ",
-    "??0?$basic_ios@GU?$char_traits@G@std@@@std@@QAE@PAV?$basic_streambuf@GU?$char_traits@G@std@@@1@@Z",
-    "??0?$basic_ios@_WU?$char_traits@_W@std@@@std@@IAE@XZ",
-    "??0?$basic_ios@_WU?$char_traits@_W@std@@@std@@QAE@PAV?$basic_streambuf@_WU?$char_traits@_W@std@@@1@@Z",
+tests_symbols_ok = [
+    ("??$_Getvals@_W@?$time_get@DV?$istreambuf_iterator@DU?$char_traits@D@std@@@std@@@std@@IAEX_WABV_Locinfo@1@@Z",
+     "void std::time_get<char,std::istreambuf_iterator<char,std::char_traits<char> > >::_Getvals<wchar_t>(wchar_t,std::_Locinfo const &)"),
+    ("??$_Getvals@_W@?$time_get@GV?$istreambuf_iterator@GU?$char_traits@G@std@@@std@@@std@@IAEX_WABV_Locinfo@1@@Z",
+     "void std::time_get<unsigned short,std::istreambuf_iterator<unsigned short,std::char_traits<unsigned short> > >::_Getvals<wchar_t>(wchar_t,std::_Locinfo const &)"),
+    ("??$_Getvals@_W@?$time_get@_WV?$istreambuf_iterator@_WU?$char_traits@_W@std@@@std@@@std@@IAEX_WABV_Locinfo@1@@Z",
+     "void std::time_get<wchar_t,std::istreambuf_iterator<wchar_t,std::char_traits<wchar_t> > >::_Getvals<wchar_t>(wchar_t,std::_Locinfo const &)"),
+    ("??0?$_Yarn@D@std@@QAE@ABV01@@Z",
+     "std::_Yarn<char>::_Yarn<char>(std::_Yarn<char> const &)"),
+    ("??0?$_Yarn@D@std@@QAE@PBD@Z",
+     "std::_Yarn<char>::_Yarn<char>(char const *)"),
+    ("??0?$_Yarn@D@std@@QAE@XZ",
+     "std::_Yarn<char>::_Yarn<char>(void)"),
+    ("??0?$_Yarn@_W@std@@QAE@XZ",
+     "std::_Yarn<wchar_t>::_Yarn<wchar_t>(void)"),
+    ("??0?$basic_ios@DU?$char_traits@D@std@@@std@@IAE@XZ",
+     "std::basic_ios<char,std::char_traits<char> >::basic_ios<char,std::char_traits<char> >(void)"),
+    ("??0?$basic_ios@DU?$char_traits@D@std@@@std@@QAE@PAV?$basic_streambuf@DU?$char_traits@D@std@@@1@@Z",
+     "std::basic_ios<char,std::char_traits<char> >::basic_ios<char,std::char_traits<char> >(std::basic_streambuf<char,std::char_traits<char> > *)"),
+    ("??0?$basic_ios@GU?$char_traits@G@std@@@std@@IAE@XZ",
+     "std::basic_ios<unsigned short,std::char_traits<unsigned short> >::basic_ios<unsigned short,std::char_traits<unsigned short> >(void)"),
+    ("??0?$basic_ios@GU?$char_traits@G@std@@@std@@QAE@PAV?$basic_streambuf@GU?$char_traits@G@std@@@1@@Z",
+     "std::basic_ios<unsigned short,std::char_traits<unsigned short> >::basic_ios<unsigned short,std::char_traits<unsigned short> >(std::basic_streambuf<unsigned short,std::char_traits<unsigned short> > *)"),
+    ("??0?$basic_ios@_WU?$char_traits@_W@std@@@std@@IAE@XZ",
+     "std::basic_ios<wchar_t,std::char_traits<wchar_t> >::basic_ios<wchar_t,std::char_traits<wchar_t> >(void)"),
+    ("??0?$basic_ios@_WU?$char_traits@_W@std@@@std@@QAE@PAV?$basic_streambuf@_WU?$char_traits@_W@std@@@1@@Z",
+     "std::basic_ios<wchar_t,std::char_traits<wchar_t> >::basic_ios<wchar_t,std::char_traits<wchar_t> >(std::basic_streambuf<wchar_t,std::char_traits<wchar_t> > *)"),
+    ("??0?$basic_iostream@DU?$char_traits@D@std@@@std@@QAE@PAV?$basic_streambuf@DU?$char_traits@D@std@@@1@@Z",
+     "std::basic_iostream<char,std::char_traits<char> >::basic_iostream<char,std::char_traits<char> >(std::basic_streambuf<char,std::char_traits<char> > *)"),
+    ("??0?$basic_iostream@GU?$char_traits@G@std@@@std@@QAE@PAV?$basic_streambuf@GU?$char_traits@G@std@@@1@@Z",
+     "std::basic_iostream<unsigned short,std::char_traits<unsigned short> >::basic_iostream<unsigned short,std::char_traits<unsigned short> >(std::basic_streambuf<unsigned short,std::char_traits<unsigned short> > *)"),
+    ]
+
+tests_symbols_failed = [
     "??0?$basic_iostream@DU?$char_traits@D@std@@@std@@IAE@$$QAV01@@Z",
-    "??0?$basic_iostream@DU?$char_traits@D@std@@@std@@QAE@PAV?$basic_streambuf@DU?$char_traits@D@std@@@1@@Z",
     "??0?$basic_iostream@GU?$char_traits@G@std@@@std@@IAE@$$QAV01@@Z",
-    "??0?$basic_iostream@GU?$char_traits@G@std@@@std@@QAE@PAV?$basic_streambuf@GU?$char_traits@G@std@@@1@@Z",
     "??0?$basic_iostream@_WU?$char_traits@_W@std@@@std@@IAE@$$QAV01@@Z",
 ]
 
+
+
 class PEFile_Test(unittest.TestCase):
 
-    #@unittest.skip("Not finished yet")
-    def test_pefile(self):
+    @unittest.skipIf(not is_platform_windows, "test_pefile for Windows only.")
+    def test_pefile_ok(self):
         import lib_pefile
-        for tst in tsts[:]:
-            sym = lib_pefile.UndecorateSymbol(tst)
-            print(sym)
-            print("")
+        for mangled_symbol, expected_output in tests_symbols_ok:
+            undecorated_symbol = lib_pefile.UndecorateSymbol(mangled_symbol)
+            print("in = ", mangled_symbol)
+            print("out= ",undecorated_symbol)
+            self.assertTrue(expected_output == undecorated_symbol)
+
+    @unittest.skip("test_pefile_broken MUST BE FIXED.")
+    def test_pefile_broken(self):
+        import lib_pefile
+        for mangled_symbol in tests_symbols_failed:
+            undecorated_symbol = lib_pefile.UndecorateSymbol(mangled_symbol)
+            print("in = ", mangled_symbol)
+            print("out= ",undecorated_symbol)
+            self.assertTrue(mangled_symbol != undecorated_symbol)
 
 
