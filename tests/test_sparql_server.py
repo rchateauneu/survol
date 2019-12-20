fe29646e4fbbb3e95c40297aeb3a2e6aa8b035dc
Remi Chateauneu <remi.chateauneu@gmail.com>
2019-12-20 23:32:56 +0000

    New tests.

---
+++ tests/test_sparql_server.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import sys
+import unittest
+import pkgutil
+
+try:
+    import SPARQLWrapper
+except ImportError as exc:
+    SPARQLWrapper = None
+    print("Error importing:",exc)
+
+from init import *
+
+update_test_path()
+
+# <?xml version="1.0" ?>
+# <sparql><head><variable name="caption"/></head>
+#   <results>
+#       <result><binding name="caption">rchateau-HP\\rchateau</binding></result>
+#   </results>
+# </sparql>
+#
+# ['ATTRIBUTE_NODE', 'CDATA_SECTION_NODE', 'COMMENT_NODE', 'DOCUMENT_FRAGMENT_NODE', 'DOCUMENT_NODE',
+# 'DOCUMENT_TYPE_NODE', 'ELEMENT_NODE', 'ENTITY_NODE', 'ENTITY_REFERENCE_NODE', 'NOTATION_NODE',
+# 'PROCESSING_INSTRUCTION_NODE', 'TEXT_NODE', '__doc__', '__init__', '__module__', '__nonzero__',
+# '_call_user_data_handler', '_child_node_types', '_create_entity', '_create_notation', '_elem_info',
+# '_get_actualEncoding', '_get_async', '_get_childNodes', '_get_doctype', '_get_documentElement', '_get_documentURI',
+# '_get_elem_info', '_get_encoding', '_get_errorHandler', '_get_firstChild', '_get_lastChild', '_get_localName',
+# '_get_standalone', '_get_strictErrorChecking', '_get_version', '_id_cache', '_id_search_stack', '_magic_id_count',
+# '_set_async', 'abort', 'actualEncoding', 'appendChild', 'async', 'attributes', 'childNodes', 'cloneNode',
+# 'createAttribute', 'createAttributeNS', 'createCDATASection', 'createComment', 'createDocumentFragment',
+# 'createElement', 'createElementNS', 'createProcessingInstruction', 'createTextNode', 'doctype', 'documentElement',
+# 'documentURI', 'encoding', 'errorHandler', 'firstChild', 'getElementById', 'getElementsByTagName',
+# 'getElementsByTagNameNS', 'getInterface', 'getUserData', 'hasChildNodes', 'implementation', 'importNode',
+# 'insertBefore', 'isSameNode', 'isSupported', 'lastChild', 'load', 'loadXML', 'localName', 'namespaceURI',
+# 'nextSibling', 'nodeName', 'nodeType', 'nodeValue', 'normalize', 'ownerDocument', 'parentNode', 'prefix',
+# 'previousSibling', 'removeChild', 'renameNode', 'replaceChild', 'saveXML', 'setUserData', 'standalone',
+# 'strictErrorChecking', 'toprettyxml', 'toxml', 'unlink', 'version', 'writexml']
+def SparqlResultsXMLToJSON(results_xml):
+    head_array = []
+    results_array = []
+    node_sparql = results_xml.getElementsByTagName("sparql")[0]
+    for node_head in node_sparql.getElementsByTagName("head"):
+        for node_variable in node_head.getElementsByTagName("variable"):
+            head_name = node_variable.getAttribute('name')
+            head_array.append(head_name)
+    node_results = node_sparql.getElementsByTagName("results")[0]
+    for node_result in node_results.getElementsByTagName("result"):
+        result_dict = {}
+        for node_binding in node_result.getElementsByTagName("binding"):
+            binding_name = node_binding.getAttribute('name')
+            binding_text = node_binding.childNodes[0].nodeValue
+
+            #{u'type': u'literal', u'value': u'S-1-5-21-3348735596-448992173-972389567-1001'}}
+
+            result_dict[binding_name] = {u'type': u'literal', u'value': binding_text}
+        results_array.append(result_dict)
+    json_result = { "head": {"vars": head_array}, "results": { "bindings": results_array}}
+    print("json_result=",json_result)
+    return json_result
+
+
+# https://stackoverflow.com/questions/5888020/sparql-query-on-the-remote-remote-endpoint-rdflib-redland
+
+# This returns the result as a dictionary of dictionaries
+def UrlToSparqlResult(url_rdf, sparql_query, format_str):
+    # print("UrlToSparqlResult sparql_query=",sparql_query)
+    print("UrlToSparqlResult url_rdf=",url_rdf," format_str=",format_str)
+
+    sparql_wrapper = SPARQLWrapper.SPARQLWrapper(url_rdf)
+    sparql_wrapper.setQuery(sparql_query)
+    # print("sparql_wrapper:",str(dir(sparql_wrapper)))
+    # print("sparql_wrapper.queryString:",sparql_wrapper.queryString)
+
+    # JSON and JSONLD do not work.
+
+    str_to_format = {
+        "XML": SPARQLWrapper.XML,
+        "JSON": SPARQLWrapper.JSON
+    }[format_str]
+
+    sparql_wrapper.setReturnFormat(str_to_format)
+    sparql_qry_result = sparql_wrapper.query()
+    results_conversion = sparql_qry_result.convert()
+
+    if format_str == "XML":
+        if isinstance(results_conversion, str):
+            # This is because an error occurred somewhere.
+            ERROR("UrlToSparqlResult results_conversion=%s", results_conversion)
+            return None
+        # Specific conversion of XML Sparql results to JSON, so we can use the same results data.
+        # print("dir(results_conversion)=",dir(results_conversion))
+        print("results_conversion=",results_conversion)
+        assert isinstance( format_str, str )
+        print("results_conversion.toxml()=",results_conversion.toxml())
+        results_conversion = SparqlResultsXMLToJSON(results_conversion)
+
+    return results_conversion
+
+
+# This executes a query to the Sparql server of the current machine, via TCP/IP.
+def run_remote_sparql_query(remote_agent, sparql_query, expected_header, expected_dicts, format_str, sparql_type):
+    assert sparql_type in ("survol", "WMI", "WBEM")
+    print("run_remote_sparql_query sparql_query=", sparql_query)
+
+    url_sparql = RemoteTestAgent + "/survol/sparql.py"
+
+    sparql_result = UrlToSparqlResult(url_sparql, sparql_query, format_str)
+
+
+    # sparql_result ... = {
+    # u'head': {u'vars': [u'caption']},
+    # u'results': {
+    #   u'bindings': [
+    #       {u'caption': {u'type': u'literal', u'value': u'rchateau-HP\\\\rchateau'}},
+    #       {u'caption': {u'type': u'literal', u'value': u'S-1-5-21-3348735596-448992173-972389567-1001'}},
+    #       {u'caption': {u'type': u'url', u'value': u'http://primhillcomputers.com/survol/Win32_UserAccount'}},
+    print("run_compare_survol sparql_result ... =", sparql_result)
+    print("run_compare_survol expected_header ... =", expected_header)
+    assert sparql_result['head']['vars'] == expected_header
+
+    # This builds a set of tuples from the actual results.
+    # Conversion in lower case.
+    str_actual_data_lower = set()
+    for one_dict in sparql_result['results']['bindings']:
+        values_tuple = tuple( ( one_variable, one_dict[one_variable][u'value'].lower() ) for one_variable in expected_header)
+        str_actual_data_lower.add(values_tuple)
+    print("run_compare_survol len(str_actual_data)=", len(str_actual_data_lower))
+
+    print("run_compare_survol expected_dicts=", expected_dicts)
+    print("run_compare_survol str_actual_data=", str_actual_data_lower)
+    for one_dict in expected_dicts:
+        print("run_compare_survol one_dict=",one_dict)
+        assert(one_dict in str_actual_data_lower)
+
+
+
+
+    # https://www.w3.org/TR/2013/REC-sparql11-results-json-20130321/
+    # {
+    #     "head": {"vars": ["book", "title"]
+    #              },
+    #     "results": {
+    #         "bindings": [
+    #             {
+    #                 "book": {"type": "uri", "value": "http://example.org/book/book6"},
+    #                 "title": {"type": "literal", "value": "Harry Potter and the Half-Blood Prince"}
+    #             },
+
+    # This document describes an XML format for the variable binding and boolean results formats provided by the SPARQL query language for RDF
+    #
+    # <?xml version="1.0"?>
+    # <sparql xmlns="http://www.w3.org/2005/sparql-results#">
+    #   <head>
+    #     <variable name="x"/>
+    #     <variable name="hpage"/>
+    #   </head>
+    #
+    #   <results>
+    #     <result>
+    #       <binding name="x"> ... </binding>
+    #       <binding name="hpage"> ... </binding>
+    #     </result>
+
+class SparqlServerSurvolTest(unittest.TestCase):
+    """
+    Test the Sparql server which works on Survol data.
+    """
+
+    # This executes a query to the Sparql server of the current machine, via TCP/IP.
+    @staticmethod
+    def run_compare_survol(sparql_query, expected_header, expected_dicts, format_str, sparql_type):
+        run_remote_sparql_query(sparql_query, expected_header, expected_dicts, format_str, sparql_type)
+
+    def run_complete_survol_test(self, array_queries, sparql_type):
+        for sparql_query, expected_header, expected_dicts in array_queries:
+            for fmt in ["XML","JSON"]:
+                self.run_compare_survol(sparql_query, expected_header, expected_dicts, fmt)
+
+    def test_server_survol(self):
+        array_survol_queries=[
+            [
+                """
+                PREFIX survol:  <http://www.primhillcomputers.com/survol#>
+                PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+                SELECT ?pid
+                WHERE
+                {
+                    ?url_proc rdf:type survol:CIM_Process .
+                    ?url_proc survol:Description 'python.exe' .
+                    ?url_proc survol:Handle ?pid .
+                }
+                """,
+                [ u'pid'],
+                [
+                    ((u'pid', str(CurrentPid)),),
+                ]
+            ],
+        ]
+
+        self.run_complete_survol_test(array_survol_queries, "survol")
+
+
+
+if __name__ == '__main__':
+    assert SPARQLWrapper
+    unittest.main()
