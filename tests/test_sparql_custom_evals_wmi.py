d07bc940a9911f90dc46788b563aeaf548e307fe
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-01-03 14:23:56 +0000

    test_associator_windows_directory skipped because too slow.

--- tests/test_sparql_custom_evals_wmi.py
+++ tests/test_sparql_custom_evals_wmi.py
@@ -401,6 +401,7 @@ class SparqlCallWmiAssociatorsTest(CUSTOM_EVALS_WMI_Base_Test):
         query_result = list(rdflib_graph.query(sparql_query))
         print("Result=", query_result)
 
+    @unittest.skip("Too slow !!!")
     def test_associator_windows_directory(self):
         sparql_query = """
             PREFIX survol: <%s>
