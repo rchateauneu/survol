c56f50e093161dd15c88a46ede897cd02c4f1237
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-01-24 00:36:15 +0000

    New tests.

---
+++ Experimental/WindowsPyDbg/pydbg_test.py
@@ -0,0 +1,15 @@
+"""
+The intention is to port a subset of pydbg:
+- It must be in pure Python.
+- Python 2 and 3.
+- Needed features: attach to a process, log calls to some system api funcitons,
+with the arguments.
+
+It would have been possible to fork pydbg but:
+- This project is not maintained, not is OpenRCE http://www.openrce.org/ of which it is part.
+- Not all features are needed
+- Features which are unneeded are quite dangerous and it might be impossible to install
+the package because of this reason.
+
+Hence the choice of porting a ubset of pydbg
+"""
