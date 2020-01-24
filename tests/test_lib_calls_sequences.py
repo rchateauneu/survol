fbfcde88b6da0b952528b27d00e2efe5a44c62fe
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-01-24 08:50:58 +0000

    Change in detection of repetitions.

--- tests/test_lib_calls_sequences.py
+++ tests/test_lib_calls_sequences.py
@@ -11,10 +11,6 @@ __copyright__   = "Copyright 2020, Primhill Computers"
 __license__ = "GPL"
 
 
-
-
-import cgitb
-import cgi
 import os
 import re
 import sys
@@ -81,68 +77,4 @@ class RepetitionDetectionTest(unittest.TestCase):
         self.aux_simplify([1, 2], [([([1], 1)], 1), ([([2], 1)], 1)])
 
 
-
-class SplitExecutionStreamsTest(unittest.TestCase):
-    """When parsing a strace log, there are several threads or processes all mixed together.
-    dockit splits them and processes them individually, to detect the resources used by each process.
-    This test use this feature and separate the flow of instructions into several distinct streams.
-    The intention is to detect repetition of similar sequences of functions calls."""
-    class Iterator:
-        def __init__(self):
-            print("Iterator.__init__")
-
-        def __iter__(self):
-            print("Iterator.__iter__")
-            print("Before yield from")
-            x = yield
-            print("Before yield to")
-            yield x
-            # raise StopIteration
-
-    class FunctionCallsQueue:
-        def __init__(self, thread_identifier):
-            self._thread_identifier = thread_identifier
-            self._iterator = SplitExecutionStreamsTest.Iterator()
-
-            def processing_function():
-                print("In processing_function")
-                lib_calls_sequences.squeeze_events_sequence(self._iterator, max_len=5)
-
-            self._processing_function = processing_function()
-            self.toto = self._iterator.__iter__()
-            next(self.toto)
-            #next(self._processing_function)
-
-        def push_function_name(self, function_name):
-            print("push_function_name function_name=", function_name)
-            self.toto.send(function_name)
-            #self._processing_function.send(function_name)
-
-    @unittest.skip("Not Implemented yet")
-    def test_split_stream(self):
-        dict_pids = {}
-
-        def batch_callback(one_batch):
-            aCore = one_batch.m_core
-
-            aPid = aCore.m_pid
-            print("aPid=", aPid)
-            print("aCore=", aCore.GetFunction())
-
-            try:
-                function_calls_queue = dict_pids[aPid]
-            except KeyError:
-                # This is the first system call of this process.
-                function_calls_queue = SplitExecutionStreamsTest.FunctionCallsQueue(aPid)
-                dict_pids[aPid] = function_calls_queue
-
-            function_calls_queue.push_function_name(aCore.GetFunction())
-
-        dockit.ParseInstructionsStream(
-            inputLogFile=path_prefix_input_file("sample_shell.ltrace.log"),
-            withWarning = True,
-            tracer="ltrace",
-            topPid=0,
-            verbose=True,
-            batch_callback = batch_callback)
 
