27cbc6489449c729c7be157e8b2d98ce47dd43b3
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-02-08 18:01:52 +0000

    pydbg progress.

--- Experimental/Test_pydbg.py
+++ Experimental/Test_pydbg.py
@@ -45,6 +45,30 @@ def get_string(dbg, address):
             continue
         else:
             break
+    assert offset == len(buffer)
+    print("get_string Offset=", offset)
+    if offset > 0:
+        print("get_string buffer[0]=", ord(buffer[0]), buffer[0])
+    return buffer
+
+# Windows UTF-16 string.
+def get_wstring(dbg, address):
+    # TRANSFORM INTO ASCII, FOR THE MOMENT.
+    buffer  = b""
+    tralala = b""
+    offset  = 0
+    while 1:
+        byte = dbg.read_process_memory(address + offset, 2 )
+        if byte != b"\x00\x00":
+            buffer  += byte[0]
+            tralala  += byte[1]
+            offset  += 2
+            continue
+        else:
+            break
+    print("get_wstring Offset=", offset, len(buffer))
+    if offset > 0:
+        print("get_wstring buffer[0]=", ord(buffer[0]), buffer[0], "tralala[0]=", ord(tralala[0]), tralala[0])
     return buffer
 
 def get_long(dbg, address):
@@ -81,12 +105,14 @@ def hook_function_RemoveDirectoryA(dbg, args ):
     print("hook_function_RemoveDirectoryA args=", args)
     dirname = get_string(dbg, args[0])
     print("hook_function_RemoveDirectoryA dirname=", dirname)
+    assert dirname == b"NonExistentDirBinary"
     return defines.DBG_CONTINUE
 
 def hook_function_RemoveDirectoryW(dbg, args ):
     print("hook_function_RemoveDirectoryW args=", args)
-    dirname = get_string(dbg, args[0])
+    dirname = get_wstring(dbg, args[0])
     print("hook_function_RemoveDirectoryW dirname=", dirname)
+    assert dirname == b"NonExistentDirUnicode"
     return defines.DBG_CONTINUE
 
 def hook_function_MulDiv(dbg, args ):
@@ -115,16 +141,20 @@ def hook_function_GetEnvironmentVariableW(dbg, args ):
 def processing_function(one_argument):
     print('processing_function START.')
     while True:
-        print('Subprocess hello. Sleeping ', one_argument)
+        print('Subprocess hello. Sleeping %d seconds' % one_argument)
         time.sleep(one_argument)
+        dir_binary = six.b("NonExistentDirBinary")
+        dir_unicode = six.u("NonExistentDirUnicode")
+
         try:
-            if False:
-                os.rmdir(six.b("NonExistentDirBinary")) # RemoveDirectoryA
-                os.rmdir(six.u("NonExistentDirUnicode")) # RemoveDirectoryW
-            else:
-                ctypes.windll.kernel32.RemoveDirectoryA(six.b("NonExistentDirBinary"))
-                ctypes.windll.kernel32.RemoveDirectoryW(six.u("NonExistentDirUnicode"))
-        except:
+            ctypes.windll.kernel32.RemoveDirectoryW(dir_unicode)
+            os.rmdir(dir_unicode)  # RemoveDirectoryW
+
+            ctypes.windll.kernel32.RemoveDirectoryA(dir_binary)
+            os.rmdir(dir_binary)  # RemoveDirectoryA
+
+        except Exception as exc:
+            print("=============== CAUGHT:", exc)
             pass
 
         resu = ctypes.windll.kernel32.MulDiv(20, 30, 6)
@@ -138,6 +168,22 @@ if __name__ == '__main__':
     architecture = platform.architecture()
     print("Architecture:", architecture)
     assert architecture[0] == '64bit'
+
+    # The intention is to check that Windows gets the argument.
+    try:
+        os.rmdir(six.u("NonExistentDirUnicode"))  # RemoveDirectoryW
+    except WindowsError as exc:
+        str_exc = str(exc)
+        print("As expected:(%s)" % str_exc)
+        assert str_exc == "[Error 2] The system cannot find the file specified: u'NonExistentDirUnicode'"
+
+    # The intention is to check that Windows gets the argument.
+    try:
+        os.rmdir(six.b("NonExistentDirBinary"))  # RemoveDirectoryA
+    except WindowsError as exc:
+        str_exc = str(exc)
+        print("As expected:(%s)" % str_exc)
+        assert str_exc == "[Error 2] The system cannot find the file specified: 'NonExistentDirBinary'"
 
     print("ctypes.windll.kernel32.MulDiv.argtypes=", ctypes.windll.kernel32.MulDiv.argtypes) # None
     resu = ctypes.windll.kernel32.MulDiv(20, 30, 6)
@@ -175,19 +221,23 @@ if __name__ == '__main__':
     tst_pydbg.attach(created_process.pid)
 
     hooks = pydbg.tests.utils.hook_container()
-    hook_address_WriteFile = tst_pydbg.func_resolve(b"kernel32.dll", b"WriteFile")
-    assert hook_address_WriteFile
-    hooks.add(tst_pydbg, hook_address_WriteFile, 5, hook_function_WriteFile, None)
 
-    print("RemoveDirectoryA=", ctypes.windll.kernel32.RemoveDirectoryA)
-    print("RemoveDirectoryW=", ctypes.windll.kernel32.RemoveDirectoryW)
+    if False:
+        hook_address_WriteFile = tst_pydbg.func_resolve(b"kernel32.dll", b"WriteFile")
+        assert hook_address_WriteFile
+        hooks.add(tst_pydbg, hook_address_WriteFile, 5, hook_function_WriteFile, None)
+
+    #print("RemoveDirectoryA=", ctypes.windll.kernel32.RemoveDirectoryA)
+    #print("RemoveDirectoryW=", ctypes.windll.kernel32.RemoveDirectoryW)
 
     hook_address_RemoveDirectoryA = tst_pydbg.func_resolve(b"kernel32.dll", b"RemoveDirectoryA")
     assert hook_address_RemoveDirectoryA
+    # Normally, only one argument, but we want to see what comes after.
     hooks.add(tst_pydbg, hook_address_RemoveDirectoryA, 1, hook_function_RemoveDirectoryA, None)
 
     hook_address_RemoveDirectoryW = tst_pydbg.func_resolve(b"kernel32.dll", b"RemoveDirectoryW")
     assert hook_address_RemoveDirectoryW
+    # Normally, only one argument, but we want to see what comes after.
     hooks.add(tst_pydbg, hook_address_RemoveDirectoryW, 1, hook_function_RemoveDirectoryW, None)
 
     hook_address_MulDiv = tst_pydbg.func_resolve(b"kernel32.dll", b"MulDiv")
