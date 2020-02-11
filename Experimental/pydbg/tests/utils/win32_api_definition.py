ddddd497b406b6e4ae6803281c85399d2494f516
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-02-11 23:09:09 +0000

    Progress pydbg

--- Experimental/pydbg/tests/utils/win32_api_definition.py
+++ Experimental/pydbg/tests/utils/win32_api_definition.py
@@ -10,66 +10,76 @@ __email__ = "contact@primhillcomputers.com"
 __status__ = "Development"
 
 import re
+import six
 import pydbg
 from pydbg import defines
+from pydbg import windows_h
 
 ################################################################################
+
 class Win32Hook_BaseClass(object):
     object_pydbg = None
     object_hooks = None
-    object_report_callback = None
+    callback_create_call = None
+    callback_create_object = None
+
+    # The style tells if this is a native call or an aggregate of function
+    # calls, made with some style: Factorization etc...
+    def __init__(self):
+        self.m_core = "Something"
 
     # The API signature is taken "as is" from Microsoft web site.
-    def _parse_text_definition(self, api_definition):
-        match_one = re.match("\s*([A-Za-z0-9_]+)\s+([A-Za-z0-9_]+)\s*\((.*)\)\s*;", api_definition, re.DOTALL)
-        self.return_type = match_one.group(1)
-        self.function_name = match_one.group(2)
+    @staticmethod
+    def _parse_text_definition(the_class):
+        match_one = re.match("\s*([A-Za-z0-9_]+)\s+([A-Za-z0-9_]+)\s*\((.*)\)\s*;", the_class.api_definition, re.DOTALL)
+        the_class.return_type = match_one.group(1)
+        the_class.function_name = match_one.group(2)
 
         # '\nHANDLE hFile,\nLPCVOID lpBuffer,\nDWORD nNumberOfBytesToWrite,\nLPDWORD lpNumberOfBytesWritten,\nLPOVERLAPPED lpOverlapped\n'
-        self.args_list = []
+        the_class.args_list = []
         for one_arg_pair in match_one.group(3).split(","):
             match_pair = re.match("\s*([A-Za-z0-9_]+)\s+([A-Za-z0-9_]+)\s*", one_arg_pair)
-            self.args_list.append((match_pair.group(1), match_pair.group(2)))
+            the_class.args_list.append((match_pair.group(1), match_pair.group(2)))
 
-        # function_name = b"WriteFile"
-        self.hook_address = Win32Hook_BaseClass.object_pydbg.func_resolve(b"kernel32.dll", self.function_name)
-        assert self.hook_address
+        assert isinstance(the_class.function_name, six.binary_type )
+        the_class.hook_address = Win32Hook_BaseClass.object_pydbg.func_resolve(b"kernel32.dll", the_class.function_name)
+        assert the_class.hook_address
 
-    def __init__(self, api_definition):
-        self._parse_text_definition(api_definition)
-        self.hook_address = Win32Hook_BaseClass.object_pydbg.func_resolve(b"kernel32.dll", self.function_name)
-        assert self.hook_address
+    #subclass_dict = {}
 
-        def hook_function_adapter_entry(object_pydbg, args):
-            self.hook_function_entry(args)
-            return defines.DBG_CONTINUE
+    def set_arguments(self, args, function_result):
+        self.m_parsedArgs = args
+        self.m_retValue = function_result
+
+    @classmethod
+    def add_subclass(the_class, the_subclass):
+        Win32Hook_BaseClass._parse_text_definition(the_subclass)
+        #Win32Hook_BaseClass.subclass_dict[the_subclass.function_name] = the_subclass
+
+        #def hook_function_adapter_entry(object_pydbg, args):
+        #    return defines.DBG_CONTINUE
 
         def hook_function_adapter_exit(object_pydbg, args, function_result):
-            self.hook_function_exit(args, function_result)
+            subclass_instance = the_subclass()
+            subclass_instance.set_arguments(args, function_result)
+            subclass_instance.process_arguments()
+            Win32Hook_BaseClass.callback_create_call(subclass_instance)
             return defines.DBG_CONTINUE
 
         Win32Hook_BaseClass.object_hooks.add(
             Win32Hook_BaseClass.object_pydbg,
-            self.hook_address,
-            len(self.args_list),
-            hook_function_adapter_entry,
+            the_subclass.hook_address,
+            len(the_subclass.args_list),
+            None,
             hook_function_adapter_exit)
 
     def report_cim_object(self, cim_class_name, **cim_arguments):
         print("report_cim_object", self.__class__.__name__, cim_class_name, cim_arguments)
-        self.object_report_callback(cim_class_name, cim_arguments)
-
-    def hook_function_entry(self, args):
-        print("hook_function_entry", self.__class__.__name__, args)
-
-    def hook_function_exit(self, args, function_result):
-        print("hook_function_exit", self.__class__.__name__, args, function_result)
-
+        self.callback_create_object(cim_class_name, cim_arguments)
 
 
 class Win32Hook_CreateProcessA(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateProcessA, self).__init__("""
+    api_definition = """
         BOOL CreateProcessA(
             LPCSTR                lpApplicationName,
             LPSTR                 lpCommandLine,
@@ -81,11 +91,30 @@ class Win32Hook_CreateProcessA(Win32Hook_BaseClass):
             LPCSTR                lpCurrentDirectory,
             LPSTARTUPINFOA        lpStartupInfo,
             LPPROCESS_INFORMATION lpProcessInformation
-        );""")
+        );"""
+
+    def process_arguments(self):
+        lpApplicationName = Win32Hook_BaseClass.object_pydbg.get_string(self.m_parsedArgs[0])
+        lpCommandLine = Win32Hook_BaseClass.object_pydbg.get_string(self.m_parsedArgs[1])
+        lpProcessInformation = self.m_parsedArgs[9]
+
+        # _PROCESS_INFORMATION {
+        #   HANDLE hProcess;
+        #   HANDLE hThread;
+        #   DWORD  dwProcessId;
+        #   DWORD  dwThreadId;
+        # }
+        offset_dwProcessId = windows_h.sizeof(windows_h.HANDLE) + windows_h.sizeof(windows_h.HANDLE)
+        dwProcessId = Win32Hook_BaseClass.object_pydbg.get_long(lpProcessInformation + offset_dwProcessId)
+
+        print("Win32Hook_CreateProcessA m_parsedArgs=", self.m_parsedArgs)
+        print("Win32Hook_CreateProcessA m_retValue=", self.m_retValue)
+        print("Win32Hook_CreateProcessA Handle=", dwProcessId)
+        self.callback_create_object("CIM_Process", Handle=dwProcessId)
+
 
 class Win32Hook_CreateProcessAsUserA(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateProcessAsUserA, self).__init__("""
+    api_definition = """
         BOOL CreateProcessAsUserA(
             HANDLE                hToken,
             LPCSTR                lpApplicationName,
@@ -98,11 +127,10 @@ class Win32Hook_CreateProcessAsUserA(Win32Hook_BaseClass):
             LPCSTR                lpCurrentDirectory,
             LPSTARTUPINFOA        lpStartupInfo,
             LPPROCESS_INFORMATION lpProcessInformation
-        );""")
+        );"""
 
 class Win32Hook_CreateProcessAsUserW(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateProcessAsUserW, self).__init__("""
+    api_definition = """
         BOOL CreateProcessAsUserW(
             HANDLE                hToken,
             LPCWSTR               lpApplicationName,
@@ -115,11 +143,10 @@ class Win32Hook_CreateProcessAsUserW(Win32Hook_BaseClass):
             LPCWSTR               lpCurrentDirectory,
             LPSTARTUPINFOW        lpStartupInfo,
             LPPROCESS_INFORMATION lpProcessInformation
-        );""")
+        );"""
 
 class Win32Hook_CreateProcessW(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateProcessW, self).__init__("""
+    api_definition = """
         BOOL CreateProcessW(
             LPCWSTR               lpApplicationName,
             LPWSTR                lpCommandLine,
@@ -131,18 +158,16 @@ class Win32Hook_CreateProcessW(Win32Hook_BaseClass):
             LPCWSTR               lpCurrentDirectory,
             LPSTARTUPINFOW        lpStartupInfo,
             LPPROCESS_INFORMATION lpProcessInformation
-        );""")
+        );"""
 
 class Win32Hook_ExitProcess(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_ExitProcess, self).__init__("""
+    api_definition = """
         void ExitProcess(
             UINT uExitCode
-        );""")
+        );"""
 
 class Win32Hook_CreateThread(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateThread, self).__init__("""
+    api_definition = """
         HANDLE CreateThread(
             LPSECURITY_ATTRIBUTES   lpThreadAttributes,
             SIZE_T                  dwStackSize,
@@ -150,11 +175,10 @@ class Win32Hook_CreateThread(Win32Hook_BaseClass):
             __drv_aliasesMem LPVOID lpParameter,
             DWORD                   dwCreationFlags,
             LPDWORD                 lpThreadId
-        );""")
+        );"""
 
 class Win32Hook_CreateRemoteThread(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateRemoteThread, self).__init__("""
+    api_definition = """
         HANDLE CreateRemoteThread(
             HANDLE                 hProcess,
             LPSECURITY_ATTRIBUTES  lpThreadAttributes,
@@ -163,11 +187,10 @@ class Win32Hook_CreateRemoteThread(Win32Hook_BaseClass):
             LPVOID                 lpParameter,
             DWORD                  dwCreationFlags,
             LPDWORD                lpThreadId
-        );""")
+        );"""
 
 class Win32Hook_CreateRemoteThreadEx(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateRemoteThreadEx, self).__init__("""
+    api_definition = """
         HANDLE CreateRemoteThreadEx(
             HANDLE                       hProcess,
             LPSECURITY_ATTRIBUTES        lpThreadAttributes,
@@ -177,69 +200,60 @@ class Win32Hook_CreateRemoteThreadEx(Win32Hook_BaseClass):
             DWORD                        dwCreationFlags,
             LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList,
             LPDWORD                      lpThreadId
-        );""")
+        );"""
 
 class Win32Hook_TerminateProcess(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_TerminateProcess, self).__init__("""
-        BOOL TerminateProcess(
-            HANDLE hProcess,
-            UINT   uExitCode
-        );""")
+    api_definition = """
+            BOOL TerminateProcess(
+                HANDLE hProcess,
+                UINT   uExitCode
+            );"""
 
 class Win32Hook_TerminateThread(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_TerminateThread, self).__init__("""
+    api_definition = """
         BOOL TerminateThread(
             HANDLE hThread,
             DWORD  dwExitCode
-        );""")
+        );"""
 
 class Win32Hook_CreateDirectoryA(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateDirectoryA, self).__init__("""
+    api_definition = """
         BOOL CreateDirectoryA(
             LPCWSTR               lpPathName,
             LPSECURITY_ATTRIBUTES lpSecurityAttributes
-        );""")
+        );"""
 
 class Win32Hook_CreateDirectoryW(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateDirectoryW, self).__init__("""
+    api_definition = """
         BOOL CreateDirectoryW(
             LPCWSTR               lpPathName,
             LPSECURITY_ATTRIBUTES lpSecurityAttributes
-        );""")
+        );"""
 
 class Win32Hook_RemoveDirectoryA(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_RemoveDirectoryA, self).__init__("""
+    api_definition = """
         BOOL RemoveDirectoryA(
             LPCSTR lpPathName
-        );""")
-
-    def hook_function_entry(self, args):
-        print("hook_function_RemoveDirectoryA args=", args)
-        dirname = Win32Hook_BaseClass.object_pydbg.get_string(args[0])
+        );"""
+    def process_arguments(self):
+        print("hook_function_RemoveDirectoryA args=", self.m_parsedArgs)
+        dirname = Win32Hook_BaseClass.object_pydbg.get_string(self.m_parsedArgs[0])
         print("hook_function_RemoveDirectoryA dirname=", dirname)
-        self.report_cim_object("CIM_Directory", Name=dirname)
+        self.callback_create_object("CIM_Directory", Name=dirname)
 
 class Win32Hook_RemoveDirectoryW(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_RemoveDirectoryW, self).__init__("""
+    api_definition = """
         BOOL RemoveDirectoryW(
             LPCWSTR lpPathName
-        );""")
-
-    def hook_function_entry(self, args):
-        print("hook_function_RemoveDirectoryW args=", args)
-        dirname = Win32Hook_BaseClass.object_pydbg.get_wstring(args[0])
+        );"""
+    def process_arguments(self):
+        print("hook_function_RemoveDirectoryW args=", self.m_parsedArgs)
+        dirname = Win32Hook_BaseClass.object_pydbg.get_wstring(self.m_parsedArgs[0])
         print("hook_function_RemoveDirectoryW dirname=", dirname)
-        self.report_cim_object("CIM_Directory", Name=dirname)
+        self.callback_create_object("CIM_Directory", Name=dirname)
 
 class Win32Hook_CreateFileA(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateFileA, self).__init__("""
+    api_definition = """
         HANDLE CreateFileA(
             LPCSTR                lpFileName,
             DWORD                 dwDesiredAccess,
@@ -248,15 +262,13 @@ class Win32Hook_CreateFileA(Win32Hook_BaseClass):
             DWORD                 dwCreationDisposition,
             DWORD                 dwFlagsAndAttributes,
             HANDLE                hTemplateFile
-        );""")
-
-    def hook_function_entry(self, args):
-        dirname = Win32Hook_BaseClass.object_pydbg.get_string(args[0])
-        self.report_cim_object("CIM_DataFile", Name=dirname)
+        );"""
+    def process_arguments(self):
+        dirname = Win32Hook_BaseClass.object_pydbg.get_string(self.m_parsedArgs[0])
+        self.callback_create_object("CIM_DataFile", Name=dirname)
 
 class Win32Hook_CreateFileW(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateFileW, self).__init__("""
+    api_definition = """
         HANDLE CreateFileW(
             LPCWSTR               lpFileName,
             DWORD                 dwDesiredAccess,
@@ -265,129 +277,115 @@ class Win32Hook_CreateFileW(Win32Hook_BaseClass):
             DWORD                 dwCreationDisposition,
             DWORD                 dwFlagsAndAttributes,
             HANDLE                hTemplateFile
-        );""")
-
-    def hook_function_entry(self, args):
-        dirname = Win32Hook_BaseClass.object_pydbg.get_string(args[0])
-        self.report_cim_object("CIM_DataFile", Name=dirname)
+        );"""
+    def process_arguments(self):
+        dirname = Win32Hook_BaseClass.object_pydbg.get_string(self.m_parsedArgs[0])
+        self.callback_create_object("CIM_DataFile", Name=dirname)
 
 class Win32Hook_CreateFile2(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_CreateFile2, self).__init__("""
+    api_definition = """
         HANDLE CreateFile2(
             LPCWSTR                           lpFileName,
             DWORD                             dwDesiredAccess,
             DWORD                             dwShareMode,
             DWORD                             dwCreationDisposition,
             LPCREATEFILE2_EXTENDED_PARAMETERS pCreateExParams
-        );""")
+        );"""
 
 class Win32Hook_DeleteFileA(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_DeleteFileA, self).__init__("""
+    api_definition = """
         BOOL DeleteFileA(
             LPCSTR lpFileName
-        );""")
+        );"""
 
 class Win32Hook_DeleteFileW(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_DeleteFileW, self).__init__("""
+    api_definition = """
         BOOL DeleteFileW(
             LPCWSTR lpFileName
-        );""")
+        );"""
 
 class Win32Hook_WriteFile(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_WriteFile, self).__init__("""
+    api_definition = """
         BOOL WriteFile(
             HANDLE       hFile,
             LPCVOID      lpBuffer,
             DWORD        nNumberOfBytesToWrite,
             LPDWORD      lpNumberOfBytesWritten,
             LPOVERLAPPED lpOverlapped
-        );""")
+        );"""
+    def process_arguments(self):
+        print("hook_function_WriteFile args=", self.m_parsedArgs)
 
-    def hook_function_entry(self, args):
-        print("hook_function_WriteFile args=", args)
-
-        lpBuffer = args[1]
-        nNumberOfBytesToWrite = args[2]
+        lpBuffer = self.m_parsedArgs[1]
+        nNumberOfBytesToWrite = self.m_parsedArgs[2]
         # print("lpBuffer=", lpBuffer, "nNumberOfBytesToWrite=", nNumberOfBytesToWrite)
         buffer = Win32Hook_BaseClass.object_pydbg.get_string_size(lpBuffer, nNumberOfBytesToWrite)
         print("Buffer=", buffer)
 
 
 class Win32Hook_WriteFileEx(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_WriteFileEx, self).__init__("""
+    api_definition = """
         BOOL WriteFileEx(
             HANDLE                          hFile,
             LPCVOID                         lpBuffer,
             DWORD                           nNumberOfBytesToWrite,
             LPOVERLAPPED                    lpOverlapped,
             LPOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
-        );""")
+        );"""
 
 class Win32Hook_WriteFileGather(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_WriteFileGather, self).__init__("""
+    api_definition = """
         BOOL WriteFileGather(
             HANDLE                  hFile,
             FILE_SEGMENT_ELEMENT [] aSegmentArray,
             DWORD                   nNumberOfBytesToWrite,
             LPDWORD                 lpReserved,
             LPOVERLAPPED            lpOverlapped
-        );""")
+        );"""
 
 class Win32Hook_ReadFile(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_ReadFile, self).__init__("""
+    api_definition = """
         BOOL ReadFile(
             HANDLE       hFile,
             LPVOID       lpBuffer,
             DWORD        nNumberOfBytesToRead,
             LPDWORD      lpNumberOfBytesRead,
             LPOVERLAPPED lpOverlapped
-        );""")
+        );"""
 
 class Win32Hook_ReadFileEx(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_ReadFileEx, self).__init__("""
+    api_definition = """
         BOOL ReadFileEx(
             HANDLE                          hFile,
             LPVOID                          lpBuffer,
             DWORD                           nNumberOfBytesToRead,
             LPOVERLAPPED                    lpOverlapped,
             LPOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
-        );""")
+        );"""
 
 class Win32Hook_ReadFileScatter(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_ReadFileScatter, self).__init__("""
+    api_definition = """
         BOOL ReadFileScatter(
             HANDLE                  hFile,
             FILE_SEGMENT_ELEMENT [] aSegmentArray,
             DWORD                   nNumberOfBytesToRead,
             LPDWORD                 lpReserved,
             LPOVERLAPPED            lpOverlapped
-        );""")
+        );"""
 
 
 ############### JUST FOR TESTING
 
-
 class Win32Hook_MulDiv(Win32Hook_BaseClass):
-    def __init__(self):
-        super(Win32Hook_MulDiv, self).__init__("""
+    api_definition = """
         int MulDiv(
             int nNumber,
             int nNumerator,
             int nDenominator 
-        );""")
-
-    def hook_function_entry(self, args):
-        print("hook_function_MulDiv args=", args)
-        assert args[0] == 20
-        assert args[1] == 30
-        assert args[2] == 6
+        );"""
+    def process_arguments(self):
+        print("hook_function_MulDiv args=", self.m_parsedArgs)
+        assert self.m_parsedArgs[0] == 20
+        assert self.m_parsedArgs[1] == 30
+        assert self.m_parsedArgs[2] == 6
 
