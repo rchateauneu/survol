ba02bc981515e95a3d07e0b8727ccd805de743d0
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-02-24 22:57:44 +0000

    Added new test.

--- tests/test_pydbg.py
+++ tests/test_pydbg.py
@@ -26,8 +26,6 @@ if not is_platform_linux:
             tst_pydbg = pydbg.pydbg.pydbg()
         return tst_pydbg
 
-# Faire tourner les tests de pydbg a l origine.
-
 ################################################################################
 
 @unittest.skipIf(is_platform_linux, "Windows only.")
@@ -132,14 +130,14 @@ class PydbgBasicTest(unittest.TestCase):
         def callback_CreateFileW_entry(object_pydbg, args):
             object_pydbg.count_entry += 1
             file_name = object_pydbg.get_wstring(args[0])
-            print("callback_RemoveDirectoryW_entry file_name=", file_name)
+            print("callback_CreateFileW_entry file_name=", file_name)
             self.assertTrue(file_name == non_existent_file)
             return defines.DBG_CONTINUE
 
         def callback_CreateFileW_exit(object_pydbg, args, function_result):
             object_pydbg.count_exit += 1
             file_name = object_pydbg.get_wstring(args[0])
-            print("callback_RemoveDirectoryW_exit file_name=", file_name, function_result)
+            print("callback_CreateFileW_exit file_name=", file_name, function_result)
             self.assertTrue(file_name == non_existent_file)
 
             is_invalid_handle = function_result % (1 + defines.INVALID_HANDLE_VALUE) == defines.INVALID_HANDLE_VALUE
@@ -159,6 +157,154 @@ class PydbgBasicTest(unittest.TestCase):
         # The first call might be missed.
         self.assertTrue(tst_pydbg.count_entry == num_loops)
         self.assertTrue(tst_pydbg.count_exit == num_loops)
+
+    @unittest.skipIf(is_travis_machine(), "Does not work on Travis.")
+    def test_pydbg_DOS_CreateProcessW(self):
+        import pydbg
+        from pydbg import utils
+
+        tst_pydbg = create_pydbg()
+
+        num_loops = 5
+
+        # Each loop creates a sub-process which immediately exists.
+        # This is detected by the hook.
+        python_command = "FOR /L %%A IN (1,1,%d) DO ( ping -n 2 127.0.0.1& echo %%A )" % num_loops
+
+        created_process = subprocess.Popen(python_command, shell=True)
+        print("Created process:%d" % created_process.pid)
+
+        # A bit of delay so the process can start.
+        time.sleep(1.0)
+
+        print("Attaching. Root pid=%d" % os.getpid())
+        tst_pydbg.attach(created_process.pid)
+
+        object_hooks = pydbg.utils.hook_container()
+
+        hook_address_CreateProcessW = tst_pydbg.func_resolve(b"KERNEL32.dll", b"CreateProcessW")
+
+        tst_pydbg.count_entry = 0
+        tst_pydbg.count_exit = 0
+
+#         BOOL CreateProcessW(
+        #             LPCWSTR               lpApplicationName,
+        #             LPWSTR                lpCommandLine,
+        #             LPSECURITY_ATTRIBUTES lpProcessAttributes,
+        #             LPSECURITY_ATTRIBUTES lpThreadAttributes,
+        #             BOOL                  bInheritHandles,
+        #             DWORD                 dwCreationFlags,
+        #             LPVOID                lpEnvironment,
+        #             LPCWSTR               lpCurrentDirectory,
+        #             LPSTARTUPINFOW        lpStartupInfo,
+        #             LPPROCESS_INFORMATION lpProcessInformation
+
+        def callback_CreateProcessW_entry(object_pydbg, args):
+            object_pydbg.count_entry += 1
+            lpApplicationName = object_pydbg.get_wstring(args[0])
+            print("callback_CreateProcessW_entry lpApplicationName=", lpApplicationName)
+            assert lpApplicationName == r"C:\windows\system32\PING.EXE"
+            lpCommandLine = object_pydbg.get_wstring(args[1])
+            print("callback_CreateProcessW_entry lpCommandLine=%s." % lpCommandLine)
+            assert lpCommandLine == "ping  -n 2 127.0.0.1"
+            return pydbg.defines.DBG_CONTINUE
+
+        def callback_CreateProcessW_exit(object_pydbg, args, function_result):
+            object_pydbg.count_exit += 1
+            lpApplicationName = object_pydbg.get_wstring(args[0])
+            print("callback_CreateProcessW_entry lpApplicationName=", lpApplicationName)
+            assert lpApplicationName == r"C:\windows\system32\PING.EXE"
+            lpCommandLine = object_pydbg.get_wstring(args[1])
+            print("callback_CreateProcessW_entry lpCommandLine=%s." % lpCommandLine)
+            assert lpCommandLine == "ping  -n 2 127.0.0.1"
+            return pydbg.defines.DBG_CONTINUE
+
+        object_hooks.add(
+            tst_pydbg,
+            hook_address_CreateProcessW,
+            10,
+            callback_CreateProcessW_entry,
+            callback_CreateProcessW_exit)
+
+        tst_pydbg.run()
+
+        print("END", tst_pydbg.count_entry, tst_pydbg.count_exit)
+        # The first call is missed.
+        self.assertTrue(tst_pydbg.count_entry == num_loops - 1)
+        self.assertTrue(tst_pydbg.count_exit == num_loops - 1)
+
+
+    # https://github.com/OpenRCE/pydbg/issues/5
+    # from pydbg import *
+    # from pydbg.defines import *
+    # import pefile
+    #
+    # dbg = pydbg()
+    # dbg.load("c:\windows\system32\notepad.exe")
+    # pe = pefile.PE("C:\Windows\System32\notepad.exe")
+    # entrypoint = pe.OPTIONAL_HEADER.ImageBase + pe.OPTIONAL_HEADER.AddressOfEntryPoint
+    # dbg.bp_set(entrypoint)
+    # pydbg.debug_event_loop(dbg)
+
+
+
+    @unittest.skipIf(True or is_travis_machine(), "Does not work on Travis.")
+    def test_pydbg_DOS_socket(self):
+        import pydbg
+        from pydbg import defines
+        from pydbg import utils
+
+        tst_pydbg = create_pydbg()
+
+        num_loops = 5
+
+        # This attempts several times to remove a non-existent dir.
+        # This is detected by the hook.
+        python_command = "FOR /L %%A IN (1,1,%d) DO ( ping -n 2 1.2.3.4 & echo %%A )" % num_loops
+
+        created_process = subprocess.Popen(python_command, shell=True)
+        print("Created process:%d" % created_process.pid)
+
+        # A bit of delay so the process can start.
+        time.sleep(1.0)
+
+        print("Attaching. Root pid=%d" % os.getpid())
+        tst_pydbg.attach(created_process.pid)
+
+        object_hooks = pydbg.utils.hook_container()
+
+        hook_address_socket = tst_pydbg.func_resolve(b"Ws2_32.dll.dll", b"socket")
+
+        tst_pydbg.count_entry = 0
+        tst_pydbg.count_exit = 0
+
+        def callback_socket_entry(object_pydbg, args):
+            object_pydbg.count_entry += 1
+            print("callback_socket_entry args=", args)
+            return defines.DBG_CONTINUE
+
+        def callback_socket_exit(object_pydbg, args, function_result):
+            object_pydbg.count_exit += 1
+            print("callback_socket_exit args=", args, function_result)
+
+            is_invalid_handle = function_result % (1 + defines.INVALID_HANDLE_VALUE) == defines.INVALID_HANDLE_VALUE
+            self.assertTrue(is_invalid_handle)
+            return defines.DBG_CONTINUE
+
+        object_hooks.add(
+            tst_pydbg,
+            hook_address_socket,
+            3,
+            callback_socket_entry,
+            callback_socket_exit)
+
+        tst_pydbg.run()
+
+        print("END", tst_pydbg.count_entry, tst_pydbg.count_exit)
+        # The first call might be missed.
+        self.assertTrue(tst_pydbg.count_entry == num_loops)
+        self.assertTrue(tst_pydbg.count_exit == num_loops)
+
 
 if __name__ == '__main__':
     unittest.main()
