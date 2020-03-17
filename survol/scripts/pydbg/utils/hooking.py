18333ab100c289c53ccec7185c23dbd15098287b
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-03-17 08:38:36 +0000

    Fix relative import.

--- survol/scripts/pydbg/utils/hooking.py
+++ survol/scripts/pydbg/utils/hooking.py
@@ -22,7 +22,7 @@
 @organization: www.openrce.org
 '''
 
-from pydbg.defines import *
+from survol.scripts.pydbg.defines import *
 
 ########################################################################################################################
 class hook_container:
