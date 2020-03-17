5af3a5dc8955012b80c394d3834c46ce6f0f6528
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-03-17 16:38:23 +0000

    Code cleanup.

--- survol/scripts/win32_api_definitions.py
+++ survol/scripts/win32_api_definitions.py
@@ -25,17 +25,6 @@ from .pydbg import defines
 from .pydbg import windows_h
 from .pydbg import utils
 
-#from survol.scripts import pydbg
-#from survol.scripts.pydbg import defines
-#from survol.scripts.pydbg import windows_h
-#from survol.scripts.pydbg import utils
-
-#from . import cim_objects_definitions
-#from . import pydbg
-#from pydbg import defines
-#from pydbg import windows_h
-#from pydbg import utils
-
 ################################################################################
 def create_pydbg():
     if sys.version_info < (3, 8):
