3a0e834617dced67211ebfcac2a30b6bce218a82
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-05-09 15:45:48 +0100

    Renaming.

--- survol/event_put.py
+++ survol/event_put.py
@@ -5,6 +5,7 @@ Put an event about a CIM object
 """
 
 import os
+import six
 import sys
 import cgi
 import json
@@ -18,32 +19,71 @@ import lib_event
 # The type of the data stored in these files is exactly what can be returned by any scripts.
 
 def Main():
-	# cgiArguments = cgi.FieldStorage()
+    time_start = time.time()
+    http_content_length = int(os.environ['CONTENT_LENGTH'])
 
-	# https://stackoverflow.com/questions/49171591/inets-httpd-cgi-script-how-do-you-retrieve-json-data
-	# The script MUST NOT attempt to read more than CONTENT_LENGTH bytes, even if more data is available.
-	tmStart = time.time()
-	sys.stderr.write("event_put.py tmStart=%f\n" % (tmStart))
-	try:
-		myjson = json.load(sys.stdin)
-		tmStart = time.time()
-		numTriples = len(myjson)
-		sys.stderr.write("event_put.py tmStart=%f myjson=%d triples received\n" % (tmStart,numTriples))
+    # https://stackoverflow.com/questions/49171591/inets-httpd-cgi-script-how-do-you-retrieve-json-data
+    # The script MUST NOT attempt to read more than CONTENT_LENGTH bytes, even if more data is available.
+    sys.stderr.write("event_put.py http_content_length=%d time_start=%f\n" % (http_content_length, time_start))
 
-		lib_event.data_store_list(myjson)
-		sys.stderr.write("event_put.py tmStart=%f myjson=%d triples stored\n" % (tmStart,numTriples))
+    # FIXME: This can be a lot simplified. This code results of the research of an intermittent time-out
+    # FIXME: ... on Windows and Python 3 and if the test agent is started by pytest.
+    extra_error_status = ""
+    try:
+        read_bytes_number = 0
+        loaded_bytes = b""
+        rest_to_read = http_content_length
+        loop_counter = 0
+        while True:
+            if sys.version_info >= (3,):
+                loaded_chunk = sys.stdin.buffer.read(rest_to_read)
+            else:
+                loaded_chunk = sys.stdin.read(rest_to_read)
+            assert isinstance(loaded_chunk, six.binary_type)
+            chunk_length = len(loaded_chunk)
+            loaded_bytes += loaded_chunk
+            rest_to_read -= chunk_length
+            read_bytes_number += chunk_length
+            if read_bytes_number >= http_content_length:
+                break
+            loop_counter += 1
+            time.sleep(0.1)
+            if loop_counter > 1000:
+                raise Exception(__file__ + " too many loops. read_bytes_number=%d" % read_bytes_number)
 
-		result = {'success':'true','message':'Tm=%f. NumTriples=%d' % ( tmStart, numTriples ) }
-	except Exception as exc:
-		sys.stderr.write("event_put.py Exception=%s\n" % (str(exc)))
-		# sys.stderr.write("event_put.py Envs=%s\n" % (str(os.environ)))
-		result = {'success':'false','message':'Tm=%f. Error:%s' % (tmStart, str(exc))}
+        if loop_counter > 1:
+            sys.stderr.write("event_put.py BEWARE loop_counter=%d\n" % loop_counter)
 
-	print( 'Content-Type: application/json\n\n' )
-	print( json.dumps(result) )
+        if len(loaded_bytes) != http_content_length:
+            raise Exception("len(loaded_bytes)=%d http_content_length=%d" %(len(loaded_bytes), http_content_length))
+        if read_bytes_number != http_content_length:
+            raise Exception("read_bytes_number=%d http_content_length=%d" %(read_bytes_number, http_content_length))
+        triples_json = json.loads(loaded_bytes)
+        time_loaded = time.time()
+        triples_number = len(triples_json)
 
-	sys.stderr.write("event_put.py %d Result=%s\n" % (tmStart,str(result)))
+        files_updates_total_number = lib_event.store_events_triples_list(triples_json)
+        time_stored = time.time()
+        sys.stderr.write("event_put.py time_stored=%f files_updates_total_number=%d\n" % (time_stored, files_updates_total_number))
 
+        server_result = {
+            'success': 'true',
+            'time_start': '%f' % time_start,
+            'time_loaded': '%f' % time_loaded,
+            'time_stored': '%f' % time_stored,
+            'triples_number': '%d' % triples_number}
+    except Exception as exc:
+        sys.stderr.write("event_put.py Exception=%s\n" % str(exc))
+
+        server_result = {
+            'success': 'false',
+            'time_start': '%f' % time_start,
+            'error_message': '%s:%s' % (str(exc), extra_error_status)}
+
+    json_output = json.dumps(server_result)
+    sys.stdout.write('Content-Type: application/json\n')
+    sys.stdout.write('Content-Length: %d\n\n' % len(json_output))
+    sys.stdout.write(json_output)
 
 if __name__ == '__main__':
-	Main()
+    Main()
--- survol/lib_event.py
+++ survol/lib_event.py
@@ -32,8 +32,8 @@ events_file_extension = ".events"
 valid_filename_chars = ",=-_.() %s%s" % (string.ascii_letters, string.digits)
 
 # This transforms a string into a valid filename.
-def StringToFileName(orgFilNam):
-    #sys.stderr.write("StringToFileName orgFilNam=%s\n"%(orgFilNam))
+def _string_to_filename(orgFilNam):
+    #sys.stderr.write("_string_to_filename orgFilNam=%s\n"%(orgFilNam))
     filNa = orgFilNam
     # replace spaces
     for r in '/\\ ':
@@ -46,16 +46,17 @@ def StringToFileName(orgFilNam):
 
     # keep only whitelisted chars
     retfilnam = ''.join(c for c in cleaned_filename if c in valid_filename_chars)
-    #sys.stderr.write("StringToFileName retfilnam=%s\n"%(retfilnam))
+    #sys.stderr.write("_string_to_filename retfilnam=%s\n"%(retfilnam))
     return retfilnam
 
+
 # This assumes that the properties are in the order of the ontology.
-def EntityTypeIdsToEventFile(entity_type,entity_ids_dict):
+def _entity_type_ids_to_event_file(entity_type, entity_ids_dict):
     dirEntity = events_directory + entity_type
     if not os.path.isdir(dirEntity):
         os.mkdir(dirEntity)
 
-    #sys.stderr.write("EntityTypeIdsToEventFile entity_type=%s\n"%entity_type)
+    #sys.stderr.write("_entity_type_ids_to_event_file entity_type=%s\n"%entity_type)
 
     # Build a file name; where the event will be stored.
 
@@ -68,36 +69,37 @@ def EntityTypeIdsToEventFile(entity_type,entity_ids_dict):
         eventFilNam += delim + ontoAttrNam + "=" + str(encodeVal)
         delim = ","
 
-    #sys.stderr.write("EntityTypeIdsToEventFile eventFilNam=%s\n"%eventFilNam)
+    #sys.stderr.write("_entity_type_ids_to_event_file eventFilNam=%s\n"%eventFilNam)
 
-    # Because of Windows
+    # Windows filenames should not be too long.
     if len(eventFilNam) > 240:
         eventFilNam = eventFilNam[:240]
 
     # TODO: This is a temporary solution which should check the unicity of filenames
     # by adding a hash value at the end.
 
-    eventFilNamLong = StringToFileName(eventFilNam) + events_file_extension
+    eventFilNamLong = _string_to_filename(eventFilNam) + events_file_extension
 
-    #sys.stderr.write("EntityTypeIdsToEventFile eventFilNamLong=%s\n"%eventFilNamLong)
+    #sys.stderr.write("_entity_type_ids_to_event_file eventFilNamLong=%s\n"%eventFilNamLong)
 
     return eventFilNamLong
 
 # There is one directory per entity type.
 # Then, each entity has its own file whose name is the rest of the moniker.
-def MonikerToEventFile(jsonMonik):
+def _moniker_to_event_filename(jsonMonik):
     # The subject could be parsed with the usual functions made for moniker.
     entity_type = jsonMonik["entity_type"]
-    #sys.stderr.write("MonikerToEventFile entity_type=%s\n"%entity_type)
+    #sys.stderr.write("_moniker_to_event_filename entity_type=%s\n"%entity_type)
 
-    dirEntity = lib_common.tmpDir + "/Events/" + entity_type
+    dirEntity = events_directory + entity_type
+
     if not os.path.isdir(dirEntity):
         os.makedirs(dirEntity)
 
-    #sys.stderr.write("MonikerToEventFile dirEntity=%s\n"%dirEntity)
+    #sys.stderr.write("_moniker_to_event_filename dirEntity=%s\n"%dirEntity)
     arrOnto = lib_util.OntologyClassKeys(entity_type)
 
-    #sys.stderr.write("MonikerToEventFile arrOnto=%s\n"%str(arrOnto))
+    #sys.stderr.write("_moniker_to_event_filename arrOnto=%s\n"%str(arrOnto))
     entity_ids_dict = {}
 
     # Only the properties we need.
@@ -105,78 +107,80 @@ def MonikerToEventFile(jsonMonik):
         attrVal = jsonMonik[ontoAttrNam]
         entity_ids_dict[ ontoAttrNam ] = attrVal
 
-    eventFilNam = EntityTypeIdsToEventFile(entity_type,entity_ids_dict)
+    eventFilNam = _entity_type_ids_to_event_file(entity_type,entity_ids_dict)
 
-    #sys.stderr.write("MonikerToEventFile eventFilNam=%s\n"%eventFilNam)
+    #sys.stderr.write("_moniker_to_event_filename eventFilNam=%s\n"%eventFilNam)
     eventPath = dirEntity + "/" + eventFilNam
     return eventPath
 
-def AddEventToObject(theObject,jsonData):
-    eventFilNam = MonikerToEventFile(theObject)
-    # sys.stderr.write("AddEventToObject eventFilNam=%s jsonData=%s\n"%(eventFilNam,str(jsonData)))
+
+def _add_event_to_file_of_object(theObject, jsonData):
+    event_filename = _moniker_to_event_filename(theObject)
+    # sys.stderr.write("_add_event_to_file_of_object eventFilNam=%s jsonData=%s\n"%(eventFilNam,str(jsonData)))
     # One JSON triple per line.
 
     # Try several times in case the script event_get.py would read at the same time.
     maxTry = 3
-    sleep_delay = 0.1
+    sleep_delay = 0.5
     while maxTry > 0:
         maxTry -= 1
         try:
             # Appends a new event at the end.
-            eventFd = open(eventFilNam,"a")
-            # This must be as fast as possible, so event_get is not blocked..
-            json.dump(jsonData, eventFd)
-            eventFd.write("\n")
-            eventFd.close()
-            #sys.stderr.write("AddEventToObject closing file\n")
-
+            with open(event_filename, "a") as event_filedes:
+                #eventFd = open(event_filename, "a")
+                # This must be as fast as possible, so event_get is not blocked..
+                json.dump(jsonData, event_filedes)
+                event_filedes.write("\n")
+                event_filedes.close()
             break
         except Exception as exc:
-            #sys.stderr.write("AddEventToObject waiting:%s\n"%str(exc))
+            sys.stderr.write("_add_event_to_file_of_object. Caught:%s\n" % str(exc))
             time.sleep(sleep_delay)
             sleep_delay *= 2
     if maxTry == 0:
-        WARNING("AddEventToObject leaving. Failed.")
+        WARNING("_add_event_to_file_of_object %s leaving. Failed." % event_filename)
 
 
 # This receives a json which has this structure:
-# subject: A CIM object.
+# subject: A CIM object as a dictionary.
 # predicate: A string.
-# object: A literal or a CIM object.
-# This is in reality a RDF triple, but it is not needed to import
-# the rdflib module.
-# Also, this relies on json to move the strings,
-# so there is no coding issue.
-# Also: It is not needed yet to load the ontology in the client.
-def data_store(json_data):
-    # TODO: Receive an array.
-    # sys.stderr.write("data_store entering.\n")
+# object: A literal or a CIM object, as a dictionary.
+# This is practically a RDF triple, but it is not needed to import the rdflib module.
+# This is based on json to move the strings, for convenience.
+# It is not needed yet to load the ontology in the client.
+def _store_event_triple(json_data):
+    # sys.stderr.write("_store_event_triple entering.\n")
     # The subject is always there and telles where the data are stored.
     valSubject = json_data["subject"]
-    AddEventToObject(valSubject,json_data)
-    #sys.stderr.write("data_store stored subject.\n")
+    _add_event_to_file_of_object(valSubject, json_data)
+    #sys.stderr.write("_store_event_triple stored subject.\n")
 
     valObject = json_data["object"]
 
     # The object might be another CIM object or a literal.
-    if isinstance(valObject,dict):
-        AddEventToObject(valObject,json_data)
-        #sys.stderr.write("data_store stored object.\n")
+    files_updates_number = 1
+    if isinstance(valObject, dict):
+        _add_event_to_file_of_object(valObject, json_data)
+        files_updates_number += 1
+    return files_updates_number
 
-    #sys.stderr.write("data_store leaving.\n")
 
-def data_store_list(json_data_list):
-    DEBUG("data_store_list entering. Numtriples=%d.",len(json_data_list))
+def store_events_triples_list(json_data_list):
+    DEBUG("store_events_triples_list entering. Numtriples=%d.", len(json_data_list))
+    files_updates_total_number = 0
     for json_data in json_data_list:
         try:
-            data_store(json_data)
+            files_updates_number = _store_event_triple(json_data)
+            files_updates_total_number += files_updates_number
         except Exception as exc:
-            WARNING("data_store_list caught:%s. Json=%s",str(exc),str(json_data))
+            WARNING("store_events_triples_list caught:%s. Json=%s", str(exc), str(json_data))
             traceback.print_exc()
 
-    DEBUG("data_store_list leaving.")
+    DEBUG("store_events_triples_list leaving.")
+    return files_updates_total_number
 
-def TripleJsonToRdf(jsonTriple):
+
+def _triple_json_to_rdf(jsonTriple):
     def UrlJsonToTxt(valJson):
         entity_type = valJson["entity_type"]
 
@@ -197,84 +201,87 @@ def TripleJsonToRdf(jsonTriple):
         txtObject = UrlJsonToTxt(valObject)
     else:
         txtObject = lib_kbase.MakeNodeLiteral(valObject)
-        #sys.stderr.write("data_store stored object.\n")
+        #sys.stderr.write("_store_event_triple stored object.\n")
 
     urlPred = lib_common.MakeProp(jsonTriple["predicate"])
     rdfTriple = (txtSubject,urlPred,txtObject)
     return rdfTriple
 
 
-def get_data_from_file(eventFilNam):
-    # sys.stderr.write("get_data_from_file eventFilNam=%s.\n"%eventFilNam)
+def _get_events_from_file(event_filename):
+    # sys.stderr.write("_get_events_from_file eventFilNam=%s.\n"%eventFilNam)
     # Consider deleting the files if it is empty and not written to
     # for more than X hours, with os.fstat() and the member st_mtime
 
     # Try several times in case the script event_get.py would read at the same time.
-    maxTry = 3
-    sleep_delay = 0.1
-    while maxTry > 0:
-        maxTry -= 1
+    max_try = 3
+    sleep_delay = 1
+    triples_list = []
+    while max_try > 0:
+        max_try -= 1
         try:
-            #sys.stderr.write("get_data_from_file about to open eventFilNam=%s.\n"%eventFilNam)
-            eventFd = open(eventFilNam,"r+")
-            #sys.stderr.write("get_data_from_file opened eventFilNam=%s.\n"%eventFilNam)
-            # This must be as fast as possible, so event_put is not blocked.
-            for lineJson in eventFd.readlines():
-                #sys.stderr.write("get_data_from_file lineJson=%s.\n"%lineJson)
-                jsonTriple = json.loads(lineJson)
-                # Now build Survol links which can be transformed in to valid RDF triples.
-                rdfTriple = TripleJsonToRdf(jsonTriple)
-                yield rdfTriple
+            with open(event_filename, "r+") as event_filedes:
+                # This must be as fast as possible, so event_put is not blocked.
+                for line_json in event_filedes.readlines():
+                    #sys.stderr.write("_get_events_from_file lineJson=%s.\n"%lineJson)
+                    json_triple = json.loads(line_json)
+                    # Now build Survol links which can be transformed in to valid RDF triples.
+                    rdf_triple = _triple_json_to_rdf(json_triple)
+                    triples_list.append(rdf_triple)
 
-            eventFd.seek(0)
-            # TODO: BEWARE: WHY SHOULD WE DELETE OBJECTS IN THE GENERAL CASE ?
-            # TODO: OR RATHER, THE INTERFACE SHOULD CHOOSE TO KEEP OBJECTS UNTIL THEY ARE EXPLICITLY DELETED ?
-            eventFd.truncate()
-            eventFd.close()
+                event_filedes.seek(0)
+                # TODO: BEWARE: WHY SHOULD WE DELETE OBJECTS IN THE GENERAL CASE ?
+                # TODO: OR RATHER, THE INTERFACE SHOULD CHOOSE TO KEEP OBJECTS UNTIL THEY ARE EXPLICITLY DELETED ?
+                event_filedes.truncate()
+                event_filedes.close()
             break
-        except:
+        except Exception as exc:
+            sys.stderr.write("_get_events_from_file failed event_filename=%s%s\n" % (event_filename, exc))
             # File locked or does not exist.
             time.sleep(sleep_delay)
             sleep_delay *= 2
 
-    if maxTry == 0:
-        DEBUG("get_data_from_file eventFilNam=%s No data.",eventFilNam)
+    # sys.stderr.write("_get_events_from_file num triples=%d\n" % len(triples_list))
+    return triples_list
 
-def data_retrieve(entity_type,entity_ids_arr):
-    DEBUG("data_retrieve entity_type=%s",entity_type)
 
+def _retrieve_events_by_entity(entity_type, entity_ids_arr):
+    DEBUG("_retrieve_events_by_entity entity_type=%s",entity_type)
     arrOnto = lib_util.OntologyClassKeys(entity_type)
 
     # Properties are in the right order.
     entity_ids_dict = dict(zip(arrOnto, entity_ids_arr))
 
-    eventFilNam = EntityTypeIdsToEventFile(entity_type,entity_ids_dict)
+    eventFilNam = _entity_type_ids_to_event_file(entity_type,entity_ids_dict)
 
-    DEBUG("data_retrieve eventFilNam=%s",eventFilNam)
-    arrTriples = get_data_from_file(eventFilNam)
+    DEBUG("_retrieve_events_by_entity eventFilNam=%s",eventFilNam)
+    arrTriples = _get_events_from_file(eventFilNam)
 
-    DEBUG("data_retrieve NumTriples=%d",len(arrTriples))
+    DEBUG("_retrieve_events_by_entity NumTriples=%d",len(arrTriples))
     return arrTriples
 
-# TODO: Events might appear in two objects.
-def data_retrieve_all():
-    DEBUG("data_retrieve_all events_directory=%s",events_directory)
 
+# TODO: The sane event might appear in two objects.
+def retrieve_all_events():
+    DEBUG("retrieve_all_events events_directory=%s", events_directory)
+
+    triples_list = []
     for dirpath, dnames, fnames in os.walk(events_directory):
-        for filNam in fnames:
-            #sys.stderr.write("data_retrieve_all filNam=%s\n"%filNam)
-            if filNam.endswith(events_file_extension):
-                pathNam = dirpath + "/" + filNam
-                #sys.stderr.write("data_retrieve_all pathNam=%s\n"%pathNam)
-                arrTriples = get_data_from_file(pathNam)
-                for oneTripl in arrTriples:
-                    yield oneTripl
-    DEBUG("data_retrieve_all leaving")
+        for events_filename in fnames:
+            #sys.stderr.write("retrieve_all_events filNam=%s\n"%filNam)
+            if events_filename.endswith(events_file_extension):
+                events_pathname = dirpath + "/" + events_filename
+                #sys.stderr.write("retrieve_all_events pathNam=%s\n"%pathNam)
+                triples_sublist = _get_events_from_file(events_pathname)
+                triples_list.extend(triples_sublist)
+    DEBUG("retrieve_all_events leaving with %d triples." % len(triples_list))
+    return triples_list
+
 
 def json_triples_to_rdf(json_triples, rdf_file_path):
     rdflib_graph = lib_kbase.MakeGraph()
     for tripl in json_triples:
-        rdf_triple = TripleJsonToRdf(tripl)
+        rdf_triple = _triple_json_to_rdf(tripl)
         rdflib_graph.add(rdf_triple)
     rdflib_graph.serialize(destination = rdf_file_path, format='pretty-xml')
 
