59150ffcdecb3c54cd865d2a011261add10c9a48
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-05-10 15:22:40 +0100

    One more test.

--- tests/test_lib_event.py
+++ tests/test_lib_event.py
@@ -36,6 +36,19 @@ class DockitComponentsTest(unittest.TestCase):
             print(one_test_pair[0], "=>", actual_filename, "/", one_test_pair[1])
             self.assertTrue(actual_filename == one_test_pair[1])
 
+    def test_moniker_to_event_filename(self):
+        test_data_set = [
+            ({"entity_type":"CIM_Process", "Handle":123},
+             "CIM_Process/CIM_Process.Handle=123"),
+            ({"entity_type": "CIM_DataFile", "Name": "C:/Windows/Explorer.exe"},
+             "CIM_DataFile/CIM_DataFile.Name=C_Windows_Explorer.exe"),
+        ]
+        for json_moniker, expected_basename in test_data_set:
+            expected_pathname = lib_event.events_directory + expected_basename + ".events"
+            actual_pathname = lib_event._moniker_to_event_filename(json_moniker)
+            print(actual_pathname, "=>", expected_pathname, "/", json_moniker)
+            self.assertTrue(actual_pathname == expected_pathname)
+
 
 if __name__ == '__main__':
     unittest.main()
