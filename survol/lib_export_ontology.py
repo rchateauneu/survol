9954979122f131e5c635a66c846ba868813b15fb
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-05-21 13:44:21 +0100

    Refactoring and cleanup.

--- survol/lib_export_ontology.py
+++ survol/lib_export_ontology.py
@@ -9,6 +9,7 @@ from lib_properties import pc
 
 ################################################################################
 
+
 # This dumps the triplestore graph to the current output socket if called
 # in a HTTP server.
 # Otherwise it saves the result to a text file, for testing or debugging.
@@ -31,6 +32,7 @@ def FlushOrSaveRdfGraph(grph, output_rdf_filename):
 
 ################################################################################
 
+
 # This receives a triplestore containing only the information from scripts.
 # This adds the classes and the properties information,
 # in order to send it to an external database or system.
@@ -46,36 +48,23 @@ def AddOntology(old_grph):
 
     # This takes the class from an Url and defines it in the RDF ontology.
     # This returns the class name as a string.
-    def DefineClassInOntology(urlNode):
-        (entity_label, class_name, entity_id) = lib_naming.ParseEntityUri( urlNode )
+    def _define_class_in_ontology(url_node):
+        (entity_label, class_name, entity_id) = lib_naming.ParseEntityUri(url_node)
 
-        # This could be the triplet: ("http://rchateau-hp", "http://primhillcomputers.com/survol/____Information", "HTTP url")
+        # This could be: ("http://the_host", "http://primhillcomputers.com/survol/____Information", "HTTP url")
         if not class_name:
             return None
 
         # TODO: Define base classes with rdfs:subClassOf / RDFS.subClassOf
         # "base_class" and "class_description' ???
 
-        # Pour les ontologi'ed, a-t-on les memes proprietes pour WMI, Survol et WBEM ?
-        # Et sinon, on les fait deriver les unes-des-autres ?
-        # "wmi:Handle" et "survol:Handle" sont la meme chose pour RDF ?
-        # "wmi:CIM_Process" et "survol:CIM_Process" ?
-        # Le fait que RDF puisse exprimer que "Win32_CIM_Process" derive de "CIM_Process" ... L'utiliser.
-        # Comment exprimer dans RDF qu'on veut aller chercher des infos dans le namespace (et donc le serveur)
-        # wmi ou survol, mais que ca renvoie des objects de la meme ontologie ??
-
-
-
         # A class name with the WMI namespace might be produced with this kind of URL:
-
-
-
         # "http://www.primhillcomputers.com/survol#root\CIMV2:CIM_Process"
         class_name = class_name.replace("\\","%5C")
 
         if not class_name in map_classes:
             if class_name == "":
-                raise Exception("No class name for url=%s type=%s"%(str(urlNode),str(type(urlNode))))
+                raise Exception("No class name for url=%s type=%s" % (str(url_node), str(type(url_node))))
 
             # Maybe this CIM class is not defined as an RDFS class.
             # This function might also filter duplicate and redundant insertions.
@@ -84,69 +73,69 @@ def AddOntology(old_grph):
         # The entity_id is a concatenation of CIM properties and define an unique object.
         # They are different of the triples, but might overlap.
         entity_id_dict = lib_util.SplitMoniker(entity_id)
-        for keyPred in entity_id_dict:
-            if not keyPred in map_attributes:
+        for predicate_key in entity_id_dict:
+            if not predicate_key in map_attributes:
                 # This function might also filter a duplicate and redundant insertion.
-                lib_util.AppendPropertySurvolOntology(keyPred, "CIM key predicate %s" % keyPred, class_name, None, map_attributes)
+                lib_util.AppendPropertySurvolOntology(predicate_key, "CIM key predicate %s" % predicate_key, class_name, None, map_attributes)
 
             # This value is explicitly added to the node.
-            valPred = entity_id_dict[keyPred]
-            new_grph.add((urlNode, lib_properties.MakeProp(keyPred), lib_kbase.MakeNodeLiteral(valPred)))
+            predicate_value = entity_id_dict[predicate_key]
+            new_grph.add((url_node, lib_properties.MakeProp(predicate_key), lib_kbase.MakeNodeLiteral(predicate_value)))
 
         # This adds a triple specifying that this node belongs to this RDFS class.
-        lib_kbase.AddNodeToRdfsClass(new_grph, urlNode, class_name, entity_label)
+        lib_kbase.AddNodeToRdfsClass(new_grph, url_node, class_name, entity_label)
 
         return class_name
 
     # This is needed for GraphDB which does not accept spaces and backslashes in URL.
-    def CleanupUrl(nodeUrl):
-        strUrl = str(nodeUrl)
-        strUrl = strUrl.replace(" ","%20")
-        strUrl = strUrl.replace("\\","%5C")
-        strUrl = strUrl.replace("[","%5B")
-        strUrl = strUrl.replace("]","%5D")
-        strUrl = strUrl.replace("{","%7B")
-        strUrl = strUrl.replace("}","%7D")
-        if lib_kbase.IsLiteral(nodeUrl):
-            nodeUrl = lib_kbase.MakeNodeLiteral(strUrl)
+    def _url_cleanup(url_node):
+        url_as_str = str(url_node)
+        url_as_str = url_as_str.replace(" ","%20")
+        url_as_str = url_as_str.replace("\\","%5C")
+        url_as_str = url_as_str.replace("[","%5B")
+        url_as_str = url_as_str.replace("]","%5D")
+        url_as_str = url_as_str.replace("{","%7B")
+        url_as_str = url_as_str.replace("}","%7D")
+        if lib_kbase.IsLiteral(url_node):
+            url_node = lib_kbase.MakeNodeLiteral(url_as_str)
         else:
-            nodeUrl = lib_kbase.MakeNodeUrl(strUrl)
-        return nodeUrl
+            url_node = lib_kbase.MakeNodeUrl(url_as_str)
+        return url_node
 
-    for nodeSubject, nodePredicate, nodeObject in old_grph:
-        nodeSubject = CleanupUrl(nodeSubject)
-        nodeObject = CleanupUrl(nodeObject)
-        if nodePredicate == pc.property_script:
+    for node_subject, node_predicate, node_object in old_grph:
+        node_subject = _url_cleanup(node_subject)
+        node_object = _url_cleanup(node_object)
+        if node_predicate == pc.property_script:
             # The subject might be a literal directory containing provider script files.
-            if not lib_kbase.IsLiteral(nodeSubject):
-                if lib_kbase.IsLiteral(nodeObject):
-                    new_grph.add( (nodeSubject, lib_kbase.PredicateSeeAlso, nodeObject))
+            if not lib_kbase.IsLiteral(node_subject):
+                if lib_kbase.IsLiteral(node_object):
+                    new_grph.add( (node_subject, lib_kbase.PredicateSeeAlso, node_object))
                 else:
-                    strObject = str(nodeObject)
-                    strObjectRdf = strObject + "&mode=rdf"
-                    nodeObjectRdf = lib_kbase.MakeNodeUrl(strObjectRdf)
-                    new_grph.add( (nodeSubject, lib_kbase.PredicateSeeAlso, nodeObjectRdf))
-        elif nodePredicate == pc.property_information:
-            new_grph.add( (nodeSubject, lib_kbase.PredicateComment, nodeObject))
+                    str_object = str(node_object)
+                    str_object_rdf = str_object + "&mode=rdf"
+                    node_object_rdf = lib_kbase.MakeNodeUrl(str_object_rdf)
+                    new_grph.add( (node_subject, lib_kbase.PredicateSeeAlso, node_object_rdf))
+        elif node_predicate == pc.property_information:
+            new_grph.add( (node_subject, lib_kbase.PredicateComment, node_object))
         else:
-            classSubject = DefineClassInOntology(nodeSubject)
-            if not lib_kbase.IsLiteral(nodeObject):
-                classObject = DefineClassInOntology(nodeObject)
+            class_subject = _define_class_in_ontology(node_subject)
+            if not lib_kbase.IsLiteral(node_object):
+                class_object = _define_class_in_ontology(node_object)
             else:
-                classObject = None
+                class_object = None
 
-            namePredicate, dictPredicate = lib_exports.PropToShortPropNamAndDict(nodePredicate)
+            name_predicate, dict_predicate = lib_exports.PropToShortPropNamAndDict(node_predicate)
             try:
-                descriptionPredicate = dictPredicate["property_description"]
+                description_predicate = dict_predicate["property_description"]
             except:
-                descriptionPredicate = ""
+                description_predicate = ""
 
-            if classSubject and (namePredicate not in map_attributes):
+            if class_subject and (name_predicate not in map_attributes):
                 # This function might also filter a duplicate and redundant insertion.
-                lib_util.AppendPropertySurvolOntology(namePredicate, descriptionPredicate, classSubject, classObject, map_attributes)
+                lib_util.AppendPropertySurvolOntology(name_predicate, description_predicate, class_subject, class_object, map_attributes)
 
                 # TODO: Add the property type. Experimental because we know the class of the object, or if this is a literal.
-            new_grph.add((nodeSubject, nodePredicate, nodeObject))
+            new_grph.add((node_subject, node_predicate, node_object))
 
     lib_kbase.CreateRdfsOntology(map_classes, map_attributes, new_grph)
     DEBUG("AddOntology len(grph)=%d map_classes=%d map_attributes=%d len(new_grph)=%d",
@@ -162,11 +151,10 @@ def Grph2Rdf(grph):
 
     new_grph = AddOntology(grph)
 
-    # Neither "xml/rdf" nor "text/rdf" are correct MIME-types. It should be "application/xml+rdf" or possibly "application/xml" or "text/xml"
-
-    #lib_util.WrtHeader('text/rdf') # OK with Protege
-    #lib_util.WrtHeader('xml/rdf') # OK with Protege
-    #lib_util.WrtHeader('application/xml+rdf') # Creates a file.
+    # Neither "xml/rdf" nor "text/rdf" are correct MIME-types.
+    # It should be "application/xml+rdf" or possibly "application/xml" or "text/xml"
+    # 'text/rdf' and 'xml/rdf' are OK with Protege
+    # 'application/xml+rdf' creates a file.
     lib_util.WrtHeader('application/xml')
 
     out_dest = lib_util.get_default_output_destination()
