7ec73aaef2c9fea0db12beca84adcdf08f37905b
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-06-14 13:11:50 +0100

    More SVG tests.

--- tests/test_graphviz.py
+++ tests/test_graphviz.py
@@ -13,9 +13,22 @@ from survol.scripts import dockit
 
 
 class SvgCommon(unittest.TestCase):
+    def _check_script(self, script_suffix):
+        full_url = self._agent_url + script_suffix
+        if full_url.find("?") >= 0:
+            full_url += "&mode=svg"
+        else:
+            full_url += "?mode=svg"
+        print("full_url=", full_url)
+        svg_url_response = portable_urlopen(full_url, timeout=5)
+        svg_content = svg_url_response.read()  # Py3:bytes, Py2:str
+        return svg_content
+
     # This tests the content of the main page of Survol.
-    # The content is not accurately check, just that it is a XML document with some strings
-    def _check_svg_main_page(self, svg_content):
+    # The content is not accurately checked, just that it is a SVG XML document with some strings
+    def _test_svg_main(self):
+        svg_content = self._check_script("/survol/entity.py")
+
         dom = minidom.parseString(svg_content)
 
         text_content = set([text_tag.childNodes[0].nodeValue for text_tag in dom.getElementsByTagName('text')])
@@ -26,11 +39,18 @@ class SvgCommon(unittest.TestCase):
         self.assertTrue('Processes tree' in text_content)
         self.assertTrue('System-wide open files' in text_content)
 
+    def _test_svg_linux_user_group(self):
+        self._check_script("/survol/sources_types/LMI_Group/linux_user_group.py?xid=LMI_Group.Name%3Dfedora")
 
-    def _check_page_entity(self, agent_url):
-        svg_url_response = portable_urlopen(agent_url + "/survol/entity.py?mode=svg", timeout=5)
-        svg_content = svg_url_response.read()  # Py3:bytes, Py2:str
-        self._check_svg_main_page(svg_content)
+    def _test_svg_user_processes(self):
+        self._check_script(
+            "/survol/sources_types/LMI_Account/user_processes.py?xid=LMI_Account.Name=root,Domain=%s"
+            % SurvolServerHostname)
+
+    def _test_svg_user_linux_id(self):
+        self._check_script(
+            "/survol/sources_types/LMI_Account/user_linux_id.py?xid=LMI_Account.Name=root,Domain=%s"
+            % SurvolServerHostname)
 
 
 def _is_dot_available():
@@ -51,23 +71,48 @@ class SvgLocalAgentTest(SvgCommon):
         print("AgentUrl=", self._agent_url)
 
     def tearDown(self):
+        # This stops the local agent, an HTTP server.
         stop_cgiserver(self._remote_graphviz_test_agent)
 
     def test_local_agent_svg(self):
-        # This starts a local server on the test machine.
-        self._check_page_entity(self._agent_url)
+        self._test_svg_main()
+
+    @unittest.skipIf(not is_platform_linux, "Linux only")
+    def test_local_svg_linux_user_group(self):
+        self._test_svg_linux_user_group()
+
+    @unittest.skipIf(not is_platform_linux, "Linux only")
+    def test_local_svg_user_processes(self):
+        self._test_svg_user_processes()
+
+    @unittest.skipIf(not is_platform_linux, "Linux only")
+    def test_local_svg_user_linux_id(self):
+        self._test_svg_user_linux_id()
 
 
-# Graphviz/dot must be installed on the test platform, for example Travis.
+# This targets Primhill Computers test machine.
 class SvgRemoteAgentTest(SvgCommon):
     """
     Test parsing of the SVG output.
     """
 
+    def setUp(self):
+        self._agent_url = SurvolServerAgent
+
     # Loads a remote SVG url and parses the SVG document to check the presence of important tags.
     # It relies on the public Primhill Computers server.
-    def test_remote_agent_svg(self):
-        self._check_page_entity(SurvolServerAgent)
+    def test_remote_svg_main(self):
+        self._test_svg_main()
+
+    def test_remote_svg_linux_user_group(self):
+        self._test_svg_linux_user_group()
+
+    def test_remote_svg_user_processes(self):
+        self._test_svg_user_processes()
+
+    def test_remote_svg_user_linux_id(self):
+        self._test_svg_user_linux_id()
+
 
 # TODO: Test loadbookmark
 
