251c97dfaf0b9e2518a4b7d2779f11e5e814c1e4
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-07-02 09:03:40 +0100

    Python 3 and code cleanup.

--- survol/sources_types/CIM_Process/memory_regex_search/scan_sql_queries.py
+++ survol/sources_types/CIM_Process/memory_regex_search/scan_sql_queries.py
@@ -24,7 +24,9 @@ from sources_types.CIM_Process import embedded_sql_query
 
 SlowScript = True
 
-# We get strange strings separated by "ZZZZ"
+
+# When scanning the memory of a running process,
+# it sometimes returns odd strings separated by "ZZZZ"* sequences.
 # "SELECT id FROM moz_favicons WHERE url = ZZZZZZZZSELECT id FROM moz_historyvisits vZZZZZZZZZZZZZZSELECT id FROM moz_historyvisits vZZZZZZZZZZ"
 # or also:
 # "SELECT f.id FROM moz_favicons f"
@@ -33,74 +35,74 @@ SlowScript = True
 # "sqlQry=SELECT b.id, b.guid from moz_bookmarks b WHERE b.id = ======ZZZZ"
 # The noise chars are apparently variable.
 #
-# And sometime we have several times the same because of string manipulation in the heap, I guess.
+# And the same string appears several times, possibly because of string manipulation in the heap,
+# and temporary variables.
+# TODO: This code be a good indication of useless object copies in memory.
 #
 # Beware of the side effect of scanning firefox memory which contains previous execution.
-def ProcessScannedSqlQuery(sqlQry, setQrys):
+def _process_scanned_sql_query(raw_sql_query, queries_set):
 	#sys.stderr.write("sqlQry=%s\n"%sqlQry)
 
-	allQrys = re.split("ZZZ*",sqlQry)
+	# Reasonably, at least three "Z": This is a rule-of-thumb.
+	all_qrys = re.split(b"ZZZ*", raw_sql_query)
 
-	for oneQry in allQrys:
-		oneQry.strip()
-		#sys.stderr.write("       oneQry=%s\n"%oneQry)
+	for one_qry in all_qrys:
+		one_qry.strip()
+		#sys.stderr.write("       one_qry=%s\n"%one_qry)
 		# Remove the last chars if they are identical and repeated several times.
-		lenQry = len(oneQry)
-		if lenQry < 10: # Too short to be a SQL query
+		len_qry = len(one_qry)
+		if len_qry < 10: # Too short to be a SQL query
 			continue
 
-		setQrys.add( oneQry )
+		queries_set.add(one_qry)
 
-def GenerateFromSqlQrys(grph, node_process, rgxProp, setQrys, pidint):
-	for oneQry in setQrys:
+
+def _generate_from_sql_queries(grph, node_process, regex_predicate, queries_set, pid_as_integer):
+	for one_qry in queries_set:
 		try:
 			# TODO: The query must come with the PID, so later we can find which database connection it is.
-			# nodeSqlQuery = sql_query.MakeUri(oneQry)
-			nodeSqlQuery = embedded_sql_query.MakeUri(oneQry,pidint)
-			grph.add( ( node_process, rgxProp, nodeSqlQuery ) )
+			node_sql_query = embedded_sql_query.MakeUri(one_qry, pid_as_integer)
+			grph.add((node_process, regex_predicate, node_sql_query))
+		except Exception as exc:
+			grph.add((node_process, regex_predicate, lib_common.NodeLiteral("_generate_from_sql_queries:" + str(exc))))
 
-		except Exception:
-			exc = sys.exc_info()[1]
-			grph.add( ( node_process, rgxProp, lib_common.NodeLiteral("GenerateFromSqlQrys:"+str(exc)) ) )
 
 # TODO: What is annoying is that it is in a sub-directory, but it does not have
 # TODO: a specific ontology etc ...
 def Main():
 	cgiEnv = lib_common.CgiEnv()
-	pidint = int( cgiEnv.GetId() )
+	pid_as_integer = int(cgiEnv.GetId())
 
 	grph = cgiEnv.GetGraph()
 
-	node_process = lib_common.gUriGen.PidUri(pidint)
+	node_process = lib_common.gUriGen.PidUri(pid_as_integer)
 
-	dictRegexSQL = lib_sql.SqlRegularExpressions()
+	dict_regex_sql = lib_sql.SqlRegularExpressions()
 
-	arrProps = []
+	arr_props = []
 
 	# TODO: Unfortunately it scans several times the memory process.
-	for rgxKey in dictRegexSQL:
-		rgxSQL = dictRegexSQL[rgxKey]
-		rgxProp = lib_common.MakeProp(rgxKey)
-		arrProps.append( rgxProp )
+	for rgx_key in dict_regex_sql:
+		rgx_sql = dict_regex_sql[rgx_key]
+		regex_predicate = lib_common.MakeProp(rgx_key)
+		arr_props.append(regex_predicate)
 
 		try:
 			# https://docs.python.org/3/library/re.html
 			# re.MULTILINE | re.ASCII | re.IGNORECASE
-			matchedSqls = memory_regex_search.GetRegexMatches(pidint,rgxSQL, re.IGNORECASE)
-		except Exception:
-			exc = sys.exc_info()[1]
-			lib_common.ErrorMessageHtml("Error:%s. Protection ?"%str(exc))
+			matched_sqls = memory_regex_search.GetRegexMatches(pid_as_integer, rgx_sql, re.IGNORECASE)
+		except Exception as exc:
+			lib_common.ErrorMessageHtml("Error:%s. Protection ?" % str(exc))
 
-		setQrys = set()
+		all_queries_set = set()
 
-		for sqlIdx in matchedSqls:
-			sqlQry = matchedSqls[sqlIdx]
-			ProcessScannedSqlQuery( sqlQry, setQrys)
+		for sql_idx in matched_sqls:
+			sql_qry = matched_sqls[sql_idx]
+			_process_scanned_sql_query(sql_qry, all_queries_set)
 
-		GenerateFromSqlQrys(grph, node_process, rgxProp, setQrys, pidint)
+		_generate_from_sql_queries(grph, node_process, regex_predicate, all_queries_set, pid_as_integer)
 
-
-	cgiEnv.OutCgiRdf("LAYOUT_RECT",arrProps)
+	cgiEnv.OutCgiRdf("LAYOUT_RECT",arr_props)
 
 if __name__ == '__main__':
 	Main()
