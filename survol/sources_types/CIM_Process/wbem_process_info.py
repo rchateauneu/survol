f8356f499e87516473d1caf026526af5db83cd17
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-07-16 22:01:12 +0100

    Code cleanup.

--- survol/sources_types/CIM_Process/wbem_process_info.py
+++ survol/sources_types/CIM_Process/wbem_process_info.py
@@ -8,7 +8,6 @@ import sys
 import lib_util
 import lib_common
 import lib_wbem
-# from sources_types import CIM_Process
 from lib_properties import pc
 
 Usable = lib_util.UsableLinux
@@ -16,62 +15,57 @@ Usable = lib_util.UsableLinux
 CanProcessRemote = True
 
 def Main():
-	# TODO: can_process_remote should be suppressed because it duplicates CanProcessRemote
-	cgiEnv = lib_common.CgiEnv(can_process_remote = True)
-	pid = int( cgiEnv.GetId() )
-	machineName = cgiEnv.GetHost()
+    # TODO: can_process_remote should be suppressed because it duplicates CanProcessRemote
+    cgiEnv = lib_common.CgiEnv(can_process_remote=True)
+    pid = int(cgiEnv.GetId())
+    machine_name = cgiEnv.GetHost()
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	cimomUrl = lib_wbem.HostnameToWbemServer(machineName)
+    cimom_url = lib_wbem.HostnameToWbemServer(machine_name)
 
-	DEBUG("wbem_process_info.py currentHostname=%s pid=%d machineName=%s cimomUrl=%s",
-            lib_util.currentHostname, pid, machineName, cimomUrl)
+    DEBUG("wbem_process_info.py currentHostname=%s pid=%d machine_name=%s cimom_url=%s",
+          lib_util.currentHostname, pid, machine_name, cimom_url)
 
-	connWbem = lib_wbem.WbemConnection(cimomUrl)
+    conn_wbem = lib_wbem.WbemConnection(cimom_url)
 
-	nameSpace = "root/cimv2"
-	try:
-		instLists = connWbem.ExecQuery("WQL",'select * from CIM_Process  where Handle="%s"' % pid,nameSpace)
-		# lstProc = connWbem.EnumerateInstances(ClassName="PG_UnixProcess",namespace="root/cimv2",Handle=pid)
-	except:
-		lib_common.ErrorMessageHtml("Error:"+str(sys.exc_info()))
+    name_space = "root/cimv2"
+    try:
+        inst_lists = conn_wbem.ExecQuery("WQL", 'select * from CIM_Process  where Handle="%s"' % pid, name_space)
+    except:
+        lib_common.ErrorMessageHtml("Error:" + str(sys.exc_info()))
 
-	# This is taken from entity_wbem.py with much simplification.
-	numInsts = len(instLists)
+    class_name = "CIM_Process"
+    dict_props = {"Handle": pid}
 
-	className = "CIM_Process"
-	dictProps = { "Handle" : pid }
+    root_node = lib_util.EntityClassNode(class_name, name_space, cimom_url, "WBEM")
 
-	rootNode = lib_util.EntityClassNode( className, nameSpace, cimomUrl, "WBEM" )
+    # There should be only one object, hopefully.
+    for an_inst in inst_lists:
+        dict_inst = dict(an_inst)
 
-	# There should be only one object, hopefully.
-	for anInst in instLists:
-		dictInst = dict(anInst)
+        host_only = lib_util.EntHostToIp(cimom_url)
+        if lib_util.IsLocalAddress(host_only):
+            uri_inst = lib_common.gUriGen.UriMakeFromDict(class_name, dict_props)
+        else:
+            uri_inst = lib_common.RemoteBox(host_only).UriMakeFromDict(class_name, dict_props)
 
-		hostOnly = lib_util.EntHostToIp(cimomUrl)
-		if lib_util.IsLocalAddress(hostOnly):
-			uriInst = lib_common.gUriGen.UriMakeFromDict(className, dictProps)
-		else:
-			uriInst = lib_common.RemoteBox(hostOnly).UriMakeFromDict(className, dictProps)
+        grph.add((root_node, lib_common.MakeProp(class_name), uri_inst))
 
-		grph.add( ( rootNode, lib_common.MakeProp(className), uriInst ) )
+        url_namespace = lib_wbem.NamespaceUrl(name_space, cimom_url, class_name)
+        nod_namespace = lib_common.NodeUrl(url_namespace)
+        grph.add((root_node, pc.property_cim_subnamespace, nod_namespace))
 
-		urlNamespace = lib_wbem.NamespaceUrl( nameSpace, cimomUrl, className )
-		nodNamespace = lib_common.NodeUrl( urlNamespace )
-		grph.add( ( rootNode, pc.property_cim_subnamespace , nodNamespace ) )
+        # None properties are not printed.
+        for iname_key in dict_inst:
+            iname_val = dict_inst[iname_key]
+            # TODO: If this is a reference, create a Node !!!!!!!
+            if not iname_val is None:
+                grph.add((uri_inst, lib_common.MakeProp(iname_key), lib_common.NodeLiteral(iname_val)))
 
-		# None properties are not printed.
-		for inameKey in dictInst:
-			inameVal = dictInst[inameKey]
-			# TODO: If this is a reference, create a Node !!!!!!!
-			if not inameVal is None:
-				grph.add( ( uriInst, lib_common.MakeProp(inameKey), lib_common.NodeLiteral(inameVal) ) )
+        # TODO: Call the method Associators(). Idem References().
 
-		# TODO: Call the method Associators(). Idem References().
-
-
-	cgiEnv.OutCgiRdf()
+    cgiEnv.OutCgiRdf()
 
 if __name__ == '__main__':
-	Main()
+    Main()
