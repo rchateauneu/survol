2f269f298c1fa631db02bc6550133e2bce3745d7
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-07-16 10:44:12 +0100

    Cleanup.

--- survol/sources_types/class/__init__.py
+++ survol/sources_types/class/__init__.py
@@ -2,7 +2,6 @@
 Object class as defined in a software library.
 """
 
-import cgi
 import lib_util
 import sys
 
@@ -14,26 +13,15 @@ def EntityOntology():
 
 def EntityName(entity_ids_arr):
 	entity_id = entity_ids_arr[0]
-	# PROBLEME: Double &kt;&lt !!!
-	# return entity_id
 	try:
 		# Trailing padding.
 		# TODO: Encoding is done in lib_uris.ClassUri : The encoding should be more generic.
 		# TODO: ... and done only when the content is CGI-incompatible.
 		# TODO: Or do just like sources_types.sql.query.MakeUri
 		resu = lib_util.Base64Decode(entity_id)
-		resu = cgi.escape(resu)
+		resu = lib_util.html_escape(resu)
 		return resu
-	except TypeError:
-		exc = sys.exc_info()[1]
+	except TypeError as exc:
 		ERROR("CANNOT DECODE: class=(%s):%s",entity_id,str(exc))
 		return entity_id
 
-#def MakeUri(dsnName,tableNam, columnNam):
-#	return lib_common.gUriGen.UriMakeFromDict("odbc/column", { "Dsn" : lib_util.EncodeUri(dsnName), "Table" : tableNam, "Column": columnNam })
-#
-#def AddInfo(grph,node,entity_ids_arr):
-#	dsnNam = entity_ids_arr[0]
-#	tabNam = entity_ids_arr[0]
-#	nodeTable = odbc_table.MakeUri(dsnNam,tabNam)
-#	grph.add( ( nodeTable, lib_common.MakeProp("ODBC table"), node ) )
