cbbb3753a17e9bb0321ee13b4d66832a1ebe95b6
Primhill Computers <remi.chateauneu@primhillcomputers.com>
2020-07-19 14:49:46 +0100

    twisted: Some progress.

--- survol/scripts/cgitwisted.py
+++ survol/scripts/cgitwisted.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
 # The intention is to implement another server using the Python module twisted.
 # The benefit is that it is probably faster and more powerful that the builtins class.
 # This uses CGI scripts as they are, just like Apache or cgiserver.py
 #
 # https://jcalderone.livejournal.com/tag/sixty%20seconds
+
+raise Exception("Not implemented yet")
 
 # Possible implementation: Iterate on all CGI scripts:
 from twisted.web.twcgi import CGIScript
