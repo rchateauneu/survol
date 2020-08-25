83d4205c5a8ac4878e65a47981c2a2f26fe183de
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-08-25 18:33:01 +0100

    Renaming scripts.

---
+++ survol/sources_types/Linux/events_generator_iostat_all_disks.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import lib_common
+import lib_util
+import os
+import re
+from lib_properties import pc
+import lib_properties
+
+
+# import lib_webserv
+# import lib_tabular
+
+def Usable(entity_type, entity_ids_arr):
+    """Runs on Linux only, in asynchronous mode"""
+    return lib_util.UsableLinux(entity_type, entity_ids_arr) and lib_util.UsableAsynchronousSource(entity_type,
+                                                                                                   entity_ids_arr)
+
+IsEventsGenerator = True
+
+################################################################################
+
+# Device:            tps   Blk_read/s   Blk_wrtn/s   Blk_read   Blk_wrtn
+# sda               3,00         0,00        48,00          0         48
+# sdb               0,00         0,00         0,00          0          0
+
+
+
+# This runs in the HTTP server and uses the data from the queue.
+# This reads a record from the queue and builds a RDF relation with it.
+def _io_stat_to_graph(grph, spl, iostat_header):
+    device_node = lib_common.gUriGen.DiskUri(spl[0])
+    property_name_to_node = dict()
+
+    for idx in range(1, len(spl)):
+        # No idea why doubles are written with a comma. Maybe the locale?
+        qty = float(spl[idx].replace(",", "."))
+
+        property_name = iostat_header[idx]
+        property_node = property_name_to_node.get(property_name, lib_properties.MakeProp(property_name))
+        grph.add((device_node, property_node, lib_common.NodeLiteral(qty)))
+
+
+# This runs tcpdump, parses output data from it.
+def Main(loop_number=1):
+    # TODO: The delay could be a parameter.
+    iostat_cmd = "iostat -d 1"
+
+    # Contains the last header read.
+    iostat_header = []
+
+    for lin in os.popen(iostat_cmd):
+        if not lin:
+            continue
+
+        # We transfer also the header.
+        spl = re.split(' +', lin)
+
+        if spl[0] == 'Device:':
+            iostat_header = spl
+            return
+
+        cgiEnv = lib_common.CgiEnv()
+        grph = cgiEnv.GetGraph()
+
+        _io_stat_to_graph(grph, spl, iostat_header)
+
+        cgiEnv.OutCgiRdf()
+
+        loop_number -= 1
+        if loop_number == 0:
+            break
+
+
+if __name__ == '__main__':
+    Main()
+elif __name__ == '__daemon__':
+    while True:
+        Main(1000000)
---
+++ survol/sources_types/events_generator_tcpdump.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""
+tcpdump and windump
+"""
+
+# It works also for WindDump.exe, on Windows.
+
+import os
+import re
+import sys
+import time
+import lib_util
+import lib_common
+from lib_properties import pc
+
+#import lib_webserv
+
+IsEventsGenerator = True
+
+# This parses the output of "tcpdump" or "WinDump.exe" if on Windows.
+def _parse_tcpdump_line(grph, line):
+    spl = line.split(' ')
+
+    # 22:39:56.713245 IP BThomehub.home.domain > Unknown-00-18-e7-08-02-81.home.47676: 52407* 1/0/0 (87)
+    if spl[1] == 'IP':
+
+        addrRegex = r'(.*)\.([^.]*)'
+
+        # Maybe we should have a commutative relation?
+        lMatchAddr = re.match( addrRegex, spl[2], re.M|re.I)
+        if not lMatchAddr:
+            return
+        lsocketNode = lib_common.gUriGen.AddrUri( lMatchAddr.group(1), int(lMatchAddr.group(2)) )
+
+        rMatchAddr = re.match( addrRegex, spl[4][:-1], re.M|re.I)
+        if not rMatchAddr:
+            return
+        rsocketNode = lib_common.gUriGen.AddrUri( rMatchAddr.group(1), int(rMatchAddr.group(2)) )
+
+        grph.add( ( lsocketNode, pc.property_socket_end, rsocketNode ) )
+
+
+def _get_tcmp_dump_command():
+    if lib_util.isPlatformWindows:
+        # TODO: Should test if it works.
+        return "WinDump"
+    else:
+        # TODO: sudo is probably not appropriate.
+        # Option -n so no conversion of addresses and port numbers.
+        return "sudo tcpdump -n"
+
+
+def Main(loop_number=1):
+    tcpdump_cmd = _get_tcmp_dump_command()
+
+    for lin in os.popen(tcpdump_cmd):
+        if not lin:
+            continue
+        cgiEnv = lib_common.CgiEnv()
+        grph = cgiEnv.GetGraph()
+        _parse_tcpdump_line(grph, lin)
+        cgiEnv.OutCgiRdf()
+
+        loop_number -= 1
+        if loop_number == 0:
+            break
+
+################################################################################
+
+if __name__ == '__main__':
+    Main()
+elif __name__ == '__daemon__':
+    while True:
+        Main(1000000)
+        time.sleep(20)
