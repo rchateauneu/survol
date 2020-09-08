af11823dc776c8c1704b4f5bcd6082d621ea2a52
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-09-08 08:10:08 +0100

    More testing.

--- survol/scripts/cgitwisted.py
+++ survol/scripts/cgitwisted.py
@@ -10,10 +10,12 @@ if __package__:
     from . import daemon_factory
 else:
     import daemon_factory
-daemon_factory.supervisor_startup()
 
 raise Exception("Not implemented yet")
 
 # Possible implementation: Iterate on all CGI scripts:
 from twisted.web.twcgi import CGIScript
 resource = CGIScript("/survol/entity.py")
+
+def start_server():
+    daemon_factory.supervisor_startup()
