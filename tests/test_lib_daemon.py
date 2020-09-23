1776b24c93f6164cf6a120f7823e6cdeb3e95f57
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-09-23 08:42:14 +0100

    Disable test_start_events_generator_daemon on Windows and Travis because it is hanging.

--- tests/test_lib_daemon.py
+++ tests/test_lib_daemon.py
@@ -12,7 +12,6 @@ from init import *
 update_test_path()
 
 # This is used by CGI scripts only.
-import lib_util
 import lib_daemon
 import lib_common
 
@@ -33,7 +32,6 @@ def tearDownModule():
     daemon_factory.supervisor_stop()
 
 
-# @unittest.skipIf(is_travis_machine(), "TEMPORARY DISABLED")
 class CgiScriptTest(unittest.TestCase):
     _dummy_url_prefix = "http://any.machine/any_directory/"
 
@@ -51,6 +49,7 @@ class CgiScriptTest(unittest.TestCase):
         status_running = lib_daemon.is_events_generator_daemon_running(test_url)
         self.assertTrue(not status_running)
 
+    @unittest.skipIf(is_platform_windows and is_travis_machine(), "TEMPORARY DISABLED")
     def test_start_events_generator_daemon(self):
         # http://vps516494.ovh.net/Survol/survol/sources_types/enumerate_CIM_Process.py?xid=.
         test_url = self._dummy_url_prefix + "/survol/sources_types/events_generator_one_tick_per_second.py?parama=123&paramb=START"
