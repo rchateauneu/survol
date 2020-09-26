1b587ba93dbfe49a056074f9e1dfedebb83acef8
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-09-26 13:29:13 +0100

    Enhanced installer script.

--- survol_installer.bat
+++ survol_installer.bat
@@ -1,3 +1,17 @@
-@REM https://cyrille.rossant.net/create-a-standalone-windows-installer-for-your-python-application/
-
+1>2# : ^
+'''
 @REM Create an installer in wxwidget for Apache, IIS etc.
+
+py -3.6 -m venv %TMP%\survol-env
+@REM py -2.7 -m virtualenv %TMP%\survol-env
+
+%TMP%\survol-env\Scripts\python --version
+%TMP%\survol-env\Scripts\pip --version
+%TMP%\survol-env\Scripts\pip install survol
+%TMP%\survol-env\Scripts\python -c "import survol;print('v=',survol.__version__);import survol.scripts.cgiserver;print('f=',survol.scripts.cgiserver.__file__)"
+
+pushd %TMP%\survol-env\Lib\site-packages
+%TMP%\survol-env\Scripts\python %TMP%\survol-env\Lib\site-packages\survol\scripts\cgiserver.py -b webbrowser
+popd
+
+exit /b
