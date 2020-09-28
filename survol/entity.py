d77f3d488270feac641f8c2fd8f68674bd926878
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-09-28 11:29:52 +0100

    Code cleanup.

--- survol/entity.py
+++ survol/entity.py
@@ -29,32 +29,34 @@ FunctionGetUser = CIM_Process.GetCurrentUser
 
 from sources_types import CIM_ComputerSystem
 
-################################################################################
 
-def AddDefaultNodes(grph,rootNode,entity_host):
-    DEBUG("entity.py AddDefaultNodes entity_host=%s",entity_host)
-    currentNodeHostname = lib_common.gUriGen.HostnameUri( lib_util.currentHostname )
-    grph.add( ( currentNodeHostname, pc.property_information, lib_common.NodeLiteral("Current host:"+lib_util.currentHostname) ) )
-    grph.add( ( rootNode, pc.property_rdf_data_nolist2, currentNodeHostname ) )
+def _add_default_nodes(grph, root_node, entity_host):
+    DEBUG("entity.py _add_default_nodes entity_host=%s", entity_host)
+    currentNodeHostname = lib_common.gUriGen.HostnameUri(lib_util.currentHostname)
+    grph.add((currentNodeHostname,
+              pc.property_information,
+              lib_common.NodeLiteral("Current host:" + lib_util.currentHostname)))
+    grph.add((root_node, pc.property_rdf_data_nolist2, currentNodeHostname))
 
-    currUsername = FunctionGetUser()
-    currentNodeUser = lib_common.gUriGen.UserUri( currUsername )
-    grph.add( ( currentNodeUser, pc.property_information, lib_common.NodeLiteral("Current user:"+currUsername) ) )
-    grph.add( ( rootNode, pc.property_rdf_data_nolist2, currentNodeUser ) )
+    curr_username = FunctionGetUser()
+    current_node_user = lib_common.gUriGen.UserUri(curr_username)
+    grph.add((current_node_user, pc.property_information, lib_common.NodeLiteral("Current user:" + curr_username)))
+    grph.add((root_node, pc.property_rdf_data_nolist2, current_node_user))
+
 
 # TODO: Maybe the property should be property_script ??
-def AddDefaultScripts(grph,rootNode,entity_host):
-    DEBUG("entity.py AddDefaultScripts entity_host=%s",entity_host)
-    nodeObjTypes = lib_common.NodeUrl( lib_util.uriRoot + '/objtypes.py' )
-    grph.add( ( rootNode, pc.property_rdf_data_nolist2, nodeObjTypes ) )
+def _add_default_scripts(grph, root_node, entity_host):
+    DEBUG("entity.py _add_default_scripts entity_host=%s",entity_host)
+    node_obj_types = lib_common.NodeUrl(lib_util.uriRoot + '/objtypes.py')
+    grph.add((root_node, pc.property_rdf_data_nolist2, node_obj_types))
 
     # Gives a general access to WBEM servers. In fact we might iterate on several servers, or none.
-    nodePortalWbem = lib_util.UrlPortalWbem(entity_host)
-    grph.add( ( rootNode, pc.property_rdf_data_nolist2, nodePortalWbem ) )
+    node_portal_wbem = lib_util.UrlPortalWbem(entity_host)
+    grph.add((root_node, pc.property_rdf_data_nolist2, node_portal_wbem))
 
     # Gives a general access to WMI servers.
-    nodePortalWmi = lib_util.UrlPortalWmi(entity_host)
-    grph.add( ( rootNode, pc.property_rdf_data_nolist2, nodePortalWmi ) )
+    node_portal_wmi = lib_util.UrlPortalWmi(entity_host)
+    grph.add((root_node, pc.property_rdf_data_nolist2, node_portal_wmi))
 
 
 ################################################################################
@@ -76,22 +78,21 @@ def Main():
 
     # This can process remote hosts because it does not call any script, just shows them.
     cgiEnv = lib_common.CgiEnv(
-                    can_process_remote = True,
-                    parameters = { lib_util.paramkeyShowAll : False })
+                    can_process_remote=True,
+                    parameters={lib_util.paramkeyShowAll: False})
     entity_id = cgiEnv.m_entity_id
     entity_host = cgiEnv.GetHost()
-    DEBUG("entity_host=%s",entity_host)
-    flagShowAll = int(cgiEnv.get_parameters( lib_util.paramkeyShowAll ))
+    DEBUG("entity_host=%s", entity_host)
+    flagShowAll = int(cgiEnv.get_parameters(lib_util.paramkeyShowAll))
 
-    nameSpace, entity_type = cgiEnv.get_namespace_type()
+    name_space, entity_type = cgiEnv.get_namespace_type()
 
     grph = cgiEnv.GetGraph()
 
     rootNode = lib_util.RootUri()
-    DEBUG("rootNode=%s",rootNode)
+    DEBUG("rootNode=%s", rootNode)
 
-    entity_ids_arr = lib_util.EntityIdToArray( entity_type, entity_id )
-    # entity_info_only.AddInformation(grph,rootNode,entity_id, entity_type)
+    entity_ids_arr = lib_util.EntityIdToArray(entity_type, entity_id)
 
     # Each entity type ("process","file" etc... ) can have a small library
     # of its own, for displaying a rdf node of this type.
@@ -99,47 +100,47 @@ def Main():
         entity_module = lib_util.GetEntityModule(entity_type)
         if entity_module:
             try:
-                entity_module.AddInfo( grph, rootNode, entity_ids_arr )
+                entity_module.AddInfo(grph, rootNode, entity_ids_arr)
             except AttributeError as exc:
-                INFO("entity.py No AddInfo for %s %s: %s", entity_type, entity_id, str(exc) )
+                INFO("entity.py No AddInfo for %s %s: %s", entity_type, entity_id, str(exc))
             except Exception as exc:
-                INFO("entity.py Unexpected exception for %s %s: %s", entity_type, entity_id, str(exc) )
+                INFO("entity.py Unexpected exception for %s %s: %s", entity_type, entity_id, str(exc))
     else:
-        INFO("No lib_entities for %s %s", entity_type, entity_id )
+        INFO("No lib_entities for %s %s", entity_type, entity_id)
 
     # When displaying in json mode, the scripts are shown with a contextual menu, not with D3 modes..
-    if lib_util.GuessDisplayMode() not in ["json","html"]:
+    if lib_util.GuessDisplayMode() not in ["json", "html"]:
 
         # This function is called for each script which applies to the given entity.
         # It receives a triplet: (subject,property,object) and the depth in the tree.
         # Here, this simply stores the scripts in a graph. The depth is not used yet,
         # but can help debugging.
-        def CallbackGrphAdd( tripl, depthCall ):
+        def callback_grph_add(tripl, depthCall):
             try:
                 grph.add(tripl)
             except Exception as exc:
-                ERROR("CallbackGrphAdd: tripl=%s exception=%s" % (str(tripl), str(exc)))
+                ERROR("callback_grph_add: tripl=%s exception=%s" % (str(tripl), str(exc)))
                 raise
 
         try:
-            entity_dirmenu_only.DirToMenu(CallbackGrphAdd,rootNode,entity_type,entity_id,entity_host,flagShowAll)
+            entity_dirmenu_only.DirToMenu(callback_grph_add, rootNode, entity_type, entity_id, entity_host, flagShowAll)
         except Exception as exc:
             ERROR("entity.py caught in ForToMenu:%s", exc)
 
         # This adds WBEM and WMI urls related to the current object.
         if entity_type != "":
-            CIM_ComputerSystem.AddWbemWmiServers(grph,rootNode, entity_host, nameSpace, entity_type, entity_id)
+            CIM_ComputerSystem.AddWbemWmiServers(grph, rootNode, entity_host, name_space, entity_type, entity_id)
 
-        AddDefaultScripts(grph,rootNode,entity_host)
+        _add_default_scripts(grph, rootNode, entity_host)
 
         # Special case if the currententity we are displaying, is a machine,
         # we might as well try to connect to its WMI or WBEM server, running on this machine.
         if entity_type == "CIM_ComputerSystem":
-            AddDefaultScripts(grph,rootNode,entity_id)
+            _add_default_scripts(grph, rootNode, entity_id)
 
-    AddDefaultNodes(grph,rootNode,entity_host)
+    _add_default_nodes(grph, rootNode, entity_host)
 
-    cgiEnv.OutCgiRdf( "LAYOUT_RECT", [pc.property_directory,pc.property_script])
+    cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_directory, pc.property_script])
 
 if __name__ == '__main__':
     Main()
