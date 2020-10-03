de42438dec0a31ac87528e944830b3ff4de64462
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-03 16:44:03 +0100

    Comments.

--- tests/test_html.py
+++ tests/test_html.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+"""Test of various URL, the content is checked in HTML.
+It could be done in another output format. The goal is to maximize the coverage."""
+
 from __future__ import print_function
 
 import os
@@ -22,19 +25,19 @@ class HtmlCommon(unittest.TestCase):
         return html_content
 
     def _test_html_main(self):
+        """This checks that the entry page of Survol is a correct HTML document.
+        It does not intent to be very exact, but just checks that the HTML display runs.
+        Some strings must be displayed."""
         html_page_content = self._check_script("/survol/entity.py")
 
-        # Some strings must be displayed.
-        # This checks that the entry page of Survol is a correct HTML document.
-        # It does not intent to be very exact, but just checks that the HTML display runs.
         self.assertTrue(b'<title>Overview entity.py</title>' in html_page_content)
 
     def _test_html_file_directory(self):
         html_page_content = self._check_script("/survol/sources_types/CIM_Directory/file_directory.py?xid=CIM_Directory.Name=/usr/lib")
 
-        # Some strings must be displayed.
-        # This checks that the entry page of Survol is a correct HTML document.
-        # It does not intent to be very exact, but just checks that the HTML display runs.
+        """This checks that the entry page of Survol is a correct HTML document.
+        It does not intent to be very exact, but just checks that the HTML display runs.
+        Some strings must be displayed."""
         self.assertTrue(b'<title>Files in directory' in html_page_content)
 
 
@@ -45,7 +48,7 @@ class HtmlLocalAgentTest(HtmlCommon):
     """
 
     def setUp(self):
-        # If a Survol agent does not run on this machine with this port, this script starts a local one.
+        """If a Survol agent does not run on this machine with this port, this script starts a local one."""
         self._remote_html_test_agent, self._agent_url = start_cgiserver(RemoteHtmlTestServerPort)
         print("AgentUrl=", self._agent_url)
 
@@ -53,7 +56,7 @@ class HtmlLocalAgentTest(HtmlCommon):
         stop_cgiserver(self._remote_html_test_agent)
 
     def test_local_html(self):
-        # This starts a local server on the test machine.
+        """This starts a local server on the test machine."""
         self._test_html_main()
 
     @unittest.skipIf(not is_platform_linux, "Linux only")
@@ -61,7 +64,6 @@ class HtmlLocalAgentTest(HtmlCommon):
         self._test_html_file_directory()
 
 
-# Graphviz/dot must be installed on the test platform, for example Travis.
 class HtmlRemoteAgentTest(HtmlCommon):
     """
     Test parsing of the HTML output from the "official" demo server.
@@ -69,15 +71,11 @@ class HtmlRemoteAgentTest(HtmlCommon):
     def setUp(self):
         self._agent_url = SurvolServerAgent
 
-
-    # Loads a remote SVG url and parses the SVG document to check the presence of important tags.
-    # It relies on the public Primhill Computers server.
     def test_remote_html(self):
         self._test_html_main()
 
     def test_remote_file_directory(self):
         self._test_html_file_directory()
-
 
 if __name__ == '__main__':
     unittest.main()
