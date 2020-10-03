757f16c59183efec35d9b2908474b60df64c2474
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-03 15:32:45 +0100

    Cleanup.

--- survol/sources_types/SMB/net_share.py
+++ survol/sources_types/SMB/net_share.py
@@ -4,8 +4,6 @@
 NET SHARE command
 """
 
-# D:\Projects\Divers\Reverse\PythonStyle\htbin\sources>net share
-#
 # Share name   Resource                        Remark
 #
 # -------------------------------------------------------------------------------
@@ -15,7 +13,7 @@ NET SHARE command
 # ADMIN$       C:\Windows                      Remote Admin
 # The command completed successfully.
 #
-# C:\Documents and Settings\Remi>net share
+# C:\Documents and Settings\John>net share
 #
 #           1         2         3         4
 # 01234567890123456789012345678901234567890123456789
@@ -38,67 +36,64 @@ import lib_smb
 Usable = lib_smb.UsableNetCommands
 
 def Main():
-	cgiEnv = lib_common.CgiEnv()
+    cgiEnv = lib_common.CgiEnv()
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	net_share_cmd = [ "net", "share" ]
+    net_share_cmd = [ "net", "share" ]
 
-	net_share_pipe = lib_common.SubProcPOpen(net_share_cmd)
+    net_share_pipe = lib_common.SubProcPOpen(net_share_cmd)
 
-	( net_share_last_output, net_share_err ) = net_share_pipe.communicate()
+    (net_share_last_output, net_share_err) = net_share_pipe.communicate()
 
-	# Converts to string for Python3.
-	asstr = net_share_last_output.decode("utf-8")
-	#print("Str="+asstr)
-	lines = asstr.split('\n')
+    # Converts to string for Python3.
+    as_str = net_share_last_output.decode("utf-8")
+    #print("Str="+as_str)
+    lines = as_str.split('\n')
 
-	seenHyphens = False
+    seen_hyphens = False
 
-	for lin in lines:
-		#print("se="+str(seenHyphens)+" Lin=("+lin+")")
-		if re.match(".*-------.*",lin):
-			seenHyphens = True
-			continue
+    for lin in lines:
+        #print("se="+str(seen_hyphens)+" Lin=("+lin+")")
+        if re.match(".*-------.*", lin):
+            seen_hyphens = True
+            continue
 
-		if re.match(".*The command completed successfully.*",lin):
-			break
-		#print("se="+str(seenHyphens)+" Lin1=("+lin+")")
-		if not seenHyphens:
-			continue
+        if re.match(".*The command completed successfully.*", lin):
+            break
+        #print("se="+str(seen_hyphens)+" Lin1=("+lin+")")
+        if not seen_hyphens:
+            continue
 
-		#print("se="+str(seenHyphens)+" Lin2=("+lin+")")
-		tst_share = re.match( r'^([A-Za-z0-9_$]+) +([^ ]+).*', lin )
-		if not tst_share:
-			continue
+        #print("se="+str(seen_hyphens)+" Lin2=("+lin+")")
+        tst_share = re.match(r'^([A-Za-z0-9_$]+) +([^ ]+).*', lin)
+        if not tst_share:
+            continue
 
-		shrNam = tst_share.group(1)
+        shr_nam = tst_share.group(1)
 
-		strlin = str(lin)
-		# Nasty formatting of "NET SHARE" command.
-		if len(lin) >= 45:
-			# There is a remark or a very long resource.
-			if lin[44] == ' ':
-				# Character just before remark is a space.
-				shrRes = lin[13:44].rstrip()
-			else:
-				shrRes = lin[13:]
-		else:
-			shrRes = lin[13:]
-		#print("nam="+shrNam)
-		#print("res="+shrRes)
+        # Nasty formatting of "NET SHARE" command.
+        if len(lin) >= 45:
+            # There is a remark or a very long resource.
+            if lin[44] == ' ':
+                # Character just before remark is a space.
+                shr_res = lin[13:44].rstrip()
+            else:
+                shr_res = lin[13:]
+        else:
+            shr_res = lin[13:]
 
-		shareNode = lib_common.gUriGen.SmbShareUri( "//" + lib_util.currentHostname + "/" + shrNam )
-		grph.add( ( lib_common.nodeMachine, pc.property_smbshare, shareNode ) )
+        share_node = lib_common.gUriGen.SmbShareUri("//" + lib_util.currentHostname + "/" + shr_nam)
+        grph.add((lib_common.nodeMachine, pc.property_smbshare, share_node))
 
-		# mountNode = lib_common.gUriGen.FileUri( "//" + lib_util.currentHostname + "/" + shrRes )
-		shrRes = shrRes.strip()
-		shrRes = lib_util.standardized_file_path(shrRes)
-		mountNode = lib_common.gUriGen.DirectoryUri( shrRes )
-		grph.add( ( shareNode, pc.property_smbmount, mountNode ) )
+        # mount_node = lib_common.gUriGen.FileUri( "//" + lib_util.currentHostname + "/" + shr_res )
+        shr_res = shr_res.strip()
+        shr_res = lib_util.standardized_file_path(shr_res)
+        mount_node = lib_common.gUriGen.DirectoryUri(shr_res)
+        grph.add((share_node, pc.property_smbmount, mount_node))
 
-	cgiEnv.OutCgiRdf()
+    cgiEnv.OutCgiRdf()
 
 if __name__ == '__main__':
-	Main()
+    Main()
 
