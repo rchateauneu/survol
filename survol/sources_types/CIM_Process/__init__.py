7637d2b495d48ac914cf963c8780de56c3bf3bbf
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-03 14:24:00 +0100

    More direct use of psutil and rdflib.

--- survol/sources_types/CIM_Process/__init__.py
+++ survol/sources_types/CIM_Process/__init__.py
@@ -4,6 +4,8 @@ Standard process. Uniquely associated to a CIM_ComputerSystem and a parent CIM_P
 
 import os
 import sys
+import psutil
+import rdflib
 import lib_common
 import lib_util
 from lib_properties import pc
@@ -13,106 +15,105 @@ from lib_psutil import *
 
 ################################################################################
 
+
 # Returns the value of an environment variable of a given process.
 # TODO: Apparently, it exists in psutil.Process().environ() ??
-def GetEnvVarMap(thePid):
+def GetEnvVarMap(the_pid):
     if lib_util.isPlatformLinux:
-        filproc = open("/proc/%d/environ"%thePid)
-        mapEnvs = {}
+        filproc = open("/proc/%d/environ" % the_pid)
+        map_envs = {}
         envlin = filproc.readlines()
         for li in envlin[0].split("\0"):
-            posEqu = li.find("=")
-            mapEnvs[ li[:posEqu] ] = li[posEqu+1:]
+            pos_equ = li.find("=")
+            map_envs[li[:pos_equ] ] = li[pos_equ+1:]
         filproc.close()
-        return mapEnvs
+        return map_envs
 
     # https://www.codeproject.com/kb/threads/readprocenv.aspx
     if lib_util.isPlatformWindows:
         # TODO: Not implemented yet.
         return {}
 
-
     return {}
 
-def GetEnvVarProcess(theEnvVar,thePid):
+
+def GetEnvVarProcess(the_env_var, the_pid):
     try:
-        return GetEnvVarMap(thePid)[theEnvVar]
+        return GetEnvVarMap(the_pid)[the_env_var]
     except KeyError:
         return None
 
 ################################################################################
 
+
 def EntityOntology():
-    return ( ["Handle"],)
+    return (["Handle"],)
+
 
 def EntityName(entity_ids_arr):
     entity_id = entity_ids_arr[0]
 
     # If the process is not there, this is not a problem.
     try:
-        # sys.stderr.write("psutil.Process entity_id=%s\n" % ( entity_id ) )
-        proc_obj = PsutilGetProcObjNoThrow(int(entity_id))
+        proc_obj = psutil.Process(int(entity_id))
         return PsutilProcToName(proc_obj)
-    except NoSuchProcess:
+    except psutil.NoSuchProcess:
         # This might be, on Windows, a prent process which exit.
-        return "Non-existent process:"+entity_id
+        return "Non-existent process:" + entity_id
     except ValueError:
-        return "Invalid pid:("+entity_id+")"
+        return "Invalid pid:(" + entity_id + ")"
 
-def AddLinuxCGroup(node,grph):
+
+def AddLinuxCGroup(node, grph):
     if not lib_util.isPlatformLinux:
         return
 
-    # cat /proc/self/cgroup
 
 # Each entity can have such a file with its name as file name.
 # Then in its file, by convention adds information to a node.
-def AddInfo(grph,node,entity_ids_arr):
-    pidProc = entity_ids_arr[0]
+def AddInfo(grph, node, entity_ids_arr):
+    pid_proc = entity_ids_arr[0]
     exec_node = None
-    grph.add( ( node, pc.property_pid, lib_common.NodeLiteral(pidProc) ) )
+    grph.add((exec_node, pc.property_pid, rdflib.Literal(pid_proc)))
     try:
-        proc_obj = PsutilGetProcObjNoThrow(int(pidProc))
+        proc_obj = psutil.Process(int(pid_proc))
 
         cmd_line = PsutilProcToCmdline(proc_obj)
-        grph.add( ( node, pc.property_command, lib_common.NodeLiteral(cmd_line) ) )
+        grph.add((node, pc.property_command, rdflib.Literal(cmd_line)))
 
-        ( execName, execErrMsg ) = PsutilProcToExe(proc_obj)
-        if execName == "":
-            grph.add( ( node, pc.property_runs, lib_common.NodeLiteral("Executable error:"+execErrMsg) ) )
+        (exec_name, exec_err_msg) = PsutilProcToExe(proc_obj)
+        if exec_name == "":
+            grph.add((node, pc.property_runs, rdflib.Literal("Executable error:" + exec_err_msg)))
             exec_node = None
         else:
-            exec_node = lib_common.gUriGen.FileUri( execName )
-            grph.add( ( node, pc.property_runs, exec_node ) )
+            exec_node = lib_common.gUriGen.FileUri(exec_name)
+            grph.add((node, pc.property_runs, exec_node))
 
         # A node is created with the returned string which might as well be
         # an error message, which must be unique. Otherwise all faulty nodes
         # would be merged.
         # TODO: Problem, this node is still clickable. We should return a node
         # of this smae type, but with a faulty state, which would make it unclickable.
-        user_name = PsutilProcToUser(proc_obj,"User access denied:PID=%s"%pidProc)
+        user_name = PsutilProcToUser(proc_obj, "User access denied:PID=%s" % pid_proc)
 
         # TODO: Should add the hostname to the user ???
-        user_name_host = lib_common.format_username( user_name )
+        user_name_host = lib_common.format_username(user_name)
         user_node = lib_common.gUriGen.UserUri(user_name_host)
-        grph.add( ( node, pc.property_user, user_node ) )
+        grph.add((node, pc.property_user, user_node))
 
-        szResidSetSz = PsutilResidentSetSize(proc_obj)
-        grph.add( ( node, lib_common.MakeProp("Resident Set Size"), lib_common.NodeLiteral(szResidSetSz) ) )
+        sz_resid_set_sz = PsutilResidentSetSize(proc_obj)
+        grph.add((node, lib_common.MakeProp("Resident Set Size"), rdflib.Literal(sz_resid_set_sz)))
 
-        szVirstMemSz = PsutilVirtualMemorySize(proc_obj)
-        grph.add( ( node, lib_common.MakeProp("Virtual Memory Size"), lib_common.NodeLiteral(szVirstMemSz) ) )
+        sz_virst_mem_sz = PsutilVirtualMemorySize(proc_obj)
+        grph.add((node, lib_common.MakeProp("Virtual Memory Size"), rdflib.Literal(sz_virst_mem_sz)))
 
-        AddLinuxCGroup(node,grph)
+        AddLinuxCGroup(node, grph)
 
         # TODO: Add the current directory of the process ?
 
-    # except psutil._error.NoSuchProcess:
-    # Cannot use this exception on some psutil versions
-    # AttributeError: 'ModuleWrapper' object has no attribute '_error'
     except Exception as exc:
-        ERROR("CIM_Process.AddInfo. Caught:%s",exc)
-        grph.add( ( node, pc.property_information, lib_common.NodeLiteral(str(exc)) ) )
+        ERROR("CIM_Process.AddInfo. Caught:%s", exc)
+        grph.add((node, pc.property_information, rdflib.Literal(str(exc))))
 
     # Needed for other operations.
     return exec_node
@@ -123,20 +124,16 @@ def AddInfo(grph,node,entity_ids_arr):
 def Usable(entity_type,entity_ids_arr):
     """Process must be running"""
 
-    pidProc = entity_ids_arr[0]
-    try:
-        # Any error, no display.
-        proc_obj = PsutilGetProcObjNoThrow(int(pidProc))
-        return True
-    except:
-        return False
+    pid_proc = entity_ids_arr[0]
+    return psutil.pid_exists(pid_proc)
 
-# This must return at least the properties defined in the ontology.
-# There is no constraints on the other.
-# TODO: Add "select_attributes"
+
 def SelectFromWhere( where_key_values ):
-    DEBUG("CIM_Process SelectFromWhere where_key_values=%s",str(where_key_values))
-    for proc_obj in ProcessIter():
+    """This must return at least the properties defined in the ontology.
+    There is no constraints on the other.
+    TODO: Add "select_attributes" """
+    DEBUG("CIM_Process SelectFromWhere where_key_values=%s", str(where_key_values))
+    for proc_obj in psutil.process_iter():
         user_name = PsutilProcToUser(proc_obj,None)
         if user_name:
             user_name_host = lib_common.format_username(user_name)
@@ -154,7 +151,7 @@ def SelectFromWhere( where_key_values ):
 
         # TODO: Should reuse the existing properties.
         ret_value = {
-            lib_properties.MakeProp("Handle"):lib_util.NodeLiteral(proc_obj.pid),
-            lib_properties.MakeProp("username"):lib_util.NodeLiteral(user_name_host),
-            lib_properties.MakeProp("parent_pid"):lib_util.NodeLiteral(parent_pid)}
+            lib_properties.MakeProp("Handle"): rdflib.Literal(proc_obj.pid),
+            lib_properties.MakeProp("username"): rdflib.Literal(user_name_host),
+            lib_properties.MakeProp("parent_pid"): rdflib.Literal(parent_pid)}
         yield ret_value
