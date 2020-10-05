5b8019999e4c0e01aea78bd1b90feceb6e4ec9e1
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-05 23:29:20 +0100

    Progress Survol quick installer.

--- survol_installer.bat
+++ survol_installer.bat
@@ -1,13 +1,40 @@
 1>2# : ^
 '''
-@REM Create an installer in wxwidget for Apache, IIS etc.
+@REM TODO: Create an installer in wxwidget for Apache, IIS etc.
 
-py -3.6 -m venv %TMP%\survol-env
+@REM @echo off
+set "$py=0"
+echo import sys; print('{0[0]}.{0[1]}'.format(sys.version_info^)^) >#.py
+for /f "delims=" %%a in ('python #.py ^| findstr "2"') do set "$py=2"
+for /f "delims=" %%a in ('python #.py ^| findstr "3"') do set "$py=3"
+del #.py
+
+echo 
+goto:%$py%
+
+echo python is not installed or python's path Path is not in the %%$path%% env. var
+exit/b
+
+:2
+echo Running with Python 2
 @REM py -2.7 -m virtualenv %TMP%\survol-env
+python -m virtualenv %TMP%\survol-env
+goto:common_python_install
 
-%TMP%\survol-env\Scripts\python --version
+:3
+echo Running with Python 3
+@REM py -3.6 -m venv %TMP%\survol-env
+python -m venv %TMP%\survol-env
+goto:common_python_install
+
+:common_python_install
 %TMP%\survol-env\Scripts\pip --version
+@REM Optional dependencies.
+%TMP%\survol-env\Scripts\pip install pefile wmi pywin32 supervisor-win configparser
 %TMP%\survol-env\Scripts\pip install survol
+
+REM installer python.
+
 %TMP%\survol-env\Scripts\python -c "import survol;print('v=',survol.__version__);import survol.scripts.cgiserver;print('f=',survol.scripts.cgiserver.__file__)"
 
 pushd %TMP%\survol-env\Lib\site-packages
