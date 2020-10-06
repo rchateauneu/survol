35b8533472edf48084124910d9a64f8e9c04e5a4
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-06 12:50:24 +0100

    Use six module.

--- survol/sources_types/sqlserver/session/__init__.py
+++ survol/sources_types/sqlserver/session/__init__.py
@@ -2,7 +2,7 @@
 Sqlserver session
 """
 
-import sys
+import six
 import lib_common
 from lib_properties import pc
 
@@ -26,8 +26,8 @@ def MakeUri(dsn,sessionId):
 
 
 def AddInfo(grph,node,entity_ids_arr):
-	dsnNam = lib_util.six_u(entity_ids_arr[0])
-	sessionId = lib_util.six_u(entity_ids_arr[1])
+	dsnNam = six.u(entity_ids_arr[0])
+	sessionId = six.u(entity_ids_arr[1])
 
 	nodeDsn = survol_sqlserver_dsn.MakeUri(dsnNam)
 
