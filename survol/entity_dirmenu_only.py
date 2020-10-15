9cbe9824859c38150515dc3f686d759ea42620ff
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-15 07:46:32 +0100

    Cleanup and refactor.

--- survol/entity_dirmenu_only.py
+++ survol/entity_dirmenu_only.py
@@ -15,15 +15,16 @@ import lib_util
 import lib_common
 from lib_properties import pc
 
+
 # This returns None if a module of a script is usable, otherwise an errir message which explains
 # why this script cannot be used in this context: Wrong platform, unavailable resources etc...
 # PROBLEM: When an entire directory is not Usable because the file __init__.py
 # has a function Usable which returns False, then it still displays a directory, alone.
 # Unusable scripts are not displayed in the menu of the scripts of an entity,
 # except if a special flag is given, and in this case these error messages are displayed.
-def TestUsability(importedMod,entity_type,entity_ids_arr):
+def TestUsability(imported_module, entity_type, entity_ids_arr):
     try:
-        isUsable = importedMod.Usable(entity_type,entity_ids_arr)
+        isUsable = imported_module.Usable(entity_type, entity_ids_arr)
     except :
         return None
 
@@ -31,41 +32,43 @@ def TestUsability(importedMod,entity_type,entity_ids_arr):
     if isUsable:
         return None
 
-    errorMsg = importedMod.Usable.__doc__
-    if errorMsg:
-        errorMsg = errorMsg.strip()
+    error_msg = imported_module.Usable.__doc__
+    if error_msg:
+        error_msg = error_msg.strip()
     else:
-        errorMsg = importedMod.__name__
-        if errorMsg:
-            errorMsg += " not usable"
+        error_msg = imported_module.__name__
+        if error_msg:
+            error_msg += " not usable"
         else:
-            errorMsg = "No message"
-    return errorMsg
+            error_msg = "No message"
+    return error_msg
 
-def DirMenuReport(depthCall,strMsg):
+
+def _dir_menu_report(depth_call, str_msg):
     """For debugging purpose only."""
-    txtMargin = ( "    " * depthCall )
-    DEBUG(txtMargin + strMsg)
+    txt_margin = "    " * depth_call
+    DEBUG(txt_margin + str_msg)
+
 
 # TODO: Only return json data, and this script will only return json, nothing else.
-def DirToMenu(callbackGrphAdd,parentNode,entity_type,entity_id,entity_host,flagShowAll):
+def DirToMenu(callback_grph_add, parent_node, entity_type, entity_id, entity_host, flag_show_all):
 
-    def DirectoryUsabilityErrorNode(relative_dir,depthCall):
+    def DirectoryUsabilityErrorNode(relative_dir, depthCall):
         # Maybe there is a usability test in the current module.
         # The goal is to control all scripts in the subdirectories, from here.
         try:
-            entity_class = ".".join( relative_dir.split("/")[2:] )
-            #DirMenuReport( depthCall, "entity_class=%s\n"%(entity_class))
+            entity_class = ".".join(relative_dir.split("/")[2:])
+            #_dir_menu_report( depthCall, "entity_class=%s\n"%(entity_class))
 
-            importedMod = lib_util.GetEntityModule(entity_class)
-            if importedMod:
-                errorMsg = TestUsability(importedMod,entity_type,entity_ids_arr)
-                # if flagShowAll and errorMsg ???
-                if errorMsg:
-                    DEBUG("IsDirectoryUsable errorMsg(1)=%s",errorMsg)
+            imported_module = lib_util.GetEntityModule(entity_class)
+            if imported_module:
+                error_msg = TestUsability(imported_module, entity_type, entity_ids_arr)
+                # if flagShowAll and error_msg ???
+                if error_msg:
+                    DEBUG("IsDirectoryUsable error_msg(1)=%s" ,error_msg)
                     # If set to True, the directory is displayed even if all its scripts
                     # are not usable. Surprisingly, the message is not displayed as a subdirectory, but in a separate square.
-                    return lib_common.NodeLiteral(errorMsg)
+                    return lib_common.NodeLiteral(error_msg)
         except IndexError:
             # If we are at the top-level, no interest for the module.
             pass
@@ -73,11 +76,10 @@ def DirToMenu(callbackGrphAdd,parentNode,entity_type,entity_id,entity_host,flagS
         return None
 
 
-
     # This lists the scripts and generate RDF nodes.
     # Returns True if something was added.
-    def DirToMenuAux(aParentNode,grandParentNode,curr_dir,relative_dir,depthCall = 1):
-        #DirMenuReport( depthCall, "curr_dir=%s relative_dir=%s\n"%(curr_dir,relative_dir))
+    def DirToMenuAux(a_parent_node, grand_parent_node, curr_dir, relative_dir, depth_call=1):
+        #_dir_menu_report( depthCall, "curr_dir=%s relative_dir=%s\n"%(curr_dir,relative_dir))
         # In case there is nothing.
         dirs = None
         for path, dirs, files in os.walk(curr_dir):
@@ -85,66 +87,65 @@ def DirToMenu(callbackGrphAdd,parentNode,entity_type,entity_id,entity_host,flagS
 
         # Maybe this class is not defined in our ontology.
         if dirs == None:
-            WARNING("DirToMenuAux(2) No content in %s",curr_dir)
+            WARNING("DirToMenuAux(2) No content in %s", curr_dir)
             return False
 
         # Will still be None if nothing is added.
-        rdfNode = None
-        sub_path = path[ len(curr_dir) : ]
+        rdf_node = None
+        sub_path = path[len(curr_dir):]
 
         relative_dir_sub_path = relative_dir + sub_path
 
-        argDir = relative_dir_sub_path.replace("/",".")[1:]
+        arg_dir = relative_dir_sub_path.replace("/", ".")[1:]
 
         # If this is a remote host, all scripts are checked because they might have
         # the flag CanProcessRemote which is defined at the script level, not the directory level.
         if not entity_host:
-            errDirNode = DirectoryUsabilityErrorNode(relative_dir,depthCall)
-            if errDirNode:
-                if flagShowAll:
-                    argDirSplit = argDir.split(".")
-                    currDirNode = lib_util.DirDocNode(".".join(argDirSplit[:-1]),argDirSplit[-1])
-                    if not currDirNode:
-                        currDirNode = lib_util.NodeLiteral("Cannot parse relative dir:%s"%argDir)
-                    callbackGrphAdd( ( grandParentNode, pc.property_script, currDirNode ),depthCall )
-                    callbackGrphAdd( ( currDirNode, lib_common.MakeProp("Error"), errDirNode ),depthCall )
+            err_dir_node = DirectoryUsabilityErrorNode(relative_dir, depth_call)
+            if err_dir_node:
+                if flag_show_all:
+                    arg_dir_split = arg_dir.split(".")
+                    curr_dir_node = lib_util.DirDocNode(".".join(arg_dir_split[:-1]), arg_dir_split[-1])
+                    if not curr_dir_node:
+                        curr_dir_node = lib_util.NodeLiteral("Cannot parse relative dir:%s" % arg_dir)
+                    callback_grph_add((grand_parent_node, pc.property_script, curr_dir_node), depth_call)
+                    callback_grph_add((curr_dir_node, lib_common.MakeProp("Error"), err_dir_node), depth_call)
                 # The directory is not usable, so leave immediately.
                 return False
 
-
-        containsSomething = False
+        contains_something = False
         for dir in dirs:
-            #DirMenuReport( depthCall, "dir=%s\n"%(dir))
+            #_dir_menu_report( depthCall, "dir=%s\n"%(dir))
             # Might be generated by our Python interpreter.
             if dir == "__pycache__":
                 continue
 
             full_sub_dir = os.path.join(curr_dir, dir)
 
-            currDirNode = lib_util.DirDocNode(argDir,dir)
+            curr_dir_node = lib_util.DirDocNode(arg_dir, dir)
 
-            if not currDirNode:
-                #DirMenuReport( depthCall, "currDirNode NONE: argDir=%s dir=%s\n"%(argDir,dir))
+            if not curr_dir_node:
+                #_dir_menu_report( depthCall, "curr_dir_node NONE: arg_dir=%s dir=%s\n"%(arg_dir,dir))
                 continue
 
             sub_relative_dir = relative_dir + "/" + dir
 
             sub_entity_class = ".".join( sub_relative_dir.split("/")[2:] )
-            ontoKeys = lib_util.OntologyClassKeys(sub_entity_class)
-            #DirMenuReport( depthCall, "Checked ontology of %s: ontoKeys=%s\n"%(sub_entity_class,str(ontoKeys)))
+            onto_keys = lib_util.OntologyClassKeys(sub_entity_class)
+            #_dir_menu_report( depthCall, "Checked ontology of %s: onto_keys=%s\n"%(sub_entity_class,str(onto_keys)))
 
             # TODO: Beware, if not ontology, returns empty array. Why not returning None ?
-            if ontoKeys != []:
-                #DirMenuReport( depthCall, "Module %s has an ontology so it is a class. Skipping\n"%(sub_relative_dir))
+            if onto_keys != []:
+                #_dir_menu_report( depthCall, "Module %s has an ontology so it is a class. Skipping\n"%(sub_relative_dir))
                 # BEWARE: NO MORE DEFAULT ONTOLOGY ["Id"]
                 continue
 
-            somethingAdded = DirToMenuAux(currDirNode, aParentNode, full_sub_dir,sub_relative_dir,depthCall + 1)
+            something_added = DirToMenuAux(curr_dir_node, a_parent_node, full_sub_dir, sub_relative_dir, depth_call + 1)
             # This adds the directory name only if it contains a script.
-            if somethingAdded:
+            if something_added:
                 # It works both ways, possibly with different properties.
-                callbackGrphAdd( ( aParentNode, pc.property_script, currDirNode ), depthCall )
-            containsSomething = containsSomething | somethingAdded
+                callback_grph_add((a_parent_node, pc.property_script, curr_dir_node), depth_call)
+            contains_something = contains_something | something_added
 
         for fil in files:
             # We want to list only the usable Python scripts.
@@ -153,31 +154,30 @@ def DirToMenu(callbackGrphAdd,parentNode,entity_type,entity_id,entity_host,flagS
 
             script_path = relative_dir_sub_path + "/" + fil
 
-            #DirMenuReport( depthCall, "DirToMenu encodedEntityId=%s\n" % encodedEntityId)
+            #_dir_menu_report( depthCall, "DirToMenu encoded_entity_id=%s\n" % encoded_entity_id)
 
-            url_rdf = genObj.MakeTheNodeFromScript( script_path, entity_type, encodedEntityId )
+            url_rdf = genObj.MakeTheNodeFromScript(script_path, entity_type, encoded_entity_id)
 
-            errorMsg = None
+            error_msg = None
 
             try:
-                importedMod = lib_util.GetScriptModule(argDir, fil)
-            except Exception:
-                errorMsg = sys.exc_info()[1]
-                #DirMenuReport( depthCall, "DirToMenuAux Cannot import=%s. Caught: %s\n" % (script_path, errorMsg ) )
-                importedMod = None
-                if not flagShowAll:
+                imported_mod = lib_util.GetScriptModule(arg_dir, fil)
+            except Exception as error_msg:
+                #_dir_menu_report( depthCall, "DirToMenuAux Cannot import=%s. Caught: %s\n" % (script_path, error_msg ) )
+                imported_mod = None
+                if not flag_show_all:
                     continue
 
-            if not errorMsg:
+            if not error_msg:
                 # Show only scripts which want to be shown. Each script can have an optional function
                 # called Usable(): If it is there and returns False, the script is not displayed.
-                errorMsg = TestUsability(importedMod,entity_type,entity_ids_arr)
-                if errorMsg:
+                error_msg = TestUsability(imported_mod, entity_type, entity_ids_arr)
+                if error_msg:
                     pass
-                    #DEBUG("DirToMenuAux errorMsg(2)=%s",errorMsg)
+                    #DEBUG("DirToMenuAux error_msg(2)=%s",error_msg)
 
             # If this is a local host
-            if not flagShowAll and errorMsg and not entity_host:
+            if not flag_show_all and error_msg and not entity_host:
                 continue
 
             # If the entity is on another host, does the script run on remote entities ?
@@ -187,44 +187,41 @@ def DirToMenu(callbackGrphAdd,parentNode,entity_type,entity_id,entity_host,flagS
             if entity_host:
                 try:
                     # Script can be used on a remote entity.
-                    can_process_remote = importedMod.CanProcessRemote
+                    can_process_remote = imported_mod.CanProcessRemote
                 except AttributeError:
                     can_process_remote = False
 
-                # can_process_remote = True
-                DEBUG("entity_dir_menu.py DirToMenuAux entity_host=%s can_process_remote=%d",entity_host,can_process_remote)
-
                 if not can_process_remote:
-                    if not errorMsg:
-                        errorMsg = "%s is local" % ( entity_host )
-                    # DirMenuReport( depthCall, "Script %s %s cannot work on remote entities: %s at %s\n" % ( argDir, fil, encodedEntityId , entity_host ) )
-                    #DirMenuReport( depthCall, "Script %s %s cannot work on remote entities\n" % ( argDir, fil ) )
+                    if not error_msg:
+                        error_msg = "%s is local" % entity_host
+                    # _dir_menu_report( depthCall, "Script %s %s cannot work on remote entities: %s at %s\n" % ( arg_dir, fil, encoded_entity_id , entity_host ) )
+                    #_dir_menu_report( depthCall, "Script %s %s cannot work on remote entities\n" % ( arg_dir, fil ) )
 
-                    if not flagShowAll:
+                    if not flag_show_all:
                         continue
                 else:
-                    DirMenuReport( depthCall, "Script %s %s CAN work on remote entities\n" % ( argDir, fil ) )
+                    _dir_menu_report(depth_call, "Script %s %s CAN work on remote entities\n" % (arg_dir, fil))
 
             # Here, we are sure that the script is added.
             # TODO: If no script is added, should not add the directory?
-            rdfNode = lib_common.NodeUrl(url_rdf)
-            callbackGrphAdd( ( aParentNode, pc.property_script, rdfNode ), depthCall )
+            rdf_node = lib_common.NodeUrl(url_rdf)
+            callback_grph_add((a_parent_node, pc.property_script, rdf_node), depth_call)
 
             # Default doc text is file name minus the ".py" extension.
-            nodModu = lib_util.FromModuleToDoc(importedMod,fil[:-3])
+            nod_modu = lib_util.FromModuleToDoc(imported_mod, fil[:-3])
 
-            callbackGrphAdd( ( rdfNode, pc.property_information, nodModu ), depthCall )
+            callback_grph_add((rdf_node, pc.property_information, nod_modu), depth_call)
 
-            if errorMsg:
-                callbackGrphAdd( ( rdfNode, lib_common.MakeProp("Error"), lib_common.NodeLiteral(errorMsg) ), depthCall )
+            if error_msg:
+                callback_grph_add((rdf_node, lib_common.MakeProp("Error"), lib_common.NodeLiteral(error_msg)), depth_call)
 
         # This tells if a script was added in this directory or one of the subdirs.
-        return ( rdfNode is not None ) | containsSomething
+        return (rdf_node is not None) | contains_something
 
     if entity_host:
         DEBUG("entity_dir_menu.py DirToMenu entity_host=%s",entity_host)
-    encodedEntityId=lib_util.EncodeUri(entity_id)
-    entity_ids_arr = lib_util.EntityIdToArray( entity_type, entity_id )
+    encoded_entity_id=lib_util.EncodeUri(entity_id)
+    entity_ids_arr = lib_util.EntityIdToArray(entity_type, entity_id)
 
     if entity_type:
         # entity_type might contain a slash, for example: "sqlite/table"
@@ -239,23 +236,7 @@ def DirToMenu(callbackGrphAdd,parentNode,entity_type,entity_id,entity_host,flagS
     else:
         genObj = lib_common.gUriGen
 
-    DirToMenuAux(parentNode,None,directory,relative_dir,depthCall = 1)
-
-################################################################################
-
-
-
-# Si entity_type != "" mais entity_id == "", ca n'a pas de sens
-# d'afficher les scripts du directory survol/sources/<type>
-# car on n'a pas d'id. En revanche, on pourrait afficher selectivement
-# des scripts dans "top" qui affichent toutes les entites de ce type.
-# Ca revient a selectionner certains scripts.
-# On peut faire ca grossierement en filtrant sur le nom.
-# Mais on voudrait en fait les afficher directement.
-# On peut donc avoir des scripts appeles top/<type>.index.xyzw.py .
-# Mais on voudrait en avoir plusieurs, eventuellement.
-
-
+    DirToMenuAux(parent_node, None, directory, relative_dir, depth_call=1)
 
 
 def Main():
@@ -266,36 +247,27 @@ def Main():
                     parameters = { lib_util.paramkeyShowAll : False })
     entity_id = cgiEnv.m_entity_id
     entity_host = cgiEnv.GetHost()
-    flagShowAll = int(cgiEnv.get_parameters( lib_util.paramkeyShowAll ))
+    flag_show_all = int(cgiEnv.get_parameters(lib_util.paramkeyShowAll))
 
     nameSpace, entity_type = cgiEnv.get_namespace_type()
 
-    if lib_util.IsLocalAddress( entity_host ):
+    if lib_util.IsLocalAddress(entity_host):
         entity_host = ""
 
     DEBUG("entity: entity_host=%s entity_type=%s entity_id=%s", entity_host, entity_type, entity_id )
 
     grph = cgiEnv.GetGraph()
 
-    rootNode = lib_util.RootUri()
+    root_node = lib_util.RootUri()
 
     if entity_id != "" or entity_type == "":
-        entity_ids_arr = lib_util.EntityIdToArray( entity_type, entity_id )
-
-        # TODO: Plutot qu'attacher tous les sous-directory a node parent,
-        # ce serait peut-etre mieux d'avoir un seul lien, et d'afficher
-        # les enfants dans une table, un record etc...
-        # OU: Certaines proprietes arborescentes seraient representees en mettant
-        # les objets dans des boites imbriquees: Tables ou records.
-        # Ca peut marcher quand la propriete forme PAR CONSTRUCTION
-        # un DAG (Direct Acyclic Graph) qui serait alors traite de facon specifique.
-
-        def CallbackGrphAdd( tripl, depthCall ):
+        def CallbackGrphAdd(tripl, depth_call):
             grph.add(tripl)
 
-        DirToMenu(CallbackGrphAdd,rootNode,entity_type,entity_id,entity_host,flagShowAll)
+        DirToMenu(CallbackGrphAdd, root_node, entity_type, entity_id, entity_host, flag_show_all)
 
-    cgiEnv.OutCgiRdf( "LAYOUT_RECT", [pc.property_directory,pc.property_script])
+    cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_directory, pc.property_script])
+
 
 if __name__ == '__main__':
     Main()
