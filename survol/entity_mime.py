7ec5d6f21d5bb5458c123a688107578699a125e1
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-15 07:30:45 +0100

    Cleanup and refactor.

--- survol/entity_mime.py
+++ survol/entity_mime.py
@@ -12,35 +12,35 @@ import lib_common
 from lib_properties import pc
 
 def Main():
-	cgiEnv = lib_common.CgiEnv()
-	entity_id = cgiEnv.m_entity_id
-	entity_host = cgiEnv.GetHost()
+    cgiEnv = lib_common.CgiEnv()
+    entity_id = cgiEnv.m_entity_id
 
-	nameSpace, entity_type = cgiEnv.get_namespace_type()
+    name_space, entity_type = cgiEnv.get_namespace_type()
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	rootNode = lib_util.RootUri()
+    root_node = lib_util.RootUri()
 
-	entity_ids_arr = lib_util.EntityIdToArray( entity_type, entity_id )
+    entity_ids_arr = lib_util.EntityIdToArray(entity_type, entity_id)
 
-	modeDisp = lib_util.GuessDisplayMode()
-	DEBUG("entity_mime.py entity_type=%s modeDisp=%s",entity_type,modeDisp)
+    mode_disp = lib_util.GuessDisplayMode()
+    DEBUG("entity_mime.py entity_type=%s mode_disp=%s", entity_type, mode_disp)
 
-	if not entity_type:
-		lib_common.ErrorMessageHtml("entity_mime.py needs an object")
+    if not entity_type:
+        lib_common.ErrorMessageHtml("entity_mime.py needs an object")
 
-	entity_module = lib_util.GetEntityModule(entity_type)
-	if not entity_module:
-		lib_common.ErrorMessageHtml("entity_mime.py entity_type=%s needs a module" % (entity_type))
+    entity_module = lib_util.GetEntityModule(entity_type)
+    if not entity_module:
+        lib_common.ErrorMessageHtml("entity_mime.py entity_type=%s needs a module" % entity_type)
 
-	try:
-		entity_module.DisplayAsMime( grph, rootNode, entity_ids_arr )
-	except:
-		exc = sys.exc_info()[1]
-		ERROR("entity_mime.py No DisplayAsMime for %s %s: %s", entity_type, entity_id, str(exc) )
-		lib_common.ErrorMessageHtml("entity_mime.py No DisplayAsMime for %s %s: %s\n"%( entity_type, entity_id, str(exc) ))
+    try:
+        entity_module.DisplayAsMime(grph, root_node, entity_ids_arr)
+    except:
+        exc = sys.exc_info()[1]
+        ERROR("entity_mime.py No DisplayAsMime for %s %s: %s", entity_type, entity_id, str(exc) )
+        lib_common.ErrorMessageHtml("entity_mime.py No DisplayAsMime for %s %s: %s\n"%( entity_type, entity_id, str(exc) ))
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
 
