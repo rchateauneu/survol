61632c258056202e0778f9b6bf46f8e0e8a6dda0
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-15 09:28:08 +0100

    Cleanup.

--- survol/lib_mime.py
+++ survol/lib_mime.py
@@ -5,42 +5,43 @@ import lib_uris
 from lib_properties import pc
 
 try:
-	import mimetypes
-	mimelib_present = True
+    import mimetypes
+    mimelib_present = True
 except ImportError:
-	mimelib_present = False
-
-def FilenameToMime(pathName):
-	# sys.stderr.write("FilenameToMime pathName=%s\n"%pathName)
-
-	# No risk of course. Ideally the file should not be visible.
-	if pathName.upper().find("CREDENTIALS") >= 0:
-		return [ None, None ]
-
-	# On Linux, we want to read text files in the /proc filesystem
-	if pathName.startswith("/proc/"):
-		return ['text/plain', None]
+    mimelib_present = False
 
 
-	# Some types might not be well processed.
-	fileName, fileExt = os.path.splitext(pathName)
-	extUpper = fileExt.upper()
+def FilenameToMime(path_name):
+    # sys.stderr.write("FilenameToMime pathName=%s\n"%pathName)
 
-	if extUpper in [".LOG",".JSON"]:
-		return ['text/plain', None]
+    # No risk of course. Ideally the file should not be visible.
+    if path_name.upper().find("CREDENTIALS") >= 0:
+        return [None, None]
 
-	if mimelib_present:
-		# For example: ('text/plain', None)
-		return mimetypes.guess_type( pathName )
-	else:
-		# Last chance if module is not available.
-		# TODO: This can easily be completed.
-		if extUpper in [".JPG",".JPEG"]:
-			return ['image/jpeg', None]
-		if extUpper in [".TXT"]:
-			return ['image/jpeg', None]
+    # On Linux, we want to read text files in the /proc filesystem
+    if path_name.startswith("/proc/"):
+        return ['text/plain', None]
 
-	return [ None, None ]
+    # Some types might not be well processed.
+    file_name, file_ext = os.path.splitext(path_name)
+    ext_upper = file_ext.upper()
+
+    if ext_upper in [".LOG", ".JSON"]:
+        return ['text/plain', None]
+
+    if mimelib_present:
+        # For example: ('text/plain', None)
+        return mimetypes.guess_type(path_name)
+    else:
+        # Last chance if module is not available.
+        # TODO: This can easily be completed.
+        if ext_upper in [".JPG", ".JPEG"]:
+            return ['image/jpeg', None]
+        if ext_upper in [".TXT"]:
+            return ['image/jpeg', None]
+
+    return [None, None]
+
 
 # This encodes the Mime type in the mode associated to an Url.
 # The CGI "mode" parameter can be for example:
@@ -52,35 +53,35 @@ def FilenameToMime(pathName):
 # "edit"                Edition of the other CGI parametgers.
 # "mime:text/plain"     Displayed as a Mime document.
 # "mime:image:bmp"      Same ...
-mimeModePrefix = "mime:"
+_mime_mode_prefix = "mime:"
 
-def AddMimeUrl(grph,filNode, entity_type,mime_type,entity_id_arr):
-	entity_host = None
-	if entity_host:
-		genObj = lib_uris.RemoteBox(entity_host)
-	else:
-		genObj = lib_uris.gUriGen
 
-	mimeNode = genObj.UriMakeFromScript( '/entity_mime.py', entity_type, *entity_id_arr )
+def AddMimeUrl(grph, fil_node, entity_type, mime_type, entity_id_arr):
+    entity_host = None
+    if entity_host:
+        gen_obj = lib_uris.RemoteBox(entity_host)
+    else:
+        gen_obj = lib_uris.gUriGen
 
-	# So that the MIME type is known without loading the URLs.
-	# Also, it allows to force a specific MIME type.
-	# The MIME type is not coded in the property because it is an attribute of the object.
+    mime_node = gen_obj.UriMakeFromScript('/entity_mime.py', entity_type, *entity_id_arr)
 
-	# mimeNodeWithMode =  lib_util.url_mode_replace(mimeNode, "mime/" + mime_type)
-	# sys.stderr.write("lib_mime.AddMimeUrl BEFORE mimeNode=%s\n"%(mimeNode))
-	mimeNodeWithMode = mimeNode + "&amp;amp;" + "mode=" + mimeModePrefix + mime_type
+    # So that the MIME type is known without loading the URLs.
+    # Also, it allows to force a specific MIME type.
+    # The MIME type is not coded in the property because it is an attribute of the object.
+    mime_node_with_mode = mime_node + "&amp;amp;" + "mode=" + _mime_mode_prefix + mime_type
 
-	grph.add( ( filNode, pc.property_rdf_data_nolist2, lib_util.NodeUrl(mimeNodeWithMode) ) )
+    grph.add((fil_node, pc.property_rdf_data_nolist2, lib_util.NodeUrl(mime_node_with_mode)))
+
 
 # If the CGI parameter is for example: "...&mode=
-def ModeToMimeType(urlMode):
-	return urlMode[5:]
+def ModeToMimeType(url_mode):
+    return url_mode[5:]
+
 
 def GetMimeTypeFromUrl(url):
-	urlMode = lib_util.get_url_mode(url)
-	if urlMode and urlMode.startswith(mimeModePrefix):
-		return ModeToMimeType(urlMode)
-	else:
-		return ""
+    url_mode = lib_util.get_url_mode(url)
+    if url_mode and url_mode.startswith(_mime_mode_prefix):
+        return ModeToMimeType(url_mode)
+    else:
+        return ""
 
