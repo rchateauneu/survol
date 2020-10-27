10a4db4f64363fb853f7e61715bad5e8486dc581
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-27 23:32:06 +0000

    Cleanup.

--- survol/sources_types/oracle/schema/oracle_schema_packages.py
+++ survol/sources_types/oracle/schema/oracle_schema_packages.py
@@ -12,33 +12,35 @@ import lib_common
 from sources_types.oracle import schema as oracle_schema
 from sources_types.oracle import package as oracle_package
 
+
 def Main():
-	cgiEnv = lib_oracle.OracleEnv()
+    cgiEnv = lib_oracle.OracleEnv()
 
-	# BEWARE: There is an implicit dependency on the structure of Oracle schema URI.
-	oraSchema = cgiEnv.m_entity_id_dict["Schema"]
+    # BEWARE: There is an implicit dependency on the structure of Oracle schema URI.
+    ora_schema = cgiEnv.m_entity_id_dict["Schema"]
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'PACKAGE' AND OWNER = '" + oraSchema + "'"
-	DEBUG("sql_query=%s", sql_query )
+    sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'PACKAGE' AND OWNER = '" + ora_schema + "'"
+    DEBUG("sql_query=%s", sql_query )
 
-	node_oraschema = oracle_schema.MakeUri( cgiEnv.m_oraDatabase, oraSchema )
+    node_oraschema = oracle_schema.MakeUri(cgiEnv.m_oraDatabase, ora_schema)
 
-	result = lib_oracle.ExecuteQuery( cgiEnv.ConnectStr(), sql_query)
+    result = lib_oracle.ExecuteQuery(cgiEnv.ConnectStr(), sql_query)
 
-	for row in result:
-		packageName = str(row[0])
-		# sys.stderr.write("tableName=%s\n" % tableName )
-		nodePackage = oracle_package.MakeUri( cgiEnv.m_oraDatabase , oraSchema, packageName )
-		grph.add( ( node_oraschema, pc.property_oracle_package, nodePackage ) )
+    for row in result:
+        package_name = str(row[0])
+        # sys.stderr.write("tableName=%s\n" % tableName )
+        node_package = oracle_package.MakeUri(cgiEnv.m_oraDatabase, ora_schema, package_name)
+        grph.add((node_oraschema, pc.property_oracle_package, node_package))
 
-		lib_oracle.AddLiteralNotNone(grph,nodePackage,"Status",row[1])
-		lib_oracle.AddLiteralNotNone(grph,nodePackage,"Creation",row[2])
+        lib_oracle.AddLiteralNotNone(grph, node_package, "Status", row[1])
+        lib_oracle.AddLiteralNotNone(grph, node_package, "Creation", row[2])
 
-	# It cannot work if there are too many tables.
-	# cgiEnv.OutCgiRdf("LAYOUT_RECT")
-	cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_oracle_package])
+    # It cannot work if there are too many tables.
+    # cgiEnv.OutCgiRdf("LAYOUT_RECT")
+    cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_oracle_package])
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
