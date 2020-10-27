5b3ae33be4ec204de0ab82c1d01b7202b776a87e
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-27 19:42:21 +0000

    Cleanup.

--- survol/sources_types/oracle/schema/oracle_schema_tables.py
+++ survol/sources_types/oracle/schema/oracle_schema_tables.py
@@ -13,31 +13,30 @@ from sources_types.oracle import schema as oracle_schema
 from sources_types.oracle import table as oracle_table
 
 def Main():
-	cgiEnv = lib_oracle.OracleEnv()
+    cgiEnv = lib_oracle.OracleEnv()
 
-	oraSchema = cgiEnv.m_entity_id_dict["Schema"]
+    ora_schema = cgiEnv.m_entity_id_dict["Schema"]
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'TABLE' AND OWNER = '" + oraSchema + "'"
-	DEBUG("sql_query=%s", sql_query )
+    sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'TABLE' AND OWNER = '" + ora_schema + "'"
+    DEBUG("sql_query=%s", sql_query)
 
-	node_oraschema = oracle_schema.MakeUri( cgiEnv.m_oraDatabase, oraSchema )
+    node_oraschema = oracle_schema.MakeUri(cgiEnv.m_oraDatabase, ora_schema)
 
-	result = lib_oracle.ExecuteQuery( cgiEnv.ConnectStr(), sql_query)
+    result = lib_oracle.ExecuteQuery( cgiEnv.ConnectStr(), sql_query)
 
-	for row in result:
-		tableName = str(row[0])
-		# sys.stderr.write("tableName=%s\n" % tableName )
-		nodeTable = oracle_table.MakeUri( cgiEnv.m_oraDatabase , oraSchema, tableName )
-		grph.add( ( node_oraschema, pc.property_oracle_table, nodeTable ) )
+    for row in result:
+        table_name = str(row[0])
+        node_table = oracle_table.MakeUri(cgiEnv.m_oraDatabase, ora_schema, table_name)
+        grph.add((node_oraschema, pc.property_oracle_table, node_table))
 
-		lib_oracle.AddLiteralNotNone(grph,nodeTable,"Status",row[1])
-		lib_oracle.AddLiteralNotNone(grph,nodeTable,"Creation",row[2])
+        lib_oracle.AddLiteralNotNone(grph, node_table, "Status", row[1])
+        lib_oracle.AddLiteralNotNone(grph,node_table, "Creation", row[2])
 
-	# It cannot work if there are too many tables.
-	# cgiEnv.OutCgiRdf("LAYOUT_RECT")
-	cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_oracle_table])
+    # It cannot work if there are too many tables.
+    # cgiEnv.OutCgiRdf("LAYOUT_RECT")
+    cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_oracle_table])
 
 if __name__ == '__main__':
-	Main()
+    Main()
