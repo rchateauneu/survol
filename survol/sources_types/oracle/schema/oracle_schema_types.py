eacf4805b64bce8202b715583bab83587fc77dab
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-27 23:42:18 +0000

    Cleanup.

--- survol/sources_types/oracle/schema/oracle_schema_types.py
+++ survol/sources_types/oracle/schema/oracle_schema_types.py
@@ -12,31 +12,32 @@ import lib_oracle
 from sources_types.oracle import schema as oracle_schema
 from sources_types.oracle import type as oracle_type
 
+
 def Main():
-	cgiEnv = lib_oracle.OracleEnv()
+    cgiEnv = lib_oracle.OracleEnv()
 
-	oraSchema = cgiEnv.m_entity_id_dict["Schema"]
+    ora_schema = cgiEnv.m_entity_id_dict["Schema"]
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'TYPE' AND OWNER = '" + oraSchema + "'"
-	DEBUG("sql_query=%s", sql_query )
+    sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'TYPE' AND OWNER = '" + ora_schema + "'"
+    DEBUG("sql_query=%s", sql_query )
 
-	node_oraschema = oracle_schema.MakeUri( cgiEnv.m_oraDatabase, oraSchema )
+    node_oraschema = oracle_schema.MakeUri(cgiEnv.m_oraDatabase, ora_schema)
 
-	result = lib_oracle.ExecuteQuery( cgiEnv.ConnectStr(), sql_query)
+    result = lib_oracle.ExecuteQuery(cgiEnv.ConnectStr(), sql_query)
 
-	for row in result:
-		typeName = str(row[0])
-		nodeType = oracle_type.MakeUri( cgiEnv.m_oraDatabase , oraSchema, typeName )
-		grph.add( ( node_oraschema, pc.property_oracle_type, nodeType ) )
+    for row in result:
+        type_name = str(row[0])
+        node_type = oracle_type.MakeUri(cgiEnv.m_oraDatabase , ora_schema, type_name)
+        grph.add((node_oraschema, pc.property_oracle_type, node_type))
 
-		lib_oracle.AddLiteralNotNone(grph,nodeType,"Status",row[1])
-		lib_oracle.AddLiteralNotNone(grph,nodeType,"Creation",row[2])
+        lib_oracle.AddLiteralNotNone(grph, node_type, "Status", row[1])
+        lib_oracle.AddLiteralNotNone(grph, node_type, "Creation", row[2])
 
-	# It cannot work if there are too many views.
-	# cgiEnv.OutCgiRdf("LAYOUT_RECT")
-	cgiEnv.OutCgiRdf("LAYOUT_RECT",[pc.property_oracle_type])
+    # Display in lines.
+    cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_oracle_type])
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
