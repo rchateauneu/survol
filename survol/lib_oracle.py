fa0c24231b93f618d4d5dc13119a3cf68c324d2b
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-27 23:28:10 +0000

    Cleanup. Hide password.

--- survol/lib_oracle.py
+++ survol/lib_oracle.py
@@ -4,6 +4,7 @@ from lib_properties import pc
 # TODO: This file should go to sources_types/oracle
 
 import sys
+import re
 import lib_util
 import lib_credentials
 
@@ -25,153 +26,150 @@ from sources_types.oracle import type as oracle_type
 
 import cx_Oracle
 
-def GetOraConnect(conn_str):
-	try:
-		return cx_Oracle.connect(conn_str)
-	# except cx_Oracle.InterfaceError:
-	# except cx_Oracle.DatabaseError:
-	except Exception:
-		exc = sys.exc_info()[1]
-		lib_common.ErrorMessageHtml("cx_Oracle.connect conn_str="+conn_str+" Err="+str(exc))
 
-# TODO: Check that there is only one query, and exclusively a select,
-# to avoid SQL injections.
-def ExecuteSafeQuery(aCursor,sql_query):
-	if not sql_query.strip().upper().startswith("SELECT "):
-		raise Exception("Unsafe query:%s"%sql_query)
-	aCursor.execute(sql_query)
+def GetOraConnect(conn_str):
+    try:
+        return cx_Oracle.connect(conn_str)
+    # except cx_Oracle.InterfaceError:
+    # except cx_Oracle.DatabaseError:
+    except Exception as exc:
+        secure_connect_str = re.sub("/[^@]*", "/********", conn_str)
+        lib_common.ErrorMessageHtml("cx_Oracle.connect conn_str=%s Err=%s " % (secure_connect_str, str(exc)))
+
+
+# TODO: Check that there is only one query, and exclusively a select, to avoid SQL injections.
+def _execute_safe_query(a_cursor, sql_query):
+    if not sql_query.strip().upper().startswith("SELECT "):
+        raise Exception("Unsafe query:%s"%sql_query)
+    a_cursor.execute(sql_query)
+
 
 def OracleConnectionClose(conn):
-	try:
-		conn.close()
-	except cx_Oracle.DatabaseError:
-		# "connection cannot be closed when open statements or LOBs exist"
-		# cx_oracle throws "DPI-1054: connection cannot be closed when open statements or LOBs exist" during django migration
-		exc = sys.exc_info()[1]
-		errMsg = str(exc)
-		if errMsg.find("connection cannot be closed when open statements") >= 0:
-			WARNING("OracleConnectionClose LOBs exist: exception:%s.", errMsg)
-			pass
-		else:
-			lib_common.ErrorMessageHtml("OracleConnectionClose caught:%s."% ( errMsg ) )
-
-def ExecuteQueryThrow(conn_str,sql_query):
-	result = []
-	conn = GetOraConnect(conn_str)
-	aCursor = conn.cursor()
-
-	DEBUG("ExecuteQuery %s", sql_query)
-
-	ExecuteSafeQuery(aCursor,sql_query)
-	try:
-		# This could be faster by returning a cursor
-		# or a generator, but this is not important now.
-		for row in aCursor:
-			# Use yield ? Or return c ?
-			result.append( row )
-
-	except cx_Oracle.DatabaseError:
-		pass
-	OracleConnectionClose(conn)
-
-	return result
-
-def ExecuteQuery(conn_str,sql_query):
-	try:
-		return ExecuteQueryThrow(conn_str,sql_query)
-	except cx_Oracle.DatabaseError:
-		exc = sys.exc_info()[1]
-		lib_common.ErrorMessageHtml("ExecuteQuery exception:%s in %s"% ( str(exc), sql_query ) )
+    try:
+        conn.close()
+    except cx_Oracle.DatabaseError as exc:
+        # "connection cannot be closed when open statements or LOBs exist"
+        # cx_oracle throws "DPI-1054: connection cannot be closed when open statements or LOBs exist" during django migration
+        err_msg = str(exc)
+        if err_msg.find("connection cannot be closed when open statements") >= 0:
+            WARNING("OracleConnectionClose LOBs exist: exception:%s.", err_msg)
+            pass
+        else:
+            lib_common.ErrorMessageHtml("OracleConnectionClose caught:%s."% (err_msg))
 
 
+def ExecuteQueryThrow(conn_str, sql_query):
+    result = []
+    conn = GetOraConnect(conn_str)
+    a_cursor = conn.cursor()
 
-# Faster because no object copy, and also mandatory if LOBs are returned,
-# because they disappear when the cursor is deleted.
-def CallbackQuery(conn_str,sql_query,callback):
-	conn = GetOraConnect(conn_str)
+    DEBUG("ExecuteQuery %s", sql_query)
 
-	aCursor = conn.cursor()
+    _execute_safe_query(a_cursor, sql_query)
+    try:
+        # This could be faster by returning a cursor
+        # or a generator, but this is not important now.
+        for row in a_cursor:
+            # Use yield ? Or return c ?
+            result.append(row)
 
-	ExecuteSafeQuery(aCursor,sql_query)
-	try:
-		for row in aCursor:
-			callback(row)
+    except cx_Oracle.DatabaseError:
+        pass
+    OracleConnectionClose(conn)
 
-	except cx_Oracle.DatabaseError:
-		pass
-	OracleConnectionClose(conn)
+    return result
+
+
+def ExecuteQuery(conn_str, sql_query):
+    try:
+        return ExecuteQueryThrow(conn_str, sql_query)
+    except cx_Oracle.DatabaseError as exc:
+        lib_common.ErrorMessageHtml("ExecuteQuery exception:%s in %s" % (str(exc), sql_query))
+
+
+def CallbackQuery(conn_str, sql_query, the_callback):
+    """Faster because no object copy, and also mandatory if LOBs are returned,
+    because they disappear when the cursor is deleted."""
+    conn = GetOraConnect(conn_str)
+
+    a_cursor = conn.cursor()
+
+    _execute_safe_query(a_cursor, sql_query)
+    try:
+        for row in a_cursor:
+            the_callback(row)
+    except cx_Oracle.DatabaseError:
+        pass
+    OracleConnectionClose(conn)
+
 
 # BEWARE: There is an implicit dependency on the structure of Oracle schema URI.
 # https://docs.oracle.com/cd/A91202_01/901_doc/server.901/a90125/sql_elements10.htm
 # CREATE SYNONYM emp_table FOR oe.employees@remote.us.oracle.com;
 # schema.object_name.object_part@dblink
-#def OraUri( entity_type, ora_db, ora_part = "", ora_object = "", ora_schema = ""):
-#	return lib_util.EntityUri( entity_type, ora_schema + "." + ora_object + "." + ora_part + "@" + ora_db )
 
 class OracleEnv (lib_common.CgiEnv):
-	def __init__( self ):
-		lib_common.CgiEnv.__init__( self )
+    def __init__(self):
+        lib_common.CgiEnv.__init__(self)
 
-		self.m_oraDatabase = self.m_entity_id_dict["Db"]
+        self.m_oraDatabase = self.m_entity_id_dict["Db"]
 
-	def ConnectStr(self):
-		# TODO: This can be parsed from the schema.
+    def ConnectStr(self):
+        # TODO: This can be parsed from the schema.
 
-		(oraUser,oraPwd) = lib_credentials.GetCredentials( "Oracle", self.m_oraDatabase )
-		return oraUser + "/" + oraPwd + "@" + self.m_oraDatabase
+        ora_user, ora_pwd = lib_credentials.GetCredentials("Oracle", self.m_oraDatabase)
+        return ora_user + "/" + ora_pwd + "@" + self.m_oraDatabase
 
-	def MakeUri(self, entity_type, **kwArgs ):
-		return lib_util.EntityUri( entity_type, { "Db": self.m_oraDatabase }, **kwArgs )
+    def MakeUri(self, entity_type, **kwArgs):
+        return lib_util.EntityUri(entity_type, {"Db": self.m_oraDatabase}, **kwArgs)
 
-	def OracleSchema(self):
-		# TODO: This could call GetCredentials once only.
-		(oraUser,oraPwd) = lib_credentials.GetCredentials( "Oracle", self.m_oraDatabase )
-		return oraUser
-
-# This displays the content of the Oracle table dba_dependencies.
-def AddDependency( grph, row, nodeRoot, oraDatabase, direction ):
-	depOwner = str(row[0])
-	depName = str(row[1])
-	depType = str(row[2])
-
-	if depType == "TABLE":
-		nodeObject = oracle_table.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "VIEW":
-		nodeObject = oracle_view.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "PACKAGE":
-		nodeObject = oracle_package.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "PACKAGE BODY":
-		nodeObject = oracle_package_body.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "SYNONYM":
-		nodeObject = oracle_synonym.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "TYPE":
-		nodeObject = oracle_type.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "SEQUENCE":
-		nodeObject = oracle_sequence.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "LIBRARY":
-		nodeObject = oracle_library.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "PROCEDURE":
-		nodeObject = oracle_procedure.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "FUNCTION":
-		nodeObject = oracle_function.MakeUri( oraDatabase , depOwner, depName )
-	elif depType == "TRIGGER":
-		nodeObject = oracle_trigger.MakeUri( oraDatabase , depOwner, depName )
-	else:
-		lib_common.ErrorMessageHtml("Unknown dependency depType=%s depName=%s" % ( depType, depName ) )
-		return
-
-	if direction == True:
-		grph.add( ( nodeRoot, pc.property_oracle_depends, nodeObject ) )
-	else:
-		grph.add( ( nodeObject, pc.property_oracle_depends, nodeRoot ) )
+    def OracleSchema(self):
+        # TODO: This could call GetCredentials once only.
+        ora_user, ora_pwd = lib_credentials.GetCredentials("Oracle", self.m_oraDatabase)
+        return ora_user
 
 
-def AddLiteralNotNone(grph,node,txt,data):
-	if data != None:
-		grph.add( ( node, lib_common.MakeProp(txt), lib_common.NodeLiteral(data) ) )
+_oracle_type_name_to_class = {
+    "TABLE": oracle_table,
+    "VIEW": oracle_view,
+    "PACKAGE": oracle_package,
+    "PACKAGE BODY": oracle_package_body,
+    "SYNONYM": oracle_synonym,
+    "TYPE": oracle_type,
+    "SEQUENCE": oracle_sequence,
+    "LIBRARY": oracle_library,
+    "PROCEDURE": oracle_procedure,
+    "FUNCTION": oracle_function,
+    "TRIGGER": oracle_trigger}
 
-# This returns an IP address.
-def OraMachineToIp(oraMachine):
-	# Maybe different on Linux ???  "WORKGROUP\RCHATEAU-HP"
-	user_machine = lib_util.GlobalGetHostByName( oraMachine.split("\\")[-1] )
-	return user_machine
+
+def AddDependency(grph, row, node_root, ora_database, direction):
+    """This displays the content of one row of the Oracle table dba_dependencies."""
+    dep_owner = str(row[0])
+    dep_name = str(row[1])
+    dep_type = str(row[2])
+
+    try:
+        the_oracle_class = _oracle_type_name_to_class[dep_type]
+    except KeyError:
+        lib_common.ErrorMessageHtml("Unknown dependency dep_type=%s dep_name=%s" % (dep_type, dep_name))
+        return
+
+    node_object = the_oracle_class.MakeUri(ora_database, dep_owner, dep_name)
+
+    if direction:
+        grph.add((node_root, pc.property_oracle_depends, node_object))
+    else:
+        grph.add((node_object, pc.property_oracle_depends, node_root))
+
+
+def AddLiteralNotNone(grph, node, txt, data):
+    if data != None:
+        grph.add((node, lib_common.MakeProp(txt), lib_common.NodeLiteral(data)))
+
+
+def OraMachineToIp(ora_machine):
+    """This returns an IP address."""
+    # Maybe different on Linux ???  "WORKGROUP\RCHATEAU-HP"
+    user_machine = lib_util.GlobalGetHostByName(ora_machine.split("\\")[-1])
+    return user_machine
