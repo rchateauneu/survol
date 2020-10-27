8a7a68fabb08dbb4a21470b15539686a62e44329
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-27 23:36:48 +0000

    Cleanup.

--- survol/sources_types/oracle/schema/oracle_schema_sequences.py
+++ survol/sources_types/oracle/schema/oracle_schema_sequences.py
@@ -12,31 +12,33 @@ import lib_common
 from sources_types.oracle import schema as oracle_schema
 from sources_types.oracle import sequence as oracle_sequence
 
+
 def Main():
 	cgiEnv = lib_oracle.OracleEnv()
 
-	oraSchema = cgiEnv.m_entity_id_dict["Schema"]
+	ora_schema = cgiEnv.m_entity_id_dict["Schema"]
 
 	grph = cgiEnv.GetGraph()
 
-	sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'SEQUENCE' AND OWNER = '" + oraSchema + "'"
-	DEBUG("sql_query=%s", sql_query )
+	sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'SEQUENCE' AND OWNER = '" + ora_schema + "'"
+	DEBUG("sql_query=%s", sql_query)
 
-	node_oraschema = oracle_schema.MakeUri( cgiEnv.m_oraDatabase, oraSchema )
+	node_oraschema = oracle_schema.MakeUri(cgiEnv.m_oraDatabase, ora_schema)
 
-	result = lib_oracle.ExecuteQuery( cgiEnv.ConnectStr(), sql_query)
+	result = lib_oracle.ExecuteQuery(cgiEnv.ConnectStr(), sql_query)
 
 	for row in result:
-		sequenceName = str(row[0])
-		nodeSequence = oracle_sequence.MakeUri( cgiEnv.m_oraDatabase , oraSchema, sequenceName )
-		grph.add( ( node_oraschema, pc.property_oracle_sequence, nodeSequence ) )
+		sequence_name = str(row[0])
+		node_sequence = oracle_sequence.MakeUri(cgiEnv.m_oraDatabase , ora_schema, sequence_name)
+		grph.add((node_oraschema, pc.property_oracle_sequence, node_sequence))
 
-		lib_oracle.AddLiteralNotNone(grph,nodeSequence,"Status",row[1])
-		lib_oracle.AddLiteralNotNone(grph,nodeSequence,"Creation",row[2])
+		lib_oracle.AddLiteralNotNone(grph, node_sequence, "Status", row[1])
+		lib_oracle.AddLiteralNotNone(grph, node_sequence, "Creation", row[2])
 
 	# It cannot work if there are too many views.
 	# cgiEnv.OutCgiRdf("LAYOUT_RECT")
-	cgiEnv.OutCgiRdf("LAYOUT_RECT",[pc.property_oracle_sequence])
+	cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_oracle_sequence])
+
 
 if __name__ == '__main__':
 	Main()
