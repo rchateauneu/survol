8375f7cb8826175dc8eff8c6786db6314dd824f1
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-27 20:30:20 +0000

    Cleanup.

--- survol/sources_types/oracle/schema/oracle_schema_libraries.py
+++ survol/sources_types/oracle/schema/oracle_schema_libraries.py
@@ -13,28 +13,30 @@ from sources_types.oracle import schema as oracle_schema
 from sources_types.oracle import library as oracle_library
 
 def Main():
-	cgiEnv = lib_oracle.OracleEnv()
+    cgiEnv = lib_oracle.OracleEnv()
 
-	oraSchema = cgiEnv.m_entity_id_dict["Schema"]
+    ora_schema = cgiEnv.m_entity_id_dict["Schema"]
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'LIBRARY' AND OWNER = '" + oraSchema + "'"
-	DEBUG("sql_query=%s", sql_query )
+    sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'LIBRARY' AND OWNER = '" + \
+                ora_schema + "'"
+    DEBUG("sql_query=%s", sql_query)
 
-	node_oraschema = oracle_schema.MakeUri( cgiEnv.m_oraDatabase, oraSchema )
+    node_oraschema = oracle_schema.MakeUri(cgiEnv.m_oraDatabase, ora_schema)
 
-	result = lib_oracle.ExecuteQuery( cgiEnv.ConnectStr(), sql_query)
+    result = lib_oracle.ExecuteQuery(cgiEnv.ConnectStr(), sql_query)
 
-	for row in result:
-		libraryName = str(row[0])
-		nodeLibrary = oracle_library.MakeUri( cgiEnv.m_oraDatabase , oraSchema, libraryName )
-		grph.add( ( node_oraschema, pc.property_oracle_library, nodeLibrary ) )
+    for row in result:
+        library_name = str(row[0])
+        node_library = oracle_library.MakeUri(cgiEnv.m_oraDatabase , ora_schema, library_name)
+        grph.add((node_oraschema, pc.property_oracle_library, node_library))
 
-		lib_oracle.AddLiteralNotNone(grph,nodeLibrary,"Status",row[1])
-		lib_oracle.AddLiteralNotNone(grph,nodeLibrary,"Creation",row[2])
+        lib_oracle.AddLiteralNotNone(grph, node_library, "Status", row[1])
+        lib_oracle.AddLiteralNotNone(grph, node_library, "Creation", row[2])
 
-	cgiEnv.OutCgiRdf("LAYOUT_RECT",[pc.property_oracle_library])
+    cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_oracle_library])
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
