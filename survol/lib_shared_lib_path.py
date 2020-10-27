b13d1bd189b30b86b93b41e5fab135038e7b0bdc
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-27 18:48:55 +0000

    Refactoring.

--- survol/lib_shared_lib_path.py
+++ survol/lib_shared_lib_path.py
@@ -8,28 +8,29 @@ import lib_util
 # This is done only once because it should not change in a process lifetime.
 
 if lib_util.isPlatformWindows:
-	import win32api
-	library_search_path = []
-	path = win32api.GetEnvironmentVariable('PATH')
+    import win32api
+    library_search_path = []
+    path = win32api.GetEnvironmentVariable('PATH')
 
-	# try paths as described in MSDN
-	dirs = [os.getcwd(), win32api.GetSystemDirectory(), win32api.GetWindowsDirectory()] + path.split(';')
+    # try paths as described in MSDN
+    dirs = [os.getcwd(), win32api.GetSystemDirectory(), win32api.GetWindowsDirectory()] + path.split(';')
 
-	dirs_lower = set()
-	for aDir in dirs:
-		aDirLower = aDir.lower()
-		if aDirLower not in dirs_lower:
-			dirs_lower.add(aDirLower)
-			library_search_path.append(aDir)
+    dirs_lower = set()
+    for one_dir in dirs:
+        a_dir_lower = one_dir.lower()
+        if a_dir_lower not in dirs_lower:
+            dirs_lower.add(a_dir_lower)
+            library_search_path.append(one_dir)
 
 if lib_util.isPlatformLinux:
-	library_search_path = os.environ["PATH"].split(':')
+    library_search_path = os.environ["PATH"].split(':')
 
-def FindPathFromSharedLibraryName(dllFilename):
-	for aDir in library_search_path:
-		dllPath = os.path.join(aDir, dllFilename)
-		if os.path.exists(dllPath):
-			DEBUG("FindPathFromSharedLibraryName dllPath=%s",dllPath)
-			return dllPath
-	DEBUG("FindPathFromSharedLibraryName cannot find dllFilename=%s",dllFilename)
-	return None
+
+def FindPathFromSharedLibraryName(dll_filename):
+    for a_dir in library_search_path:
+        dll_path = os.path.join(a_dir, dll_filename)
+        if os.path.exists(dll_path):
+            DEBUG("FindPathFromSharedLibraryName dll_path=%s", dll_path)
+            return dll_path
+    DEBUG("FindPathFromSharedLibraryName cannot find dllFilename=%s", dll_filename)
+    return None
