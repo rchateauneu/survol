14eb6cc4c54d036484b668a923f4269948a61b12
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-27 20:27:55 +0000

    Cleanup.

--- survol/sources_types/oracle/schema/oracle_schema_functions.py
+++ survol/sources_types/oracle/schema/oracle_schema_functions.py
@@ -12,29 +12,31 @@ import lib_common
 from sources_types.oracle import schema as oracle_schema
 from sources_types.oracle import function as oracle_function
 
+
 def Main():
-	cgiEnv = lib_oracle.OracleEnv()
+    cgiEnv = lib_oracle.OracleEnv()
 
-	oraSchema = cgiEnv.m_entity_id_dict["Schema"]
+    ora_schema = cgiEnv.m_entity_id_dict["Schema"]
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'FUNCTION' AND OWNER = '" + oraSchema + "'"
-	DEBUG("sql_query=%s", sql_query )
+    sql_query = "SELECT OBJECT_NAME,STATUS,CREATED FROM ALL_OBJECTS WHERE OBJECT_TYPE = 'FUNCTION' AND OWNER = '" + ora_schema + "'"
+    DEBUG("sql_query=%s", sql_query )
 
-	node_oraschema = oracle_schema.MakeUri( cgiEnv.m_oraDatabase, oraSchema )
+    node_oraschema = oracle_schema.MakeUri(cgiEnv.m_oraDatabase, ora_schema)
 
-	result = lib_oracle.ExecuteQuery( cgiEnv.ConnectStr(), sql_query)
+    result = lib_oracle.ExecuteQuery(cgiEnv.ConnectStr(), sql_query)
 
-	for row in result:
-		functionName = str(row[0])
-		nodeFunction = oracle_function.MakeUri( cgiEnv.m_oraDatabase , oraSchema, functionName )
-		grph.add( ( node_oraschema, pc.property_oracle_function, nodeFunction ) )
+    for row in result:
+        function_name = str(row[0])
+        node_function = oracle_function.MakeUri(cgiEnv.m_oraDatabase , ora_schema, function_name)
+        grph.add((node_oraschema, pc.property_oracle_function, node_function))
 
-		lib_oracle.AddLiteralNotNone(grph,nodeFunction,"Status",row[1])
-		lib_oracle.AddLiteralNotNone(grph,nodeFunction,"Creation",row[2])
+        lib_oracle.AddLiteralNotNone(grph, node_function, "Status", row[1])
+        lib_oracle.AddLiteralNotNone(grph, node_function, "Creation", row[2])
 
-	cgiEnv.OutCgiRdf("LAYOUT_RECT",[pc.property_oracle_function])
+    cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_oracle_function])
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
