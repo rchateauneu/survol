cd0ecc863f7205b7b1754cc1e9541e9ad441ee0d
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-27 18:49:25 +0000

    Refactoring.

--- survol/lib_smb.py
+++ survol/lib_smb.py
@@ -1,24 +1,27 @@
 import re
 import lib_common
 
-def UsableNetCommands(entity_type,entity_ids_arr):
+
+def UsableNetCommands(entity_type, entity_ids_arr):
     """NET command must be available. This can be the case on Linux."""
     return True
 
-def SmbBothUriSplit(smbBoth):
-    shr_mtch = re.match( "//([^/]+)/([^/]+)/(.*)", smbBoth )
+
+def SmbBothUriSplit(smb_both):
+    """This receives a SMB share and returns its node, plus the share and the directory."""
+    shr_mtch = re.match("//([^/]+)/([^/]+)/(.*)", smb_both)
 
     if not shr_mtch:
         return None
 
-    smbShr = "//" + shr_mtch.group(1) + "/" + shr_mtch.group(2)
-    smbDir = shr_mtch.group(3)
+    smb_shr = "//" + shr_mtch.group(1) + "/" + shr_mtch.group(2)
+    smb_dir = shr_mtch.group(3)
 
     # Needed if this is the top directory.
-    if smbDir == "" or smbDir == "/" :
-        nodeSmb = lib_common.gUriGen.SmbShareUri( smbShr )
+    if smb_dir == "" or smb_dir == "/":
+        node_smb = lib_common.gUriGen.SmbShareUri(smb_shr)
     else:
         # Otherwise it is the directory of the current file.
-        nodeSmb = lib_common.gUriGen.SmbFileUri( smbShr, smbDir )
-    return nodeSmb,smbShr,smbDir
+        node_smb = lib_common.gUriGen.SmbFileUri(smb_shr, smb_dir)
+    return node_smb, smb_shr, smb_dir
 
