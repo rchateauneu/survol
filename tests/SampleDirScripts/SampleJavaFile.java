ab56b8fa26d98b2094a4889b2244103de97fe0d3
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-29 17:44:21 +0000

    New file for tests.

---
+++ tests/SampleDirScripts/SampleJavaFile.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    // This is just to test that Java files are properly detected.
+    public static void main (String[] args) {
+	System.out.println ("Hello World!");
+    }
+}
