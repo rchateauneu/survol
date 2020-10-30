271f1067b36e479e4f33a3ab1404e761cc2e0036
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-30 12:29:56 +0000

    Cleanup.

--- survol/objtypes.py
+++ survol/objtypes.py
@@ -19,47 +19,47 @@ from lib_properties import pc
 
 
 def Main():
-	cgiEnv = lib_common.CgiEnv()
+    cgiEnv = lib_common.CgiEnv()
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	rootNode = lib_util.RootUri()
+    root_node = lib_util.RootUri()
 
-	# This assumes that we have no namespace.
-	for entity_type in lib_util.ObjectTypes():
+    # This assumes that we have no namespace.
+    for entity_type in lib_util.ObjectTypes():
 
-		tmpNode = rootNode
-		idx = 0
+        tmp_node = root_node
+        idx = 0
 
-		while idx >= 0:
-			nextSlash = entity_type.find( "/", idx + 1 )
-			if nextSlash == -1:
-				intermedType = entity_type
-			else:
-				intermedType = entity_type[ : nextSlash ]
+        while idx >= 0:
+            next_slash = entity_type.find("/", idx + 1)
+            if next_slash == -1:
+                intermed_type = entity_type
+            else:
+                intermed_type = entity_type[:next_slash]
 
-			entityNode = lib_util.EntityClassNode(intermedType)
-			grph.add( ( tmpNode, pc.property_directory, entityNode ) )
+            entity_node = lib_util.EntityClassNode(intermed_type)
+            grph.add((tmp_node, pc.property_directory, entity_node))
 
-			try:
-				# This reloads all classes without cache because if it does not load
-				# we want to see the error message.
-				entity_module = lib_util.GetEntityModuleNoCatch(entity_type)
-				entDoc = entity_module.__doc__
-			except:
-				exc = sys.exc_info()[1]
-				entDoc = "Error:"+str(exc)
+            try:
+                # This reloads all classes without cache because if it does not load
+                # we want to see the error message.
+                entity_module = lib_util.GetEntityModuleNoCatch(entity_type)
+                ent_doc = entity_module.__doc__
+            except Exception as exc:
+                ent_doc = "Error:" + str(exc)
 
-			if entDoc:
-				entDoc = entDoc.strip()
-				grph.add( ( entityNode, pc.property_information, lib_common.NodeLiteral(entDoc) ) )
+            if ent_doc:
+                ent_doc = ent_doc.strip()
+                grph.add((entity_node, pc.property_information, lib_util.NodeLiteral(ent_doc)))
 
-			# TODO: If this is a CIM class, add WMI or WBEM documentation, or add the link.
+            # TODO: If this is a CIM class, add WMI or WBEM documentation, or add the link.
 
-			tmpNode = entityNode
-			idx = nextSlash
+            tmp_node = entity_node
+            idx = next_slash
 
-	cgiEnv.OutCgiRdf("LAYOUT_RECT")
+    cgiEnv.OutCgiRdf("LAYOUT_RECT")
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
