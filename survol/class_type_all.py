0ccaae4fafe5e399d85848df64a1bb009d3d53c9
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-30 09:21:47 +0000

    Cleanup.

--- survol/class_type_all.py
+++ survol/class_type_all.py
@@ -8,228 +8,235 @@ import os
 import sys
 import lib_util
 import lib_common
+
 try:
-	import lib_wbem
-	wbemOk = True
+    import lib_wbem
+    wbem_ok = True
 except ImportError:
-	wbemOk = False
+    wbem_ok = False
 import lib_wmi
 from lib_properties import pc
 
-# Now, adds the base classes of this one, at least one one level.
-def WbemAddBaseClass(grph,connWbem,wbemNode,entity_host, wbemNamespace, entity_type):
-	wbemKlass = lib_wbem.WbemGetClassObj(connWbem,entity_type,wbemNamespace)
-	if not wbemKlass:
-		return ( None, None )
 
-	superKlassName = wbemKlass.superclass
+def _wbem_add_base_class(grph, conn_wbem, wbem_node, entity_host, wbem_namespace, entity_type):
+    # Adds the base classes of this one, at least one one level."""
+    wbem_klass = lib_wbem.WbemGetClassObj(conn_wbem, entity_type, wbem_namespace)
+    if not wbem_klass:
+        return None, None
 
-	# sys.stderr.write("WBEM superKlassName=%s\n" % superKlassName)
-	# An empty string or None.
-	if not superKlassName:
-		return ( None, None )
+    super_klass_name = wbem_klass.superclass
 
-	# TODO: Should be changed, this is slow and inconvenient.
-	wbemSuperUrlsList = lib_wbem.GetWbemUrls( entity_host, wbemNamespace, superKlassName, "" )
-	if not wbemSuperUrlsList:
-		return ( None, None )
+    # sys.stderr.write("WBEM super_klass_name=%s\n" % super_klass_name)
+    # An empty string or None.
+    if not super_klass_name:
+        return None, None
 
-	# TODO: Which one should we take, http or https ???
-	wbemSuperUrl = wbemSuperUrlsList[0][0]
-	DEBUG("WBEM wbemSuperUrl=%s", wbemSuperUrl)
+    # TODO: Should be changed, this is slow and inconvenient.
+    wbem_super_urls_list = lib_wbem.GetWbemUrls(entity_host, wbem_namespace, super_klass_name, "")
+    if not wbem_super_urls_list:
+        return ( None, None )
 
-	wbemSuperNode = lib_common.NodeUrl(wbemSuperUrl)
+    # TODO: Which one should we take, http or https ???
+    wbem_super_url = wbem_super_urls_list[0][0]
+    DEBUG("WBEM wbem_super_url=%s", wbem_super_url)
 
-	grph.add( ( wbemSuperNode, pc.property_cim_subclass, wbemNode ) )
-	klaDescrip = lib_wbem.WbemClassDescription(connWbem,superKlassName,wbemNamespace)
-	if not klaDescrip:
-		klaDescrip = "Undefined class %s %s" % ( wbemNamespace, superKlassName )
-	grph.add( ( wbemSuperNode, pc.property_information, lib_common.NodeLiteral(klaDescrip ) ) )
+    wbem_super_node = lib_common.NodeUrl(wbem_super_url)
 
-	return ( wbemSuperNode, superKlassName )
+    grph.add((wbem_super_node, pc.property_cim_subclass, wbem_node))
+    kla_descrip = lib_wbem.WbemClassDescription(conn_wbem, super_klass_name, wbem_namespace)
+    if not kla_descrip:
+        kla_descrip = "Undefined class %s %s" % (wbem_namespace, super_klass_name)
+    grph.add((wbem_super_node, pc.property_information, lib_util.NodeLiteral(kla_descrip)))
 
-# Adds the list of base classes. Returns the list of pairs (name node),
-# so it can be matched againt another inheritance tree.
-def WbemAddBaseClasses(grph,connWbem,wbemNode,entity_host, wbemNamespace, entity_type):
-	pairNameNode = dict()
-	while wbemNode:
-		( wbemSuperNode, superClass ) = WbemAddBaseClass(grph,connWbem,wbemNode,entity_host, wbemNamespace, entity_type)
-		pairNameNode[entity_type] = wbemNode
-		wbemNode = wbemSuperNode
-		entity_type = superClass
-	return pairNameNode
+    return wbem_super_node, super_klass_name
 
-def CreateWbemNode(grph,rootNode,entity_host, nameSpace, className, entity_id):
-	wbemNamespace = nameSpace.replace("\\","/")
-	wbem_servers_desc_list = lib_wbem.GetWbemUrls( entity_host, wbemNamespace, className, entity_id )
 
-	# If there are no servers.
-	pairNameNode = None
+def _wbem_add_all_base_classes(grph, conn_wbem, wbem_node, entity_host, wbem_namespace, entity_type):
+    """Adds the list of base classes. Returns the list of pairs (name node),
+    so it can be matched against another inheritance tree."""
+    pair_name_node = dict()
+    while wbem_node:
+        wbem_super_node, super_class = _wbem_add_base_class(
+            grph, conn_wbem, wbem_node, entity_host, wbem_namespace, entity_type)
+        pair_name_node[entity_type] = wbem_node
+        wbem_node = wbem_super_node
+        entity_type = super_class
+    return pair_name_node
 
-	for url_server in wbem_servers_desc_list:
-		wbemNode = lib_common.NodeUrl(url_server[0])
-		grph.add( ( rootNode, pc.property_wbem_data, wbemNode ) )
 
-		wbemHostNode = lib_common.gUriGen.HostnameUri( url_server[1] )
-		grph.add( ( wbemNode, pc.property_host, wbemHostNode ) )
+def _create_wbem_node(grph, root_node, entity_host, name_space, class_name, entity_id):
+    wbem_namespace = name_space.replace("\\", "/")
+    wbem_servers_desc_list = lib_wbem.GetWbemUrls(entity_host, wbem_namespace, class_name, entity_id)
 
-		# TODO: Add a Yawn server ??
-		grph.add( ( wbemNode, pc.property_wbem_server, lib_common.NodeLiteral( url_server[1] ) ) )
+    # If there are no servers.
+    pair_name_node = None
 
-		# Now adds the description of the class.
-		try:
-			connWbem = lib_wbem.WbemConnection(entity_host)
-		except Exception as exc:
-			sys.stderr.write("WbemConnection throw:%s\n" % str(exc))
-			continue
+    for url_server in wbem_servers_desc_list:
+        wbem_node = lib_common.NodeUrl(url_server[0])
+        grph.add((root_node, pc.property_wbem_data, wbem_node))
 
-		klaDescrip = lib_wbem.WbemClassDescription(connWbem,className,wbemNamespace)
-		okWbemClass = True
-		if not klaDescrip:
-			okWbemClass = False
-			klaDescrip = "Undefined class %s %s" % ( wbemNamespace, className )
-		grph.add( ( wbemNode, pc.property_information, lib_common.NodeLiteral(klaDescrip) ) )
+        wbemHostNode = lib_common.gUriGen.HostnameUri(url_server[1])
+        grph.add( ( wbem_node, pc.property_host, wbemHostNode))
 
-		# Maybe this class is not Known in WBEM.
-		try:
-			pairNameNode = WbemAddBaseClasses(grph,connWbem,wbemNode,entity_host, nameSpace, className)
-		except:
-			pairNameNode = None
+        # TODO: Add a Yawn server ??
+        grph.add((wbem_node, pc.property_wbem_server, lib_util.NodeLiteral(url_server[1])))
 
-		if okWbemClass and wbemOk and nameSpace != "" and entity_host != "":
-			namespaceUrl = lib_wbem.NamespaceUrl(nameSpace,entity_host,className)
-			namespaceNode = lib_common.NodeUrl( namespaceUrl )
-			grph.add( ( wbemNode, pc.property_information, namespaceNode ) )
+        # Now adds the description of the class.
+        try:
+            conn_wbem = lib_wbem.WbemConnection(entity_host)
+        except Exception as exc:
+            sys.stderr.write("WbemConnection throw:%s\n" % str(exc))
+            continue
 
-	# TODO: This is a bit absurd because we return just one list.
-	return pairNameNode
+        kla_descrip = lib_wbem.WbemClassDescription(conn_wbem, class_name, wbem_namespace)
+        ok_wbem_class = True
+        if not kla_descrip:
+            ok_wbem_class = False
+            kla_descrip = "Undefined class %s %s" % (wbem_namespace, class_name)
+        grph.add((wbem_node, pc.property_information, lib_util.NodeLiteral(kla_descrip)))
 
-# Adds a WMI node and other stuff, for the class name.
-def CreateWmiNode(grph,rootNode,entity_host, nameSpace, className, entity_id):
-	wmiurl = lib_wmi.GetWmiUrl( entity_host, nameSpace, className, entity_id )
-	if wmiurl is None:
-		return
+        # Maybe this class is not Known in WBEM.
+        try:
+            pair_name_node = _wbem_add_all_base_classes(grph, conn_wbem, wbem_node, entity_host, name_space, class_name)
+        except:
+            pair_name_node = None
 
-	# There might be "http:" or the port number around the host.
-	# hostOnly = lib_util.EntHostToIp(entity_host)
-	# sys.stderr.write("entity_host=%s nameSpace=%s entity_type=%s className=%s wmiurl=%s\n" % ( entity_host, nameSpace, entity_type, className, str(wmiurl) ) )
-	wmiNode = lib_common.NodeUrl(wmiurl)
-	grph.add( ( rootNode, pc.property_wmi_data, wmiNode ) )
+        if ok_wbem_class and wbem_ok and name_space != "" and entity_host != "":
+            namespace_url = lib_wbem.NamespaceUrl(name_space, entity_host, class_name)
+            namespace_node = lib_common.NodeUrl( namespace_url )
+            grph.add((wbem_node, pc.property_information, namespace_node))
 
-	# TODO: Shame, we just did it in GetWmiUrl.
-	ipOnly = lib_util.EntHostToIp(entity_host)
-	try:
-		# It simply returns if it cannot connect.
-		connWmi = lib_wmi.WmiConnect(ipOnly,nameSpace,False)
-		if not connWmi:
-			raise Exception("Cannot connect")
-		lib_wmi.WmiAddClassQualifiers( grph, connWmi, wmiNode, className, False )
+    # TODO: This is a bit absurd because we return just one list.
+    return pair_name_node
 
-		# Now displays the base classes, to the top of the inheritance tree.
-		pairNameNode = lib_wmi.WmiAddBaseClasses(grph,connWmi,wmiNode,ipOnly, nameSpace, className)
 
-	except Exception:
-		pairNameNode = None
-		# TODO: If the class is not defined, maybe do not display it.
-		exc = sys.exc_info()[1]
-		errMsg = "WMI connection %s: %s" % (ipOnly,str(exc))
-		grph.add( ( wmiNode, lib_common.MakeProp("WMI Error"), lib_common.NodeLiteral(errMsg) ) )
+def _create_wmi_node(grph, root_node, entity_host, name_space, class_name, entity_id):
+    """Adds a WMI node and other stuff, for the class name."""
+    wmiurl = lib_wmi.GetWmiUrl(entity_host, name_space, class_name, entity_id)
+    if wmiurl is None:
+        return
 
-	urlNameSpace = lib_wmi.NamespaceUrl(nameSpace,ipOnly,className)
-	# sys.stderr.write("entity_host=%s urlNameSpace=%s\n"%(entity_host,urlNameSpace))
-	grph.add( ( wmiNode, pc.property_information, lib_common.NodeUrl(urlNameSpace) ) )
+    # There might be "http:" or the port number around the host.
+    wmi_node = lib_common.NodeUrl(wmiurl)
+    grph.add((root_node, pc.property_wmi_data, wmi_node))
 
-	return pairNameNode
+    # TODO: Shame, we just did it in GetWmiUrl.
+    ip_only = lib_util.EntHostToIp(entity_host)
+    try:
+        # It simply returns if it cannot connect.
+        conn_wmi = lib_wmi.WmiConnect(ip_only, name_space, False)
+        if not conn_wmi:
+            raise Exception("Cannot connect")
+        lib_wmi.WmiAddClassQualifiers(grph, conn_wmi, wmi_node, class_name, False)
 
-# entity_type = "CIM_Process", "Win32_Service" etc...
-# This might happen at an intermediary level, with inheritance (To be implemented).
-def AddCIMClasses(grph,rootNode,entity_host, nameSpace, className, entity_id):
-	# Maybe some of these servers are not able to display anything about this object.
+        # Now displays the base classes, to the top of the inheritance tree.
+        pair_name_node = lib_wmi.WmiAddBaseClasses(grph, conn_wmi, wmi_node, ip_only, name_space, class_name)
 
-	pairNameNodeWbem = None
-	if wbemOk:
-		if lib_wbem.ValidClassWbem(className):
-			pairNameNodeWbem = CreateWbemNode(grph,rootNode,entity_host, nameSpace, className, entity_id)
+    except Exception as exc:
+        pair_name_node = None
+        # TODO: If the class is not defined, maybe do not display it.
+        err_msg = "WMI connection %s: %s" % (ip_only,str(exc))
+        grph.add((wmi_node, lib_common.MakeProp("WMI Error"), lib_util.NodeLiteral(err_msg)))
 
-	pairNameNodeWmi = None
-	if lib_wmi.ValidClassWmi(className):
-		pairNameNodeWmi = CreateWmiNode(grph,rootNode,entity_host, nameSpace, className, entity_id)
+    url_name_space = lib_wmi.NamespaceUrl(name_space, ip_only, class_name)
+    # sys.stderr.write("entity_host=%s url_name_space=%s\n"%(entity_host,url_name_space))
+    grph.add( ( wmi_node, pc.property_information, lib_common.NodeUrl(url_name_space) ) )
 
-	# Match the two inheritance trees.
-	if pairNameNodeWbem and pairNameNodeWmi:
-		for ( baseClsNam, nodeWbem ) in lib_util.six_iteritems( pairNameNodeWbem ):
-			try:
-				nodeWmi = pairNameNodeWmi[baseClsNam]
-			except KeyError:
-				continue
+    return pair_name_node
 
-			nodeClsAll = lib_util.EntityClassNode( baseClsNam, nameSpace, entity_host, "WBEM ou WMI" )
-			grph.add( ( nodeClsAll, pc.property_wbem_data, nodeWbem))
-			grph.add( ( nodeClsAll, pc.property_wmi_data, nodeWmi))
 
-def CreateOurNode(grph,rootNode,entity_host, nameSpace, className, entity_id):
-	# This try to find a correct url for an entity type, without an entity id.
-	# At the moment, we just expect a file called "enumerate_<entity>.py"
-	enumerateScript = "enumerate_" + className + ".py"
-	# sys.stderr.write("enumerateScript=%s\n" % enumerateScript)
+def AddCIMClasses(grph, root_node, entity_host, name_space, class_name, entity_id):
+    """entity_type = "CIM_Process", "Win32_Service" etc...
+    This might happen at an intermediary level, with inheritance (To be implemented).
+    Maybe some of these servers are not able to display anything about this object."""
 
-	baseDir = lib_util.gblTopScripts + "/sources_types"
+    pair_name_node_wbem = None
+    if wbem_ok:
+        if lib_wbem.ValidClassWbem(class_name):
+            pair_name_node_wbem = _create_wbem_node(grph, root_node, entity_host, name_space, class_name, entity_id)
 
-	# TODO: This is absurd !!! Why looping, because the filename is already known !?!?
-	for dirpath, dirnames, filenames in os.walk( baseDir ):
-		# sys.stderr.write("dirpath=%s\n" % dirpath)
-		for filename in [f for f in filenames if f == enumerateScript ]:
+    pair_name_node_wmi = None
+    if lib_wmi.ValidClassWmi(class_name):
+        pair_name_node_wmi = _create_wmi_node(grph, root_node, entity_host, name_space, class_name, entity_id)
 
-			shortDir = dirpath[ len(lib_util.gblTopScripts) : ]
-			fullScriptNam = lib_util.standardized_file_path(os.path.join(shortDir, filename))
-			DEBUG("fullScriptNam=%s", fullScriptNam)
+    # Match the two inheritance trees.
+    if pair_name_node_wbem and pair_name_node_wmi:
+        for base_cls_nam, node_wbem in lib_util.six_iteritems(pair_name_node_wbem):
+            try:
+                nodeWmi = pair_name_node_wmi[base_cls_nam]
+            except KeyError:
+                continue
 
-			# TODO: Maybe remove the beginning of the file.
-			localClassUrl = lib_util.ScriptizeCimom( fullScriptNam, className, entity_host )
+            node_cls_all = lib_util.EntityClassNode(base_cls_nam, name_space, entity_host, "WBEM ou WMI")
+            grph.add((node_cls_all, pc.property_wbem_data, node_wbem))
+            grph.add((node_cls_all, pc.property_wmi_data, nodeWmi))
 
-			localClassNode =  lib_common.NodeUrl( localClassUrl )
-			grph.add( ( rootNode, pc.property_directory, localClassNode ) )
+
+def _create_our_node(grph, root_node, entity_host, name_space, class_name, entity_id):
+    """This try to find a correct url for an entity type, without an entity id.
+    At the moment, we just expect a file called "enumerate_<entity>.py" """
+    enumerate_script = "enumerate_" + class_name + ".py"
+    # sys.stderr.write("enumerate_script=%s\n" % enumerate_script)
+
+    base_dir = lib_util.gblTopScripts + "/sources_types"
+
+    # TODO: This is absurd !!! Why looping, because the filename is already known !?!?
+    for dirpath, dirnames, filenames in os.walk(base_dir):
+        # sys.stderr.write("dirpath=%s\n" % dirpath)
+        for filename in [f for f in filenames if f == enumerate_script ]:
+
+            short_dir = dirpath[len(lib_util.gblTopScripts):]
+            full_script_nam = lib_util.standardized_file_path(os.path.join(short_dir, filename))
+            DEBUG("full_script_nam=%s", full_script_nam)
+
+            # TODO: Maybe remove the beginning of the file.
+            local_class_url = lib_util.ScriptizeCimom(full_script_nam, class_name, entity_host)
+
+            local_class_node =  lib_common.NodeUrl(local_class_url)
+            grph.add((root_node, pc.property_directory, local_class_node))
+
 
 def Main():
-	# This should be able to process remote hosts because it calls scripts which can access remote data.
-	cgiEnv = lib_common.CgiEnv(can_process_remote = True)
+    """This should be able to process remote hosts because it calls scripts which can access remote data."""
+    cgiEnv = lib_common.CgiEnv(can_process_remote=True)
 
-	nameSpace, className = cgiEnv.get_namespace_type()
+    name_space, class_name = cgiEnv.get_namespace_type()
 
-	# If nameSpace is not provided, it is set to "root/CIMV2" by default.
-	if not className:
-		lib_common.ErrorMessageHtml("Class name should not be empty")
+    # If name_space is not provided, it is set to "root/CIMV2" by default.
+    if not class_name:
+        lib_common.ErrorMessageHtml("Class name should not be empty")
 
-	# Just in case ...
-	if nameSpace == "/":
-		nameSpace = ""
+    # Just in case ...
+    if name_space == "/":
+        name_space = ""
 
-	entity_host = cgiEnv.GetHost()
-	entity_id = cgiEnv.m_entity_id
+    entity_host = cgiEnv.GetHost()
+    entity_id = cgiEnv.m_entity_id
 
-	# QUERY_STRING=xid=http%3A%2F%2F192.168.1.88%3A5988%2Froot%2FPG_Internal%3APG_WBEMSLPTemplate
-	DEBUG("class_type_all entity_host=%s entity_id=%s", entity_host, entity_id )
+    # QUERY_STRING=xid=http%3A%2F%2F192.168.1.88%3A5988%2Froot%2FPG_Internal%3APG_WBEMSLPTemplate
+    DEBUG("class_type_all entity_host=%s entity_id=%s", entity_host, entity_id )
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	rootNode = lib_util.RootUri()
+    root_node = lib_util.RootUri()
 
-	objtypeNode = lib_common.NodeUrl( lib_util.uriRoot + '/objtypes.py' )
-	grph.add( ( rootNode, pc.property_rdf_data_nolist2, objtypeNode ) )
+    objtypeNode = lib_common.NodeUrl(lib_util.uriRoot + '/objtypes.py')
+    grph.add((root_node, pc.property_rdf_data_nolist2, objtypeNode))
 
-	# This displays the documentation of the Python module of this entity class.
-	entity_module = lib_util.GetEntityModule(className)
-	entDoc = entity_module.__doc__
-	if entDoc:
-		entDoc = entDoc.strip()
-		grph.add( ( rootNode, pc.property_information, lib_common.NodeLiteral(entDoc) ) )
+    # This displays the documentation of the Python module of this entity class.
+    entity_module = lib_util.GetEntityModule(class_name)
+    ent_doc = entity_module.__doc__
+    if ent_doc:
+        ent_doc = ent_doc.strip()
+        grph.add((root_node, pc.property_information, lib_util.NodeLiteral(ent_doc)))
 
-	CreateOurNode(grph,rootNode,entity_host, nameSpace, className, entity_id)
+    _create_our_node(grph, root_node, entity_host, name_space, class_name, entity_id)
 
-	# Do this for each intermediary entity type (Between slashes).
-	AddCIMClasses(grph,rootNode,entity_host, nameSpace, className, entity_id)
+    # Do this for each intermediary entity type (Between slashes).
+    AddCIMClasses(grph, root_node, entity_host, name_space, class_name, entity_id)
 
-	cgiEnv.OutCgiRdf("LAYOUT_RECT_TB")
+    cgiEnv.OutCgiRdf("LAYOUT_RECT_TB")
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
