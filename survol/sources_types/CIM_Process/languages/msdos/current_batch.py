183d2b26d26758536fb28b816776dae34fd5ae1b
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-10-31 09:32:37 +0000

    Cleanup.

--- survol/sources_types/CIM_Process/languages/msdos/current_batch.py
+++ survol/sources_types/CIM_Process/languages/msdos/current_batch.py
@@ -41,54 +41,51 @@ Usable = survol_msdos.Usable
 # /V:OFF  Disable delayed environment expansion.
 
 
-
 def Main():
     cgiEnv = lib_common.CgiEnv()
-    pidProc = int( cgiEnv.GetId() )
+    pid_proc = int(cgiEnv.GetId())
 
     grph = cgiEnv.GetGraph()
 
-    node_process = lib_common.gUriGen.PidUri(pidProc)
-    proc_obj = CIM_Process.PsutilGetProcObj(pidProc)
+    node_process = lib_common.gUriGen.PidUri(pid_proc)
+    proc_obj = CIM_Process.PsutilGetProcObj(pid_proc)
 
     # Python 2
     # cmd_arr=['C:\\Python27\\python.exe', 'test_survol_client_library.py', '--debug', 'SurvolLocalTest.test_msdos_current_batch']
     # Python 3
     # cmd_arr=['C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\Python36_64\\python.exe', 'test_survol_client_library.py', '--debug', 'SurvolLocalTest.test_msdos_current_batch']
-    argvArray = CIM_Process.PsutilProcToCmdlineArray(proc_obj)
-    DEBUG("argvArray=%s",str(argvArray))
+    argv_array = CIM_Process.PsutilProcToCmdlineArray(proc_obj)
+    DEBUG("argv_array=%s", str(argv_array))
 
     # This extracts the command file name and creates a node for it.
-    for theArg in argvArray[1:]:
-        if theArg[0] == "/":
+    for the_arg in argv_array[1:]:
+        if the_arg[0] == "/":
             continue
 
         # Check if the file exists in the current directory.
-        currPwd,errMsg = CIM_Process.PsutilProcCwd(proc_obj)
-        if not currPwd:
+        curr_pwd, err_msg = CIM_Process.PsutilProcCwd(proc_obj)
+        if not curr_pwd:
             break
 
-        allDirsToSearch = [ currPwd ]
+        all_dirs_to_search = [curr_pwd]
 
-        envPath = CIM_Process.GetEnvVarProcess("PATH",proc_obj.pid)
-        if envPath:
-            allDirsToSearch += envPath.split(";")
+        env_path = CIM_Process.GetEnvVarProcess("PATH", proc_obj.pid)
+        if env_path:
+            all_dirs_to_search += env_path.split(";")
 
         # Now tries all possible dirs, starting with current directory.
-        for aDir in allDirsToSearch:
-            fullScriptPath = os.path.join(aDir,theArg)
-            DEBUG("fullScriptPath=%s",fullScriptPath)
-            if os.path.isfile(fullScriptPath):
-                DEBUG("fullScriptPath=%s",fullScriptPath)
-                scriptNode = lib_common.gUriGen.FileUri( fullScriptPath )
-                grph.add( ( node_process, pc.property_runs, scriptNode ) )
+        for a_dir in all_dirs_to_search:
+            full_script_path = os.path.join(a_dir, the_arg)
+            DEBUG("full_script_path=%s", full_script_path)
+            if os.path.isfile(full_script_path):
+                DEBUG("full_script_path=%s",full_script_path)
+                script_node = lib_common.gUriGen.FileUri(full_script_path)
+                grph.add( (node_process, pc.property_runs, script_node))
                 break
 
         break
-
-
-
     cgiEnv.OutCgiRdf()
+
 
 if __name__ == '__main__':
     Main()
