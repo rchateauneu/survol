fa07324cf27f7dd6e4824b70f06b86854698cfe6
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-03 15:06:57 +0000

    Cleanup.

--- survol/sources_types/enumerate_cgroup.py
+++ survol/sources_types/enumerate_cgroup.py
@@ -26,26 +26,27 @@ from sources_types.Linux import cgroup as survol_cgroup
 
 Usable = lib_util.UsableLinux
 
+
 def Main():
     cgiEnv = lib_common.CgiEnv()
     grph = cgiEnv.GetGraph()
 
-    filCG = open("/proc/cgroups")
-    propCGroup = lib_common.MakeProp("cgroup")
+    fil_cg = open("/proc/cgroups")
+    prop_cgroup = lib_common.MakeProp("cgroup")
 
-    linHeader = filCG.readline()
-    for linCG in filCG.readlines():
-        splitCG = linCG.split("\t")
-        cgroupName = splitCG[0]
-        cgroupNode = survol_cgroup.MakeUri(cgroupName)
-        grph.add( ( cgroupNode, lib_common.MakeProp("Hierarchy"), lib_util.NodeLiteral(splitCG[1] ) ) )
-        grph.add( ( cgroupNode, lib_common.MakeProp("Num cgroups"), lib_util.NodeLiteral(splitCG[2] ) ) )
-        grph.add( ( cgroupNode, lib_common.MakeProp("Enabled"), lib_util.NodeLiteral(splitCG[3] ) ) )
+    linHeader = fil_cg.readline()
+    for lin_cg in fil_cg.readlines():
+        split_cg = lin_cg.split("\t")
+        cgroup_name = split_cg[0]
+        cgroup_node = survol_cgroup.MakeUri(cgroup_name)
+        grph.add((cgroup_node, lib_common.MakeProp("Hierarchy"), lib_util.NodeLiteral(split_cg[1])))
+        grph.add((cgroup_node, lib_common.MakeProp("Num cgroups"), lib_util.NodeLiteral(split_cg[2])))
+        grph.add((cgroup_node, lib_common.MakeProp("Enabled"), lib_util.NodeLiteral(split_cg[3])))
 
-        grph.add( ( lib_common.nodeMachine, propCGroup, cgroupNode ) )
+        grph.add((lib_common.nodeMachine, prop_cgroup, cgroup_node))
 
-        
-    cgiEnv.OutCgiRdf("LAYOUT_RECT", [propCGroup] )
+    cgiEnv.OutCgiRdf("LAYOUT_RECT", [prop_cgroup])
+
 
 if __name__ == '__main__':
     Main()
