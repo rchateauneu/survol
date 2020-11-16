9a56155e35b021d4972470ca0bfed1b70bba1b6d
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-16 23:11:21 +0000

    Cleanup.

--- survol/sources_types/dbus/connection/__init__.py
+++ survol/sources_types/dbus/connection/__init__.py
@@ -10,40 +10,41 @@ import dbus
 from lib_properties import pc
 
 def EntityOntology():
-	return ( ["Bus","Connect"], )
+    return (["Bus", "Connect"],)
 
-# This must add information about the dbus connection.
-def AddInfo(grph,node,entity_ids_arr):
-	# Example: entity_id=['system', 'org.freedesktop.UDisks2']
-	# sys.stderr.write("AddInfo entity_id=%s\n" % str(entity_ids_arr) )
 
-	try:
-		busAddr = entity_ids_arr[0]
-		# sys.stderr.write("AddInfo busAddr=%s\n" % ( busAddr ) )
-		theBus = lib_dbus.MakeBusFromAddress( busAddr )
+def AddInfo(grph, node, entity_ids_arr):
+    """This must add information about the dbus connection.
+    Example: entity_id=['system', 'org.freedesktop.UDisks2']
+    """
+    # sys.stderr.write("AddInfo entity_id=%s\n" % str(entity_ids_arr) )
 
-		connctNam = entity_ids_arr[1]
-		# sys.stderr.write("AddInfo connctNam=%s\n" % ( connctNam ) )
+    try:
+        bus_addr = entity_ids_arr[0]
+        # sys.stderr.write("AddInfo bus_addr=%s\n" % bus_addr)
+        the_bus = lib_dbus.MakeBusFromAddress(bus_addr)
 
-		proxy=theBus.get_object('org.freedesktop.DBus', '/org/freedesktop/DBus')
-		# sys.stderr.write("AddInfo proxy=%s\n" % str( proxy ) )
+        connct_nam = entity_ids_arr[1]
+        # sys.stderr.write("AddInfo connct_nam=%s\n" % connct_nam)
 
-		# itf = proxy
-		itf = dbus.Interface(proxy, 'org.freedesktop.DBus')
+        proxy=the_bus.get_object('org.freedesktop.DBus', '/org/freedesktop/DBus')
+        # sys.stderr.write("AddInfo proxy=%s\n" % str(proxy))
 
-		pid = str(itf.GetConnectionUnixProcessID(connctNam))
-		uid = itf.GetConnectionUnixUser(connctNam)
-		# sys.stderr.write("AddInfo pid=%s uid=%s\n" % ( pid, uid ) )
-	except dbus.exceptions.DBusException:
-		# Helas: "org.freedesktop.DBus.Error.AccessDenied"
-		exc = sys.exc_info()[1]
-		WARNING("AddInfo Caught=%s", str(exc) )
-		# Apparently happens with the first line.
-		return
-	usrnam = pwd.getpwuid( uid ).pw_name
+        # itf = proxy
+        itf = dbus.Interface(proxy, 'org.freedesktop.DBus')
 
-	nodeProc = lib_common.gUriGen.PidUri(pid)
-	nodeUser = lib_common.gUriGen.UserUri(usrnam)
-	grph.add( ( node, pc.property_pid, nodeProc ) )
-	grph.add( ( node, pc.property_user, nodeUser ) )
+        pid = str(itf.GetConnectionUnixProcessID(connct_nam))
+        uid = itf.GetConnectionUnixUser(connct_nam)
+        # sys.stderr.write("AddInfo pid=%s uid=%s\n" % (pid, uid))
+    except dbus.exceptions.DBusException as exc:
+        # Could be: "org.freedesktop.DBus.Error.AccessDenied"
+        WARNING("AddInfo Caught=%s", str(exc))
+        # Apparently happens with the first line.
+        return
+    usrnam = pwd.getpwuid(uid).pw_name
+
+    node_proc = lib_common.gUriGen.PidUri(pid)
+    node_user = lib_common.gUriGen.UserUri(usrnam)
+    grph.add((node, pc.property_pid, node_proc))
+    grph.add((node, pc.property_user, node_user))
 
