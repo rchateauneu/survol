0e86f9d4abde16b548932f8cb5948d5e386592b3
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-17 19:18:55 +0000

    Cleanup.

--- survol/sources_types/mysql/instance/instance_databases.py
+++ survol/sources_types/mysql/instance/instance_databases.py
@@ -4,7 +4,6 @@
 Databases in a MySql instance
 """
 
-
 import sys
 import re
 import socket
@@ -18,62 +17,58 @@ from sources_types import mysql as survol_mysql
 from sources_types.mysql import database as survol_mysql_database
 from sources_types.mysql import instance as survol_mysql_instance
 
+
 def Main():
+    cgiEnv = lib_common.CgiEnv( )
 
-	cgiEnv = lib_common.CgiEnv( )
+    instance_name = cgiEnv.m_entity_id_dict["Instance"]
+    instance_node = survol_mysql_instance.MakeUri(instance_name)
 
-	instanceName = cgiEnv.m_entity_id_dict["Instance"]
-	instanceNode = survol_mysql_instance.MakeUri(instanceName)
+    hostname, hostport = survol_mysql.InstanceToHostPort(instance_name)
 
-	(hostname,hostport) = survol_mysql.InstanceToHostPort(instanceName)
+    cgiEnv = lib_common.CgiEnv()
 
-	cgiEnv = lib_common.CgiEnv()
+    grph = cgiEnv.GetGraph()
 
-	grph = cgiEnv.GetGraph()
+    # BEWARE: The rule whether we use the host name or the host IP is not very clear !
+    # The IP address would be unambiguous but less clear.
+    host_node = lib_common.gUriGen.HostnameUri(hostname)
 
-	hostAddr = lib_util.GlobalGetHostByName(hostname)
+    prop_db = lib_common.MakeProp("Mysql database")
 
-	# BEWARE: The rule whether we use the host name or the host IP is not very clear !
-	# The IP address would be unambiguous but less clear.
-	hostNode = lib_common.gUriGen.HostnameUri(hostname)
+    a_cred = lib_credentials.GetCredentials("MySql", instance_name)
 
-	# BEWARE: This is duplicated.
-	propDb = lib_common.MakeProp("Mysql database")
+    # If user/password incorrect, nothing we can do.
+    try:
+        a_user = a_cred[0]
+        conn_mysql = survol_mysql.MysqlConnect(instance_name, a_user, aPass=a_cred[1])
+    except Exception as exc:
+        lib_common.ErrorMessageHtml("Cannot connect to instance=%s user=%s:%s" % (instance_name, a_user, str(exc)))
 
-	aCred = lib_credentials.GetCredentials("MySql", instanceName)
+    cursor_mysql = conn_mysql.cursor()
 
-	# If user/password incorrect, nothing we can do.
-	try:
-		aUser = aCred[0]
-		connMysql = survol_mysql.MysqlConnect(instanceName,aUser,aPass=aCred[1])
-	except :
-		exc = sys.exc_info()
-		lib_common.ErrorMessageHtml("Cannot connect to instance=%s user=%s:%s"%(instanceName,aUser,str(exc)))
+    cursor_mysql.execute("show databases")
 
-	cursorMysql = connMysql.cursor()
+    grph.add((host_node, lib_common.MakeProp("Mysql instance"), instance_node))
 
-	cursorMysql.execute("show databases")
+    for db_info in cursor_mysql:
+        #('information_schema',)
+        #('primhilltcsrvdb1',)
+        DEBUG("db_info=%s", str(db_info))
+        db_nam = db_info[0]
 
-	propDb = lib_common.MakeProp("Mysql database")
+        node_mysql_database = survol_mysql_database.MakeUri(instance_name, db_nam)
 
-	grph.add( ( hostNode, lib_common.MakeProp("Mysql instance"), instanceNode ) )
+        # Create a node for each database.
+        user_node = lib_common.gUriGen.UserUri(a_cred[0])
+        grph.add((node_mysql_database, pc.property_user, user_node))
+        grph.add((instance_node, prop_db, node_mysql_database))
 
-	for dbInfo in cursorMysql:
-		#('information_schema',)
-		#('primhilltcsrvdb1',)
-		DEBUG("dbInfo=%s",str(dbInfo))
-		dbNam = dbInfo[0]
+    cursor_mysql.close()
+    conn_mysql.close()
 
-		nodeMysqlDatabase = survol_mysql_database.MakeUri(instanceName,dbNam)
+    cgiEnv.OutCgiRdf("LAYOUT_SPLINE")
 
-		# Create a node for each database.
-		grph.add( ( nodeMysqlDatabase, pc.property_user, lib_util.NodeLiteral(aCred[0]) ) )
-		grph.add( ( instanceNode, propDb, nodeMysqlDatabase ) )
-
-	cursorMysql.close()
-	connMysql.close()
-
-	cgiEnv.OutCgiRdf("LAYOUT_SPLINE")
 
 if __name__ == '__main__':
-	Main()
+    Main()
