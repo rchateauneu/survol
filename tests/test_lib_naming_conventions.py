dc8b7d6644b55f0d9f930580ebb74f2c9fa76e62
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-18 10:31:58 +0000

    Fix docstring.

--- tests/test_lib_naming_conventions.py
+++ tests/test_lib_naming_conventions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-"""Tests the ability to detect repetieion in sequences of symbols.
-This is designed for sequences of system calls.
+"""Tests the file lib_naming_conventions.py.
 """
 
 from __future__ import print_function
