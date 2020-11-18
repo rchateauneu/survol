53ff42ccc332abdba37ccd9440f4d7625cff402e
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-18 23:32:32 +0000

    Comment.

--- survol/entity_dirmenu_only.py
+++ survol/entity_dirmenu_only.py
@@ -16,6 +16,8 @@ import lib_util
 import lib_common
 from lib_properties import pc
 
+# TODO: When converted in RDF, the URL should use see_also, PredicateSeeAlso, RDFS.seeAlso
+
 
 def _test_usability(imported_module, entity_type, entity_ids_arr):
     """
