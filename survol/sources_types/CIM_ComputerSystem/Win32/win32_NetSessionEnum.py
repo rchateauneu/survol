59b83888058ce1ecb53f30f079eb36f5e5a9d729
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-21 23:14:22 +0000

    Cleanup.

--- survol/sources_types/CIM_ComputerSystem/Win32/win32_NetSessionEnum.py
+++ survol/sources_types/CIM_ComputerSystem/Win32/win32_NetSessionEnum.py
@@ -13,31 +13,31 @@ import lib_win32
 import win32net
 
 def Main():
-	cgiEnv = lib_common.CgiEnv()
-	hostname = cgiEnv.GetId()
+    cgiEnv = lib_common.CgiEnv()
+    hostname = cgiEnv.GetId()
 
-	nodeHost = lib_common.gUriGen.HostnameUri(hostname)
+    node_host = lib_common.gUriGen.HostnameUri(hostname)
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	# Return the name of the computer, name of the user, and active and idle times for the session.
-	# No special group membership is required for level 0 or level 10 calls.
-	level = 10
+    # Return the name of the computer, name of the user, and active and idle times for the session.
+    # No special group membership is required for level 0 or level 10 calls.
+    level = 10
 
-	try:
-		# hostname = "Titi" for example
-		lib_win32.WNetAddConnect(hostname)
+    try:
+        lib_win32.WNetAddConnect(hostname)
 
-		sessionList = win32net.NetSessionEnum(level, hostname)
-	except Exception:
-		lib_common.ErrorMessageHtml("Hostname="+hostname+". Exception:"+str(sys.exc_info()))
+        session_list = win32net.NetSessionEnum(level, hostname)
+    except Exception as exc:
+        lib_common.ErrorMessageHtml("Hostname="+hostname+". Exception:"+str(exc))
 
-	for eltLst in sessionList:
-		for keyLst in eltLst:
-			valLst = eltLst[keyLst]
-			grph.add( ( nodeHost, lib_util.MakeProp(keyLst), lib_util.NodeLiteral( valLst ) ) )
+    for elt_lst in session_list:
+        for key_lst in elt_lst:
+            val_lst = elt_lst[key_lst]
+            grph.add((node_host, lib_common.MakeProp(key_lst), lib_util.NodeLiteral(val_lst)))
 
-	cgiEnv.OutCgiRdf()
+    cgiEnv.OutCgiRdf()
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
