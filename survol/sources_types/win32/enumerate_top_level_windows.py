dc4df52d9c4b076c2c61fb99aaf364051bd57a98
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-21 14:52:50 +0000

    Refactor.

--- survol/sources_types/win32/enumerate_top_level_windows.py
+++ survol/sources_types/win32/enumerate_top_level_windows.py
@@ -14,56 +14,58 @@ from sources_types import CIM_Process
 import win32gui
 import win32process
 
-def windowEnumerationHandler(hwnd, topWindowsHnd):
+
+def _window_enumeration_handler(hwnd, top_windows_hnd):
 	if win32gui.IsWindowVisible(hwnd):
-		topWindowsHnd.append(hwnd)
+		top_windows_hnd.append(hwnd)
+
 
 def Main():
 	cgiEnv = lib_common.CgiEnv()
 
 	grph = cgiEnv.GetGraph()
 
-	rootNode = lib_common.nodeMachine
+	root_node = lib_common.nodeMachine
 
-	topWindowsHnd = []
-	win32gui.EnumWindows(windowEnumerationHandler, topWindowsHnd)
+	top_windows_hnd = []
+	win32gui.EnumWindows(_window_enumeration_handler, top_windows_hnd)
 
-	prpProcToWindow = lib_common.MakeProp("Top_level_window")
+	prp_proc_to_window = lib_util.MakeProp("Top_level_window")
 
-	def PidToNode(pid):
+	def pid_to_node(pid):
 		try:
-			nodPid = PidToNode.Cache[pid]
+			nod_pid = pid_to_node.Cache[pid]
 		except KeyError:
-			nodPid = lib_common.gUriGen.PidUri(pid)
-			PidToNode.Cache[pid] = nodPid
+			nod_pid = lib_common.gUriGen.PidUri(pid)
+			pid_to_node.Cache[pid] = nod_pid
 
-			grph.add( (nodPid, pc.property_pid, lib_util.NodeLiteral(pid) ) )
-			grph.add( (rootNode, pc.property_host, nodPid ) )
+			grph.add((nod_pid, pc.property_pid, lib_util.NodeLiteral(pid)))
+			grph.add((root_node, pc.property_host, nod_pid))
 
-		return nodPid
+		return nod_pid
 
-	PidToNode.Cache = dict()
+	pid_to_node.Cache = dict()
 
+	sys.stdout.write("Len=%d\n" % len(top_windows_hnd))
 
-	sys.stdout.write("Len=%d\n"%len(topWindowsHnd))
-
-	for hwnd in topWindowsHnd:
-		wnText = win32gui.GetWindowText(hwnd)
-		thrId, procId = win32process.GetWindowThreadProcessId(hwnd)
-		nodProcess = PidToNode(procId)
-		DEBUG("procId=%d wnText=%s",procId,wnText)
-		if wnText:
-			# wnText = wnText.encode("ascii" ,errors='replace')
+	for hwnd in top_windows_hnd:
+		wn_text = win32gui.GetWindowText(hwnd)
+		thr_id, proc_id = win32process.GetWindowThreadProcessId(hwnd)
+		nod_process = pid_to_node(proc_id)
+		DEBUG("proc_id=%d wn_text=%s",proc_id,wn_text)
+		if wn_text:
+			# wn_text = wn_text.encode("ascii" ,errors='replace')
 			# It drops the accent: "Livres, BD, Vidos"
 			try:
 				# Python 3: "AttributeError: 'str' object has no attribute 'decode' "
-				wnText = wnText.decode("utf8" ,'ignore')
+				wn_text = wn_text.decode("utf8" ,'ignore')
 			except:
 				# If Python 3, nothing to do>
 				pass
-			grph.add( (nodProcess, prpProcToWindow, lib_util.NodeLiteral(wnText) ) )
+			grph.add((nod_process, prp_proc_to_window, lib_util.NodeLiteral(wn_text)))
 
-	cgiEnv.OutCgiRdf("LAYOUT_RECT", [prpProcToWindow])
+	cgiEnv.OutCgiRdf("LAYOUT_RECT", [prp_proc_to_window])
+
 
 if __name__ == '__main__':
 	Main()
