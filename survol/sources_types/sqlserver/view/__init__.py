6a10486a759452ac615eb28be7e4c57efbf2f333
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-21 23:11:46 +0000

    Cleanup.

--- survol/sources_types/sqlserver/view/__init__.py
+++ survol/sources_types/sqlserver/view/__init__.py
@@ -5,20 +5,23 @@ Sqlserver view
 import lib_common
 from sources_types.sqlserver import schema as sqlserver_schema
 
-def AddInfo(grph,node,entity_ids_arr):
-	# TODO: Ca serait quand meme mieux de passer au AddInfo un dict plutot qu un tableau.
-	dsnNam = entity_ids_arr[0]
-	schemaNam = entity_ids_arr[1]
-	nodeSchema = sqlserver_schema.MakeUri(dsnNam,schemaNam)
 
-	grph.add( ( nodeSchema, lib_util.MakeProp("sqlserver view"), node ) )
+def AddInfo(grph,node,entity_ids_arr):
+    dsn_nam = entity_ids_arr[0]
+    schema_nam = entity_ids_arr[1]
+    node_schema = sqlserver_schema.MakeUri(dsn_nam, schema_nam)
+
+    grph.add((node_schema, lib_common.MakeProp("sqlserver view"), node))
+
 
 def EntityOntology():
-	return ( ["Dsn", "Schema", "View"], )
+    return (["Dsn", "Schema", "View"],)
+
 
 # Beware of the possible confusion with normal users.
-def MakeUri(dsnNam,schemaName,viewName):
-	return lib_common.gUriGen.UriMakeFromDict("sqlserver/view", { "Dsn" : dsnNam, "Schema" : schemaName, "View" : viewName } )
+def MakeUri(dsn_nam, schema_name, view_name):
+    return lib_common.gUriGen.UriMakeFromDict("sqlserver/view", {"Dsn": dsn_nam, "Schema": schema_name, "View": view_name})
+
 
 def EntityName(entity_ids_arr):
-	return entity_ids_arr[0] + "." + entity_ids_arr[1] + "." + entity_ids_arr[2]
+    return entity_ids_arr[0] + "." + entity_ids_arr[1] + "." + entity_ids_arr[2]
