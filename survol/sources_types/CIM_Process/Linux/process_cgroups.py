de930057a72e2716af66c73915d075c50536629b
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-21 23:30:41 +0000

    Cleanup.

--- survol/sources_types/CIM_Process/Linux/process_cgroups.py
+++ survol/sources_types/CIM_Process/Linux/process_cgroups.py
@@ -21,35 +21,34 @@ from sources_types.Linux import cgroup as survol_cgroup
 def Main():
     cgiEnv = lib_common.CgiEnv()
     try:
-        thePid = int( cgiEnv.GetId() )
+        the_pid = int(cgiEnv.GetId())
     except Exception:
         lib_common.ErrorMessageHtml("Must provide a pid")
 
     grph = cgiEnv.GetGraph()
 
-    proc_obj = CIM_Process.PsutilGetProcObj(thePid)
+    node_process = lib_common.gUriGen.PidUri(the_pid)
+    CIM_Process.AddInfo(grph, node_process, [str(the_pid)])
 
-    node_process = lib_common.gUriGen.PidUri(thePid)
-    CIM_Process.AddInfo( grph, node_process, [ str(thePid) ] )
+    fil_cgroups = "/proc/%d/cgroup" % the_pid
 
-    filCGroups = "/proc/%d/cgroup" % thePid
-
-    for lin_cg in open(filCGroups):
+    for lin_cg in open(fil_cgroups):
         split_cg = lin_cg.split(':')
         hierarchy = split_cg[0]
         subsys_name_list = split_cg[1]
         mount_path = split_cg[2]
         mount_path = mount_path[:-1] # Strip trailing backslash-N
-        mount_path_node = lib_common.gUriGen.DirectoryUri( mount_path )
+        mount_path_node = lib_common.gUriGen.DirectoryUri(mount_path)
 
         for subsys_name in subsys_name_list.split(","):
             if subsys_name:
-                cgrpNode = survol_cgroup.MakeUri( subsys_name )
-                grph.add( ( node_process, lib_util.MakeProp("CGroup"), cgrpNode ) )
-                grph.add( ( cgrpNode, lib_util.MakeProp("Hierarchy"), lib_util.NodeLiteral(hierarchy) ) )
-                grph.add( ( cgrpNode, lib_util.MakeProp("Control group path"), mount_path_node ) )
+                cgrpNode = survol_cgroup.MakeUri(subsys_name)
+                grph.add((node_process, lib_common.MakeProp("CGroup"), cgrpNode))
+                grph.add((cgrpNode, lib_common.MakeProp("Hierarchy"), lib_util.NodeLiteral(hierarchy)))
+                grph.add((cgrpNode, lib_common.MakeProp("Control group path"), mount_path_node))
 
     cgiEnv.OutCgiRdf()
+
 
 if __name__ == '__main__':
     Main()
