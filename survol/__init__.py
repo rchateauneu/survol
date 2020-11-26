de09327c6854d10a5cb9c294fd269a980e9e3b2e
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-11-26 07:01:45 +0000

    New release.

--- survol/__init__.py
+++ survol/__init__.py
@@ -8,5 +8,5 @@ to help understand an existing information system and legacy applications.
 
 __author__ = 'Primhill Computers'
 __email__ = 'contact@primhillcomputers.com'
-__version__ = '1.0.30'
+__version__ = '1.0.31'
 __version_info__ = [s for s in __version__.split(".")]
