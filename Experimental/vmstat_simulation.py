ba94a0349f16698d6ad78f65827c5206aaa237e2
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-12-13 19:03:01 +0000

    Add counter.

--- Experimental/vmstat_simulation.py
+++ Experimental/vmstat_simulation.py
@@ -11,22 +11,23 @@ def _print_header():
 """procs -----------memory---------- ---swap-- -----io---- -system-- ------cpu-----
  r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa st""")
 
+_counter = 0
 
 def _print_one_line():
-    print(
-" 0  0 188160 342936 121352 1379096    0    0     1    45    1    1  1  1 97  0  0"
-    )
+    global _counter
+    print(" %d  0 188160 342936 121352 1379096    0    0     1    45    1    1  1  1 97  0  0" % _counter)
+    _counter += 1
 
-sys.stdout.write("START " + __file__)
-sys.stderr.write("START " + __file__)
+#sys.stdout.write("START " + __file__)
+#sys.stderr.write("START " + __file__)
 
 _print_header()
 _print_one_line()
 if len(sys.argv) > 1:
     delay = int(sys.argv[1])
     while True:
-        sys.stdout.write("LOOP " + __file__)
-        sys.stderr.write("LOOP " + __file__)
+        #sys.stdout.write("LOOP " + __file__)
+        #sys.stderr.write("LOOP " + __file__)
         time.sleep(delay)
         _print_one_line()
 
