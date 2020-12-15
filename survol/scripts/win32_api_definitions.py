2577e5862830dfda6072f80141534b4a4d562034
Remi Chateauneu <remi.chateauneu@gmail.com>
2020-12-15 21:06:55 +0000

    Cleanup.

--- survol/scripts/win32_api_definitions.py
+++ survol/scripts/win32_api_definitions.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 __author__      = "Remi Chateauneu"
 __copyright__   = "Primhill Computers, 2018-2020"
-__credits__ = ["","",""]
+__credits__ = ["", "", ""]
 __license__ = "GPL"
 __version__ = "0.0.1"
 __maintainer__ = "Remi Chateauneu"
