5ba688f597b17e97169cc5b5534fc76a7cf356a3
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-01-02 22:08:50 +0000

    New function _url_to_process_name() is reversible.

--- survol/lib_daemon.py
+++ survol/lib_daemon.py
@@ -1,5 +1,7 @@
 
-# This expects that the daemon factory, i.e. the supervisor, is running.
+"""
+This expects that the daemon factory, i.e. the supervisor, is running.
+"""
 
 import os
 import sys
@@ -8,41 +10,82 @@ from scripts import daemon_factory
 # BEWARE: This CANNOT import lib_common, otherwise circular reference.
 import lib_util
 
-# Test if a CGI script is running.
 
-# Get the events created by the daemon process running the Deamon() function of a CGI script.
+def _char_to_hex(one_char):
+    """ 'a' => '61' """
+    return format(ord(one_char), "x")
+
+
+def _hex_to_char(chr_pair):
+    """ '61' => 'a' """
+    return bytearray.fromhex(chr_pair).decode()
+
+
+# Underscore "_" is not forbidden but is used to encode forbidden chars,
+# therefore it must be encoded too.
+_forbidden_chars = ":/\\?=&+*()[]{}%." + "_"
 
 
 def _url_to_process_name(script_url):
-    assert "mode=" not in script_url
-    # Some characters are not accepted by process names by supervisor, which throws for example:
-    # <Fault 2: "INCORRECT_PARAMETERS: Invalid name: 'http://vps516494.ovh.net/x/y/z/script.py?param=24880'
-    # because of character: ':' in section 'http://vps516494.ovh.net/x/y/z/script.py?param=24880'>
-    # Also, the process name is used to create stdout and stderr log file names,
-    # so the process name must contain only chars allowed in filenames.
-    for forbidden_char in ":/\\?=&+*()[]{}%.":
-        script_url = script_url.replace(forbidden_char, "_")
+    """
+    This is one of the isolation layer between Survol daemons and urls one one side,
+    and supervisor processes on the other side.
+
+    Survol needs to run a daemon for some scripts plus their CGI arguments: These CGI arguments
+    define an unique object: Its class and the attributes defined in the ontology.
+    On the other hand, the supervisor library has no idea of what urls does, it just knowns process names.
+
+    Some characters are not accepted by process names by supervisor, which throws for example:
+    <Fault 2: "INCORRECT_PARAMETERS: Invalid name: 'http://vps516494.ovh.net/x/y/z/script.py?param=24880'
+    because of character: ':' in section 'http://vps516494.ovh.net/x/y/z/script.py?param=24880'>
+
+    Also, the process name is used to create stdout and stderr log file names,
+    so the process name must contain only chars allowed in filenames.
+    """
+
+    process_name = ""
+    # The encoding scheme uses an undersocre to prefix the hexadecimal value.
+    # Therefore, underscores must also be encoded although they are acceptable by supervisor library.
+    for one_char in script_url:
+        if one_char in _forbidden_chars:
+            process_name += "_" + _char_to_hex(one_char)
+        else:
+            process_name += one_char
 
     # start_user_process: start_user_process exc=<Fault 2:
     # "INCORRECT_PARAMETERS: No closing quotation in section 'program:ama_123_paramb_START'
     # (file: 'survol/scripts/supervisord.conf')">
+    return process_name
 
-    # Strips "_" at the beginning because it is confusing. It should not happen many times.
-    while script_url.startswith("_"):
-        script_url = script_url[1:]
+
+def _process_name_to_url(process_name):
+    """
+    This transforms a process name created for supervisor, into the original URL.
+    There might be alimitiation if the process name cannot be long enough,
+    """
+    script_url = ""
+    index = 0
+    # This is not extremely fast but it does not matter because it is rarely called and the string is not very long.
+    while index < len(process_name):
+        if process_name[index] == "_":
+            # Prefix of an encoded char,
+            script_url += _hex_to_char(process_name[index+1:index+3])
+            index += 3
+        else:
+            # This char is not encoded.
+            script_url += process_name[index]
+            index += 1
     return script_url
 
 
 def start_events_generator_daemon(script_url):
     """
+    This starts a daemon running the url, but in daemon mode instaed of snapshot mode.
+    This url is a CGI script plus CGI arguments defining an object: The class and the attributes values.
 
     :param script_url: Something like http://vps516494.ovh.net/Survol/survol/sources_types/enumerate_CIM_Process.py?xid=.
     :return: True if it worked.
     """
-
-    # Parse the URL, finds the script, fails if script not there.
-    # The host is not important but check that this is the current machine.
-    # Look for the string "sources_types" starting from the end.
 
     # http://vps516494.ovh.net/Survol/survol/print_environment_variables.py?toto=1&tutu=2
     # QUERY_STRING	toto=1&tutu=2
@@ -54,6 +97,9 @@ def start_events_generator_daemon(script_url):
     # ParseResult(scheme='http', netloc='vps516494.ovh.net', path='/Survol/survol/print_environment_variables.py',
     # params='', query='toto=1&tutu=2', fragment='')
 
+    # Parse the URL, finds the script, fails if script not there.
+    # The host is not important but check that this is the current machine.
+    # Look for the string "sources_types" starting from the end.
     parsed_url = lib_util.survol_urlparse(script_url)
 
     split_path = parsed_url.path.split("/")
@@ -88,7 +134,7 @@ def start_events_generator_daemon(script_url):
     # This is needed by the CGI scripts.
     request_uri = "%s?%s" % (parsed_url.path, query_string_with_daemon_mode)
 
-    # This is needed by the CGI scripts, with a default value, which is easy to spot.
+    # This is needed by the CGI scripts, with a default value, easy to see if not explicitly set.
     server_port = os.environ.get("SERVER_PORT", 54321)
 
     # KEY1="value1",KEY2="value2"
@@ -140,4 +186,16 @@ def stop_events_generator_daemon(script_url):
     process_name = _url_to_process_name(script_url)
     sys.stderr.write("stop_events_generator_daemon process_name=%s\n" % process_name)
     return daemon_factory.stop_user_process(process_name)
+
+
+def get_running_daemons():
+    """
+    The key of the dict is the plain input URL.
+    """
+    user_processes_dict = daemon_factory.get_all_user_processes()
+    urls_dict = {}
+    for process_name, process_object in user_processes_dict:
+        script_url = _process_name_to_url(process_name)
+        urls_dict[script_url] = process_object
+    return urls_dict
 
