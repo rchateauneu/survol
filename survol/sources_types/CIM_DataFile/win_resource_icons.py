93aa86c0c79412a984431f36f61cdf75bdc4997d
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-01-03 23:28:23 +0000

    Remove dead code.

--- survol/sources_types/CIM_DataFile/win_resource_icons.py
+++ survol/sources_types/CIM_DataFile/win_resource_icons.py
@@ -21,34 +21,28 @@ from sources_types.win32 import resource as survol_win32_resource
 # This script works only on a Windows executable or DLL etc...
 Usable = lib_util.UsableWindowsBinary
 
-# This specifies that the object is an Url which returns a MIME object,
-# and can therefore be displayed as such.
-def MakeMimeProp(prp):
-	ret = primns_slash + "/MIME_PROPERTY/" + prp
-	url = ret.replace(" ","_")
-	return rdflib.term.URIRef( url )
-
 
 def Main():
-	cgiEnv = lib_common.CgiEnv()
+    cgiEnv = lib_common.CgiEnv()
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	pathName = cgiEnv.GetId()
+    path_name = cgiEnv.GetId()
 
-	iconGroups = survol_win32_resource.GetIconNamesList(pathName)
+    icon_groups = survol_win32_resource.GetIconNamesList(path_name)
 
-	filNode = lib_common.gUriGen.FileUri(pathName)
+    fil_node = lib_common.gUriGen.FileUri(path_name)
 
-	#propNam = ( "/MIME_PROPERTY/" + survol_win32_resource.mimeTypeResource ).replace(" ","_")
-	#propIcon = lib_properties.MakeProp(survol_win32_resource.mimeTypeResource)
+    #propNam = ( "/MIME_PROPERTY/" + survol_win32_resource.mimeTypeResource ).replace(" ","_")
+    #propIcon = lib_properties.MakeProp(survol_win32_resource.mimeTypeResource)
 
-	for groupName in iconGroups:
-		# TODO: Is it sorted in lib_export_html.py ??
-		lib_mime.AddMimeUrl(grph,filNode, "win32/resource",survol_win32_resource.mimeTypeResource,[pathName,groupName])
+    for group_name in icon_groups:
+        # TODO: Is it sorted in lib_export_html.py ??
+        lib_mime.AddMimeUrl(grph,fil_node, "win32/resource", survol_win32_resource.mimeTypeResource, [path_name, group_name])
 
-	cgiEnv.OutCgiRdf("LAYOUT_RECT",[pc.property_rdf_data_nolist2] )
+    cgiEnv.OutCgiRdf("LAYOUT_RECT", [pc.property_rdf_data_nolist2] )
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
 
