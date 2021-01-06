69667a96ca3d91cdbc9b837c44c39bc5f215df4d
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-01-06 01:17:21 +0000

    Cleanup.

--- survol/sources_types/CIM_Process/process_memmaps.py
+++ survol/sources_types/CIM_Process/process_memmaps.py
@@ -27,17 +27,18 @@ def Main():
         lib_common.ErrorMessageHtml("get_memory_maps Pid=%d. Caught %s\n" % (pid, str(exc)))
 
     propMemoryRSS = lib_common.MakeProp("Resident Set Size")
-    for map in all_maps:
+    for map_obj in all_maps:
         # This, because all Windows paths are "standardized" by us.
-        # TODO: clean_map_path = lib_util.standardized_file_path(map.path)
-        clean_map_path = map.path.replace("\\", "/")
+        # TODO: clean_map_path = lib_util.standardized_file_path(map_obj.path)
+        clean_map_path = map_obj.path.replace("\\", "/")
 
         uri_mem_map = lib_common.gUriGen.MemMapUri(clean_map_path)
 
-        grph.add((uri_mem_map, propMemoryRSS, rdflib.Literal(map.rss)))
+        grph.add((uri_mem_map, propMemoryRSS, rdflib.Literal(map_obj.rss)))
         grph.add((node_process, pc.property_memmap, uri_mem_map))
 
     cgiEnv.OutCgiRdf( "LAYOUT_SPLINE")
+
 
 if __name__ == '__main__':
     Main()
