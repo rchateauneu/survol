5a04147f2e619c9c7f8f0c755128ffe90a26f8fa
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-03-17 23:10:22 +0000

    Cleanup.

--- survol/sources_types/SMB/net_view.py
+++ survol/sources_types/SMB/net_view.py
@@ -22,50 +22,52 @@ import sys
 import re
 import socket
 import lib_util
+import lib_uris
 import lib_common
 from lib_properties import pc
 import lib_smb
 
 Usable = lib_smb.UsableNetCommands
 
+
 def Main():
-	cgiEnv = lib_common.CgiEnv()
+    cgiEnv = lib_common.CgiEnv()
 
-	grph = cgiEnv.GetGraph()
+    grph = cgiEnv.GetGraph()
 
-	net_view_cmd = [ "net", "view" ]
+    net_view_cmd = ["net", "view"]
 
-	net_view_pipe = lib_common.SubProcPOpen(net_view_cmd)
+    net_view_pipe = lib_common.SubProcPOpen(net_view_cmd)
 
-	( net_view_last_output, net_view_err ) = net_view_pipe.communicate()
+    net_view_last_output, net_view_err = net_view_pipe.communicate()
 
-	# Converts to string for Python3.
-	asstr = net_view_last_output.decode("utf-8")
-	lines = asstr.split('\n')
+    # Converts to string for Python3.
+    asstr = net_view_last_output.decode("utf-8")
+    lines = asstr.split('\n')
 
-	seenHyphens = False
+    seen_hyphens = False
 
-	for lin in lines:
-		if re.match(".*-------.*",lin):
-			seenHyphens = True
-			continue
+    for lin in lines:
+        if re.match(".*-------.*",lin):
+            seen_hyphens = True
+            continue
 
-		if re.match(".*The command completed successfully.*",lin):
-			break
-		if not seenHyphens:
-			continue
+        if re.match(".*The command completed successfully.*",lin):
+            break
+        if not seen_hyphens:
+            continue
 
-		#print("se="+str(seenHyphens)+" Lin2=("+lin+")")
-		tst_view = re.match( r'^\\\\([A-Za-z0-9_$]+)', lin )
-		if not tst_view:
-			continue
+        tst_view = re.match(r'^\\\\([A-Za-z0-9_$]+)', lin)
+        if not tst_view:
+            continue
 
-		shrSrv = tst_view.group(1)
+        shr_srv = tst_view.group(1)
 
-		shareSrvNode = lib_common.gUriGen.SmbServerUri( shrSrv )
-		grph.add( ( lib_common.nodeMachine, pc.property_smbview, shareSrvNode ) )
+        share_srv_node = lib_uris.gUriGen.SmbServerUri(shr_srv)
+        grph.add((lib_common.nodeMachine, pc.property_smbview, share_srv_node))
 
-	cgiEnv.OutCgiRdf()
+    cgiEnv.OutCgiRdf()
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
