b19db90fc823b2ae310d4a014a387e39701f1ae0
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-03-17 11:10:52 +0000

    Comments.

--- survol/sources_types/SMB/__init__.py
+++ survol/sources_types/SMB/__init__.py
@@ -2,6 +2,10 @@
 MS-DOS SMB commands.
 """
 
+# This is a set of scripts about disk shares, SMB etc...
+# This package does not define an instance class.
+# For the associate class, see Win32_Share.
+
 import lib_util
 
 # Necessary otherwise it is displayed on Linux machines.
