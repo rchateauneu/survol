b39e2061b6e707a0403fac86853064cc45edac3f
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-03-17 14:57:28 +0000

    Proper lib_uris import.

--- survol/lib_smb.py
+++ survol/lib_smb.py
@@ -1,5 +1,6 @@
 import re
 import lib_common
+import lib_uris
 
 
 def UsableNetCommands(entity_type, entity_ids_arr):
@@ -14,14 +15,15 @@ def SmbBothUriSplit(smb_both):
     if not shr_mtch:
         return None
 
-    smb_shr = "//" + shr_mtch.group(1) + "/" + shr_mtch.group(2)
+    smb_host = shr_mtch.group(1)
+    smb_shr = shr_mtch.group(2)
     smb_dir = shr_mtch.group(3)
-
+    smb_box = lib_uris.MachineBox(smb_host)
     # Needed if this is the top directory.
     if smb_dir == "" or smb_dir == "/":
-        node_smb = lib_common.gUriGen.SmbShareUri(smb_shr)
+        node_smb = smb_box.SmbShareUri(smb_shr)
     else:
         # Otherwise it is the directory of the current file.
-        node_smb = lib_common.gUriGen.SmbFileUri(smb_shr, smb_dir)
+        node_smb = smb_box.SmbFileUri(smb_shr, smb_dir)
     return node_smb, smb_shr, smb_dir
 
