831db82e8ed393b56dd23e24808d4f176c00f8d4
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-03-17 23:22:14 +0000

    New type.

---
+++ survol/sources_types/Win32_NetworkConnection/__init__.py
@@ -0,0 +1,17 @@
+"""
+Windows mapped drive
+"""
+
+
+def EntityOntology():
+    return (["Name"],)
+
+
+def EntityName(entity_ids_arr):
+    """This returns a display string given the parameter of the object."""
+
+    # It should get the path.
+    entity_id = entity_ids_arr[0]
+    return entity_id
+
+# AddInfo will display the path.
