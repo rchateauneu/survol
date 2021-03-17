08b17b8607528ae62cbb01c125649cc2e7abbc3f
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-03-17 14:56:19 +0000

    Cleanup.

--- survol/sources_types/CIM_ComputerSystem/Win32/win32_NetShareEnum.py
+++ survol/sources_types/CIM_ComputerSystem/Win32/win32_NetShareEnum.py
@@ -8,9 +8,11 @@ import sys
 import logging
 import lib_util
 import lib_common
+import lib_uris
 from lib_properties import pc
 
 import win32net
+
 
 def Main():
 	cgiEnv = lib_common.CgiEnv()
@@ -36,10 +38,9 @@ def Main():
 
 			sharedata, total, shareresume = win32net.NetShareEnum(hostname_or_None, level, shareresume)
 
-		except Exception:
+		except Exception as exc:
 			# "Access is denied."
-			exc = sys.exc_info()[1]
-			lib_common.ErrorMessageHtml("Hostname="+hostname+". Exception:"+str(exc))
+			lib_common.ErrorMessageHtml("Hostname=" + hostname + ". Exception:" + str(exc))
 
 		for share in sharedata:
 			logging.debug("share=%s", str(share) )
@@ -52,22 +53,23 @@ def Main():
 				share_path = ""
 				share_remark = ""
 
-			shareNode = lib_common.gUriGen.SmbShareUri( "//" + hostname + "/" + share_netname )
-			grph.add( ( nodeHost, pc.property_smbshare, shareNode ) )
+			shareNode = lib_uris.MachineBox(hostname).SmbShareUri(share_netname)
+			grph.add((nodeHost, pc.property_smbshare, shareNode))
 
 			if share_path:
 				# TODO: Horrible display. Strange because this is encoded in the function.
 				# mountNode = lib_common.gUriGen.FileUri( share_path.replace('\\','/') )
-				mountNode = lib_common.gUriGen.FileUri( share_path )
-				grph.add( ( shareNode, pc.property_smbmount, mountNode ) )
+				mountNode = lib_common.gUriGen.FileUri(share_path)
+				grph.add((shareNode, pc.property_smbmount, mountNode))
 
 			if share_remark:
-				grph.add( ( shareNode, pc.property_information, lib_util.NodeLiteral(share_remark) ) )
+				grph.add((shareNode, pc.property_information, lib_util.NodeLiteral(share_remark)))
 
 		if not shareresume:
 			break
 
 	cgiEnv.OutCgiRdf()
+
 
 if __name__ == '__main__':
 	Main()
