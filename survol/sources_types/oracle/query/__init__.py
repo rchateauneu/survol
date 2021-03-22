5003f955a6b0047cd3de4d1329965aa151f43e97
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-03-22 23:49:52 +0000

    Cleanup.

--- survol/sources_types/oracle/query/__init__.py
+++ survol/sources_types/oracle/query/__init__.py
@@ -2,43 +2,46 @@
 Oracle database query
 """
 
+import sys
+
+import lib_uris
+import lib_util
+import lib_common
 from sources_types.sql import query as sql_query
 from sources_types.oracle import db as oracle_db
 from sources_types.oracle import table as oracle_table
 from sources_types.oracle import view as oracle_view
 from sources_types.oracle import synonym as oracle_synonym
-import lib_util
-import lib_common
-import sys
+
 
 def Graphic_colorbg():
 	return "#CC99FF"
 
+
 # The result should be ["Query","Db"]
 # We do not know if CIM_Process.EntityOntology() is available.
 def EntityOntology():
-	# return sql_query.EntityOntology() + lib_util.OntologyClassKeys("oracle/db")
-	# return ( ["Db","Query"],)
-	return ( ["Query","Db"],)
+	return (["Query", "Db"],)
+
 
 # The SQL query is encoded in base 64 because it contains many special characters which would be too complicated to
 # encode as HTML entities. This is not visible as EntityName() does the reverse decoding.
 def MakeUri(strQuery,theDb):
-	#strQueryEncoded = lib_util.Base64Encode(strQuery)
 	# TODO: We have hard-coded the process definition with "Db".
 	# TODO: The entity parameter should be passed differently, more elegant. Not sure.
-	#return lib_common.gUriGen.UriMakeFromDict("sql/query",{ "Query" : strQueryEncoded, "Handle" : thePid })
-	return sql_query.MakeUri( strQuery, "oracle/query", Db = theDb )
+	return sql_query.MakeUri(strQuery, "oracle/query", Db=theDb)
+
 
 # TODO: This should maybe receive a dictionary instead of a list.
 def AddInfo(grph,node,entity_ids_arr):
 	theDb = entity_ids_arr[1]
 	nodeDb = oracle_db.MakeUri(theDb)
-	grph.add((node,lib_common.MakeProp("Db"),nodeDb))
+	grph.add((node,lib_common.MakeProp("Db"), nodeDb))
+
 
 # For the moment, we assume that these are all table names, without checking.
 # TODO: Find a quick way to check if these are tables or views.
-def QueryToNodesList(sqlQuery,connectionKW,list_of_tables,defaultSchemaName=None):
+def QueryToNodesList(sqlQuery, connectionKW, list_of_tables, defaultSchemaName=None):
 	nodesList = []
 	# This should be taken from the credentials.
 	if not defaultSchemaName:
@@ -51,12 +54,13 @@ def QueryToNodesList(sqlQuery,connectionKW,list_of_tables,defaultSchemaName=None
 		else:
 			schemaName = defaultSchemaName
 			tableNameNoSchema = tabNam
-		tmpNode = oracle_table.MakeUri( connectionKW["Db"], schemaName, tableNameNoSchema )
+		tmpNode = oracle_table.MakeUri(connectionKW["Db"], schemaName, tableNameNoSchema )
 		nodesList.append( tmpNode )
 	return nodesList
+
 
 # TODO: This produces a nice message but what is also needed is the decoding of the query.
 def EntityName(entity_ids_arr):
 	sqlQuery = entity_ids_arr[0]
 	dbNam = entity_ids_arr[1]
-	return sql_query.EntityNameUtil( "Database " + dbNam,sqlQuery)
+	return sql_query.EntityNameUtil("Database " + dbNam,sqlQuery)
--- survol/sources_types/oracle/type/__init__.py
+++ survol/sources_types/oracle/type/__init__.py
@@ -2,17 +2,22 @@
 Oracle data type
 """
 
+import lib_uris
 import lib_common
 from lib_properties import pc
+
 
 def Graphic_colorbg():
 	return "#CCCC66"
 
+
 def EntityOntology():
-	return ( ["Db", "Schema", "Type"], )
+	return (["Db", "Schema", "Type"],)
+
 
 def MakeUri(dbName,schemaName,typeName):
-	return lib_common.gUriGen.UriMakeFromDict("oracle/type", { "Db" : dbName, "Schema" : schemaName, "Type" : typeName } )
+	return lib_uris.gUriGen.UriMakeFromDict("oracle/type", {"Db": dbName, "Schema": schemaName, "Type": typeName})
+
 
 def AddInfo(grph,node,entity_ids_arr):
 	# TODO: SPECIAL. Imported here to avoid circular inclusions, see oracle/package_body/__init__.py
@@ -21,8 +26,9 @@ def AddInfo(grph,node,entity_ids_arr):
 	argDb = entity_ids_arr[0]
 	argSchema = entity_ids_arr[1]
 
-	node_oraschema = oracle_schema.MakeUri( argDb, argSchema )
-	grph.add( ( node_oraschema, pc.property_oracle_type, node ) )
+	node_oraschema = oracle_schema.MakeUri(argDb, argSchema)
+	grph.add((node_oraschema, pc.property_oracle_type, node))
+
 
 def EntityName(entity_ids_arr):
 	return entity_ids_arr[0] + "." + entity_ids_arr[1] + "." + entity_ids_arr[2]
