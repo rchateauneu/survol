993a4c22f0b4adffafde9e6bbebf9f81aee6242c
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-04-23 10:45:13 +0100

    Fix.

--- survol/lib_associators.py
+++ survol/lib_associators.py
@@ -13,15 +13,18 @@ except ImportError:
 
 def add_associated_instances(grph, root_node, entity_type, entity_id, associator_attribute):
     logging.info("This is implemented for WMI only, yet.")
+    logging.debug("entity_type=%s entity_id=%s associator_attribute=%s", entity_type, entity_id, associator_attribute)
     result_class, result_role = lib_ontology_tools.get_associated_attribute(
         "wmi", lib_wmi.extract_specific_ontology_wmi, associator_attribute)
 
-    associator_name, _ = associator_attribute.partition(".")
+    associator_name, _, input_role = associator_attribute.partition(".")
+    if not input_role:
+        raise Exception("associator_attribute %s is invalid" % associator_attribute)
 
     # This path will eventually be reformatted for WMI needs.
     wmi_path = entity_type + "." + entity_id
 
-    iter_objects = lib_wmi.WmiSparqlExecutor.enumerate_associated_instances(
+    iter_objects = lib_wmi.WmiSparqlExecutor().enumerate_associated_instances(
         wmi_path, associator_name, result_class, result_role)
 
     for associated_dict_key_values in iter_objects:
