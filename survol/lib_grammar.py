8d50ebb19e680ac275a1da1eab3ab1d478d7ce7e
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-04-25 19:24:38 +0100

    Renaming and comments.

--- survol/lib_grammar.py
+++ survol/lib_grammar.py
@@ -1,15 +1,18 @@
 # Consider using the Pythonpackage inflect.
 
 
-# Win32_ReliabilityRecordses
-# Win32_LogicalFileAccesses
-def ToPlural(elt_nam, num_nod_lst):
+def to_plural(elt_nam, num_nod_lst):
+    """
+    This adds an s to the end of the word and respectes English rules.
+    """
     if num_nod_lst == 1:
         return elt_nam
 
     if not elt_nam[-1].isalpha():
         return elt_nam
 
+    # Win32_ReliabilityRecordses
+    # Win32_LogicalFileAccesses
     if elt_nam[-1] == "s":
         if (len(elt_nam) > 1) and (elt_nam[-2] != "s"):
             # Maybe it is already plural.
@@ -24,7 +27,7 @@ def ToPlural(elt_nam, num_nod_lst):
 
 
 # https://www.theenglishspace.com/grammar/determiners/articles-introduction.html
-def IndefiniteArticle(the_word):
+def indefinite_article(the_word):
     if the_word[0].upper() in "AEIOUY":
         return "an"
     else:
