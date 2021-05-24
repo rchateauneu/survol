d79c91d8cd4166d419afccc76670aa8dec2abc57
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-05-24 22:36:45 +0100

    Created test.

--- survol/sources_types/odbc/__init__.py
+++ survol/sources_types/odbc/__init__.py
@@ -6,6 +6,8 @@ import re
 import sys
 import logging
 
+import six
+
 import lib_util
 
 # ('C:\\Program Files (x86)\\Microsoft Visual Studio 8\\Crystal Reports\\Samples\\en\\Databases\\xtreme', None, 'MSysAccessObjects', 'SYSTEM TABLE', None)
@@ -64,9 +66,10 @@ _rgx_true_false = "[a-zA-Z01]*"
 
 _rgx_user = r"\w+"
 
-# TODO: Will not work if "=" in the password, even if it is escaped.
-# Should reasonably contain more than four or five chars.
-_rgx_password = ".+"
+# TODO: Check if "=" in the password, possibly escaped.
+# HINT: Should reasonably contain more than four or five chars.
+# This does not work if it contains an escaped double-quote between double-quotes.
+_rgx_password = '([^ "=;]+|"[^"]*")'
 
 # Hexadecimal number
 _rgx_hexa = "[0-9a-fA-F]+"
@@ -78,17 +81,24 @@ _rgx_alpha = "[a-zA-Z]+"
 # Unfortunately it is not able to filter escaped equal signs.
 _rgx_anything = "[^=]+"
 
-_rgx_file_name = _rgx_anything
+_rgx_file_name = r"[^;=]+"
+
+# For example:
+# SERVER=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=MyHost)(PORT=MyPort))(CONNECT_DATA=(SERVICE_NAME=MyOracleSID)));
+# Data Source=(DESCRIPTION=(CID=GTU_APP)(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=myHost)(PORT=myPort)))(CONNECT_DATA=(SID=MyOracleSID)(SERVER=DEDICATED)));
+# Specific difficulty here: Some keywords in Oracle descriptions are also ODBC keywords: "PROTOCOL" and "SERVER".
+_rgx_oracle_description = r'\([^;]*\)'
 
 # The individual regular expressions do not contain the pipe character,
 # because it is already used between each regular expression.
 
 # https://www.connectionstrings.com/oracle/
 
-mapRgxODBC = {
+_map_rgx_odbc_heavy = {
     "ALLOWBATCH"                  : _rgx_true_false,             #
     "ALLOWUSERVARIABLES"          : _rgx_true_false,             #
     "ALLOWZERODATETIME"           : _rgx_true_false,             #
+    "ATTACHDBFILENAME"            : _rgx_file_name,              #
     "AUTOENLIST"                  : _rgx_true_false,             #
     "CACHESERVERPROPERTIES"       : _rgx_true_false,             #
     "CACHETYPE"                   : _rgx_alpha,                  # "File"
@@ -103,9 +113,16 @@ mapRgxODBC = {
     "CONNECTION TIMEOUT"          : _rgx_number,                 #
     "CONNECTIONRESET"             : _rgx_true_false,             #
     "CONVERTZERODATETIME"         : _rgx_true_false,             #
-    "DATA SOURCE"                 : "[a-zA-Z_0-9\\/]+",          # "C:\myFolder\myAccessFile.accdb"
+    # "DATA SOURCE"                 : _rgx_file_name,              # "C:\myFolder\myAccessFile.accdb"
+    # FIXME: It cannot start with a open parenthesis because this would be taken as an Oracle description.
+    # It can be a file name such as "
+    "DATA SOURCE"                 : r"([^\(]%s|%s)" % (_rgx_file_name, _rgx_oracle_description),
+    # "DATA SOURCE": "[a-zA-Z_0-9\\/]+",  # "C:\myFolder\myAccessFile.accdb"
                                                                  # "|DataDirectory|\myAccessFile.accdb"
                                                                  # "\\server\share\folder\myAccessFile.accdb"
+
+
+
     "DATABASE"                    : "[ a-zA-Z0-9._]+",
     "DB"                          : "[a-zA-Z0-9._]*",
     "DBA PRIVILEGE"               : _rgx_anything,               # "SYSDBA", "SYSOPER"
@@ -118,12 +135,12 @@ mapRgxODBC = {
     "ENCRYPT"                     : _rgx_true_false,             # "true"
     "EXCLUSIVE"                   : _rgx_true_false,             # "1"
     "EXTENDEDANSISQL"             : _rgx_true_false,             # "1"
-    "EXTENDED PROPERTIES"         : _rgx_anything,               #
+    "EXTENDED PROPERTIES"         : '"[^"*]"',
     "FILEDSN"                     : _rgx_file_name,
     "IGNORE PREPARE"              : _rgx_true_false,             #
     "INCR POOL SIZE"              : _rgx_number,                 #
-    "INITIAL CATALOG"             : _rgx_anything,               # "myDataBase"
-    "INTEGRATEDSECURITY"          : _rgx_true_false,             #
+    "INITIAL CATALOG"             : "[a-zA-Z0-9_ ]+",            # "myDataBase"
+    "INTEGRATED SECURITY"         : _rgx_true_false,             #
     "JET OLEDB:DATABASE PASSWORD" : _rgx_password,
     "KEEPALIVE"                   : _rgx_number,                 #
     "LOCALE IDENTIFIER"           : r"\d+",                      # "2057" is en-gb locale identifier
@@ -133,6 +150,7 @@ mapRgxODBC = {
     "MAXIMUMPOOLSIZE"             : _rgx_number,                 #
     "MINIMUMPOOLSIZE"             : _rgx_number,                 #
     "MODE"                        : "[a-zA-Z ]+",                # "Share Exclusive"
+    "NETWORK LIBRARY"             : "[a-zA-Z ]+",                #
     "ODBCKEY[12]"                 : _rgx_anything,
     "OLDGUIDS"                    : _rgx_true_false,             #
     "OLEDBKEY[12]"                : _rgx_anything,
@@ -148,8 +166,10 @@ mapRgxODBC = {
     "PROVIDER"                    : "[ a-zA-Z0-9._]+",           # "Microsoft.ACE.OLEDB.12.0"
     "PWD"                         : _rgx_password,
     "REMOTE SERVER"               : _rgx_anything,               # "http://server.adress.com"
-    "SERVER"                      : r"[- a-zA-Z0-9\._\\\]+",     # "serverAddress1, serverAddress2, serverAddress3"
-                                                                 # This Oracle omission of tnsnames.ora is not taken into account.
+    "SERVER": r"([- a-zA-Z0-9\._\\]+|\(localdb\)\\[0-9a-zA-Z\.\\]*|%s)" % _rgx_oracle_description,
+                                                                 # "serverAddress"
+                                                                 # (localdb)\v11.0
+                                                                 # Server=(localdb)\.\MyInstanceShare
                                                                  # "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=MyHost)(PORT=MyPort))(CONNECT_DATA=(SERVICE_NAME=MyOracleSID)))"
     "SHARED MEMORY NAME"          : r"\w+",                      # "MYSQL" for example. If "Protocol" = "memory".
     "SOCKET"                      : _rgx_anything,               #
@@ -174,13 +194,13 @@ mapRgxODBC = {
 
 # Keys which are specifically coded as passwords.
 # Should be ["PWD","PASSWORD","JET OLEDB:DATABASE PASSWORD"]
-_odbc_keys_confidential = [key_wrd for key_wrd in mapRgxODBC if mapRgxODBC[key_wrd] == _rgx_password]
+_odbc_keys_confidential = [key_wrd for key_wrd in _map_rgx_odbc_heavy if _map_rgx_odbc_heavy[key_wrd] == _rgx_password]
 
 # Values which do not need to be encoded, making things easier to understand.
 _odbc_keys_uncoded = [
     key_wrd
-    for key_wrd in mapRgxODBC
-    if mapRgxODBC[key_wrd] in [_rgx_alpha, _rgx_user, _rgx_true_false, _rgx_hexa, _rgx_number]]
+    for key_wrd in _map_rgx_odbc_heavy
+    if _map_rgx_odbc_heavy[key_wrd] in [_rgx_alpha, _rgx_user, _rgx_true_false, _rgx_hexa, _rgx_number]]
 
 # This contains the most often used keys in DSN connection strings.
 restrict_rgx_odbc_keys = [
@@ -204,10 +224,70 @@ restrict_rgx_odbc_keys = [
 ]
 
 # Only the commonest parameters, to make memory scans faster.
-mapRgxODBC_Light = {key : mapRgxODBC[key] for key in restrict_rgx_odbc_keys}
+_map_rgx_odbc_light = {key: _map_rgx_odbc_heavy[key] for key in restrict_rgx_odbc_keys}
 
 # This need a to be not-too-reserved character.
 _delimiter_connection_string_odbc = "/" # "-"
+
+_rgx_before_keyword = "[; ]*"
+_rgx_after_keyword = " *[;\n\r\t\0x00]"
+
+def _concatenate_regex_dict(map_rgx):
+    """
+    This builds a regular expression for finding ODBC connection strings.
+    The input is a dict of ODBC character key. mapped to the regular expression for finding it.
+    This might be very slow.
+    """
+    # Not letter, then the keyword, then "=", then the value regex, then possibly the delimiter.
+    rgx_dsn = "|".join([_rgx_before_keyword + key + " *= *" + map_rgx[key] + " *" for key in map_rgx])
+
+    # This works also. Both are very slow.
+    #rgx_dsn = "|".join([ ";? *" + key + " *= *" + survol_odbc.mapRgxODBC[key] + " *" for key in survol_odbc.mapRgxODBC ])
+    return rgx_dsn
+
+
+regex_odbc_heavy = _concatenate_regex_dict(_map_rgx_odbc_heavy)
+
+regex_odbc_light = _concatenate_regex_dict(_map_rgx_odbc_light)
+
+
+def aggregate_dsn_pieces(resu_matches):
+    """
+    This aggregates contiguous, or nearly contiguous, mactked tokens, in bigger strings.
+    This is useful when matching a regular expression made as an OR of possible tokens.
+    :param resu_matches: A dict of offsets to matched strings. Each matched string is an individual detected token.
+    :return: A similar dict, but contiguous matched strings are concatenated
+    """
+
+    sorted_keys = sorted(resu_matches.keys())
+    aggreg_dsns = dict()
+    curr_offset = -1
+    for the_off in sorted_keys:
+        curr_mtch = resu_matches[the_off]
+        assert isinstance(curr_mtch, six.binary_type)
+        if curr_offset == -1:
+            curr_offset = the_off
+            aggreg_dsns[curr_offset] = curr_mtch
+        elif the_off == next_offset:
+            # The current string comes just after the last one.
+            # Concatenation of the current string.
+            # Maybe we can tolerate a small difference for spaces.
+            aggreg_dsns[curr_offset] += curr_mtch
+        else:
+            # This starts a new DSN string because the current match comes far after the previous one.
+            curr_offset = the_off
+            aggreg_dsns[curr_offset] = curr_mtch
+        next_offset = the_off + len(curr_mtch)
+
+    # TODO: Eliminate aggregated strings containing one or two tokens,
+    # because they cannot be genuine DSNs.
+    # 29812569: SERVER=\MYMACHINE
+    # 34515016: Driver={SQL Server};Server=.\SQLEXPRESS;Database=ExpressDB;Trusted_Connection=yes
+    # 34801013: SERVER=\MYMACHINE
+    # 35634904: Driver={SQL Server};Server=.\SQLEXPRESS;Database=ExpressDB;Trusted_Connection=yes
+
+    return aggreg_dsns
+
 
 # This behaves like a string plus some properties for serialization.
 # So it can be used as a keyword for encoding parameters in the id of an object,
---
+++ tests/test_odbc.py
@@ -0,0 +1,158 @@
+# accdb file accessed with ODBC.
+# This is a feature if CIM_DataFile extension is accdb or csv or any extension compatible with an ODBC driver.
+# Conditional because ODBC and pyodbc must be there, and the ODBC driver must be available too.
+
+# This can also be used if an ODBC connection string is detected in a process memory or anywhere.
+
+# This must also handle a connection string.
+
+from __future__ import print_function
+
+import unittest
+import sys
+import re
+
+import six
+
+from sources_types import odbc as survol_odbc
+
+
+class MatchesAggregationTest(unittest.TestCase):
+    # Things to test:
+    # - From bytes.
+    # - From UTF8, on one, two or four bytes.
+
+    def test_aggregate_dsn_pieces(self):
+        test_data = [
+            (
+                {
+                    0: b"a",
+                    1: b"b",
+                },
+                {
+                    0: b"ab",
+                }
+            ),
+            (
+                {
+                    0: b"a",
+                    100: b"b",
+                },
+                {
+                    0: b"a",
+                    100: b"b",
+                }
+            ),
+            (
+                {
+                    0: b"a",
+                    1: b"b",
+                    2: b"c",
+                    3: b"d",
+                },
+                {
+                    0: b"abcd",
+                }
+            ),
+            (
+                {
+                    0: b"a",
+                    1: b"b",
+                    102: b"c",
+                    103: b"d",
+                },
+                {
+                    0: b"ab",
+                    102: b"cd",
+                }
+            ),
+        ]
+
+        for input_dict, expected_dict in test_data:
+            actual_dict = survol_odbc.aggregate_dsn_pieces(input_dict)
+            self.assertEqual(expected_dict, actual_dict)
+
+
+# Many examples of connection strings here: https://www.connectionstrings.com/
+connection_strings_light = [
+    r'Driver={Microsoft Text Driver (*.txt; *.csv)};Dbq=C:\AnyDir\odbc_sample.csv;Extensions=asc,csv,tab,txt;',
+    r'DSN=myDsn;Uid=myUsername;Pwd=;',
+    r'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\AnyDir\AnyFile.tmp;Extended Properties="text;HDR=Yes;FMT=Delimited";',
+    r'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=c:\txtFilesFolder\;Extended Properties="text;HDR=Yes;FMT=Delimited";',
+    r'Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Documents;Extended Properties=\"Text;HDR=Yes\"";',
+    r'Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\myFolder\myExcel2007file.xlsx;Extended Properties="Excel 12.0 Xml;HDR=YES";',
+    r'Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\myFolder\myBinaryExcel2007file.xlsb;Extended Properties="Excel 12.0;HDR=YES";',
+]
+
+connection_strings_heavy = connection_strings_light + [
+    r'Data Source=MyOracleDB;User Id=myUsername;Password=myPassword;Integrated Security=no;',
+    r'Data Source=myOracleDB;User Id=myUsername;Password=myPassword;Min Pool Size=10;Connection Lifetime=120;Connection Timeout=60;Incr Pool Size=5;Decr Pool Size=2;',
+    r'Data Source=myOracleDB;User Id=SYS;Password=SYS;DBA Privilege=SYSOPER;',
+    r'Data Source=username/password@//myserver:1521/my.service.com;',
+    r'Data Source=190.190.200.100,1433;Network Library=DBMSSOCN;Initial Catalog=myDataBase;User ID=myUsername;Password=myPassword;',
+    r'Driver=MapR Drill ODBC Driver;ConnectionType=Direct;Host=192.168.222.160;Port=31010',
+    r'Driver={SQL Server};Server=lmtest;Database=lmdb;Uid=sa;Pwd=pass',
+    r'Driver={MySQL ODBC 3.51 driver};server=lmtest;database=lmdb;uid=mysqluser;pwd=pass;',
+    r'DSN=DSN_Name;Server=lmtest;Uid=lmuser;Pwd=pass',
+    r'FILEDSN=c:\myDsnFile.dsn;Uid=myUsername;Pwd=;',
+    r'Provider=OraOLEDB.Oracle;Data Source=localhost:1521/XE;Initial Catalog=myDataBase;User Id=myUsername;Password=myPassword;',
+    r'Provider=OraOLEDB.Oracle;Data Source=MyOracleDB;User Id=myUsername;Password=myPassword;OLEDB.NET=True;SPPrmsLOB=False;NDatatype=False;SPPrmsLOB=False;',
+    r'Provider=OraOLEDB.Oracle;Data Source=(DESCRIPTION=(CID=GTU_APP)(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=myHost)(PORT=myPort)))(CONNECT_DATA=(SID=MyOracleSID)(SERVER=DEDICATED)));User Id=myUsername;Password=myPassword;',
+    r'SERVER=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=MyHost)(PORT=MyPort))(CONNECT_DATA=(SERVICE_NAME=MyOracleSID)));uid=myUsername;pwd=myPassword;',
+    r'Server=(localdb)\v11.0;Integrated Security=true;AttachDbFileName=C:\MyFolder\MyData.mdf;',
+    r'Server=.\SQLExpress;AttachDbFilename=|DataDirectory|mydbfile.mdf;Database=dbname;Trusted_Connection=Yes;',
+    r'Server=np:\\.\pipe\LOCALDB#F365A78E\tsql\query;',
+    r'Server=(localdb)\.\MyInstanceShare;Integrated Security=true;',
+]
+
+# Various text strings to be added before and after.
+noise_text = [
+    " lksdj;laksjdf;lkajsdf",
+    " 9087sdva98uvohj.134mn5.23m4 ",
+    " -098_)( *76586 ",
+    " nothing=something ",
+    " ;==; ",
+]
+
+
+class DetectConnectionStringTest(unittest.TestCase):
+
+    # Things to test:
+    # - From bytes.
+    # - From UTF8, on one, two or four bytes.
+
+    def _aux_test_from_python_bytes(self, text_display, the_odbc_regex, various_connections):
+        print(text_display, "=", the_odbc_regex)
+        compiled_regex = re.compile(the_odbc_regex.encode('utf-8'), re.IGNORECASE)
+
+        for one_connect_str in various_connections:
+            print("")
+            print("one_connect_str=", one_connect_str)
+            for one_prefix in noise_text:
+                for one_suffix in noise_text:
+                    full_line = one_prefix + one_connect_str + one_suffix
+                    bytes_array = full_line.encode('utf-8')
+                    self.assertTrue(isinstance(bytes_array, six.binary_type))
+                    #matched_connect = compiled_regex.findall(bytes_array)
+
+                    resu_matches = {}
+                    for mtch in compiled_regex.finditer(bytes_array):
+                        resu_matches[mtch.start()] = mtch.group()
+
+                    for one_offset, one_match in resu_matches.items():
+                        print("    ", one_match)
+                        self.assertTrue(isinstance(one_match, six.binary_type))
+
+                    aggregated_matches = survol_odbc.aggregate_dsn_pieces(resu_matches)
+                    print("aggregated_matches=", aggregated_matches)
+                    self.assertTrue(isinstance(aggregated_matches, dict))
+                    self.assertEqual(len(aggregated_matches), 1)
+                    first_key = next(iter(aggregated_matches))
+                    first_dsn = aggregated_matches[first_key]
+                    print("first_dsn=", first_dsn)
+
+    def test_from_python_light_bytes(self):
+        self._aux_test_from_python_bytes("regex_odbc_light", survol_odbc.regex_odbc_light, connection_strings_light)
+
+    def test_from_python_heavy_bytes(self):
+        self._aux_test_from_python_bytes("regex_odbc_heavy", survol_odbc.regex_odbc_heavy, connection_strings_heavy)
