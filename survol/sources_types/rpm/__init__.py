ffd123a3d5835956f9ed6ceb0350cc8103727f25
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-05-27 22:04:36 +0100

    Cleanup.

--- survol/sources_types/rpm/__init__.py
+++ survol/sources_types/rpm/__init__.py
@@ -11,6 +11,7 @@ import lib_uris
 import lib_util
 import lib_common
 
+
 def EntityOntology():
     return (["Rpm",],)
 
@@ -27,7 +28,7 @@ def rpm_properties():
     list_props = ["epoch", "version", "release", "arch"]
 
     # Add a dot, so they come first.
-    rpm_props = {propKey: lib_common.MakeProp("." + propKey) for propKey in list_props}
+    rpm_props = {prop_key: lib_common.MakeProp("." + prop_key) for prop_key in list_props}
 
     return rpm_props
 
@@ -38,14 +39,10 @@ def AddInfo(grph, node, entity_ids_arr):
     rpm_props = rpm_properties()
 
     ts = rpm.TransactionSet()
-    mi = ts.dbMatch('name',rpm_name)
+    mi = ts.dbMatch('name', rpm_name)
     for h in mi:
         for prop_key in rpm_props:
             prop_rpm = rpm_props[prop_key]
             # The value might be None.
             prop_val = h[prop_key] or ""
             grph.add((node, prop_rpm, lib_util.NodeLiteral(prop_val)))
-
-
-
-
