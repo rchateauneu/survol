3f5559a8a6861d785d8f197a67565e781e7b6eac
Primhill Computers <remi.chateauneu@primhillcomputers.com>
2021-05-29 18:17:40 +0100

    Useless code.

--- Experimental/Test_ODBC_csv_file.py
+++ Experimental/Test_ODBC_csv_file.py
@@ -30,37 +30,6 @@ def test_text_odbc_driver():
     print("Finished")
 
 
-def test_access_database():
-    # File taken from https://www.599cd.com/access/studentdatabases/
-
-    # cn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=192.168.0.14;PORT=1433;UID=essaisql;PWD=xyz')
-
-    # ('IM002', '[IM002] [Microsoft][ODBC Driver Manager] Data source name not found and no default driver specified (0) (SQLDriverConnect)')
-    # connection_string = r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=' + curr_dir + 'PCResale_Customer_Database.accdb;'
-
-    # ('IM002', '[IM002] [Microsoft][ODBC Driver Manager] Data source name not found and no default driver specified (0) (SQLDriverConnect)')
-    #connection_string = r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=' \
-    #                    + curr_dir + 'PCResale_Customer_Database.accdb;Provider=MSDASQL;'
-
-    # Maybe this is Python in 64 accessing a 32 bits driver.
-    accdb_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Test_ODBC_SampleFile.accdb')
-    connection_string = r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=' + accdb_file
-
-    # Provider=MSDASQL
-    print("connection_string=" + connection_string)
-    conn = pyodbc.connect(connection_string)
-
-    c = conn.cursor()
-
-    sql_query = "select * from FirstTable"
-
-    c.execute(sql_query)
-
-    for the_row in c.fetchall():
-        print(the_row)
-    print("Finished")
-    print("")
-
 # Data Source=(LocalDb)\v11.0;Initial Catalog=Data1;Integrated Security=true;AttachDBFilename=data1.mdf
 #
 # Data Source=(LocalDb)\v11.0;Initial Catalog=Data2;Integrated Security=true;AttachDBFilename=data2.mdf
