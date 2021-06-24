7d1f9db9b253d32e7ff6f07c4b141b312c741e91
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-06-24 09:45:53 +0100

    Fix and cleanup.

--- survol/sources_types/CIM_ComputerSystem/java/rmi_registry.py
+++ survol/sources_types/CIM_ComputerSystem/java/rmi_registry.py
@@ -9,25 +9,30 @@ import re
 import socket
 import psutil
 import lib_util
+import lib_uris
 import lib_common
 
-# TODO: This script is not used yet.
+# TODO: Not implemented yet.
+# TODO: The registry is used to locate the first remote object on which an application needs to invoke methods.
+# TODO: The intention is to display these remote objects.
+# TODO: See https://www.researchgate.net/publication/341271544_Remote_Method_Invocation_Using_Python
+
 from sources_types import java as survol_java
 from lib_properties import pc
 
 
 def Main():
 
-    cgiEnv = lib_common.ScriptEnvironment( )
+    cgiEnv = lib_common.ScriptEnvironment()
     hostname = cgiEnv.GetId()
 
     cgiEnv = lib_common.ScriptEnvironment()
 
     grph = cgiEnv.GetGraph()
 
-    hostAddr = lib_util.GlobalGetHostByName(hostname)
+    host_addr = lib_util.GlobalGetHostByName(hostname)
 
-    hostNode = lib_uris.gUriGen.HostnameUri(hostname)
+    host_node = lib_uris.gUriGen.HostnameUri(hostname)
 
     for the_proc in psutil.process_iter():
         the_pid = the_proc.pid
