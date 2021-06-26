ffb1e5e9e38f6e7adb1a36cf09cb1f750acf781a
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-06-26 23:14:17 +0100

    Cleanup.

--- survol/sources_types/CIM_DataFile/portable_executable/pefile_exports.py
+++ survol/sources_types/CIM_DataFile/portable_executable/pefile_exports.py
@@ -53,32 +53,6 @@ def Main():
     except Exception as exc:
         lib_common.ErrorMessageHtml("File: %s. Exception:%s:" % (fil_nam, str(exc)))
 
-    # sys.stderr.write("%s\n" % hex(pe.VS_VERSIONINFO.Length) )
-    # sys.stderr.write("%s\n" % hex(pe.VS_VERSIONINFO.Type) )
-    # sys.stderr.write("%s\n" % hex(pe.VS_VERSIONINFO.ValueLength) )
-    # sys.stderr.write("%s\n" % hex(pe.VS_FIXEDFILEINFO.Signature) )
-    # sys.stderr.write("%s\n" % hex(pe.VS_FIXEDFILEINFO.FileFlags) )
-    # sys.stderr.write("%s\n" % hex(pe.VS_FIXEDFILEINFO.FileOS) )
-    # for fileinfo in pe.FileInfo:
-    #     if fileinfo.Key == 'StringFileInfo':
-    #         for st in fileinfo.StringTable:
-    #             for entry in st.entries.items():
-    #                 #UnicodeEncodeError: 'ascii' codec can't encode character u'\xa9' in position 16: ordinal not in range(128)
-    #                 # sys.stderr.write("%s %s\n"% (entry[0], entry[1]) )
-    #                 key = entry[0]
-    #                 val = entry[1]
-    #                 key = key
-    #                 if val is None:
-    #                     val = "None"
-    #                 else:
-    #                     val = val.encode("ascii", errors="replace")
-    #                     # val = val.encode("utf-8", errors="replace")
-    #                 # val = val[:2]
-    #                 sys.stderr.write("%s %s\n"% (key,val) )
-    #     elif fileinfo.Key == 'VarFileInfo':
-    #         for var in fileinfo.Var:
-    #             sys.stderr.write('%s: %s\n' % var.entry.items()[0] )
-    #
 
     # If the PE file was loaded using the fast_load=True argument, we will need to parse the data directories:
     # pe.parse_data_directories()
@@ -90,6 +64,9 @@ def Main():
         prop_address = lib_common.MakeProp("Address")
         prop_ordinal = lib_common.MakeProp("Ordinal")
         for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+            if exp.name is None:
+                logging.warning("Symbol is None")
+                continue
             decoded_sym_nam = lib_pefile.UndecorateSymbol(exp.name)
             sym_node = lib_uris.gUriGen.SymbolUri(decoded_sym_nam, fil_nam)
             grph.add((fil_node, pc.property_symbol_defined, sym_node))
