b34389027995c04bc22f0275ddda9c25ef8fddc3
Remi Chateauneu <remi.chateauneu@gmail.com>
2021-07-04 12:15:52 +0100

    More documentation about records.

--- survol/sources_types/record/__init__.py
+++ survol/sources_types/record/__init__.py
@@ -30,30 +30,64 @@ def EntityOntology():
 	return (["Name", "Path"],)
 
 
-# Autre idee pour definir un record.
-# Il peut etre defini dans un fichier mais on veut exprimer l idee que le meme record existe avec
-# plusieurs rrpresentations physiques. Un record, c est a la limite un nom, mais surtout
-# une suite de nms de champs (eventuellement avec leus types).
-# Ou peut-on trouver un record:
-# - Une table est un record.
-# - Idem fichier CSV.
-# - Classes C++ (doxygen ?), Python, COM.
-# Idealement, il faudrait retrouver des records similaires dans plusieurs fichiers ou bases de donnees.
-#
-# En pratique, les records sont un peu modifies: Champs manquants, orthographie differente,
-# ordre des champs differents (ainsi que les types bien entendu).
-# Il faudrait les comparer comme des chaines, et donc les rassembler et les traiter.
-# L'architecture en scripts s'y prete peu pour le moment, sauf a tout charger puis faire une analyse,
-# une grande boucle de comparaison.
-# Il faudrait trouver un identifiant qui caracterise un record de facon robuste,
-# mais ca ne peut pas permettre d'aller d'un champ vers d'autres ressources: Les records n'ont qu'une definition
-# semantique et ne se rattachent pas a des entites concretes.
-#
-# Si on en a plusieurs, on peut tracer un graphe de dependances en reliant les champs de records differents.
-#
 """
+Autre idee pour definir un record.
+Il peut etre defini dans un fichier mais on veut exprimer l idee que le meme record existe avec
+plusieurs rrpresentations physiques. Un record, c est a la limite un nom, mais surtout
+une suite de nms de champs (eventuellement avec leus types).
+Ou peut-on trouver un record:
+- Une table est un record.
+- Idem fichier CSV.
+- Classes C++ (doxygen ?), Python, COM.
+Idealement, il faudrait retrouver des records similaires dans plusieurs fichiers ou bases de donnees.
+En pratique, les records sont un peu modifies: Champs manquants, orthographie differente,
+ordre des champs differents (ainsi que les types bien entendu).
+Il faudrait les comparer comme des chaines, et donc les rassembler et les traiter.
+L'architecture en scripts s'y prete peu pour le moment, sauf a tout charger puis faire une analyse,
+une grande boucle de comparaison.
+Il faudrait trouver un identifiant qui caracterise un record de facon robuste,
+mais ca ne peut pas permettre d'aller d'un champ vers d'autres ressources: Les records n'ont qu'une definition
+semantique et ne se rattachent pas a des entites concretes.
+Si on en a plusieurs, on peut tracer un graphe de dependances en reliant les champs de records differents.
+
 On pourrait aussi creer dynamiquement des classes et les rattacher au modele de Survol:
 La liste des champs formant une ontologie.
+En tout cas, c'est ce qu'on fait quand on genere le RDF: Property etc...
+
+https://stackoverflow.com/questions/9090246/how-to-extract-classes-from-jar-file/40481263
+https://stackoverflow.com/questions/42128533/java-list-all-methods-and-classes-in-a-jar-file-of-specific-package-using-cmd/42128571
+
+C:\Program Files\LibreOffice\share\extensions\nlpsolver>jar tf "C:\Program Files\LibreOffice\share\extensions\nlpsolver\Evolutionary
+Solver.jar"
+...
+net/adaptivebox/deps/behavior/AbsGTBehavior.class
+...
+
+C:\Program Files\LibreOffice\share\extensions\nlpsolver>javap -classpath "C:\Program Files\LibreOffice\share\extensions\nlpsolver\Ev
+olutionarySolver.jar" net/adaptivebox/deps/behavior/AbsGTBehavior
+Compiled from "AbsGTBehavior.java"
+public abstract class net.adaptivebox.deps.behavior.AbsGTBehavior {
+  protected net.adaptivebox.knowledge.Library socialLib;
+  public net.adaptivebox.deps.behavior.AbsGTBehavior();
+  public void setLibrary(net.adaptivebox.knowledge.Library);
+  public abstract void generateBehavior(net.adaptivebox.knowledge.SearchPoint, net.adaptivebox.problem.ProblemEncoder);
+  public abstract void testBehavior(net.adaptivebox.knowledge.SearchPoint, net.adaptivebox.goodness.IGoodnessCompareEngine);
+}
+
+Definition d'origine dans  nlpsolver/ThirdParty/EvolutionarySolver/src/net/adaptivebox/deps/behavior/AbsGTBehavior.java :
+Peut-etre les champs "private" ne sont-ils pas visibles.
+Ca ne doit pas etre la meme version.
+On dispose du nom du fichier d'origine, on peut donc creer un node mais qui ne pointe vers rien.
+
+abstract public class AbsGTBehavior {
+  protected Library socialLib;
+
+  public void setLibrary(Library lib) {
+
+  abstract public void testBehavior(SearchPoint trailPoint, IGoodnessCompareEngine qualityComparator);
+}
+
+
 """
 
 
