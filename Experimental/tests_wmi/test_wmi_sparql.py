bdcd07fc20d787d3d087e1da28c145a1a7f62daa
Primhill Computers <remi.chateauneu@primhillcomputers.com>
2022-03-05 22:27:11 +0000

    More testing.

--- Experimental/tests_wmi/test_wmi_sparql.py
+++ Experimental/tests_wmi/test_wmi_sparql.py
@@ -20,6 +20,7 @@ samples_list = dict()
 
 def test_classes_dictionary():
     # On-the-fly check of the classes dictionary.
+    print("")
     print("CIM_ProcessExecutable=", wmi_sparql.classes_dictionary['CIM_ProcessExecutable'])
     assert wmi_sparql.classes_dictionary['CIM_ProcessExecutable'] == {
         'Antecedent': 'ref:CIM_DataFile', 'BaseAddress': 'uint64', 'Dependent': 'ref:CIM_Process',
@@ -27,10 +28,259 @@ def test_classes_dictionary():
     assert wmi_sparql.keys_dictionary['CIM_DataFile'] == ('Name',)
 
 
-def test_create_ontology():
+ontology_filename = "wmi_ontology.xml"
+
+
+def test_create_ontology(ontology_filename):
     rdf_graph = rdflib.Graph()
     wmi_sparql._convert_ontology_to_rdf(wmi.WMI(), rdf_graph)
-    rdf_graph.serialize(destination="wmi_ontology.xml", format='xml')
+    rdf_graph.serialize(destination=ontology_filename, format='xml')
+
+
+def to_literals(results):
+    def tuple_literals(one_tuple):
+        return tuple(one_term.toPython() for one_term in one_tuple)
+    return [tuple_literals(one_tuple) for one_tuple in results]
+
+
+def test_content_ontology(ontology_filename):
+    """
+    This checks the presence of some very important triples.
+    :param ontology_filename: The filename containing the ontology in RDF/XML format.
+    :return: Nothing
+    """
+
+    rdf_graph = rdflib.Graph()
+    rdf_graph.parse(ontology_filename)
+    assert len(list(rdf_graph.triples((rdflib.URIRef(wmi_sparql.LDT["CIM_ProcessExecutable"]), None, None)))) == 4
+    assert len(list(rdf_graph.triples((rdflib.URIRef(wmi_sparql.LDT["CIM_ProcessExecutable"]), wmi_sparql.property_association_node, None)))) == 1
+
+    query_all_classes = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        select distinct ?the_class
+        where {
+        ?the_class rdf:type rdfs:Class .
+        }
+        """
+    results_all_classes = set(rdf_graph.query(query_all_classes))
+    print("All classes")
+    if False:
+        for x in results_all_classes:
+            print(x)
+    assert (rdflib.URIRef(wmi_sparql.LDT["CIM_ProcessExecutable"]),) in results_all_classes
+
+    if False:
+        print("Plain loop on all classes")
+        for s, p, o in rdf_graph.triples((rdflib.URIRef(wmi_sparql.LDT["CIM_ProcessExecutable"]), None, None)):
+            print(s, p, o)
+
+    query_CIM_ProcessExecutable = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?prop
+        where {
+        cim:CIM_ProcessExecutable ?prop ?obj .
+        }
+        """
+    print("CIM_ProcessExecutable")
+    results_CIM_ProcessExecutable = set(rdf_graph.query(query_CIM_ProcessExecutable))
+    for x in results_CIM_ProcessExecutable:
+        print(x)
+    assert (rdflib.URIRef(wmi_sparql.LDT["is_association"]),) in results_CIM_ProcessExecutable
+    assert (rdflib.namespace.RDF.type,) in results_CIM_ProcessExecutable
+    assert (rdflib.namespace.RDFS.label,) in results_CIM_ProcessExecutable
+    assert (rdflib.namespace.RDFS.comment,) in results_CIM_ProcessExecutable
+    assert len(results_CIM_ProcessExecutable) == 4
+    if False:
+        print("Plain loop on CIM_ProcessExecutable")
+        for s, p, o in rdf_graph.triples((rdflib.URIRef(wmi_sparql.LDT["CIM_ProcessExecutable"]), None, None)):
+            print(s, p, o)
+
+    # Classes which are associators.
+    query_associators = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?class_node
+        where {
+        ?class_node cim:is_association ?obj .
+        }
+        """
+    print("Associators")
+    results_associators = set(rdf_graph.query(query_associators))
+    if False:
+        for x in results_associators:
+            print(x)
+    print("Number of associators:", len(results_associators))
+    assert (rdflib.URIRef(wmi_sparql.LDT["CIM_ProcessExecutable"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["CIM_DirectoryContainsFile"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["Win32_COMApplicationClasses"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["Win32_ApplicationCommandLine"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["CIM_ProcessThread"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["Win32_DependentService"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["Win32_SystemServices"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["Win32_UserInDomain"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["Win32_SystemUsers"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["Win32_SessionProcess"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["CIM_ProductSoftwareFeatures"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["Win32_SystemProcesses"]),) in results_associators
+    assert (rdflib.URIRef(wmi_sparql.LDT["Win32_SubDirectory"]),) in results_associators
+    if False:
+        print("Plain loop on associators")
+        for s, p, o in rdf_graph.triples((None, rdflib.URIRef(wmi_sparql.LDT["is_association"]), None)):
+            print(s, p, o)
+
+    # Properties of associator classes.
+    query_associators_properties = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?class_name ?property_name
+        where {
+        ?class_node cim:is_association ?obj .
+        ?class_node rdfs:label ?class_name .
+        ?property_node rdfs:domain ?class_node .
+        ?property_node rdfs:label ?property_name .
+        }
+        """
+    print("Properties of associators")
+    results_associators_properties = set(rdf_graph.query(query_associators_properties))
+    results_associators_properties = to_literals(results_associators_properties)
+    assert ("CIM_DirectoryContainsFile", "CIM_DirectoryContainsFile.GroupComponent",) in results_associators_properties
+    assert ("CIM_DirectoryContainsFile", "CIM_DirectoryContainsFile.PartComponent",) in results_associators_properties
+
+    # Range of properties of associator CIM_DirectoryContainsFile.
+    query_CIM_DirectoryContainsFile_properties_range = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?property_name ?range_class_node
+        where {
+        ?class_node cim:is_association ?obj .
+        ?class_node rdfs:label "CIM_DirectoryContainsFile" .
+        ?property_node rdfs:domain ?class_node .
+        ?property_node rdfs:range ?range_class_node .
+        ?property_node rdfs:label ?property_name .
+        }
+        """
+    print("Range of properties of associator CIM_DirectoryContainsFile")
+    results_CIM_DirectoryContainsFile_properties_range = rdf_graph.query(query_CIM_DirectoryContainsFile_properties_range)
+    for x in results_CIM_DirectoryContainsFile_properties_range:
+        print(x)
+    assert (rdflib.term.Literal("CIM_DirectoryContainsFile.GroupComponent"), rdflib.URIRef(wmi_sparql.LDT["CIM_Directory"]),) in results_CIM_DirectoryContainsFile_properties_range
+    assert (rdflib.term.Literal("CIM_DirectoryContainsFile.PartComponent"), rdflib.URIRef(wmi_sparql.LDT["CIM_DataFile"]),) in results_CIM_DirectoryContainsFile_properties_range
+
+    # Name of class of range of properties of associator Win32_ShareToDirectory.
+    query_Win32_ShareToDirectory_properties_range = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?property_name ?range_class_name
+        where {
+        ?class_node cim:is_association ?obj .
+        ?class_node rdfs:label "Win32_ShareToDirectory" .
+        ?property_node rdfs:domain ?class_node .
+        ?property_node rdfs:range ?range_class_node .
+        ?property_node rdfs:label ?property_name .
+        ?range_class_node rdfs:label ?range_class_name .
+        }
+        """
+    print("Range of properties of associator Win32_ShareToDirectory")
+    results_Win32_ShareToDirectory_properties_range = rdf_graph.query(query_Win32_ShareToDirectory_properties_range)
+    results_Win32_ShareToDirectory_properties_range = to_literals(results_Win32_ShareToDirectory_properties_range)
+    for x in results_Win32_ShareToDirectory_properties_range:
+        print(x)
+    assert ("Win32_ShareToDirectory.Share", "Win32_Share",) in results_Win32_ShareToDirectory_properties_range
+    assert ("Win32_ShareToDirectory.SharedElement", "CIM_Directory",) in results_Win32_ShareToDirectory_properties_range
+
+    # Associators pointing to a CIM_DataFile.
+    query_associators_to_CIM_DataFile = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?class_name ?property_name
+        where {
+        ?class_node cim:is_association ?obj .
+        ?class_node rdfs:label ?class_name .
+        ?property_node rdfs:domain ?class_node .
+        ?property_node rdfs:range ?range_class_node .
+        ?property_node rdfs:label ?property_name .
+        ?range_class_node rdfs:label "CIM_DataFile" .
+        }
+        """
+    print("Associators to CIM_DataFile")
+    results_associators_to_CIM_DataFile = rdf_graph.query(query_associators_to_CIM_DataFile)
+    results_associators_to_CIM_DataFile = to_literals(results_associators_to_CIM_DataFile)
+    assert ('CIM_DirectoryContainsFile', 'CIM_DirectoryContainsFile.PartComponent', ) in results_associators_to_CIM_DataFile
+    assert ('Win32_PnPSignedDriverCIMDataFile', 'Win32_PnPSignedDriverCIMDataFile.Dependent', ) in results_associators_to_CIM_DataFile
+    assert ('Win32_LogicalProgramGroupItemDataFile', 'Win32_LogicalProgramGroupItemDataFile.Dependent', ) in results_associators_to_CIM_DataFile
+    assert ('CIM_ProcessExecutable', 'CIM_ProcessExecutable.Antecedent', ) in results_associators_to_CIM_DataFile
+    assert ('Win32_PrinterDriverDll', 'Win32_PrinterDriverDll.Antecedent', ) in results_associators_to_CIM_DataFile
+    assert ('Win32_ClientApplicationSetting', 'Win32_ClientApplicationSetting.Client', ) in results_associators_to_CIM_DataFile
+    assert ('Win32_CIMLogicalDeviceCIMDataFile', 'Win32_CIMLogicalDeviceCIMDataFile.Dependent', ) in results_associators_to_CIM_DataFile
+
+    # Name of class of range of properties of associator CIM_ProcessThread.
+    query_Win32_CIM_ProcessThread_properties_range = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?property_name ?range_class_name
+        where {
+        ?class_node cim:is_association ?obj .
+        ?class_node rdfs:label "CIM_ProcessThread" .
+        ?property_node rdfs:domain ?class_node .
+        ?property_node rdfs:range ?range_class_node .
+        ?property_node rdfs:label ?property_name .
+        ?range_class_node rdfs:label ?range_class_name .
+        }
+        """
+    print("Range of properties of associator CIM_ProcessThread")
+    results_CIM_ProcessThread_properties_range = rdf_graph.query(query_Win32_CIM_ProcessThread_properties_range)
+    results_CIM_ProcessThread_properties_range = to_literals(results_CIM_ProcessThread_properties_range)
+    for x in results_CIM_ProcessThread_properties_range:
+        print(x)
+    assert ("CIM_ProcessThread.GroupComponent", "CIM_Process",) in results_CIM_ProcessThread_properties_range
+    assert ("CIM_ProcessThread.PartComponent", "CIM_Thread",) in results_CIM_ProcessThread_properties_range
+
+    # Associators pointing to a CIM_Thread.
+    query_associators_to_CIM_Thread = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?class_name ?property_name
+        where {
+        ?class_node cim:is_association ?obj .
+        ?class_node rdfs:label ?class_name .
+        ?property_node rdfs:domain ?class_node .
+        ?property_node rdfs:range ?range_class_node .
+        ?property_node rdfs:label ?property_name .
+        ?range_class_node rdfs:label "CIM_Thread" .
+        }
+        """
+    print("Associators to CIM_Thread")
+    results_associators_to_CIM_Thread = rdf_graph.query(query_associators_to_CIM_Thread)
+    results_associators_to_CIM_Thread = to_literals(results_associators_to_CIM_Thread)
+    for x in results_associators_to_CIM_Thread:
+        print(x)
+    assert ('CIM_ProcessThread', 'CIM_ProcessThread.PartComponent', ) in results_associators_to_CIM_Thread
+
+    # Associators pointing to CIM_Process.
+    query_associators_to_CIM_Process = """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?class_name ?property_name
+        where {
+        ?class_node cim:is_association ?obj .
+        ?class_node rdfs:label ?class_name .
+        ?property_node rdfs:domain ?class_node .
+        ?property_node rdfs:range ?range_class_node .
+        ?property_node rdfs:label ?property_name .
+        ?range_class_node rdfs:label "CIM_Process" .
+        }
+        """
+    print("Associators to CIM_Process")
+    results_associators_to_CIM_Process = rdf_graph.query(query_associators_to_CIM_Process)
+    results_associators_to_CIM_Process = to_literals(results_associators_to_CIM_Process)
+    for x in results_associators_to_CIM_Process:
+        print(x)
+    assert ('CIM_OSProcess', 'CIM_OSProcess.PartComponent') in results_associators_to_CIM_Process
+    assert ('CIM_ProcessExecutable', 'CIM_ProcessExecutable.Dependent') in results_associators_to_CIM_Process
+    assert ('CIM_ProcessThread', 'CIM_ProcessThread.GroupComponent') in results_associators_to_CIM_Process
 
 
 def test_keys_lists():
@@ -63,24 +313,24 @@ def no_check(query_results):
 
 #################################################################################################
 
-
-samples_list["Metadata1"] = (
-    """
-    prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
-    prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
-    select ?same_caption
-    where {
-    ?my_process rdf:type cim:CIM_Process .
-    ?my_process ?cim_property ?same_caption .
-    ?cim_property rdf:name ?cim_property_name .
-    }
-    """,
-    [
-        _CimObject(VARI('my_process'), 'CIM_Process', {'Caption': VARI('same_caption')}),
-        _CimObject(VARI('my_file'), 'CIM_DataFile', {'Caption': VARI('same_caption')}),
-    ],
-    no_check
-)
+if False:
+    samples_list["Metadata1"] = (
+        """
+        prefix cim:  <http://www.primhillcomputers.com/ontology/survol#>
+        prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        select ?same_caption
+        where {
+        ?my_process rdf:type cim:CIM_Process .
+        ?my_process ?cim_property ?same_caption .
+        ?cim_property rdf:name ?cim_property_name .
+        }
+        """,
+        [
+            _CimObject(VARI('my_process'), 'CIM_Process', {'Caption': VARI('same_caption')}),
+            _CimObject(VARI('my_file'), 'CIM_DataFile', {'Caption': VARI('same_caption')}),
+        ],
+        no_check
+    )
 
 #################################################################################################
 
@@ -393,25 +643,33 @@ def shuffle_lst_objects(test_description, test_details):
 
     custom_eval = wmi_sparql.CustomEvalEnvironment(test_description, test_details[0], test_details[1])
 
-    custom_eval.run_tests()
+    custom_eval.run_query_in_rdflib()
 
 
-def test_wmi():
+def test_sparql_data():
     for test_description, test_details in samples_list.items():
         # CIM_ProcessExecutable full scan
-        if test_description != "CIM_ProcessExecutable CIM_DirectoryContainsFile":
-            continue
+        #if test_description != "CIM_Process CIM_DataFile Same Caption":
+        #    continue
 
         shuffle_lst_objects(test_description, test_details)
 
 """
 Queries to test:
-The chain of chains of classes and propeerties linking two properties with a name containing X and Y.
+The chain of chains of classes and properties linking two properties with a name containing X and Y.
 In other words: What links two concepts.
 """
 
 if __name__ == '__main__':
-    test_create_ontology()
+    # test_create_ontology(ontology_filename)
+    # test_content_ontology(ontology_filename)
+    """
+    Seulement si le fichier n est pas la sinon on initialise avec.
+    Charger les ontologies dans le graph.
+    Faire les tests de meta data.
+    L'insertion dans le graph peut se faire a partir d'un graph rdflib car de toute facon on l'a sos la main,
+    et bien separer l execution sparql: On peut la faire dans le custom_eval vers un endpoint.
+    """
     #test_classes_dictionary()
     #test_keys_lists()
-    #test_wmi()
+    test_sparql_data()
