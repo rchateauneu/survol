0d2d2a7586d5e66149b36a8d9ae797b19e2abd6f
Primhill Computers <remi.chateauneu@primhillcomputers.com>
2022-04-19 15:15:55 +0100

    Progress SKOS for "Taxonomies for Confluence".

--- survol/ontologies/WMI_SKOS.py
+++ survol/ontologies/WMI_SKOS.py
@@ -10,7 +10,10 @@
 # https://stackoverflow.com/questions/24017320/using-owlclass-prefix-with-rdflib-and-xml-serialization
 
 import os
+import platform
+
 import rdflib
+
 import lib_ontology_tools
 import lib_export_ontology
 import lib_wmi
@@ -21,14 +24,21 @@ def _convert_ontology_to_skos(input_graph):
 
     skos_graph = rdflib.Graph()
 
-    skos = rdflib.Namespace('http://www.w3.org/2004/02/skos/core#')
-    skos_graph.bind('skos', skos)
+    pav = rdflib.Namespace('http://purl.org/pav/')
+    skos_graph.bind('pav', pav)
 
     survol = rdflib.Namespace('http://www.primhillcomputers.com/survol#')
     skos_graph.bind('survol', survol)
 
-    skos_graph.add((survol['classesScheme'], rdflib.namespace.RDF.type, skos['ConceptScheme']))
-    skos_graph.add((survol['propertiesScheme'], rdflib.namespace.RDF.type, skos['ConceptScheme']))
+    # Something like '10.0.19041' for Windows 10.
+    windows_version = rdflib.Literal(platform.version())
+
+    skos_graph.add((survol['classesScheme'], rdflib.namespace.RDF.type, rdflib.namespace.SKOS.ConceptScheme))
+    skos_graph.add((survol['classesScheme'], rdflib.namespace.RDFS.label, rdflib.Literal("WMI Classes")))
+    skos_graph.add((survol['classesScheme'], pav['version'], windows_version))
+    skos_graph.add((survol['propertiesScheme'], rdflib.namespace.RDF.type, rdflib.namespace.SKOS.ConceptScheme))
+    skos_graph.add((survol['propertiesScheme'], rdflib.namespace.RDFS.label, rdflib.Literal("WMI Properties")))
+    skos_graph.add((survol['propertiesScheme'], pav['version'], windows_version))
 
     # First pass to get top classes and derived classes, and all properties.
     all_classes = set()
@@ -46,40 +56,30 @@ def _convert_ontology_to_skos(input_graph):
     top_classes = all_classes - set(derived_classes.keys())
 
     for derived_class_node, base_class_node in derived_classes.items():
-        skos_graph.add((derived_class_node, skos['broader'], base_class_node))
+        skos_graph.add((derived_class_node, rdflib.namespace.SKOS.broader, base_class_node))
 
     for top_class_node in top_classes:
-        skos_graph.add((top_class_node, skos['topConceptOf'], survol['classesScheme']))
+        skos_graph.add((top_class_node, rdflib.namespace.SKOS.topConceptOf, survol['classesScheme']))
 
     for one_class_node in all_classes:
-        skos_graph.add((one_class_node, rdflib.namespace.RDF.type, skos['Concept']))
-        skos_graph.add((one_class_node, skos['inScheme'], survol['classesScheme']))
+        skos_graph.add((one_class_node, rdflib.namespace.RDF.type, rdflib.namespace.SKOS.Concept))
+        skos_graph.add((one_class_node, rdflib.namespace.SKOS.inScheme, survol['classesScheme']))
 
     for one_property_node in all_properties:
-        skos_graph.add((one_property_node, rdflib.namespace.RDF.type, skos['Concept']))
-        skos_graph.add((one_property_node, skos['topConceptOf'], survol['propertiesScheme']))
-        skos_graph.add((one_property_node, skos['inScheme'], survol['propertiesScheme']))
+        skos_graph.add((one_property_node, rdflib.namespace.RDF.type, rdflib.namespace.SKOS.Concept))
+        skos_graph.add((one_property_node, rdflib.namespace.SKOS.topConceptOf, survol['propertiesScheme']))
+        skos_graph.add((one_property_node, rdflib.namespace.SKOS.inScheme, survol['propertiesScheme']))
 
     # Now scan all triples for comments and labels.
     for rdf_subject, rdf_property, rdf_object in input_graph:
         skos_graph.add((rdf_subject, rdf_property, rdf_object))
 
         if rdf_property == rdflib.namespace.RDFS.label:
-            skos_graph.add((rdf_subject, skos['prefLabel'], rdf_object))
+            skos_graph.add((rdf_subject, rdflib.namespace.SKOS.prefLabel, rdf_object))
         elif rdf_property == rdflib.namespace.RDFS.comment:
-            skos_graph.add((rdf_subject, skos['altLabel'], rdf_object))
+            skos_graph.add((rdf_subject, rdflib.namespace.SKOS.altLabel, rdf_object))
 
     return skos_graph
-
-
-"""
-    # Add skos:ConceptScheme
-    # For each rdfs:Class that you already have, create skos:Concept and:
-    #     Map rdfs:label to skos:prefLabel and rdfs:comment to skos:definition
-    #     Create skos:broader pointing to rdfs:subClassOf
-    #     Set skos:inScheme and skos:topConceptOf for top concepts
-    # Do the same for rdfs:Property, although you may need to be selective
-"""
 
 
 def Main():
@@ -91,11 +91,13 @@ def Main():
 
     skos_graph = _convert_ontology_to_skos(graph)
 
-    onto_filnam = os.path.splitext(__file__)[0] + ".rdfs"
+    path_base = os.path.splitext(__file__)[0]
+
+    onto_filnam = path_base + ".rdfs"
     lib_export_ontology.flush_or_save_rdf_graph(skos_graph, onto_filnam)
 
     # Writes the same content in turtle format.
-    onto_filnam_turtle = os.path.splitext(__file__)[0] + ".ttl"
+    onto_filnam_turtle = path_base + ".ttl"
     lib_export_ontology.flush_or_save_rdf_graph(graph, onto_filnam_turtle, 'ttl')
 
 
