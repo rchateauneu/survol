f529afea822ea48f397cf5e35a8a1e0f79fbb867
Primhill Computers <remi.chateauneu@primhillcomputers.com>
2026-06-07 23:09:37 +0100

    Fix deprecated module.

--- survol/lib_python.py
+++ survol/lib_python.py
@@ -1,14 +1,18 @@
+import logging
+
 def PipGetInstalledDistributions():
     try:
         import pip
         return pip.get_installed_distributions()
-    except:
+    except Exception as exc:
+        logging.warning("PipGetInstalledDistributions (1) caught: %s", exc)
         pass
 
     try:
         import pip.utils
         return pip.utils.get_installed_distributions()
-    except:
+    except Exception as exc:
+        logging.warning("PipGetInstalledDistributions (2) caught: %s", exc)
         pass
 
     try:
@@ -16,8 +20,18 @@ def PipGetInstalledDistributions():
         # https://stackoverflow.com/questions/49923671/are-there-any-function-replacement-for-pip-get-installed-distributions-in-pip
         from pip._internal.utils.misc import get_installed_distributions
         return get_installed_distributions()
-    except:
+    except Exception as exc:
+        logging.warning("PipGetInstalledDistributions (3) caught: %s", exc)
         pass
 
+    try:
+        import pkg_resources
+        lst = list(pkg_resources.working_set)
+        return lst
+    except Exception as exc:
+        logging.warning("PipGetInstalledDistributions (4) caught: %s", exc)
+        pass
+
+    logging.error("PipGetInstalledDistributions cannot get list of installed packages.")
     return None
 
