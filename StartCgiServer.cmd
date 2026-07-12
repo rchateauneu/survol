14eb18cc1345eb4d20a94715d15fbb54e2966d60
Primhill Computers <remi.chateauneu@primhillcomputers.com>
2026-07-12 02:20:56 +0100

    Progress.

--- StartCgiServer.cmd
+++ StartCgiServer.cmd
@@ -1,2 +1,10 @@
-cd D:\Developpement\Survol\survol\survol\
-python scripts\cgiserver.py
+@REM TODO: These two lines do not work and should be fixed.
+on break gosub gotabreak
+on error gosub gotabreak
+
+pushd D:\Developpement\Survol\survol\survol\
+python scripts\cgiserver.py %*
+:gotabreak
+popd
+
+@REM %~dp0\survol\scripts\cgiserver.py %*
--- StartWsgiServer.cmd
+++ StartWsgiServer.cmd
@@ -1,3 +1,5 @@
 REM http://192.168.1.105:8000/survol/www/index.htm
 cd D:\Developpement\Survol\survol\survol
 python scripts\wsgiserver.py
+
+@REM %~dp0\survol\scripts\cgiserver.py %*
