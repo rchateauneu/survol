e5b0e623f3b2609ae7ae5635b434cf757a1e0d3e
Primhill Computers <remi.chateauneu@primhillcomputers.com>
2026-08-03 01:13:34 +0100

    Simplify script.

--- StartCgiServer.cmd
+++ StartCgiServer.cmd
@@ -1,10 +1,1 @@
-@REM TODO: These two lines do not work and should be fixed.
-on break gosub gotabreak
-on error gosub gotabreak
-
-pushd D:\Developpement\Survol\survol\survol\
-python scripts\cgiserver.py %*
-:gotabreak
-popd
-
-@REM python %~dp0\survol\scripts\cgiserver.py %*
+python %~dp0\survol\scripts\cgiserver.py %*
